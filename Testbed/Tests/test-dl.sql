419304ecaadac8ef2910b542d160ad1460c56dcb
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-07 09:10:55 +0000

    Add script to add test data

---
+++ Testbed/Tests/test-dl.sql
@@ -0,0 +1,5 @@
+delete from t_replica_state where node = 'TEST_LAT' or node = 'castorgrid_mss';
+delete from t_transfer_state where dest_node = 'TEST_LAT';
+insert into t_replica_state (select guid, 'TEST_LAT', 0, 0, 0, 0 from t_files_for_transfer);
+insert into t_replica_state (select guid, 'castorgrid_mss', 2, 0, 0, 0 from t_files_for_transfer);
+insert into t_transfer_state (select guid, 'castorgrid_mss', 'TEST_LAT', 0, 0, 0, 0 from t_files_for_transfer);
