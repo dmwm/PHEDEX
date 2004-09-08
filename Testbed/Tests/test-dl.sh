1a69935117339d6f34c26fabcd47009f089ad23b
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-08 11:08:28 +0000

    Need to use node name with FileRouter

--- Testbed/Tests/test-dl.sh
+++ Testbed/Tests/test-dl.sh
@@ -9,7 +9,7 @@ rm -fr {fr,dl,si,cs,cm,exp}/{inbox,work}/*
 DBARGS="-db devdb9 -dbuser cms_transfermgmt -dbpass smalland_round"
 STARGS="-stagehost stagecmsprod -stagepool cms_prod2"
 
-./FileRouter -state fr $DBARGS >& log-fr &
+./FileRouter -state fr $DBARGS -node GLOBAL >& log-fr &
 ./FileDownload -state dl $DBARGS -node TEST_LAT -pfndest ./FileDownloadDest -wanted 1G >& log-dl &
 ./FileCastorExport -state exp $DBARGS $STARGS -node castorgrid_mss >& log-exp &
 ./FileCastorStager -state si $DBARGS $STARGS -node castorgrid_mss -prefix sfn://castorgrid.cern.ch >& log-si &
