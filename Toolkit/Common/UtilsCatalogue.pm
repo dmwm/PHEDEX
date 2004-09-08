13efed08426467649a06ebcc793d85b1c1287425
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-08 13:42:06 +0000

    Optimise GUID/PFN lookups

--- Toolkit/Common/UtilsCatalogue.pm
+++ Toolkit/Common/UtilsCatalogue.pm
@@ -2,47 +2,100 @@ package UtilsCatalogue; use strict; use warnings; use base 'Exporter';
 our @EXPORT = qw(guidToPFN guidToLFN pfnToGUID);
 use UtilsLogging;
 
-# Map a GUID to a PFN using a catalogue
+# Map a GUID to a PFN using a catalogue.  Arguments are either
+# a triplet (GUID, CATALOGUE-CONTACT, HOST-KEY) where GUID can
+# be either a single GUID or a reference to an array of GUIDs.
+# The HOST-KEY is a pattern that is matched against the PFNs
+# to pick the one specific to the desired host.  Returns a
+# PFN for single GUID and a hash of GUID => PFN mappings for
+# an array of GUIDs; the hash will have a value for each GUID,
+# undef if there was no PFNs for it in the catalogue.
 sub guidToPFN
 {
-    my ($guid, $catalogue, $host) = @_;
+    my ($guid, $catalogue, $hostkey) = @_;
+    my $home = $0; $home =~ s|/[/]+$||;
 
     # FIXME: remove message suppression when pool has learnt to print
     # diagnostic output somewhere else other than stdout...
-    open (PFNS, "POOL_OUTMSG_LEVEL=100 FClistPFN -u '$catalogue' -q \"guid='$guid'\" |")
-	or do { &alert ("cannot run FClistPFN: $!"); return undef; };
-    my @pfns = grep (/$host/, map { chomp; $_ } <PFNS>);
-    close (PFNS);
-    return $pfns[0];
+
+    # Query at most 50 at a time to keep command line short enough.
+    my %data = ();
+    my @items = ref $guid ? @$guid : $guid;
+    while (@items)
+    {
+	my $n = $#items > 50 ? 50 : $#items;
+	my @slice = @items[$#items - $n .. $#items];
+	delete @items[$#items - $n .. $#items];
+
+        open (CAT, "POOL_OUTMSG_LEVEL=100 $home/FClistGuidPFN -u '$catalogue' -g @slice |")
+	    or do { &alert ("cannot run FClistGuidPFN: $!"); return undef; };
+        while (<CAT>)
+        {
+	    chomp;
+	    my ($resguid, $respfn) = /(\S+)/g;
+	    $data{$resguid} = $respfn if $respfn =~ /$hostkey/;
+        }
+        close (CAT);
+    }
+
+    return ref $guid ? { map { $_ => $data{$_} } @$guid } : $data{$guid};
 }
 
-# Map a GUID to a LFN using a catalogue
+# Map a GUID to a LFN using a catalogue.  Like guidToPFN but
+# returns LFNs instead of PFNs.  Doesn't take a host key as
+# LFNs are not host-specific.
 sub guidToLFN
 {
-    my ($guid, $catalogue) = @_;
+	my ($guid, $catalogue) = @_;
 
     # FIXME: remove message suppression when pool has learnt to print
     # diagnostic output somewhere else other than stdout...
-    open (LFNS, "POOL_OUTMSG_LEVEL=100 FClistLFN -u '$catalogue' -q \"guid='$guid'\" |")
-	or do { &alert ("cannot run FClistPFN: $!"); return undef; };
-    my @lfns = map { chomp; $_ } <LFNS>;
-    close (LFNS);
-    return $lfns[0];
+
+    # There's no way to do batches.  Do one at a time.
+    my %data = ();
+    my @items = ref $guid ? @$guid : $guid;
+    foreach my $g (@items)
+    {
+        open (CAT, "POOL_OUTMSG_LEVEL=100 FClistLFN -u '$catalogue' -q \"guid='$g'\" |")
+	    or do { &alert ("cannot run FClistPFN: $!"); return undef; };
+    	<CAT>; chomp; $data{$g} = $_;
+        close (CAT);
+    }
+
+    return ref $guid ? { map { $_ => $data{$_} } @$guid } : $data{$guid};
 }
 
-# Map a PFN to GUID using a catalogue
+# Map a PFN to a GUID using a catalogue.  Like guidToPFN but
+# works the other way around.  Doesn't take a host key.
 sub pfnToGUID
 {
-    my ($pfn, $catalogue, $host) = @_;
+    my ($pfn, $catalogue) = @_;
+    my $home = $0; $home =~ s|/[/]+$||;
 
     # FIXME: remove message suppression when pool has learnt to print
     # diagnostic output somewhere else other than stdout...
-    my $home = $0; $home =~ s|/[^/]+$||;
-    open (GUIDS, "POOL_OUTMSG_LEVEL=100 $home/FClistGuidPFN -u '$catalogue' -p '$pfn' |")
-	or do { &alert ("cannot run FClistGuidPFN: $!"); return undef; };
-    my @guids = map { chomp; [ split (/\s+/, $_) ] } <GUIDS>;
-    close (GUIDS);
-    return $guids[0][0];
+
+    # Query at most 50 at a time to keep command line short enough.
+    my %data = ();
+    my @items = ref $pfn ? @$pfn : $pfn;
+    while (@items)
+    {
+	my $n = $#items > 50 ? 50 : $#items;
+	my @slice = @items[$#items - $n .. $#items];
+	delete @items[$#items - $n .. $#items];
+
+        open (CAT, "POOL_OUTMSG_LEVEL=100 $home/FClistGuidPFN -u '$catalogue' -p @slice |")
+	    or do { &alert ("cannot run FClistGuidPFN: $!"); return undef; };
+        while (<CAT>)
+        {
+	    chomp;
+	    my ($resguid, $respfn) = /(\S+)/g;
+	    $data{$respfn} = $resguid;
+        }
+        close (CAT);
+    }
+
+    return ref $pfn ? { map { $_ => $data{$_} } @$pfn } : $data{$pfn};
 }
 
 1;
