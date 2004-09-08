e31f161d68962587393b48008aebfebe80fcbd80
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-08 10:55:24 +0000

    Add router and mss migrator

--- Testbed/Tests/test-dl.sh
+++ Testbed/Tests/test-dl.sh
@@ -3,13 +3,15 @@
 eval `cd /afs/cern.ch/sw/lcg/app/releases/POOL/POOL_1_6_2/src; scram -arch rh73_gcc32 run -sh`
 source /afs/cern.ch/project/oracle/script/setoraenv.sh -s 8174
 
-mkdir -p {dl,si,cs,exp}
-rm -fr {dl,si,cs,exp}/{inbox,work}/*
+mkdir -p {fr,dl,si,cs,exp}
+rm -fr {fr,dl,si,cs,exp}/{inbox,work}/*
 
 DBARGS="-db devdb9 -dbuser cms_transfermgmt -dbpass smalland_round"
 STARGS="-stagehost stagecmsprod -stagepool cms_prod2"
 
+./FileRouter -state fr $DBARGS >& log-fr &
 ./FileDownload -state dl $DBARGS -node TEST_LAT -pfndest ./FileDownloadDest -wanted 1G >& log-dl &
 ./FileCastorExport -state exp $DBARGS $STARGS -node castorgrid_mss >& log-exp &
 ./FileCastorStager -state si $DBARGS $STARGS -node castorgrid_mss -prefix sfn://castorgrid.cern.ch >& log-si &
 ./FileCastorChecksum -state cs $DBARGS $STARGS -node castorgrid_mss -prefix sfn://castorgrid.cern.ch >& log-cs &
+./FileCastorMigrate -state cm $DBARGS $STARGS -node castorgrid_mss_test -prefix sfn://castorgrid.cern.ch >& log-cs &
