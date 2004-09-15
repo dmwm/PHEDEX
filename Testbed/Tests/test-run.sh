031315175eb37313afc008611ddae9be8a58cfd6
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-15 20:26:50 +0000

    Automate more of the test

--- Testbed/Tests/test-run.sh
+++ Testbed/Tests/test-run.sh
@@ -3,6 +3,8 @@
 eval `cd /afs/cern.ch/sw/lcg/app/releases/POOL/POOL_1_6_2/src; scram -arch rh73_gcc32 run -sh`
 source /afs/cern.ch/project/oracle/script/setoraenv.sh -s 8174
 
+rm -f entry
+ln -s xml entry
 mkdir -p {xml,exist,pfx,cat,tmdb}
 rm -fr {xml,exist,pfx,cat,tmdb}/{inbox,work}/*
 
@@ -10,7 +12,10 @@ rm -fr {xml,exist,pfx,cat,tmdb}/{inbox,work}/*
 ./DropCastorFileCheck -in exist -out pfx >& log-exist &
 ./DropCatPFNPrefix -in pfx -out cat -prefix sfn://castorgrid.cern.ch >& log-pfx &
 ./DropCatPublisher -in cat -out tmdb -catalogue file:catalog.xml >& log-cat &
-./DropTMDBPublisher -in tmdb -db "" -node TEST_LAT >& log-tmdb &
+./DropTMDBPublisher -in tmdb -db "devdb9" -dbuser cms_transfermgmt -dbpass smalland_round -node TEST_Transfer >& log-tmdb &
 
-: ./RefDBReady -a 4965
-: cp -rp drops-for-4965/drops/* xml/inbox
+rm -fr foodata
+./TRNew foodata
+./TRNewData -a foodata 4965
+./TRSyncDrops foodata
+./TRSyncFeed $PWD devdb9 t_files_for_transfer foodata
