8abe626b2299d7aae906fd7a36b02b57336a6b2a
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-21 11:10:55 +0000

    Default to no model

--- Testbed/Tests/test-funnel.sh
+++ Testbed/Tests/test-funnel.sh
@@ -8,7 +8,12 @@ ln -s null entry
 mkdir -p {null,xml,exist,mrg,out,ref}
 rm -fr {null,xml,exist,mrg,out,ref}/{inbox,work}/*
 
-./DropNullAgent -in null -out xml -model ../RLSTest/models/25hz >& log-null &
+# FIXME: also run in "maximal mode", first putting everything through to funnel
+# inbox, then running funnel maxed out on its entire collection of drops
+
+# ./DropNullAgent -in null -out xml -model ../RLSTest/models/25hz >& log-null &
+# ./DropNullAgent -in null -out xml -model ../RLSTest/models/100hz >& log-null &
+./DropNullAgent -in null -out xml >& log-null &
 ./DropXMLUpdate -in xml -out exist >& log-xml &
 ./DropCastorFileCheck -in exist -out mrg -out ref >& log-exist &
 ./DropFunnel -in mrg -out out \
