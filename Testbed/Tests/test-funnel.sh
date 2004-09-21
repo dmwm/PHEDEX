5cc6c47b47e2f3200389f004b94c25794d66b693
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2004-09-21 10:14:14 +0000

    Add leading null agent

--- Testbed/Tests/test-funnel.sh
+++ Testbed/Tests/test-funnel.sh
@@ -4,10 +4,11 @@ eval `cd /afs/cern.ch/sw/lcg/app/releases/POOL/POOL_1_6_2/src; scram -arch rh73_
 source /afs/cern.ch/project/oracle/script/setoraenv.sh -s 8174
 
 rm -f entry
-ln -s xml entry
-mkdir -p {xml,exist,mrg,out}
-rm -fr {xml,exist,mrg,out}/{inbox,work}/*
+ln -s null entry
+mkdir -p {null,xml,exist,mrg,out}
+rm -fr {null,xml,exist,mrg,out}/{inbox,work}/*
 
+./DropNullAgent -in null -out xml -model ../RLSTest/models/25hz >& log-null &
 ./DropXMLUpdate -in xml -out exist >& log-xml &
 ./DropCastorFileCheck -in exist -out mrg >& log-exist &
 ./DropFunnel -in mrg -out out \
