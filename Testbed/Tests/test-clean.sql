1adda8c693e732847a76fb1b2cbeaf5799443757
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-01-21 10:12:41 +0000

    Remove obsolete stuff

--- Testbed/Tests/test-clean.sql
+++
@@ -1,7 +0,0 @@
-delete from t_lookup;
-delete from t_agents;
-delete from t_transfer_state;
-delete from t_replica_state;
-delete from t_replica_metadata;
-delete from t_destinations;
-delete from t_files_for_transfer;
