77a56d6d5bfb44b153a6de4b29aec1f9533e3470
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-01-21 09:41:41 +0000

    Use strftime to generate time output, not invoking 'date'

--- Toolkit/Common/UtilsLogging.pm
+++ Toolkit/Common/UtilsLogging.pm
@@ -1,12 +1,12 @@
 package UtilsLogging; use strict; use warnings; use base 'Exporter';
 our @EXPORT = qw(logmsg alert warn note);
+use POSIX;
 
 # Produce an alert message
 sub logmsg
 {
-    my $date = `date +"%Y-%m-%d %H:%M:%S"`;
+    my $date = strftime ("%Y-%m-%d %H:%M:%S", gmtime);
     my $me = $0; $me =~ s|.*/||;
-    chomp ($date);
     print STDERR "$date: ${me}\[$$]: ", @_, "\n";
 }
 
