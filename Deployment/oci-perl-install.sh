694535b6d379f4c295df72bbb0b5a1b305e09764
Tim Barrass <sha1-0b65ee94231dd08d3d21963410d6dc362a2b0008@cern.ch>
2005-03-14 19:53:25 +0000

    WARNING: errors in this script- not finished

--- Deployment/oci-perl-install.sh
+++ Deployment/oci-perl-install.sh
@@ -13,32 +13,32 @@ echo "You need to set \$BASE to your top level directory for installation"
 exit
 fi
 
-#cd $BASE
+cd $BASE
 # download instantclient-basic-linux32-10.1.0.3.zip
 # download instantclient-sqlplus-linux32-10.1.0.3.zip
 # download instantclient-sdk-linux32-10.1.0.3.zip
-#if [ ! -e instantclient-basic-linux32-10.1.0.3.zip ]; then
-#echo "Please download the Oracle Instant Client install zips and place them in $BASE"
-#exit
-#fi
+if [ ! -d instantclient10_1 ]; then
+echo "Please download the Oracle Instant Client install zips and place them in $BASE"
+exit
+fi
 #unzip *.zip # creates dir instantclient10_1
-#export ORACLE_HOME=${BASE}/instantclient10_1
-#cd $ORACLE_HOME
-#ln -s libclntsh.so.10.1 libclntsh.so
-#mkdir lib
-#mkdir bin
-#mkdir java
-#mv lib* lib
-#mv sqlplus bin
-#mv glogin.sql bin
-#mv *jar java
-#mv sdk/demo .
-#mv sdk/include .
-#rm -fr sdk
-#export PATH=$ORACLE_HOME/bin:$PATH
-#export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
-#export SQLPATH=$ORACLE_HOME/bin
-#cd ..
+export ORACLE_HOME=${BASE}/instantclient10_1
+cd $ORACLE_HOME
+ln -s libclntsh.so.10.1 libclntsh.so
+mkdir lib
+mkdir bin
+mkdir java
+mv lib* lib
+mv sqlplus bin
+mv glogin.sql bin
+mv *jar java
+mv sdk/demo .
+mv sdk/include .
+rm -fr sdk
+export PATH=$ORACLE_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export SQLPATH=$ORACLE_HOME/bin
+cd ..
 mkdir perl-modules
 cd perl-modules
 wget http://search.cpan.org/CPAN/authors/id/T/TI/TIMB/DBD-Oracle-1.16.tar.gz
@@ -54,16 +54,16 @@ echo "diff Makefile.PL.orig Makefile.PL
 1254a1259
 >        \"$OH/include\", # Tim Barrass, hacked for OIC install from zips" > Makefile.PL.patch
 patch Makefile.PL Makefile.PL.patch
-#perl Makefile.PL prefix=$BASE/perl-modules -m $ORACLE_HOME/demo/demo.mk
+perl Makefile.PL prefix=$BASE/perl-modules -m $ORACLE_HOME/demo/demo.mk
 
-#echo "If you see WARNING: I could not determine Oracle client version ..."
-#echo "remove the trailing slash from ORACLE_HOME"
+echo "If you see WARNING: I could not determine Oracle client version ..."
+echo "remove the trailing slash from ORACLE_HOME"
 
-#make
-#make install
+make
+make install
 
 # Create and environment script
-echo 'Writing local-oci-env.sh: please take a look and edit'
+echo 'Writing local-oci-env.sh: please take a look and edit in your own PERL5LIB and TNS_ADMIN'
 echo "export BASE=$BASE
 export ORACLE_HOME=${BASE}/instantclient10_1
 export LD_LIBRARY_PATH=${ORACLE_HOME}/lib:${LD_LIBRARY_PATH}
