2aa5615a63d92d33b2b7d6c600d15ea0caab91ae
Tim Barrass <sha1-0b65ee94231dd08d3d21963410d6dc362a2b0008@cern.ch>
2005-05-17 17:27:29 +0000

    first entry

---
+++ Testbed/Routing/SimpleTwoNodes/clean.sql
@@ -0,0 +1,6 @@
+delete from t_routing where to_node like 'Tim-%'
+delete from t_routing where from_node like 'Tim-%'
+delete from t_routing where gateway like 'Tim-%'
+delete from t_agent_status where node like 'Tim-%'
+delete from t_node_neighbour where node like 'Tim-%'
+delete from t_node where name like 'Tim-%'
---
+++ Testbed/Routing/SimpleTwoNodes/prep.sql
@@ -0,0 +1,12 @@
+delete from t_routing where to_node like 'Tim-%'
+delete from t_routing where from_node like 'Tim-%'
+delete from t_routing where gateway like 'Tim-%'
+delete from t_agent_status where node like 'Tim-%'
+delete from t_node_neighbour where node like 'Tim-%'
+delete from t_node where name like 'Tim-%'
+insert into t_node values ('Tim-A')
+insert into t_node values ('Tim-B')
+insert into t_node_neighbour values ('Tim-A','Tim-B',1)
+insert into t_node_neighbour values ('Tim-B','Tim-A',1)
+insert into t_routing values (0, 'Tim-A', 'Tim-B', 'Tim-B', 1)
+insert into t_routing values (0, 'Tim-B', 'Tim-A', 'Tim-A', 1)
