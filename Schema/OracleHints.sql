a23e323d9f99615f2d682c2e186decaa92175c6b
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-05-25 15:21:43 +0000

    Implement role-based authentication

--- Schema/OracleHints.sql
+++ Schema/OracleHints.sql
@@ -10,3 +10,25 @@
 -- oradoc.cern.ch
 -- sqlplus / l / 16 / del / &foo
 -- create trigger for transfer history
+
+/*
+I have enabled the feature allowing you to manage access privileges to
+your data with password-protected roles for 3 CMS Transfer Management
+accounts on CMSSG. What you should do in order to implement your data
+access policy is the following:
+- as CMS_TRANSFERMGMT user create appropriate roles with command:
+    CREATE ROLE role_name IDENTIFIED BY some_password;
+- Grant all desired object privileges to those roles:
+    GRANT privilege_name ON object_name TO role_name;
+- Grant roles to CMS_TRANSFERMGMT_READER and/or CMS_TRANSFERMGMT_WRITER
+  accounts:
+    GRANT role_name to CMS_TRANSFERMGMT_READER | CMS_TRANSFERMGMT_WRITER
+- Change your application in a way that after connection to
+  CMS_TRANSFERMGMT_READER or CMS_TRANSFERMGMT_WRITER account it enables
+  one of the created roles (I guess that it doesn't matter too much that
+  it is not possible to use bind variables in this case as long as the
+  query below is not executed too often):
+    SET ROLE role_name IDENTIFIED BY some_password, DESIGNER;
+
+Role DESIGNER is a role that all your account have by default so probably
+it is reasonable to keep this role active.  */
--- Schema/OraclePrivs.sh
+++ Schema/OraclePrivs.sh
@@ -21,14 +21,48 @@ fi
 
 connect="$1" reader="$2" writer="$3"
 
-# Query all tables
-(echo "set lines 1000;";
- echo "set pages 0;";
- echo "select * from tab;") |
- sqlplus -S "$connect" |
- awk '/^T_[A-Z0-9_]+/ {print $1} {}' |
- while read tab; do
-    echo "grant select on $tab to $reader;";
-    echo "grant alter, delete, insert, select, update on $tab to $writer;";
- done |
- sqlplus -S "$connect"
+# Update privileges for all roles and tables.  Note that some tables
+# have restricted access: t_node_neighbour and t_subscription can be
+# modified only by the admin account, most t_info_* tables can only
+# be updated by the central agents at CERN (as a precaution to avoid
+# sites accidentally overloading the database by running agents they
+# should not be running).
+#
+# The assumption here is that nobody except few selected admins have
+# access privileges to the admin account, and all sites use roles to
+# gain modification access to tables; the reader account has read-only
+# access to all the tables.
+for role in \
+  $((echo "select granted_role from user_role_privs;") |
+    sqlplus -S "$connect" | awk '/SITE_/ { print $1 } {}'); do
+  echo; echo; echo "-- role $role"
+  echo "grant $role to $writer;"
+
+  for table in \
+    $((echo "select table_name from user_tables;") |
+      sqlplus -S "$connect" | awk '/^T_[A-Z0-9_]+/ { print $1 } {}'); do
+
+    echo; echo "grant select on $table to $reader;"
+    case $table:$role in
+      T_INFO_AGENT_STATUS:* | \
+      T_INFO*:SITE_CERN | \
+      T_DBS*:SITE_CERN | \
+      T_DLS*:SITE_CERN | \
+      T_REQUEST*:SITE_CERN | \
+      T_BLOCK_*:SITE_CERN )
+        echo "grant alter, delete, insert, select, update on $table to $role;" ;;
+
+      T_SUBSCRIPTION:* | \
+      T_NODE*:* | \
+      T_INFO*:* | \
+      T_DBS*:* | \
+      T_DLS*:* | \
+      T_REQUEST*:* | \
+      T_BLOCK_*:* )
+      	echo "grant select on $table to $role;" ;;
+
+      *:* )
+      	echo "grant alter, delete, insert, select, update on $table to $role;" ;;
+    esac
+  done
+done | sqlplus -S "$connect"
