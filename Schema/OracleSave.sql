125af02f1ec43b8dd1c2113e6b4cb2741095e496
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-06-25 13:59:40 +0000

    Reformat.  Automatically truncate names that would be too long

--- Schema/OracleSave.sql
+++ Schema/OracleSave.sql
@@ -5,47 +5,67 @@
 
 set serveroutput on size 100000
 BEGIN
+   --------------------
    -- Tables
    FOR o IN
-     (SELECT table_name name FROM user_tables WHERE table_name not like 'X%')
+     (SELECT table_name name FROM user_tables
+      WHERE table_name NOT LIKE 'X%')
    LOOP
       dbms_output.put_line ('Renaming table ' || o.name);
-      execute immediate 'rename ' || o.name || ' to X' || o.name;
+      execute immediate
+          'rename ' || o.name
+	  || ' to X' || substr (o.name, 1, 29);
    END LOOP;
 
+   --------------------
    -- Sequences
    FOR o IN
-     (SELECT sequence_name name FROM user_sequences WHERE sequence_name not like 'X%')
+     (SELECT sequence_name name FROM user_sequences
+      WHERE sequence_name NOT LIKE 'X%')
    LOOP
       dbms_output.put_line ('Renaming sequence ' || o.name);
-      execute immediate 'rename ' || o.name || ' to X' || o.name;
+      execute immediate
+          'rename ' || o.name
+	  || ' to X' || substr (o.name, 1, 29);
    END LOOP;
 
+   --------------------
    -- Constraints
    FOR o IN
      (SELECT constraint_name name, table_name FROM user_constraints
-      WHERE constraint_name not like 'X%' and constraint_name not like 'SYS%')
+      WHERE constraint_name NOT LIKE 'X%'
+        AND constraint_name NOT LIKE 'SYS%')
    LOOP
-      dbms_output.put_line ('Renaming constraint ' || o.name || ' in table ' || o.table_name);
-      execute immediate 'alter table ' || o.table_name || ' rename constraint '
-			|| o.name || ' to X' || o.name;
+      dbms_output.put_line ('Renaming constraint ' || o.name || ' [' || o.table_name || ']');
+      execute immediate
+          'alter table ' || o.table_name
+	  || ' rename constraint ' || o.name
+	  || ' to X' || substr (o.name, 1, 29);
    END LOOP;
 
+   --------------------
    -- Indices
    FOR o IN
-     (SELECT index_name name FROM user_indexes
-      WHERE index_name not like 'X%' and index_name not like 'SYS%')
+     (SELECT index_name name, table_name FROM user_indexes
+      WHERE index_name NOT LIKE 'X%'
+        AND index_name NOT LIKE 'SYS%')
    LOOP
-      dbms_output.put_line ('Renaming index ' || o.name);
-      execute immediate 'alter index ' || o.name || ' rename to X' || o.name;
+      dbms_output.put_line ('Renaming index ' || o.name || ' [' || o.table_name || ']');
+      execute immediate
+          'alter index ' || o.name
+	  || ' rename to X' || substr (o.name, 1, 29);
    END LOOP;
 
+   --------------------
    -- Triggers
    FOR o IN
-     (SELECT trigger_name name FROM user_triggers WHERE trigger_name not like 'X%')
+     (SELECT trigger_name name, table_name FROM user_triggers
+      WHERE trigger_name NOT LIKE 'X%')
    LOOP
-      dbms_output.put_line ('Renaming trigger ' || o.name);
-      execute immediate 'alter trigger ' || o.name || ' rename to X' || o.name;
+      dbms_output.put_line ('Renaming trigger ' || o.name || ' [' || o.table_name || ']');
+      execute immediate
+          'alter trigger ' || o.name
+	  || ' rename to X' || substr (o.name, 1, 29);
    END LOOP;
 END;
 /
