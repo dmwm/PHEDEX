d15d264e9fba7b2732298277e18f4def9fca8d30
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-09-24 14:17:45 +0000

    Update request schema

--- Schema/OracleReq.sql
+++ Schema/OracleReq.sql
@@ -28,10 +28,26 @@ create table t_request_dataspec
   (timestamp		float		not null,
    request		integer		not null,
    operation		varchar (20)	not null,
-   owner		varchar (100),
-   dataset		varchar (100),
-   block		varchar (200));
+   owner		varchar (1000),
+   dataset		varchar (1000),
+   block		varchar (2000));
 
+create table t_request_status
+  (timestamp		float		not null,
+   request		integer		not null,
+   block		varchar (200)	not null,
+   destination		varchar (20)	not null,
+   dbs_isopen		char (1),
+   dbs_files		integer,
+   dbs_bytes		integer,
+   tmdb_isopen		char (1),
+   tmdb_files		integer,
+   tmdb_bytes		integer,
+   dest_files		integer,
+   dest_bytes		integer,
+   common_files		integer,
+   dbs_only_files	integer,
+   tmdb_only_files	integer);
 
 ----------------------------------------------------------------------
 -- Add constraints
@@ -60,6 +76,16 @@ alter table t_request_dataspec
   add constraint fk_request_dataspec_req
   foreign key (request) references t_request (id);
 
+
+alter table t_request_status
+  add constraint fk_request_status_req
+  foreign key (request) references t_request (id);
+
+alter table t_request_status
+  add constraint pk_request_status
+  primary key (request, block, destination)
+  using index tablespace CMS_TRANSFERMGMT_INDX01;
+
 ----------------------------------------------------------------------
 -- Add indices
 
@@ -75,4 +101,9 @@ create index ix_request_subscription_req
 
 create index ix_request_dataspec_req
   on t_request_dataspec (request)
+  tablespace CMS_TRANSFERMGMT_INDX01;
+
+
+create index ix_request_status_req
+  on t_request_status (request)
   tablespace CMS_TRANSFERMGMT_INDX01;
