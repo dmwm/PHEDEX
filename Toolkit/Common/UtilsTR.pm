03b17bac0951a9e6fa02741ba8eaa07efa43efa0
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2005-09-24 11:23:47 +0000

    Prune unnecessary code

--- Toolkit/Common/UtilsTR.pm
+++ Toolkit/Common/UtilsTR.pm
@@ -1,9 +1,7 @@
 package UtilsTR; use strict; use warnings; use base 'Exporter';
-our @EXPORT = qw(usage readPatterns expandPatterns expandAssignments
+our @EXPORT = qw(usage readPatterns expandPatterns
 	         assignmentData assignmentInfo assignmentFileCategory
-	         assignmentDrops expandXMLFragment
-		 listDatasetOwners listAssignments
-	         castorCheck checkAssignmentFiles feedDropsToAgents);
+	         expandXMLFragment listDatasetOwners listAssignments);
 use TextGlob 'glob_to_regex';
 use UtilsWriters;
 use UtilsReaders;
@@ -48,10 +46,7 @@ sub expandPatterns
 {
     my ($mode, @patterns) = @_;
 
-    # If "-a" option, the patterns are really assignment numbers
-    return @patterns if $mode eq 'a';
-
-    # If "-f" or "-p" option, the patterns are dataset.owners.
+    # If "-f" or "-p" option, the patterns are owner/dataset.
     # For "-p", expand to the matching pairs.  For "-f", expand
     # the resulting list by navigating backwards using assignment
     # InputOwnerName in the DST/Digi/Hits chain.
@@ -65,23 +60,6 @@ sub expandPatterns
     }
 
     return @pats;
-}
-
-# Expand patterns to assignment ids
-sub expandAssignments
-{
-    my @patterns = @_;
-    my @assignments = ();
-    foreach my $item (@patterns)
-    {
-	if ($item =~ /^\d+$/) {
-	    push (@assignments, $item);
-	} else {
-	    push (@assignments, &listAssignments (@$item));
-	}
-    }
-
-    return @assignments;
 }
 
 # Get assignment data from RefDB: the result xml catalogues for each run
@@ -133,37 +111,6 @@ sub assignmentFileCategory
     }
 }
 
-# Generate XML fragments and summary information for every run of the
-# assignment.  Returns a hash with members XML and SMRY in each.
-sub assignmentDrops
-{
-    my ($assid, $adata, $ainfo, $subdir, $basedir) = @_;
-    $adata ||= &assignmentData ($assid);
-    $ainfo ||= &assignmentInfo ($assid);
-    $subdir ||= &assignmentFileCategory ($ainfo);
-    $basedir ||= "/castor/cern.ch/cms/PCP04";
-    die "$assid: cannot determine file category\n" if ! $subdir;
-
-    # Expand XML fragments and summary info
-    my $result = {};
-    my ($junk, @rows) = split(/\n/, $adata->{DATA});
-    foreach my $row (@rows)
-    {
-	my ($run, $junk1, $xmlfrag, @rest) = split(/\s+/, $row);
-	my $dropid = "$ainfo->{DatasetName}.$ainfo->{OutputOwnerName}"
-		     . ".$assid-$ainfo->{ProdStepType}-$ainfo->{ProductionCycle}"
-		     . ".$run";
-	do { warn "$dropid: empty xml fragment\n"; next } if ($xmlfrag eq '0');
-	my $xml = &expandXMLFragment ($dropid, $xmlfrag);
-	$result->{$dropid} = {
-	    XML => &genXMLPreamble() . $xml . &genXMLTrailer(),
-	    SMRY => "EVDS_OutputPath=$basedir/$subdir/$ainfo->{DatasetName}\n"
-	};
-    }
-
-    return $result;
-}
-
 # Utility subroutine to expand a run XML fragment and to clean it up.
 sub expandXMLFragment
 {
@@ -196,7 +143,7 @@ sub listDatasetOwners
 	    $ds = $1;
 	} elsif (/OPTION.*value=["'\''](.*)\(\d+\)["'\'']/) {
 	    $owner = $1;
-	    push (@result, [ $ds, $owner ]) if grep ("$ds.$owner" =~ /$_/, @patterns);
+	    push (@result, [ $ds, $owner ]) if grep ("$owner/$ds" =~ /$_/, @patterns);
 	}
     }
 
@@ -253,211 +200,6 @@ sub listAssignments
     die "no assignments for $ds, $owner\n" if ! $everything;
     return map { s/.*<TD>//; ($_ =~ /(\d+)/g) }
 	   grep(/Assignments.*<TD>/, split(/\n/, $everything));
-}
-
-# Efficiently check which files exist in castor
-sub castorCheck
-{
-    my ($known, @files) = @_;
-
-    # Determine all directories we need to look in
-    my %dirs = ();
-    foreach my $file (@files)
-    {
-	$file =~ s|/[^/]+$||;
-	$dirs{$file} = 1;
-    }
-	
-    # List the contents of all the directories
-    foreach my $dir (sort keys %dirs)
-    {
-	open (NSLS, "nsls $dir |") or die "cannot run nsls: $!\n";
-	while (<NSLS>)
-	{
-	    chomp;
-	    $known->{"$dir/$_"} = 1;
-	}
-	close (NSLS);
-    }
-}
-
-# /drop/box/area should be the directory containing the state
-# directories for the drop box agents, so the script can check
-# which drops are still being processed ("pending")
-#
-# database should be the name of the tmdb oracle database, and
-# table should be the name of the file table (filesfortransfer
-# for v1, t_files for v2).
-#
-# The rest of the arguments are expected to be "drop" directories.
-# This tool reads the XML catalogue fragments from the drops, gets
-# the list of the GUIDs, and compares both with the tmdb database
-# and the drop box area to evaluate the status of the drop.
-#
-# For each drop, a line is printed out with the name of the drop,
-# the number of files found in the catalogue, the number of guids
-# already registered in tmdb, and two flags, "TRANSFERRED" and/or
-# "PENDING".  The former indicates that all the files were found
-# in tmdb, the second indicates there is still a drop in the disk
-# queues.
-sub checkAssignmentFiles
-{
-    my ($mouth, $dbh, @dirs) = @_;
-    my %pending = map { $_ => 1 }
-        map { s|.*/||; $_ }
-	<$mouth/*/{work,inbox}/*>,
-	<$mouth/*/worker-*/{work,inbox}/*>;
-
-    # Collect information for all drops first.
-    my $result = {};
-    foreach my $drop (@dirs)
-    {
-        # Evaluate drop data
-        my $xml = (<$drop/XMLCatFragment.*.{txt,xml}>)[0];
-        my $smry = (<$drop/Smry.*.txt>)[0];
-        my $pfnroot = (map { split('=', $_) } grep(/^EVDS_OutputPath=/, split(/\n/, &input($smry))))[1];
-        defined $xml or die "no xml catalogue in $drop\n";
-        defined $smry or die "no summary file in $drop\n";
-        defined $pfnroot or die "no pfn root in $drop\n";
-
-        # Get files, for each the guid and pfn
-        my $cat = &readXMLCatalogue ($xml);
-	my @files = map { { GUID => $_->{GUID}, PFN => $_->{PFN}[0]{PFN} } } values %$cat;
-	map { $_->{PFN} =~ s|^\./|$pfnroot/| } @files;
-	map { $_->{PFN} =~ s|^sfn://castorgrid.cern.ch/|/| } @files;
-
-        # Initialise results
-        my $dropname = $drop; $dropname =~ s|.*/||;
-	$result->{$drop} = { DROPNAME => $dropname, FILES => [ @files ] };
-    }
-
-    # Check all known pfns in the directories involved
-    my %knownpfn;
-    &castorCheck (\%knownpfn, map { $_->{PFN} } map { @{$_->{FILES}} } values %$result);
-
-    # Get all known guids (this *is* faster than asking for each guid)
-    my %knownguid;
-    my $stmt = $dbh->prepare ("select guid from t_file");
-    $stmt->execute();
-    while (my @row = $stmt->fetchrow_array()) {
-	$knownguid{$row[0]} = 1;
-    }
-    undef $dbh;
-
-    # Now finish off the results
-    foreach my $drop (values %$result)
-    {
-	my @files = @{$drop->{FILES}};
-
-        # Find guids/files known in tmdb and castor
-	map { $_->{TRANSFERRED} = 1 } grep ($knownguid{$_->{GUID}}, @files);
-	map { $_->{IN_MSS} = 1 } grep ($knownpfn{$_->{PFN}}, @files);
-	map { $_->{PENDING} = 1 } @files if $pending{$drop->{DROPNAME}};
-
-	# Fill in result
-	my $nfiles = scalar @files;
-	my $nindb = scalar grep($_->{TRANSFERRED}, @files);
-	my $ninmss = scalar grep($_->{IN_MSS}, @files);
-	my $npending = scalar grep($_->{PENDING}, @files);
-
-	$drop->{N_FILES}		= $nfiles;
-	$drop->{N_TRANSFERRED}		= $nindb;
-	$drop->{N_IN_MSS}		= $ninmss;
-	$drop->{N_PENDING_TRANSFER}	= $npending;
-	$drop->{IS_PENDING_TRANSFER}	= $npending != 0;
-	$drop->{IS_FULLY_TRANSFERRED}	= $nfiles == $nindb;
-	$drop->{IS_FULLY_IN_MSS}	= $nfiles == $ninmss;
-    }
-
-    return $result;
-}
-
-sub feedDropsToAgents
-{
-    my ($mouth, $request, $doit, $droptype, $status, @drops) = @_;
-    foreach my $drop (@drops)
-    {
-	my $info = $status->{$drop};
-	die "Error: no status for $drop\n" if ! defined $info;
-
-	my $dropname = $info->{DROPNAME};
-	my $type = $droptype;
-
-	# If we find multiple copies, remove replicas and pretend
-	# it's one we found elsewhere (= preferred version).
-	if ($type ne 'Done' && -d "$request/Drops/Done/$dropname")
-	{
-	    print "$type $drop already in Done, removing\n";
-	    (! $doit || &rmtree ($drop));
-	    $type = 'Done';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-        }
-	elsif ($type ne 'NotReady' && -d "$request/Drops/NotReady/$dropname")
-	{
-	    print "$type $drop already in NotReady, removing\n";
-	    (! $doit || &rmtree ($drop));
-	    $type = 'NotReady';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-    	}
-
-	# Check if it it's done (and make sure done are still done!)
-	if ($type ne 'Done' && $info->{IS_FULLY_TRANSFERRED})
-	{
-	    print "$type $drop already transferred, marking Done\n";
-	    (! $doit || system ("mv $drop $request/Drops/Done/$dropname") == 0)
-	        or die "Error: $drop: cannot move to Done: $?\n";
-	    $type = 'Done';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-	}
-	elsif ($type ne 'Done' && $info->{IS_PENDING_TRANSFER})
-	{
-	    print "$type $drop already queued, marking Done\n";
-	    (! $doit || system ("mv $drop $request/Drops/Done/$dropname") == 0)
-	        or die "Error: $drop: cannot move to Done: $?\n";
-	    $type = 'Done';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-	}
-	elsif ($type eq 'Done'
-	       && ! ($info->{IS_FULLY_TRANSFERRED}
-		     || $info->{IS_PENDING_TRANSFER}))
-	{
-	    print "$type $drop no longer done, marking Pending again\n";
-	    (! $doit || system ("mv $drop $request/Drops/Pending/$dropname") == 0)
-		or die "Error: $drop: cannot move to Pending: $?\n";
-	    $type = 'Pending';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-	}
-
-	# Process truly pending drops
-	if ($type ne 'Done' && $info->{IS_FULLY_IN_MSS})
-	{
-	    print "$type $drop available, feeding to agents and marking Done\n";
-	    (-f "$drop/go" || ! $doit || system ("touch $drop/go") == 0)
-		or die "Error: $drop: cannot touch go: $?\n";
-	    (! $doit || system ("cp -rp $drop $mouth/entry/inbox/$dropname") == 0)
-		or die "Error: $drop: cannot copy to $mouth/entry/inbox: $?\n";
-	    (! $doit || system ("mv $drop $request/Drops/Done/$dropname") == 0)
-	    	or die "Error: $drop: cannot move to Done: $?\n";
-	    $type = 'Done';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-	}
-	elsif ($type ne 'NotReady' && ! $info->{IS_FULLY_IN_MSS})
-	{
-	    print "$type $drop not yet available, marking NotReady\n";
-	    (! $doit || unlink ("$drop/go"));
-	    (! $doit || system ("mv $drop $request/Drops/NotReady/$dropname") == 0)
-	    	or die "Error: $drop: cannot move to NotReady: $?\n";
-	    $type = 'NotReady';
-	    $drop = "$request/Drops/$type/$dropname";
-	    $status->{$drop} = $info;
-	}
-    }
 }
 
 1;
