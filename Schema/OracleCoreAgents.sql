f638dbccd9aced12a7464486cb8c138f50e79719
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-01-25 12:57:40 +0000

    Add table for agent version info.

--- Schema/OracleCoreAgents.sql
+++ Schema/OracleCoreAgents.sql
@@ -19,6 +19,17 @@ create table t_agent_status
    agent		varchar (20)	not null,
    state		char (1)	not null);
 
+create table t_agent_version
+  (timestamp		float		not null,
+   node			varchar (20)	not null,
+   agent		varchar (20)	not null,
+   filename		varchar (100)	not null,
+   filesize		integer,
+   checksum		varchar (100),
+   release		varchar (100),
+   revision		varchar (100),
+   tag			varchar (100));
+
 ----------------------------------------------------------------------
 -- Add constraints
 
@@ -48,6 +59,20 @@ alter table t_agent_status
 
 alter table t_agent_status
   add constraint fk_agent_status_agent
+  foreign key (agent) references t_agent (name);
+
+
+alter table t_agent_version
+  add constraint pk_agent_version
+  primary key (node, agent, filename)
+  using index tablespace CMS_TRANSFERMGMT_INDX01;
+
+alter table t_agent_version
+  add constraint fk_agent_version_node
+  foreign key (node) references t_node (name);
+
+alter table t_agent_version
+  add constraint fk_agent_version_agent
   foreign key (agent) references t_agent (name);
 
 ----------------------------------------------------------------------
