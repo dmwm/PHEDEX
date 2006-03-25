e97727b1c005e35ec662b4c0e2e7815c3a5aea6a
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-03-25 00:41:23 +0000

    Don't insert rows if the column is being set to null.

--- Schema/OracleCoreTriggers.sql
+++ Schema/OracleCoreTriggers.sql
@@ -3,37 +3,45 @@
 
 create or replace trigger tr_xfer_state_available
   after update of time_available on t_xfer_state for each row begin
-    insert into t_xfer_tracking (timestamp, from_node, to_node,
-				 priority, fileid, reason)
-    values (:new.time_available, :new.from_node, :new.to_node,
-	     :new.priority, :new.fileid, 'avail');
+    if :new.time_available is not null then
+      insert into t_xfer_tracking (timestamp, from_node, to_node,
+				   priority, fileid, reason)
+      values (:new.time_available, :new.from_node, :new.to_node,
+	      :new.priority, :new.fileid, 'avail');
+    end if;
   end;
 /
 
 create or replace trigger tr_xfer_state_start
   after update of time_xfer_start on t_xfer_state for each row begin
-    insert into t_xfer_tracking (timestamp, from_node, to_node,
-				 priority, fileid, reason)
-    values (:new.time_xfer_start, :new.from_node, :new.to_node,
-	     :new.priority, :new.fileid, 'try');
+    if :new.time_xfer_start is not null then
+      insert into t_xfer_tracking (timestamp, from_node, to_node,
+				   priority, fileid, reason)
+      values (:new.time_xfer_start, :new.from_node, :new.to_node,
+	      :new.priority, :new.fileid, 'try');
+    end if;
   end;
 /
 
 create or replace trigger tr_xfer_state_end
   after update of time_xfer_end on t_xfer_state for each row begin
-    insert into t_xfer_tracking (timestamp, from_node, to_node,
-			         priority, fileid, reason)
-    values (:new.time_xfer_end, :new.from_node, :new.to_node,
-	    :new.priority, :new.fileid, 'done');
+    if :new.time_xfer_end is not null then
+      insert into t_xfer_tracking (timestamp, from_node, to_node,
+			           priority, fileid, reason)
+      values (:new.time_xfer_end, :new.from_node, :new.to_node,
+	      :new.priority, :new.fileid, 'done');
+    end if;
   end;
 /
 
 create or replace trigger tr_xfer_state_error
   after update of time_error_start on t_xfer_state for each row begin
-    insert into t_xfer_tracking (timestamp, from_node, to_node,
-				 priority, fileid, reason)
-    values (:new.time_error_start, :new.from_node, :new.to_node,
-	     :new.priority, :new.fileid, 'fail');
+    if :new.time_error_start is not null then
+      insert into t_xfer_tracking (timestamp, from_node, to_node,
+				   priority, fileid, reason)
+      values (:new.time_error_start, :new.from_node, :new.to_node,
+	      :new.priority, :new.fileid, 'fail');
+    end if;
   end;
 /
 
