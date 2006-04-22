f33589d194c277c7ce8760d9cd9006c1900cfd41
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-04-22 21:14:48 +0000

    Back to 10*1000 files.

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -2,5 +2,5 @@ sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleRe
 sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleInit.sql </dev/null
 sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleStatsEnable.sql </dev/null
 sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
-Testbed/RouterScaling/SetupScaleTest -db Schema/DBParam:Testbed -count 100 -blocks 10
+Testbed/RouterScaling/SetupScaleTest -db Schema/DBParam:Testbed -count 1000 -blocks 10
 sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleStatsUpdate.sql </dev/null
