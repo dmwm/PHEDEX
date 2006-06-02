446ca4847fac3b2c4434fbfaeb99146f71adfa22
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-06-02 13:47:50 +0000

    Increase grace period to 15 seconds to allow ftscp to exit.

--- Toolkit/Common/UtilsJobManager.pm
+++ Toolkit/Common/UtilsJobManager.pm
@@ -227,7 +227,7 @@ sub killAllJobs
 	# While there are jobs to run, mark them timed out,
 	# then wait job processing to terminate all those.
 	# This allows job actions to clean up properly.
-	map { $_->{TIMEOUT} = 1; $_->{TIMEOUT_GRACE} = 5 } @{$self->{JOBS}};
+	map { $_->{TIMEOUT} = 1; $_->{TIMEOUT_GRACE} = 15 } @{$self->{JOBS}};
 	$self->pumpJobs();
 	select (undef, undef, undef, 0.1);
     }
