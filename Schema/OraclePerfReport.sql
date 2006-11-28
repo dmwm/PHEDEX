9a118f7cace2821fee597559560052fdf98c8db7
Jens Rehn <sha1-cb7d17d8b03b6f839afb7b1302df15def39823c4@cern.ch>
2006-11-28 10:53:44 +0000

    Improved date and time format suggested by Jacek

--- Schema/OraclePerfReport.sql
+++ Schema/OraclePerfReport.sql
@@ -2,6 +2,7 @@
 -- Dump database performance report.
 
 set lines 100 pages 9000 define :
+alter session set nls_timestamp_format='YY-MON-DD HH24-MI-SS';
 select snap_id, begin_interval_time from dba_hist_snapshot where instance_number=1 order by 1;
 spool report.txt
 select * from table(DBMS_WORKLOAD_REPOSITORY.AWR_REPORT_TEXT(194199460, 1, :begin_id, :end_id));
