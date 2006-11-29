33a6b81eb6bfbff21bf8b557c1f9c06d287dc934
Jens Rehn <sha1-cb7d17d8b03b6f839afb7b1302df15def39823c4@cern.ch>
2006-11-29 15:05:34 +0000

    Adjusted to V2.5 schema. Uses now NodeNew and LinkNew to create nodes and links respectively.

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -1,6 +1,8 @@
-sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleReset.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleInit.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleStatsEnable.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
-Testbed/RouterScaling/SetupScaleTest -db Schema/DBParam:Testbed -count 1000 -blocks 10
-sqlplus -S $(Schema/OracleConnectId -db Schema/DBParam:Testbed) @Schema/OracleStatsUpdate.sql </dev/null
+#!/bin/sh
+
+sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleReset.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleInit.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleStatsEnable.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
+Testbed/RouterScaling/SetupScaleTest -db Schema/DBParam:Prod/Testnode -count 10 -blocks 1 -nodes 4:Buffer,2:MSS,2:Disk
+sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleStatsUpdate.sql </dev/null
--- Testbed/RouterScaling/SetupScaleTest.sql
+++ Testbed/RouterScaling/SetupScaleTest.sql
@@ -1,45 +1,8 @@
--- create nodes
-insert into t_node (id,name) values (seq_node.nextval,'A');
-insert into t_node (id,name) values (seq_node.nextval,'A_SPUR');
-insert into t_node (id,name) values (seq_node.nextval,'B');
-insert into t_node (id,name) values (seq_node.nextval,'B_SPUR');
-insert into t_node (id,name) values (seq_node.nextval,'C');
-insert into t_node (id,name) values (seq_node.nextval,'C_SPUR');
-insert into t_node (id,name) values (seq_node.nextval,'D');
-insert into t_node (id,name) values (seq_node.nextval,'D_SPUR');
-insert into t_node (id,name) values (seq_node.nextval,'E');
-insert into t_node (id,name) values (seq_node.nextval,'E_SPUR');
-
-insert into t_link (id, from_node, to_node, distance, local_boost)
-  select seq_link.nextval, n1.id, n2.id, 1, 1
-  from t_node n1, t_node n2
-  where (n1.name like '_' and n2.name = n1.name || '_SPUR')
-     or (n2.name like '_' and n1.name = n2.name || '_SPUR');
-
-insert into t_link (id, from_node, to_node, distance, local_boost)
-  select seq_link.nextval, n1.id, n2.id, 1, 0
-  from t_node n1, t_node n2
-  where n1.name like '_' and n2.name like '_';
-
-insert into t_link_share (link, priority, link_share)
-  select id, 0, 7 from t_link;
-insert into t_link_share (link, priority, link_share)
-  select id, 1, 4 from t_link;
-insert into t_link_share (link, priority, link_share)
-  select id, 2, 1 from t_link;
-insert into t_link_share (link, priority, link_share)
-  select id, 3, 1 from t_link;
-
 -- seed blocks
-insert into t_dps_dbs (id,name,time_create)
-  values (seq_dps_dbs.nextval,'TestDBS',0);
+insert into t_dps_dbs (id,name,dls,time_create)
+  values (seq_dps_dbs.nextval,'TestDBS','mysql://lxgate10.cern.ch:18081',0);
 insert into t_dps_dataset (id,dbs,name,is_open,is_transient,time_create)
   values (seq_dps_dataset.nextval,seq_dps_dbs.currval,'TestDS','y','n',0);
 
--- seed subscriptions
-insert into t_dps_subscription
-  (dataset, destination, priority, is_move, is_transient, time_create)
-  select ds.id, n.id, 1, 'n', 'n', 0
-  from t_dps_dataset ds, t_node n;
 
 commit;
