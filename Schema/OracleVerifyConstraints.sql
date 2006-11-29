d8c2b3f7f669b7204c849ecb444c4b9728c32df3
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-11-29 21:50:16 +0000

    Rewrite query in a form I can understand better.

--- Schema/OracleVerifyConstraints.sql
+++ Schema/OracleVerifyConstraints.sql
@@ -1,15 +1,13 @@
-select position, table_name, column_name from
- (select t1.table_name, t1.column_name, t1.position, t2.index_name ind_name
-  from (select constraint_name, table_name, column_name, position
-        from user_cons_columns where (table_name, constraint_name) in
-          (select table_name, constraint_name from user_constraints
-           where constraint_type = 'R')
-        order by table_name, constraint_Name, position) t1,
-       (select index_name, table_name, column_name, column_position
-        from all_ind_columns
-        order by table_name, index_name, column_position) t2
-  where t1.table_name = t2.table_name(+)
-    and t1.column_name = t2.column_name(+)
-    and t1.position = t2.column_position(+)
-  order by t1.table_name, t1.column_name)
-where ind_name is null;
+select position, constraint_name, column_name
+from user_cons_columns cc
+where exists
+  (select 1 from user_constraints c
+   where c.constraint_type = 'R'
+     and c.table_name = cc.table_name
+     and c.constraint_name = cc.constraint_name)
+and not exists
+  (select 1 from user_ind_columns i
+   where i.table_name = cc.table_name
+     and i.column_name = cc.column_name
+     and i.column_position = cc.position)
+order by table_name, constraint_name;
