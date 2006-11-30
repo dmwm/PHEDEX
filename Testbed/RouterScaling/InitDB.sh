a85cc0cc57a9ecf38ffcd6c50d2f067addc35f48
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-11-30 13:25:05 +0000

    Correct paths, source environment for sqlplus.

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleReset.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleInit.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleStatsEnable.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
-Testbed/RouterScaling/SetupScaleTest -db Schema/DBParam:Prod/Testnode -count 10 -blocks 1 -nodes 4:Buffer,2:MSS,2:Disk
-sqlplus -S $(Schema/OracleConnectId  -db Schema/DBParam:Prod/Testnode) @Schema/OracleStatsUpdate.sql </dev/null
+source ../sw/slc*/cms/PHEDEX/PHEDEX_*/etc/profile.d/env.sh
+sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleReset.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleInit.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsEnable.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
+Testbed/RouterScaling/SetupScaleTest -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode -count 10 -blocks 1 -nodes 4:Buffer,2:MSS,2:Disk
+sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsUpdate.sql </dev/null
