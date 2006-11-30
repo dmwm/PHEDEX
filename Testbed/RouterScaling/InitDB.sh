04da59a75ffa586e1c1e4ca592c9a47aa5dc0dcc
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-11-30 16:07:16 +0000

    Create a larger test sample.

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -5,5 +5,5 @@ sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Te
 sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleInit.sql </dev/null
 sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsEnable.sql </dev/null
 sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
-Testbed/RouterScaling/SetupScaleTest -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode -count 10 -blocks 1 -nodes 4:Buffer,2:MSS,2:Disk
+Testbed/RouterScaling/SetupScaleTest -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode -count 10000 -blocks 10 -nodes 4:Buffer,2:MSS,2:Disk
 sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsUpdate.sql </dev/null
