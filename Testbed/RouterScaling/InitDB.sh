70d1bc17f8cc16722b1b7aaca327b42ae8cad39e
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-12-02 14:31:54 +0000

    Factor out database parameters.  Use new setup script options.

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 source ../sw/slc*/cms/PHEDEX/PHEDEX_*/etc/profile.d/env.sh
-sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleReset.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleInit.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsEnable.sql </dev/null
-sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
-Testbed/RouterScaling/SetupScaleTest -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode -count 10000 -blocks 10 -nodes 4:Buffer,2:MSS,2:Disk
-sqlplus -S $(Schema/OracleConnectId  -db ../SITECONF/CERN/PhEDEx/DBParam:Prod/Testnode) @Schema/OracleStatsUpdate.sql </dev/null
+db=../SITECONF/CERN/PhEDEx/DBParam.lat:Validation
+sqlplus -S $(Schema/OracleConnectId  -db $db) @Schema/OracleReset.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db $db) @Schema/OracleInit.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db $db) @Schema/OracleStatsEnable.sql </dev/null
+sqlplus -S $(Schema/OracleConnectId  -db $db) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
+Testbed/RouterScaling/SetupScaleTest -db $db -datasets 100 -blocks 10 -files 1000 -nodes 8:T1,40:T2
+sqlplus -S $(Schema/OracleConnectId  -db $db) << EOF
+  update t_adm_node set kind = 'Disk' where kind = 'Buffer';
+EOF
+sqlplus -S $(Schema/OracleConnectId  -db $db) @Schema/OracleStatsUpdate.sql </dev/null
