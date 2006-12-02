fdc5aec5bdb54a3e4c05cdd2572e178c71fd96d7
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2006-12-02 14:31:25 +0000

    Datasets are now created by the setup script.

--- Testbed/RouterScaling/SetupScaleTest.sql
+++ Testbed/RouterScaling/SetupScaleTest.sql
@@ -1,8 +1,4 @@
 -- seed blocks
 insert into t_dps_dbs (id,name,dls,time_create)
   values (seq_dps_dbs.nextval,'TestDBS','mysql://lxgate10.cern.ch:18081',0);
-insert into t_dps_dataset (id,dbs,name,is_open,is_transient,time_create)
-  values (seq_dps_dataset.nextval,seq_dps_dbs.currval,'TestDS','y','n',0);
-
-
 commit;
