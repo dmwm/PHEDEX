c89d77b206268a98f667011e6370cde398486f37
Jens Rehn <sha1-cb7d17d8b03b6f839afb7b1302df15def39823c4@cern.ch>
2007-02-09 15:25:14 +0000

    Check for correct number of arguments. Fix documentation.

--- Schema/OracleInitRole.sh
+++ Schema/OracleInitRole.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ##H Usage:
-##H   ApplyRole DBPARAM:SECTION KEY-DIRECTORY/USERCERT-FILE SITE-NAME NODE[,NODE...]
+##H   OracleInitRole.sh DBPARAM:SECTION KEY-DIRECTORY/USERCERT-FILE SITE-NAME NODE[,NODE...]
 ##H
 ##H Where:
 ##H DBPARAM         is the database parameter file with
@@ -21,7 +21,7 @@
 ##H SITE-NAME       is the name of the site (e.g. "CERN")
 ##H NODE[,NODE...]  comma-separated list of node names
 
-[ $# != 3 ] && { echo "Insufficient parameters." 1>&2; exit 1; }
+[ $# != 4 ] && { echo "Insufficient parameters." 1>&2; exit 1; }
 
 dbparam="$(echo $1 | sed 's/:.*//')"
 section="$(echo $1 | sed 's/.*://')"
