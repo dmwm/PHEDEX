5fa959b8b03aae5751a2356832fe2cabcbebac4f
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2007-05-31 13:53:16 +0000

    site/user schema changes

--- Schema/OracleCoreAdm.sql
+++ Schema/OracleCoreAdm.sql
@@ -2,37 +2,28 @@
 -- Create tables
 
 create sequence seq_adm_identity;
-create sequence seq_adm_identity_attr;
 create sequence seq_adm_client;
 create sequence seq_adm_contact;
 create sequence seq_adm_contact_attr;
-create sequence seq_adm_site;
 
+/* Information gathered from Site DB via Security Module.
+   This information is for historical reference only, not an
+   up-to-date list of users.  For that, site DB is used. As this is
+   historical information, entries here should not be deleted.
+*/
 create table t_adm_identity
-  (id			integer		not null,
+  (id			integer		not null, -- ID for PhEDEx use
+   secmod_id		integer			, -- SecurityModule ID number
+   name			varchar (4000)		, -- SecurityModule name (forename + surname)
+   email		varchar (4000)		, -- SecurityModule email
+   dn			varchar (4000)		, -- SecurityModule Distinguished name
+   certificate		varchar (4000)		, -- SecurityModule certificate
+   username		varchar (4000)		, -- SecurityModule username (hypernews)
+   time_update		integer			, -- Time last updated
    --
    constraint pk_adm_identity
-     primary key (id));
-
-create table t_adm_identity_attr
-  (id			integer		not null,
-   identity		integer		not null,
-   name			varchar (1000)	not null,
-   value		varchar (4000),
-   --
-   constraint pk_adm_identity_attr
-     primary key (id),
-   --
-   constraint uq_adm_identity_attr_key
-     unique (identity, name),
-   --
-   constraint uq_adm_identity_attr_value
-     unique (identity, name, value),
-   --
-   constraint fk_adm_identity_attr_identity
-     foreign key (identity) references t_adm_identity (id)
-     on delete cascade);
-
+     primary key (id)
+);
 
 create table t_adm_contact
   (id			integer		not null,
@@ -76,71 +67,14 @@ create table t_adm_client
      foreign key (contact) references t_adm_contact (id));
 
 
-create table t_adm_site
-  (id			integer		not null,
-   name			varchar (1000)	not null,
-   role_name		varchar (40)	not null,
-   --
-   constraint pk_adm_site
-     primary key (id),
-   --
-   constraint uq_adm_site_name
-     unique (name),
-   --
-   constraint uq_adm_site_role
-     unique (role_name));
-
-
-create table t_adm_site_node
-  (site			integer		not null,
-   node			integer		not null,
-   --
-   constraint pk_adm_site_node
-     primary key (site, node),
-   --
-   constraint fk_adm_site_node_site
-     foreign key (site) references t_adm_site (id)
-     on delete cascade,
-   --
-   constraint fk_adm_site_node_node
-     foreign key (node) references t_adm_node (id)
-     on delete cascade);
-
-
-create table t_adm_site_admin
-  (site			integer		not null,
-   identity		integer		not null,
-   --
-   constraint pk_adm_site_admin
-     primary key (site, identity),
-   --
-   constraint fk_adm_site_admin_site
-     foreign key (site) references t_adm_site (id)
-     on delete cascade,
-   --
-   constraint fk_adm_site_admin_identity
-     foreign key (identity) references t_adm_identity (id)
-     on delete cascade);
-
-
-create table t_adm_global_admin
-  (identity		integer		not null,
-   --
-   constraint pk_adm_global_admin
-     primary key (identity),
-   --
-   constraint fk_adm_global_admin_id
-     foreign key (identity) references t_adm_identity (id)
-     on delete cascade);
-
 ----------------------------------------------------------------------
 -- Create indices
 
+create index ix_adm_identity_sitedb_id
+  on t_adm_identity (sitedb_id);
+
+create index ix_adm_identity_dn
+  on t_adm_identity (dn);
+
 create index ix_adm_client_contact
   on t_adm_client (contact);
-
-create index ix_adm_site_admin_identity
-  on t_adm_site_admin (identity);
-
-create index ix_adm_site_node_node
-  on t_adm_site_node (node);
