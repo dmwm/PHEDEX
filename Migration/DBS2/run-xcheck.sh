db9d4ab35288d8c26f364958ef15c48a43943486
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2007-06-04 13:50:53 +0000

    Adding script for automated consistency check

---
+++ Migration/DBS2/run-xcheck.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+DBS_INSTANCE=$1
+TIME_FMT=$2
+DIR=$3
+
+if [ -z "$DBS_INSTANCE" ]
+then
+  DBS_INSTANCE="global"
+fi
+
+if [ -z "$TIME_FMT" ]
+then
+  TIME_FMT="%s"
+fi
+
+if [ -z "$DIR" ]
+then
+  DIR=$PWD
+fi
+
+if [ -z "$PHEDEX_DBPARAM"]
+then
+  echo "ERROR:  Set the PHEDEX_DBPARAM variable"
+  exit 1
+fi
+
+if [ -z "$MIGRATION_FILE"]
+then
+  echo "ERROR:  Set the MIGRATION_FILE variable"
+  exit 2
+fi
+
+source /data/PHEDEX/DBS2Migration/slc3_ia32_gcc323/external/python/2.4.3/etc/profile.d/init.sh
+source /data/PHEDEX/DBS2Migration/slc3_ia32_gcc323/external/py2-cx-oracle/4.2/etc/profile.d/init.sh
+source /data/PHEDEX/DBS2Migration/slc3_ia32_gcc323/cms/dbs-client/DBS_1_0_0/etc/profile.d/init.sh
+
+PHEDEX_DB_R="${DBPARAM}:Prod/Reader"
+DBS2_R="http://cmsdbsprod.cern.ch/cms_dbs_prod_${DBS_INSTANCE}/servlet/DBSServlet"
+
+XCHECK_TIME=`date +$TIME_FMT`
+XCHECK_FILE="$DIR/xcheck-${DBS_INSTANCE}-${XCHECK_TIME}.txt"
+
+echo "Running DBS/PhEDEx cross-check"
+echo "DBS            $DBS2_R"
+echo "TMDB           Prod/Reader"
+echo "MIGRATION_FILE $MIGRATION_FILE"
+echo "Output file    $XCHECK_FILE"
+echo "Beginning cross-check..."
+PHEDEX/Migration/DBS2/TMDBPostMigrationStats -d -R -f $MIGRATION_FILE -u "$DBS2_R" -c $(PHEDEX/Utilities/OracleConnectId -d $PHEDEX_DB_R) > $XCHECK_FILE
+echo "Done"
