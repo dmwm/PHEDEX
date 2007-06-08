2a188015e96165b7a539515dcbeeab063445e92d
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2007-06-08 08:25:00 +0000

    Added t_adm_my_node and some documentation.

--- Schema/OracleCoreAdm.sql
+++ Schema/OracleCoreAdm.sql
@@ -7,10 +7,10 @@ create sequence seq_adm_contact;
 create sequence seq_adm_contact_attr;
 
 /* Information gathered from Site DB via Security Module.
-   This information is for historical reference only, not an
-   up-to-date list of users.  For that, site DB is used. As this is
-   historical information, entries here should not be deleted.
-*/
+ * This information is for historical reference only, not an
+ * up-to-date list of users.  For that, site DB is used. As this is
+ * historical information, entries here should not be deleted.
+ */
 create table t_adm_identity
   (id			integer		not null, -- ID for PhEDEx use
    secmod_id		integer			, -- SecurityModule ID number
@@ -25,13 +25,14 @@ create table t_adm_identity
      primary key (id)
 );
 
+/* A logged access from the web page */
 create table t_adm_contact
   (id			integer		not null,
    --
    constraint pk_adm_contact
      primary key (id));
 
-
+/* contact name/value attributes */
 create table t_adm_contact_attr
   (id			integer		not null,
    contact		integer		not null,
@@ -48,7 +49,7 @@ create table t_adm_contact_attr
      foreign key (contact) references t_adm_contact (id)
      on delete cascade);
 
-
+/* map of identity to contact */
 create table t_adm_client
   (id			integer		not null,
    identity		integer		not null,
@@ -66,6 +67,21 @@ create table t_adm_client
    constraint fk_adm_client_contact
      foreign key (contact) references t_adm_contact (id));
 
+/* Map of identity to node for web page user preferences */
+create table t_adm_my_node
+  (identity		integer		not null,
+   node			integer		not null,
+   --
+   constraint pk_adm_my_node
+     primary key (identity, node),
+   --
+   constraint fk_adm_my_node_identity
+     foreign key (identity) references t_adm_identity (id)
+     on delete cascade,
+   constraint fk_adm_my_node_node
+     foreign key (node) references t_adm_node (id)
+     on delete cascade);
+);
 
 ----------------------------------------------------------------------
 -- Create indices
