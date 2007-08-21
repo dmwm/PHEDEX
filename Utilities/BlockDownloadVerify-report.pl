bcb11453042905445acec73de4cda2609ae22b5c
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2007-08-21 08:31:44 +0000

    simple printout of the reports

---
+++ Utilities/BlockDownloadVerify-report.pl
@@ -0,0 +1,182 @@
+#!/usr/bin/env perl
+use strict;
+
+##H
+##H  Report block-test results from the BlockDownloadVerify agent
+##H
+##H  This help is far from complete, ask Tony if you want to use this script.
+##H
+##H  Options:
+##H 
+##H   --db = DBCONFIG     The usual PhEDEx db configuration file
+##H   --node = <string>   PhEDEx node to search for replicas
+##H 
+
+BEGIN {
+    $^W = 1; use strict; use warnings;
+    our $me = $0; $me =~ s|.*/||;
+    our $home = $0;
+    if ( $home !~ m%/% ) { $home = '.'; }
+    $home =~ s|/[^/]+$||;
+    $home ||= ".";
+    $home .= "/../Toolkit/Common";
+    unshift(@INC, $home);
+}
+
+# Process command line arguments.
+use Getopt::Long;
+use UtilsHelp;
+use UtilsDB;
+use UtilsCatalogue;
+
+my ($dbh,$conn,$dbconfig,$r,$s);
+my ($nodes,@nodes,$help);
+my ($id,$block,$n_files,$n_tested,$n_ok,$status,$test,$time_reported);
+my ($debug_me);
+
+$debug_me = 1;
+
+$n_files  = 0;
+GetOptions(	"db=s"		=> \$dbconfig,
+		"node=s"	=> \@nodes,
+
+		"help|h"	=> sub { &usage() }
+          );
+
+#-------------------------------------------------------------------------------
+$dbconfig or die "'--dbconfig' argument is mandatory\n";
+@nodes    or die "'--node' argument is mandatory\n";
+
+$conn = { DBCONFIG => $dbconfig };
+$dbh = &connectToDatabase ( $conn, 0 );
+
+#-------------------------------------------------------------------------------
+$nodes = expandNodeList(@nodes);
+$DB::single=1;
+
+$r = dvsTestResults(keys %{$nodes});
+printf("%24s %6s %7s %7s %7s %10s %10s %s\n",
+	  'Time Reported',
+	  'ID',
+	  'NFiles',
+	  'NTested',
+	  'NOK',
+	  'Test',
+	  'Status',
+	  'Block Name'
+      );
+foreach $s ( @{$r} )
+{
+  printf("%24s %6d %7d %7d %7d %10s %10s %s\n",
+	  scalar localtime $s->{TIME_REPORTED},
+	  $s->{ID},
+	  $s->{N_FILES},
+	  $s->{N_TESTED},
+	  $s->{N_OK},
+	  $s->{TEST},
+	  $s->{STATUS},
+	  $s->{BLOCK}
+	);
+}
+
+exit 0;
+
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Everything below here should find its way into a Perl module at some point
+#-------------------------------------------------------------------------------
+sub dvsTestResults
+{
+  my ($sql,$q,$nodelist,@r);
+
+  $nodelist = join(',',@_);
+  $sql = qq{ select v.id, b.name block, n_files, n_tested, n_ok,
+	     s.name status, t.name test, time_reported
+	     from t_status_block_verify v join t_dvs_status s on v.status = s.id
+	     join t_dps_block b on v.block = b.id
+	     join t_dvs_test t on v.test = t.id
+	     where node in ($nodelist) and status > 0
+	     order by time_reported, status
+	   };
+
+  $q = execute_sql( $sql, () );
+  while ( $_ = $q->fetchrow_hashref() ) { push @r, $_; }
+
+  return \@r;
+}
+
+#-------------------------------------------------------------------------------
+sub expandNodeList
+{
+  my ($item,%result,$sql);
+  foreach my $item ( @_ )
+  {
+    my $tmp = getNodeFromWildCard($item);
+    map { $result{$_} = $tmp->{$_} } keys %$tmp;
+  }
+  return \%result;
+}
+
+#-------------------------------------------------------------------------------
+sub getNodeFromWildCard
+{
+  my $sql =
+        qq {select id, name, technology from t_adm_node
+		 where upper(name) like :node };
+  my %p = ( ":node" => uc shift );
+  my $r = select_hash( $sql, 'ID', %p );
+  return $r;
+}
+
+#-------------------------------------------------------------------------------
+sub select_single
+{
+  my ( $query, %param ) = @_;
+  my ($q,@r);
+
+  $q = execute_sql( $query, %param );
+  @r = map {$$_[0]} @{$q->fetchall_arrayref()};
+  return \@r;
+}
+
+#-------------------------------------------------------------------------------
+sub select_all
+{
+  my ( $query ) = @_;
+  my ($q,@r);
+
+  $q = execute_sql( $query, () );
+  @r = @{$q->fetchall_arrayref()};
+  return \@r;
+}
+
+#-------------------------------------------------------------------------------
+sub select_hash
+{
+  my ( $query, $key, %param ) = @_;
+  my ($q,$r);
+
+  $q = execute_sql( $query, %param );
+  $r = $q->fetchall_hashref( $key );
+
+  my %s;
+  map { $s{$_} = $r->{$_}; delete $s{$_}{$key}; } keys %$r;
+  return \%s;
+}
+
+#-------------------------------------------------------------------------------
+sub execute_sql
+{
+  my ( $query, %param ) = @_;
+  my ($q,$r);
+
+  if ( $query =~ m%\blike\b%i )
+  {
+    foreach ( keys %param ) { $param{$_} =~ s%_%\\_%g; }
+    $query =~ s%like\s+(:[^\)\s]+)%like $1 escape '\\' %gi;
+  }
+
+  $q = &dbexec($dbh, $query, %param);
+  return $q;
+}
