14768dce3f8a0dc5b74da86507e1c0a14555d89a
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2007-10-10 14:51:15 +0000

    address #26969 by suicide if I cannot find my hostname

--- Toolkit/Common/UtilsNet.pm
+++ Toolkit/Common/UtilsNet.pm
@@ -3,21 +3,37 @@ our @EXPORT = qw(getfullhostname resolvehostname getURL);
 require Socket;
 use Net::hostent;
 
+eval { require Sys::Hostname; };
+$@ && print STDERR "No Sys::Hostname? You should consider installing it!\n";
+
 sub getfullhostname {
   my ($hostname) = @_;
+
+# Sys::Hostname exports hostname by default, but the debugger namespace
+# confuses the issue. If you want consistant results in the debugger, you have
+# to fully specify which hostname function you want to check the existance of.
+  if ( defined(&Sys::Hostname::hostname) )
+  {
+#   No need to check it myself, hostname croaks if it can't find a name
+    return Sys::Hostname::hostname();
+  }
+
   my ($name,$aliases,$addrtype,$length,@addrs);
   if (! defined $hostname) {
     chop($hostname = `hostname`);
   }
+
   ($name,$aliases,$addrtype,$length,@addrs) = CORE::gethostbyname($hostname);
   my @names = ($hostname, $name, split(' ', $aliases));
   foreach my $n (@names) {
     return $n if ($n =~ /^[^.]+\.[^.]+/ && $n !~ /\.local$/);
   }
+  defined $hostname or die "Cannot get hostname, spitting the dummy...\n";
   return $hostname;
 }
 
 sub resolvehostname {
+# This appears to be redundant, I can find no code that calls it... TW.
   my @result = ();
   foreach my $hostname (@_) {
     my ($name,$aliases,$addrtype,$length,@addrs) = CORE::gethostbyname($hostname);
