36575815081ecafdc6ed8bcbc05505401641307a
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2007-10-11 15:49:57 +0000

    css to make it prettier. Gotta do something in the meeting :-)

--- perl_lib/pod.sh
+++ perl_lib/pod.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+cd `dirname $0`
 dest=$1
 if [ -z "$dest" ]; then
   echo "Destination directory is required."
   exit 1
 fi
 
+css=$2
+if [ -z "$css" ]; then
+  css=http://cern.ch/wildish/PHEDEX/phedex_pod.css
+  echo "Using default CSS file: $css"
+  echo "Copy (and edit) that file if you want something different..."
+fi
+
+version=`cat ../VERSION`
+ssss="$dest/$version"
+index="$dest/index.html"
 for dir in `find . -type d -print | grep -v CVS`; do
   echo -n "Check existence of $dest/$dir: "
   if [ -d "$dest/$dir" ]; then
@@ -16,11 +27,24 @@ for dir in `find . -type d -print | grep -v CVS`; do
   fi
 done
 
+echo "<html>
+<head>PhEDEx module index for version $version</head>
+<body>
+  <ul>
+" | tee $index >/dev/null
+
 htmlroot=$dest
 for f in `find . -type f -name '*.pm'`; do
   echo "Podding $f..."
   g=`echo $f | sed -e 's%pm$%html%'`
-  pod2html --infile $f --outfile $dest/$g --header --htmlroot $htmlroot
+  pm=`echo $f | sed -e 's%pm$%%' -e 's%^\./%%' -e 's%/%::%g'`
+  pod2html --infile $f --outfile $dest/$g --header --htmlroot $htmlroot \
+  	-css http://cern.ch/wildish/PHEDEX/phedex_pod.css
+  echo "<li><a href='$g'>$pm</a></li>" >> $index
 done
+
+echo "</ul>
+</body>
+</html>" >> $index
 
 echo "Finished..."
