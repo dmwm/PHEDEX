93b1a27c1fa4cb91eaf62c129d554530b2dd4f67
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2007-12-04 12:27:28 +0000

    re-worked core logic to deactivate all genuine candidates in one cycle, not one per cycle

--- perl_lib/PHEDEX/BlockDeactivate/Agent.pm
+++ perl_lib/PHEDEX/BlockDeactivate/Agent.pm
@@ -20,9 +20,9 @@ L<PHEDEX::Core::Agent|PHEDEX::Core::Agent>
 
 use strict;
 use warnings;
-use base 'PHEDEX::Core::Agent';
+use base 'PHEDEX::Core::Agent', 'PHEDEX::BlockDeactivate::SQL';
 use PHEDEX::Core::Logging;
-use PHEDEX::Core::Timing;
+use PHEDEX::Core::Timing qw / mytimeofday /;
 use PHEDEX::Core::DB;
 
 our %params =
@@ -38,130 +38,132 @@ sub new
   my $proto = shift;
   my $class = ref($proto) || $proto;
 
-  my $self = $class->SUPER::new(@_);
-  foreach ( keys %params )
-  {
-    $self->{$_} = $params{$_} unless defined $self->{$_};
-  }
+# Prepend my own default parameters to the arguments. This maintains the
+# correct hierarchical order
+  my $self = $class->SUPER::new(%params, @_);
 
   bless $self, $class;
   return $self;
 }
 
+sub AUTOLOAD
+{
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  if ( exists($params{$attr}) )
+  {
+    $self->{$attr} = shift if @_;
+    return $self->{$attr};
+  }
+  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+  my $parent = "SUPER::" . $attr;
+  $self->$parent(@_);
+}
+
 # Run the main loop of this agent.
 sub idle
 {
-    my ($self, @pending) = @_;
-    my $dbh = undef;
+  my ($self, @pending) = @_;
+  my $dbh = undef;
+
+  $self->maybeStop;
+  $dbh = &connectToDatabase ($self);
+
+  # Deactivate complete blocks.  Get all blocks whose all replicas
+  # are complete and active.  First of all, ignore blocks where
+  # replicas are already inactive.  Secondly, ignore all blocks
+  # which have been touched "recently" to allow things to settle.
+  #
+  # Consider remaining (active) block replicas.  If all replicas
+  # of a block have as many files as the block has, deactive the
+  # block (and all replicas).  We also require for extra safety
+  # that there can be no files in transfer, or export, and the
+  # block is not scheduled for deletion
+  #
+  # We do *not* require that dest_files = node_files, as at file
+  # source nodes dest_files is usually zero, and we still want
+  # to deactivate.  This should be safe -- we deactivate a block
+  # on intermediate node is if a) the entire block has already
+  # reached all current destination nodes, b) either all or none
+  # of the files have been removed in intermediate nodes, and
+  # c) several days has passed like this.  It is unlikely that
+  # all these criteria will be met simultaneously, and in any
+  # case the fix is easy: reactivate the block.
+  #
+  # What we do here requires a great degree of transactinal
+  # consistency.  For one, we don't want someone to open blocks
+  # while we are deactiving them; this is prevented by locking
+  # the t_dps_block row with "select .. for update" while operating
+  # on it, a convention followed by all programs which modify
+  # block table.  Secondly, we need to make sure we delete the
+  # exact number of file replicas we expected to delete; if an
+  # inconsitency is detected, we roll back the transcation.
+  #
+  # Change of loop logic with 2.5.4.2: Now we get the list of blocks which are
+  # candidates for deactivation, then we go through the list one by one, lock
+  # the block, check that it is still a candidate, then process it. This allows
+  # us to process many blocks in one pass of the agent.
+  #
+  # This also means that the logic for stopping the agent has changed, there's
+  # a call to maybeStop inside the loop, just in case it gets too busy, and
+  # the eval has moved inside the loop too.
+  my $limit = &mytimeofday () - $self->{HOLDOFF};
+  my $qblocks = $self->getBlockDeactivationCandidates( LIMIT => $limit );
+  my ($qb,$id,$name,$nfr,$b);
+  &logmsg("Found " . scalar @{$qblocks} . " candidates");
+  foreach $qb ( @{$qblocks} )
+  {
     eval
     {
-	$dbh = &connectToDatabase ($self);
-
-	# Deactivate complete blocks.  Get all blocks whose all replicas
-	# are complete and active.  First of all, ignore blocks where
-	# replicas are already inactive.  Secondly, ignore all blocks
-	# which have been touched "recently" to allow things to settle.
-	#
-	# Consider remaining (active) block replicas.  If all replicas
-	# of a block have as many files as the block has, deactive the
-	# block (and all replicas).  We also require for extra safety
-	# that there can be no files in transfer, or export, and the
-	# block is not scheduled for deletion
-	#
-	# We do *not* require that dest_files = node_files, as at file
-	# source nodes dest_files is usually zero, and we still want
-	# to deactivate.  This should be safe -- we deactivate a block
-	# on intermediate node is if a) the entire block has already
-	# reached all current destination nodes, b) either all or none
-	# of the files have been removed in intermediate nodes, and
-	# c) several days has passed like this.  It is unlikely that
-	# all these criteria will be met simultaneously, and in any
-	# case the fix is easy: reactivate the block.
-	#
-	# Process just one block each time to avoid overloading undo
-	# segments unncessarily.  We can always deactive more later.
-	#
-	# What we do here requires a great degree of transactinal
-	# consistency.  For one, we don't want someone to open blocks
-	# while we are deactiving them; this is prevented by locking
-	# the t_dps_block row with "select .. for update" while operating
-	# on it, a convention followed by all programs which modify
-	# block table.  Secondly, we need to make sure we delete the
-	# exact number of file replicas we expected to delete; if an
-	# inconsitency is detected, we roll back the transcation.
-	my $qblocks = &dbexec ($dbh, qq{
-	    select b.id, b.name
-	    from t_dps_block b
-	    where b.is_open = 'n'
-	      and b.time_create < :limit
-      	      and exists (select 1 from t_dps_block_replica br
-      		          where br.block = b.id)
-	      and (b.files, b.bytes, 0, 1, 'y') = all
-      	          (select br.node_files, br.node_bytes, br.xfer_files,
-		  	  sign(:limit - br.time_update), br.is_active
-		   from t_dps_block_replica br
-		   where br.block = b.id)
-	      and not exists (select 1 from t_dps_block_delete bd
-                               where bd.block = b.id
-                                 and bd.time_complete is not null)
-	    order by b.files desc
-	    for update of b.id},
-	    ":limit" => &mytimeofday () - $$self{HOLDOFF});
-        if (my ($id, $name) = $qblocks->fetchrow())
-	{
-	    # Get the number of file replicas expected to delete.
-	    my ($nfr) = &dbexec ($dbh, qq{
-		select sum(node_files)
-		from t_dps_block_replica
-		where block = :block},
-		":block" => $id)
-		->fetchrow();
-	    if (! $nfr)
-	    {
-		&alert ("refusing to deactivate block $name with no files");
-		&dbexec ($dbh, qq{
-		    update t_dps_block set is_open = 'y'
-		    where id = :block},
-		    ":block" => $id);
-		$dbh->commit();
-		return 0;
-	    }
-
-	    # Deactivate.
-	    my ($dr, $nr) = &dbexec ($dbh, qq{
-		delete from t_xfer_replica where fileid in
-		(select id from t_xfer_file where inblock = :block)},
-		":block" => $id);
-	    &dbexec ($dbh, qq{
-		delete from t_xfer_file where inblock = :block},
-		     ":block" => $id);
-	    
-	    if ($nr != $nfr)
-	    {
-		&alert ("deactivating $name deleted $nr file replicas,"
-			. " expected to delete $nfr, undoing deactivation");
-		$dbh->rollback ();
-	    }
-	    else
-	    {
-	        my ($db, $nb) = &dbexec ($dbh, qq{
-		    update t_dps_block_replica
-		    set is_active = 'n'
-		    where block = :block},
-		    ":block" => $id);
-	        &logmsg ("deactivated $name: $nr file replicas, $nb block replicas");
-	        $dbh->commit ();
-	    }
+      $id   = $qb->{ID};
+      $name = $qb->{NAME};
+      # Get the number of file replicas expected to delete.
+      $b = $self->getBlockDeactivationCandidates
+			(
+			  BLOCK => $id,
+			  LIMIT => $limit,
+			  LOCK_FOR_UPDATE => 1,
+			);
+      goto DONE unless $b && ref($b) eq 'HASH';
+      goto DONE unless $b->{ID} == $id && $b->{NAME} eq $name;
+      $nfr = $self->nExpectedDeletions( BLOCK => $id );
+      if (! $nfr)
+      {
+        &alert ("refusing to deactivate block $name with no files");
+           $self->setBlockActive( BLOCK => $id );
+        $dbh->commit();
+      }
+      else
+      {
+        # Deactivate.
+        my $nr = $self->deactivateReplicas( BLOCK => $id );   
+        if ($nr != $nfr)
+        {
+          &alert ("deactivating $name deleted $nr file replicas,"
+	      . " expected to delete $nfr, undoing deactivation");
+          $dbh->rollback();
         }
+        else
+        {
+          my $nb = $self->setBlockInactive( BLOCK => $id );
+          &logmsg ("deactivated $name: $nr file replicas, $nb block replicas");
+          $dbh->commit ();
+        }
+      }
+DONE:
+      $self->maybeStop;
     };
     do { chomp ($@); &alert ("database error: $@");
-	 eval { $dbh->rollback() } if $dbh } if $@;
+      eval { $dbh->rollback() } if $dbh } if $@;
+  }
 
-    # Disconnect from the database
-    &disconnectFromDatabase ($self, $dbh);
+  # Disconnect from the database
+  &disconnectFromDatabase ($self, $dbh);
 
-    # Have a little nap
-    $self->nap ($$self{WAITTIME});
+  # Have a little nap
+print "Nap for ",$self->{WAITTIME}," seconds\n";
+  $self->nap ($self->{WAITTIME});
 }
 
 sub isInvalid
