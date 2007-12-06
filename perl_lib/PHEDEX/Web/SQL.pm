44426001bd2a3e273999d7ea33f5c075369f4c54
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2007-12-06 12:25:37 +0000

    First commit of consolidated webpage SQL

---
+++ perl_lib/PHEDEX/Web/SQL.pm
@@ -0,0 +1,111 @@
+package PHEDEX::Web::SQL;
+
+=head1 NAME
+
+PHEDEX::Web::SQL - encapsulated SQL for the web data service
+
+=head1 SYNOPSIS
+
+This package simply bundles SQL statements into function calls.
+It's not a true object package as such, and should be inherited from by
+anything that needs its methods.
+
+=head1 DESCRIPTION
+
+pending...
+
+=head1 METHODS
+
+=over
+
+=item getTransferStatus($self)
+
+returns a reference to an array of hashes with the following keys:
+TIME_UPDATE, DEST_NODE, SRC_NODE, STATE, PRIORITY, FILES, BYTES.
+Each hash represents the current amount of data queued for transfer
+(has tasks) for a link given the state and priority
+
+=over
+
+=item *
+
+C<$self> is an object with a DBH member which is a valid DBI database
+handle. To call the routine with a bare database handle, use the 
+procedural call method.
+
+=back
+
+=head1 SEE ALSO...
+
+L<PHEDEX::Core::SQL|PHEDEX::Core::SQL>,
+
+=cut
+
+use strict;
+use warnings;
+use base 'PHEDEX::Core::SQL';
+
+use PHEDEX::Core::DB;
+use Carp;
+
+our @EXPORT = qw( );
+our (%params);
+%params = (
+		DBH	=> undef,
+	  );
+
+sub new
+{
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+# my $self  = ref($proto) ? $class->SUPER::new(@_) : {};
+  my $self  = $class->SUPER::new(@_);
+
+  my %args = (@_);
+  map {
+        $self->{$_} = defined($args{$_}) ? $args{$_} : $params{$_}
+      } keys %params;
+  bless $self, $class;
+}
+
+sub AUTOLOAD
+{
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  if ( exists($params{$attr}) )
+  {
+    $self->{$attr} = shift if @_;
+    return $self->{$attr};
+  }
+  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+  my $parent = "SUPER::" . $attr;
+  $self->$parent(@_);
+}
+
+sub getTransferStatus
+{
+    my $self = shift;
+    my ($sql,$q,@r,%h);
+    
+    my %h = @_;
+    
+    my $sql = qq{
+    select
+      time_update,
+      nd.name dest_node, ns.name src_node,
+      state, priority,
+      files, bytes
+    from t_status_task xs
+      join t_adm_node ns on ns.id = xs.from_node
+      join t_adm_node nd on nd.id = xs.to_node
+     order by nd.name, ns.name, state
+ };
+
+    $q = execute_sql( $self, $sql, () );
+    while ( $_ = $q->fetchrow_hashref() ) { push @r, $_; }
+
+    return \@r;
+}
+
+1;
