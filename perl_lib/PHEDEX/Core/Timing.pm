8e10dd45827c70ecfc5461622447549add787b18
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-01-30 17:14:47 +0000

    Add formatTime() function

--- perl_lib/PHEDEX/Core/Timing.pm
+++ perl_lib/PHEDEX/Core/Timing.pm
@@ -9,14 +9,31 @@ PHEDEX::Core::Timing - a drop-in replacement for Toolkit/UtilsTiming
 use strict;
 use warnings;
 use base 'Exporter';
-our @EXPORT = qw(timeStart elapsedTime formatElapsedTime mytimeofday);
+our @EXPORT = qw(mytimeofday formatTime timeStart elapsedTime formatElapsedTime);
 use Time::HiRes 'gettimeofday';
+use POSIX qw(strftime);
+
 
 # High-resolution timing.
 sub mytimeofday
 {
     return scalar (&gettimeofday());
 }
+
+# Format TIME as unit of RANGE ("hour", "day", "week" or "month").
+sub formatTime
+{
+  my ($time, $range) = @_;
+  return undef unless ($time && $range);
+  return undef if ($time <= 0);
+  if ($range eq 'hour') { return strftime ('%Y%m%dZ%H00', gmtime(int($time))); }
+  elsif ($range eq 'day') { return strftime ('%Y%m%d', gmtime(int($time))); }
+  elsif ($range eq 'week') { return strftime ('%Y%V', gmtime(int($time))); }
+  elsif ($range eq 'month') { return strftime ('%Y%m', gmtime(int($time))); }
+  elsif ($range eq 'stamp') { return strftime ('%Y-%m-%d %H:%M:%S UTC', gmtime(int($time))); }
+  elsif ($range eq 'http') { return strftime("%a, %d %b %Y %H:%M:%S UTC", gmtime(int($time))); }
+}
+
 
 sub timeStart
 {
