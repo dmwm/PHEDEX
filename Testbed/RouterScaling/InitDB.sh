3dac8b7e3a9153fa989ac9ac6d30d4d8bec7e860
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-04-01 16:19:41 +0000

    Update configuration for 3.0 scaling test

--- Testbed/RouterScaling/InitDB.sh
+++ Testbed/RouterScaling/InitDB.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-source ../sw/slc*/cms/PHEDEX/PHEDEX_*/etc/profile.d/env.sh
-db=../SITECONF/CERN/PhEDEx/DBParam.lat:Validation
+db=$1
+echo "InitDB.sh  $db"
+
+echo "Resetting DB"
 sqlplus -S $(Utilities/OracleConnectId  -db $db) @Schema/OracleReset.sql </dev/null
+
+echo "Creating Schema"
 sqlplus -S $(Utilities/OracleConnectId  -db $db) @Schema/OracleInit.sql </dev/null
+
+echo "Enabling Stats"
 sqlplus -S $(Utilities/OracleConnectId  -db $db) @Schema/OracleStatsEnable.sql </dev/null
+
+echo "Init Schema Data"
 sqlplus -S $(Utilities/OracleConnectId  -db $db) @Testbed/RouterScaling/SetupScaleTest.sql </dev/null
+
+echo "Setup nodes"
 Testbed/RouterScaling/SetupNodes -db $db 8:T1 50:T2
+
+echo "Setup data"
 Testbed/RouterScaling/SetupData -db $db -datasets 10 -blocks 10 -files 1000
+
+echo "Stats update"
 sqlplus -S $(Utilities/OracleConnectId  -db $db) @Schema/OracleStatsUpdate.sql </dev/null
