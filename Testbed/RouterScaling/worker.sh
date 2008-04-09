7075a564c70e7bb04d5e0e49168b6b38f167e8d2
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-04-09 10:05:51 +0000

    workaround nasty environment bug

--- Testbed/RouterScaling/worker.sh
+++ Testbed/RouterScaling/worker.sh
@@ -10,36 +10,32 @@ export PHEDEX_BASE="$1"
 export PHEDEX_NODE="$2"
 export PHEDEX_ARCH=slc4_amd64_gcc345;
 export PHEDEX_LOCAL=$TMPDIR;
-mkdir -p ${PHEDEX_LOCAL}/logs;
-mkdir -p ${PHEDEX_LOCAL}/state;
 PHEDEX_LOGLABEL=`echo ${PHEDEX_NODE} |sed 's|%||g' |sed 's|,|+|g'`;
-
-echo "INFO:  PHEDEX_BASE=$PHEDEX_BASE"
-echo "INFO:  PHEDEX_NODE=$PHEDEX_NODE"
-echo "INFO:  PHEDEX_LOCAL=$PHEDEX_LOCAL"
 
 # Now start the agents.
 echo "INFO:  Starting agents..."
-${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${PHEDEX_BASE}/PHEDEX/Testbed/RouterScaling/Config.Site start;
+(
+  unset WORKDIR # workaround an environment bug in 3_0_0_pre14...
+  ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${PHEDEX_BASE}/PHEDEX/Testbed/RouterScaling/Config.Site start;
+)
 
 # Wait for the agents to exit.  They can be terminated at any time via
 # the database, or by us if the job exceeds our run time (20 hours).
 while true; do
   sleep 300
-
+  
   # Quit if the agent stopped or died
-  [ ! -f $PHEDEX_LOCAL/state/download/pid ] && break
-  echo "INFO:  Killing download agent:  no pid"
-  kill -0 $(cat $PHEDEX_LOCAL/state/download/pid) || break
+  [ ! -f ${PHEDEX_LOCAL}/state/download/pid ] && break
+  kill -0 $(cat ${PHEDEX_LOCAL}/state/download/pid) || break
 
   # If the agent ran too long, quit
   if [ $(expr $(date +%s) - $START) -gt 72000 ]; then
     echo "INFO:  Stopping agents after 20 hours of running"
-    if [ ! -f $PHEDEX_LOCAL/state/download/stop ]; then 
+    if [ ! -f ${PHEDEX_LOCAL}/state/download/stop ]; then 
       ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${PHEDEX_BASE}/PHEDEX/Testbed/RouterScaling/Config.Site stop;
-    elif [ ! -f $PHEDEX_LOCAL/state/download/terminating ]; then
+    elif [ ! -f ${PHEDEX_LOCAL}/state/download/terminating ]; then
       ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${PHEDEX_BASE}/PHEDEX/Testbed/RouterScaling/Config.Site terminate;
-      touch $PHEDEX_LOCAL/state/download/terminating
+      touch ${PHEDEX_LOCAL}/state/download/terminating
     else
       ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${PHEDEX_BASE}/PHEDEX/Testbed/RouterScaling/Config.Site kill;
     fi
@@ -47,13 +43,13 @@ while true; do
 
   # Show tail of the log on AFS
   echo "INFO:  Tailing log to ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.tail"
-  tail -1000 $PHEDEX_LOCAL/logs/download > ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.tail 2>/dev/null
+  tail -1000 ${PHEDEX_LOCAL}/logs/download > ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.tail 2>/dev/null
 done
+echo "INFO:  Agent is dead, job finished"
 
 # Copy compressed logs to AFS.
 echo "INFO:  Sending log to ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.gz"
 gzip -c --best < $PHEDEX_LOCAL/logs/download > ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.gz
-rm -fr $PHEDEX_LOCAL
 
 # Return happy and joyful!
 echo "INFO:  Exiting job"
