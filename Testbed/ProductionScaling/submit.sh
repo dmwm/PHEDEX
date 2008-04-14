9b9ea4d0d8e2dce075e8c1ac21702619b2776b78
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-04-14 15:14:44 +0000

    group multiple nodes in a single agent/job

--- Testbed/ProductionScaling/submit.sh
+++ Testbed/ProductionScaling/submit.sh
@@ -5,11 +5,14 @@ if [ "X$1" = 'X' ]; then
     exit 1
 fi
 
+nodes_per_job=5
 nodes=$1
 cmd='bsub -q cmsphedex'
 
-for (( i=1; $i<=$nodes; i+=1 )); do
-    nodelist="%$(printf '%03d' $i)%"
+for (( i=1; $i<=$nodes; i+=$nodes_per_job )); do
+    nodelist=""
+    for (( j=$i; $j<=$nodes && $j<$i+$nodes_per_job; j+=1 )); do
+        nodelist="${nodelist:+$nodelist,}%$(printf '%03d' $j)%"
+    done
     (set -x; $cmd $PHEDEX_BASE/PHEDEX/Testbed/ProductionScaling/worker.sh $PHEDEX_BASE "$nodelist")
 done
-
