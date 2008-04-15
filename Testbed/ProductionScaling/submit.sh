a08e02bb260901394f827998ce3ee2d05bdefe50
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-04-15 14:00:05 +0000

    submit single-node agents in a job

--- Testbed/ProductionScaling/submit.sh
+++ Testbed/ProductionScaling/submit.sh
@@ -5,14 +5,19 @@ if [ "X$1" = 'X' ]; then
     exit 1
 fi
 
+start=1
+if [ $2 ]; then
+  start=$2
+fi
+
 nodes_per_job=5
 nodes=$1
 cmd='bsub -q cmsphedex'
 
-for (( i=1; $i<=$nodes; i+=$nodes_per_job )); do
+for (( i=$start; $i<$nodes+$start; i+=$nodes_per_job )); do
     nodelist=""
-    for (( j=$i; $j<=$nodes && $j<$i+$nodes_per_job; j+=1 )); do
-        nodelist="${nodelist:+$nodelist,}%$(printf '%03d' $j)%"
+    for (( j=$i; $j<$nodes+$start && $j<$i+$nodes_per_job; j+=1 )); do
+        nodelist="${nodelist:+$nodelist,}$(printf '%03d' $j)"
     done
     (set -x; $cmd $PHEDEX_BASE/PHEDEX/Testbed/ProductionScaling/worker.sh $PHEDEX_BASE "$nodelist")
 done
--- Testbed/ProductionScaling/worker.sh
+++ Testbed/ProductionScaling/worker.sh
@@ -8,60 +8,79 @@ RUNTIME=$((24*3600*7)) # 1 week
 # the node for which this agent runs and is used by the download agent
 # configuration.  The $PHEDEX_LOCAL is used by the configuration too.
 export PHEDEX_BASE="$1"
-export PHEDEX_NODE="$2"
+NODELIST=`echo $2 | sed 's/,/ /g'`
+AGENTLIST="download remove exp-pfn blockverify"
+
 export PHEDEX_ARCH=slc4_amd64_gcc345;
 export PHEDEX_LOCAL=$TMPDIR;
 CONFIG=${PHEDEX_BASE}/PHEDEX/Testbed/ProductionScaling
-PHEDEX_LOGLABEL=`echo ${PHEDEX_NODE} |sed 's|%||g' |sed 's|,|+|g'`;
 
 mkdir -p ${PHEDEX_BASE}/failconf;
-export PHEDEX_FAIL_CONF=${PHEDEX_BASE}/failconf/${PHEDEX_LOGLABEL}-fail.conf
-[ ! -f $PHEDEX_FAIL_CONF ] && ${CONFIG}/LinkFailureRates.conf $PHEDEX_FAIL_CONF
-
-# Now start the agents.
-echo "INFO:  Starting agents..."
-(
-  unset WORKDIR # workaround an environment bug in 3_0_0_pre14...
-  ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site start;
-)
+for node in $NODELIST; do
+    # Make failure configuration file
+    export PHEDEX_FAIL_CONF=${PHEDEX_BASE}/failconf/$node-fail.conf
+    [ ! -f $PHEDEX_FAIL_CONF ] && cp ${CONFIG}/LinkFailureRates.conf $PHEDEX_FAIL_CONF
+    
+    # Now start the agents.
+    echo "INFO:  Starting agents for $node"
+    (
+	unset WORKDIR # workaround an environment bug in 3_0_0_pre14...
+	export PHEDEX_NODE=$node
+	${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site start;
+    )
+done
 
 # Wait for the agents to exit.  They can be terminated at any time via
 # the database, or by us if the job exceeds our run time.
 while true; do
-  sleep 300
-  
-  # Quit if the agent stopped or died
-  [ ! -f ${PHEDEX_LOCAL}/state/download/pid ] && break
-  kill -0 $(cat ${PHEDEX_LOCAL}/state/download/pid) || break
+    sleep 300
 
-  # If the agent ran too long, quit
-  if [ $(expr $(date +%s) - $START) -gt $RUNTIME ]; then
-    echo "INFO:  Stopping agents after $RUNTIME seconds of running"
-    if [ ! -f ${PHEDEX_LOCAL}/state/download/stop ]; then 
-      ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site stop;
-    elif [ ! -f ${PHEDEX_LOCAL}/state/download/terminating ]; then
-      ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site terminate;
-      touch ${PHEDEX_LOCAL}/state/download/terminating
-    else
-      ${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site kill;
+    # Check if agents are still alive.
+    AGENTSALIVE=0
+    for node in $NODELIST; do  
+	[ ! -f ${PHEDEX_LOCAL}/$node/state/download/pid ] && break
+	kill -0 $(cat ${PHEDEX_LOCAL}/$node/state/download/pid) || break
+	AGENTSALIVE=1
+    done
+    if [ ! $AGENTSALIVE ]; then
+	break
     fi
-  fi
+    
+    # If the agents ran too long, quit
+    if [ $(expr $(date +%s) - $START) -gt $RUNTIME ]; then
+	echo "INFO:  Stopping agents after $RUNTIME seconds of running"
+	for node in $NODELIST; do
+	    if [ ! -f ${PHEDEX_LOCAL}/$node/state/download/stop ]; then 
+		${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site stop;
+	    elif [ ! -f ${PHEDEX_LOCAL}/$node/state/download/terminating ]; then
+		${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site terminate;
+		touch ${PHEDEX_LOCAL}/$node/state/download/terminating
+	    else
+		${PHEDEX_BASE}/PHEDEX/Utilities/Master -config ${CONFIG}/Config.Site kill;
+	    fi
+	done
+    fi
 
   # Show tail of the log on AFS
-  echo "INFO:  Tailing logs to ${PHEDEX_BASE}/logs"
-  tail -1000 ${PHEDEX_LOCAL}/logs/download    > ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.tail 2>/dev/null
-  tail -1000 ${PHEDEX_LOCAL}/logs/exp-pfn     > ${PHEDEX_BASE}/logs/exp-pfn-$PHEDEX_LOGLABEL.tail 2>/dev/null
-  tail -1000 ${PHEDEX_LOCAL}/logs/remove      > ${PHEDEX_BASE}/logs/remove-$PHEDEX_LOGLABEL.tail 2>/dev/null
-  tail -1000 ${PHEDEX_LOCAL}/logs/blockverify > ${PHEDEX_BASE}/logs/blockverify-$PHEDEX_LOGLABEL.tail 2>/dev/null
+    echo "INFO:  Tailing logs to ${PHEDEX_BASE}/logs"
+    for node in $NODELIST; do
+	for agent in $AGENTLIST; do
+	    tail -1000 ${PHEDEX_LOCAL}/$node/logs/$agent \
+		> ${PHEDEX_BASE}/logs/$agent-$node.tail 2>/dev/null
+	done
+    done
 done
+
 echo "INFO:  Agents are dead, job finished"
 
 # Copy compressed logs to AFS.
 echo "INFO:  Sending logs to ${PHEDEX_BASE}/logs"
-gzip -c --best < $PHEDEX_LOCAL/logs/exp-pfn     > ${PHEDEX_BASE}/logs/exp-pfn-$PHEDEX_LOGLABEL.gz
-gzip -c --best < $PHEDEX_LOCAL/logs/download    > ${PHEDEX_BASE}/logs/download-$PHEDEX_LOGLABEL.gz
-gzip -c --best < $PHEDEX_LOCAL/logs/remove      > ${PHEDEX_BASE}/logs/remove-$PHEDEX_LOGLABEL.gz
-gzip -c --best < $PHEDEX_LOCAL/logs/blockverify > ${PHEDEX_BASE}/logs/blockverify-$PHEDEX_LOGLABEL.gz
+mkdir -p ${PHEDEX_BASE}/logs
+for node in $NODELIST; do
+    for agent in $AGENTLIST; do
+	gzip -c --best < $PHEDEX_LOCAL/$node/logs/$agent > ${PHEDEX_BASE}/logs/$agent-$node.gz
+    done
+done
 
 # Return happy and joyful!
 echo "INFO:  Exiting job"
