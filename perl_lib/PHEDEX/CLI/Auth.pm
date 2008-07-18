9dfe1c86e0776a12878f49e033c091c26f4176ef
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2008-07-18 10:35:27 +0000

    initial CLI plugins for \'phedex\' command.

---
+++ perl_lib/PHEDEX/CLI/Auth.pm
@@ -0,0 +1,112 @@
+package PHEDEX::CLI::Auth;
+use Getopt::Long;
+use Data::Dumper;
+use strict;
+
+sub new
+{
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+
+  my ($help,%params,%options);
+  %params = (
+	      VERBOSE	=> 0,
+	      DEBUG	=> 0,
+	      NODES	=> undef,
+	    );
+  %options = (
+               'help'		=> \$help,
+	       'verbose!'	=> \$params{VERBOSE},
+	       'debug!'		=> \$params{DEBUG},
+	       'node=s@'	=> \$params{NODES},
+	     );
+  GetOptions(%options);
+  my $self = \%params;
+  print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+
+  bless $self, $class;
+  $self->Help() if $help;
+  return $self;
+}
+
+sub AUTOLOAD
+{
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  if ( exists($self->{$attr}) )
+  {
+    $self->{$attr} = shift if @_;
+    return $self->{$attr};
+  }
+}
+
+sub Help
+{
+  print "\n Usage for ",__PACKAGE__,"\n";
+  die <<EOF;
+
+ Help description goes here...
+
+ ...and of course, this module takes the standard options:
+ --help, --(no)debug, --(no)verbose
+
+EOF
+}
+
+sub Payload
+{
+  my $self = shift;
+  my $payload = { };
+  print __PACKAGE__," created payload\n" if $self->{VERBOSE};
+  return $self->{PAYLOAD} = $payload;
+}
+
+sub Call { return 'checkAuth'; }
+
+sub ParseResponse
+{
+# assume the response is in Perl Data::Dumper format!
+  my ($self,$response) = @_;
+
+  no strict;
+  my $content = eval($response->content());
+  $content = $content->{phedex}{auth} || {};
+  foreach ( qw / STATE DN NODES / )
+  { $self->{RESPONSE}{$_} = $content->{$_}; }
+  @{$self->{RESPONSE}{ROLES}} = ();
+  foreach my $role ( keys %{$content->{ROLES}} )
+  {
+    foreach ( @{$content->{ROLES}{$role}} ) 
+    { push @{$self->{RESPONSE}{ROLES}},$role if m%^phedex$%; }
+  }
+}
+
+sub ResponseIsValid
+{
+  my $self = shift;
+  my $response = $self->{RESPONSE};
+
+# Check that the user certificate was accepted, that they have valid roles
+# for PhEDEx, and that they have a set of nodes they can act on
+  die "Certificate not accepted\n"    unless $response->{STATE} eq 'cert';
+  die "You have no valid roles\n"     unless scalar @{$response->{ROLES}};
+  die "You have no nodes to act on\n" unless scalar keys %{$response->{NODES}};
+
+# Check the list of nodes for validity
+  if ( $self->{NODES} )
+  {
+    foreach ( @{$self->{NODES}} )
+    {
+      print $self->Dump() if $self->{DEBUG};
+      die "Required node \"$_\" not found in authorised list\n" unless
+	$response->{NODES}{$_};
+    }
+  }
+  print __PACKAGE__," response is valid\n" if $self->{VERBOSE};
+  return 1;
+}
+
+sub Dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
+
+1;
---
+++ perl_lib/PHEDEX/CLI/Template.pm
@@ -0,0 +1,119 @@
+package PHEDEX::CLI::Template;
+#
+# This is a template for the CLI function modules. The module name must be
+# initial-caps followed by lowercase, i.e. no embedded caps. It must support
+# the same functions as listed here.
+#
+use Getopt::Long;
+use Data::Dumper;
+use strict;
+
+sub new
+{
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+
+  my ($help,%params,%options);
+  %params = (
+	       VERBOSE	=> 0,
+	       DEBUG	=> 0,
+	    );
+  %options = (
+	       'help'		=> \$help,
+	       'verbose!'	=> \$params{VERBOSE},
+	       'debug!'		=> \$params{DEBUG},
+	     );
+  GetOptions(%options);
+  my $self = \%params;
+  print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+
+  bless $self, $class;
+  $self->Help() if $help;
+  return $self;
+}
+
+sub AUTOLOAD
+{
+  my $self = shift;
+  my $attr = our $AUTOLOAD;
+  $attr =~ s/.*:://;
+  if ( exists($self->{$attr}) )
+  {
+    $self->{$attr} = shift if @_;
+    return $self->{$attr};
+  }
+}
+
+sub Help
+{
+  print "\n Usage for ",__PACKAGE__,"\n";
+  die <<EOF;
+
+ Help description goes here...
+
+ ...and of course, this module takes the standard options:
+ --help, --(no)debug, --(no)verbose
+
+EOF
+}
+
+sub Payload
+{
+# This function returns a hashref, the contents of which can be POSTed to
+# the server. This forms the body of the request to the data-service
+  my $self = shift;
+  my $payload = {
+                  this_is => 'something to send to the server',
+                  it_should => 'be a hashref!'
+                };
+  print __PACKAGE__," created payload\n" if $self->{VERBOSE};
+  return $self->{PAYLOAD} = $payload;
+}
+
+sub Call
+{
+# This function returns a string, which is the name of the data-service
+# function to call. Ideally, this should be the same as the name of this
+# package, but may not always be. As here, using the 'bounce' call will
+# simply return the parameters the user sent, which is fine for debugging
+  return 'bounce';
+}
+
+sub ParseResponse
+{
+  my ($self,$response) = @_;
+  no strict;
+  my $content = eval($response->content());
+  $content = $content->{phedex}{args} || {};
+  foreach ( keys %{$self->{PAYLOAD}} )
+  { $self->{RESPONSE}{$_} = $content->{$_}; }
+}
+
+sub ResponseIsValid
+{
+# This function checks that the response from the server is OK. Returns true
+# if so, false otherwise. This example is not a rigorous validation, and
+# assumes the response is in Perl Data::Dumper format!
+  my $self = shift;
+  my $payload  = $self->{PAYLOAD};
+  my $response = $self->{RESPONSE};
+  foreach ( keys %{$payload} )
+  {
+    if ( $payload->{$_} ne $response->{$_} )
+    {
+      print __PACKAGE__," response is invalid\n" if $self->{VERBOSE};
+      print $self->Dump() if $self->{DEBUG};
+      return 0;
+    }
+  }
+  print __PACKAGE__," response is valid\n" if $self->{VERBOSE};
+  return 1;
+}
+
+sub Dump
+{
+# For debugging purposes only
+  return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]);
+}
+
+1;
