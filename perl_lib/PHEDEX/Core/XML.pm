6fd0dcf83382f71502031fb4464637c46e21eee1
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2008-08-03 16:36:52 +0000

    add parseDataNew, to parse new format XML files

--- perl_lib/PHEDEX/Core/XML.pm
+++ perl_lib/PHEDEX/Core/XML.pm
@@ -5,6 +5,95 @@ use strict;
 
 use XML::Parser;
 
+sub parseDataNew
+{
+    my %h = @_;
+
+    my $verbose = $h{VERBOSE};
+
+    my $info;
+    if ($h{FILE}) {
+	# Ensure file is readable
+	-r $h{FILE} || die "$h{FILE}: cannot read: $!\n";
+
+	print "Reading file information from $h{FILE}\n" if $verbose;
+	$info = (new XML::Parser (Style => "Tree"))->parsefile ($h{FILE});
+    } elsif ($h{XML}) {
+	$info = (new XML::Parser (Style => "Tree"))->parse ($h{XML});
+    } else {
+	die "parseData requires either FILE or XML to parse\n";
+    }
+
+    my $result = {};
+    while (my ($dataattrs, @datacontent) = next_element($info, 'data'))
+    {
+	
+	print "Processing data\n" if $verbose;
+        while (my ($dbsattrs, @dbscontent) = next_element(\@datacontent, 'dbs'))
+        {
+	    die "parseData: <dbs name=''> attribute missing or empty\n"
+	    if ! defined $$dbsattrs{'name'} || $$dbsattrs{'name'} eq '';
+	    my $dbsname = $$dbsattrs{'name'};
+	    $result->{DBS}->{$dbsname} = { NAME => $dbsname,
+				           DLS  => $$dbsattrs{'dls'} };
+
+	    while (my ($dsattrs, @dscontent) = next_element(\@dbscontent, 'dataset'))
+	    {
+	        die "parseData: <dataset name=''> attribute missing or empty\n"
+		    if ! defined $$dsattrs{'name'} || $$dsattrs{'name'} eq '';
+	        die "parseData: <dataset is-open=''> attribute missing or empty\n"
+		    if ! defined $$dsattrs{'is-open'} || $$dsattrs{'is-open'} eq '';
+#	        die "parseData: <dataset is-transient=''> attribute missing or empty\n"
+#		    if ! defined $$dsattrs{'is-transient'} || $$dsattrs{'is-transient'} eq '';
+
+	        my $dsname = $dsattrs->{'name'};
+	        $result->{DBS}->{$dbsname}
+	        ->{DATASETS}->{$dsname} = { NAME => $$dsattrs{'name'},
+					    IS_OPEN => $$dsattrs{'is-open'},
+					    IS_TRANSIENT => 'n',
+#					    IS_TRANSIENT => $$dsattrs{'is-transient'}
+					  };
+	    
+	        print " Processing dataset $dsname\n" if $verbose;
+	        while (my ($battrs, @bcontent) = next_element(\@dscontent, 'block'))
+	        {
+		    die "parseData: <block name=''> attribute missing or empty\n"
+		        if ! defined $$battrs{'name'} || $$battrs{'name'} eq '';
+		    die "parseData: <block is-open=''> attribute missing or empty\n"
+		        if ! defined $$battrs{'is-open'} || $$battrs{'is-open'} eq '';
+
+		    my $bname = $battrs->{'name'};
+		    $result->{DBS}->{$dbsname}
+		    ->{DATASETS}->{$dsname}
+		    ->{BLOCKS}->{$bname} ={ NAME => $$battrs{'name'},
+					    IS_OPEN => $$battrs{'is-open'} };
+
+		    while (my ($fattrs, @fcontent) = next_element(\@bcontent, 'file'))
+		    {
+		        die "parseData: <file> may not have content\n"
+			    if @fcontent;
+		        die "parseData: <file name=''> attribute missing or empty\n"
+			    if ! defined $$fattrs{'name'} || $$fattrs{'name'} eq '';
+		        die "parseData: <file bytes=''> attribute missing or bad value\n"
+			    if ! defined $$fattrs{'bytes'} || $$fattrs{'bytes'} !~ /^\d+$/;
+		        die "parseData: <file checksum=''> attribute missing or bad value\n"
+			    if ! defined $$fattrs{'checksum'} || $$fattrs{'checksum'} !~ /^cksum:\d+$/;
+
+		        my $fname = $fattrs->{'name'};
+		        $result->{DBS}->{$dbsname}
+		        ->{DATASETS}->{$dsname}
+		        ->{BLOCKS}->{$bname}
+		        ->{FILES}->{$fname} = { NAME => $fattrs->{'name'},
+					        BYTES => $fattrs->{'bytes'},
+					        CHECKSUM => $fattrs->{'checksum'} };
+		    } # /files
+	        } # /blocks
+	    } # /datasets
+        } # /dbses
+    } # /datas
+    return $result;
+}
+
 sub parseData
 {
     my %h = @_;
