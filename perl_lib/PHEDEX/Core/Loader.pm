37296900560505e91ca2537b7c725ad11ee53f49
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2008-08-27 09:42:35 +0000

    change KnownCommands to Commands, call it in the constructor. Add code to discover sub-namespaces and a method to return them

--- perl_lib/PHEDEX/Core/Loader.pm
+++ perl_lib/PHEDEX/Core/Loader.pm
@@ -24,13 +24,16 @@ sub new
     { push @{$self->{REJECT}}, $_; }
   }
   bless $self, $class;
+  $self->Commands();
   return $self;
 }
 
 sub ModuleName
 {
   my ($self,$Action) = @_;
-  return $self->{NAMESPACE} . '::' . $self->KnownCommands()->{lc $Action};
+  my $module = $self->Commands()->{lc $Action};
+  return undef unless $module;
+  return $self->{NAMESPACE} . '::' . $module;
 }
 
 sub Load
@@ -42,68 +45,48 @@ sub Load
   return $module;
 }
 
-#sub separateArgs()
-#{
-#  my @argv;
-#  my $global = 0;
-#
-## static variables!
-#  our @ARGV_orig;
-#  our $notFirst;
-#
-#  if ( !$notFirst )
-#  {
-##   first call: set global arguments array and 'global' flag
-#    @ARGV_orig = @ARGV;
-#    $notFirst = $global = 1;
-#  }
-#
-## The easy way out if there are no arguments
-#  return undef unless @ARGV_orig;
-#
-## If we're looking for global arguments, first argument better start with '-'
-#  return undef if ( $global && $ARGV_orig[0] !~ m%^-% );
-#
-## Remove leading '--' arguments, unless this is the first call, in
-## which case they signify no global arguments.
-#  $_ = shift @ARGV_orig;
-#  while ( m%^--$% )
-#  {
-#    return undef if $global;
-#    $_ = shift @ARGV_orig;
-#  }
-#  unshift @ARGV_orig, $_; # put back last argument for next step...
-#
-## Loop over remaining arguments. Terminate on '--' or when no arguments left
-#  while ( $_ = shift @ARGV_orig )
-#  {
-#    last if m%^--$%;
-#    push @argv, $_;
-#  }
-#
-#  return \@argv;
-#}
-
-sub KnownCommands
+sub Commands
 {
   my ($self,%h) = @_;
+  return $self->{COMMANDS} if $self->{COMMANDS};
+
   my ($command,%commands,$namespace);
   ($namespace = $self->{NAMESPACE}) =~ s%::%/%g;
 
+  $self->{SUBSPACES} = [];
   foreach ( @INC )
   {
-    foreach ( <$_/$namespace/*pm> )
+    foreach ( <$_/$namespace/*> )
     {
-      m%^.*/$namespace/(.*).pm$%;
-      $command = $1;
-      $commands{lc $command} = $command;
+      if ( m%\.pm$% ) # is a Perl module
+      {
+        m%^.*/$namespace/(.*).pm$%;
+        $command = $1;
+        $commands{lc $command} = $command;
+        next;
+      }
+
+      if ( -d ) # is a subdirectory
+      {
+        next if ( m%/CVS$% ); # Explicitly ignore CVS...
+        m%^.*/$namespace/(.*)%;
+        ($_ = $1) =~ s%/%::%;
+        push @{$self->{SUBSPACES}}, $self->{NAMESPACE} . '::' . $_;
+        next;
+      }
     }
   }
+
 
   foreach ( @{$self->{REJECT}} )
   { $_ = lc $_; delete $commands{$_} if exists $commands{$_}; }
 
-  return \%commands;
+  return $self->{COMMANDS} = \%commands;
 }
 
+sub Subspaces
+{
+  my $self = shift;
+  return $self->{SUBSPACES};
+}
 1;
