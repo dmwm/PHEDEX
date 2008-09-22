5f888772fdfde87876c9fd975b184d46644dcbde
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2008-09-22 15:21:59 +0000

    format output object for XML

--- perl_lib/PHEDEX/Web/API/Auth.pm
+++ perl_lib/PHEDEX/Web/API/Auth.pm
@@ -10,12 +10,45 @@ PHEDEX::Web::API::Auth - check or enforce authentication
 
 =head1 SYNOPSIS
 
-Return a hash of the users' authentication state. The hash contains keys for
-the STATE (cert|passwd|failed), the DN, the ROLES (from sitedb) and the
-NODES (from TMDB) that the user is allowed to operate on.
+Return the users' authentication state.
 
-if 'require_cert' is passed in the input arguments with a non-zero value,
-the call will die unless the user is authenticated with a certificate.
+=head2 output
+
+Returns the following structure
+
+  <auth>
+     <role/>
+     <role/>
+     ...
+     <node/>
+     <node/>
+     ...
+  </auth>
+   ...
+
+=head3 options
+
+ ability : (optional) if passed then the nodes (from TMDB) that the user is
+           allowed to operate on is are also returned.
+
+ require_cert : (optional) if passed then the call will die if the
+                user is not authenticated by certificate
+
+=head3 <auth> attributes
+
+ state : the authentication state (cert|passwd|failed)
+ dn    : the user's distinguished name
+ ability : the ability that authorized nodes were requested for (see options)
+
+=head3 <role> attributes
+
+ name  : the name of the role
+ group : the group (or site) associated with the role
+
+=head3 <node> attributes
+
+ name : the name of the node
+ id   : the id of the node
 
 =cut
 
@@ -24,8 +57,29 @@ sub auth
 {
   my ($core,%args) = @_;
 
-  $core->{SECMOD}->reqAuthnCert() if $args{require_cert};
-  return $core->getAuth();
+  $core->{SECMOD}->reqAuthnCert() if $args{require_cert};  
+  my $auth = $core->getAuth($args{ability});
+
+  # make XML-able data structure from our data
+  my $obj = { 'state' => $auth->{STATE},
+	      'dn' => $auth->{DN},
+	      'ability' => $args{ability}
+	  };
+
+  foreach my $role (keys %{$auth->{ROLES}}) {
+      foreach my $group (@{$auth->{ROLES}->{$role}}) {
+	  $obj->{role} ||= [];
+	  push @{$obj->{role}}, { name => $role, group => $group }
+      }
+  }
+
+  foreach my $node (keys %{$auth->{NODES}}) {
+      $obj->{node} ||= [];
+      push @{$obj->{node}}, { name => $node,
+			      id => $auth->{NODES}->{$node} };
+  }
+
+  return { auth => $obj };
 }
 
 1;
