268ef53251790ae6cf75bfe9f271d4cb8b6d6caa
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2008-12-05 18:18:59 +0000

    remove global space-token, and the command-line option for it. Require space-tokens to come via the TFC only.

--- perl_lib/PHEDEX/Transfer/FTS.pm
+++ perl_lib/PHEDEX/Transfer/FTS.pm
@@ -50,7 +50,6 @@ sub new
     $options->{'service=s'}            = \$params->{FTS_SERVICE};
     $options->{'myproxy=s'}            = \$params->{FTS_MYPROXY};
     $options->{'passfile=s'}           = \$params->{FTS_PASSFILE};
-    $options->{'spacetoken=s'}         = \$params->{FTS_SPACETOKEN};
     $options->{'priority=s'}           = \$params->{FTS_PRIORITY};
     $options->{'mapfile=s'}            = \$params->{FTS_MAPFILE};
     $options->{'q_interval=i'}         = \$params->{FTS_Q_INTERVAL};
@@ -95,7 +94,6 @@ sub init
 	 );
 
     $glite->MYPROXY($self->{FTS_MYPROXY}) if $self->{FTS_MYPROXY};
-    $glite->SPACETOKEN($self->{FTS_SPACETOKEN}) if $self->{FTS_SPACETOKEN};
 
     if ($self->{FTS_PASSFILE}) {
 	my $passfile = $self->{FTS_PASSFILE};
