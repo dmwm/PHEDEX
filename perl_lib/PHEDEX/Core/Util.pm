01bd3acf4a2803eb492b1506ddee5a0ff0fb6637
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2009-01-27 22:04:24 +0000

    add str2time()

--- perl_lib/PHEDEX/Core/Util.pm
+++ perl_lib/PHEDEX/Core/Util.pm
@@ -27,4 +27,64 @@ sub arrayref_expand
     return @out;
 }
 
+# str2time -- convert string to timestamp
+# possible input values:
+#    UNIX time
+#    YYYY-MM-DD[_hh:mm:ss]
+#    now
+#    last_hour
+#    last_12hours
+#    last_day
+#    last_7days
+#    last_30days
+#    last_180days
+sub str2time
+{
+    my $core = shift @_;
+    my $str = shift @_;
+
+    if ($str =~ m!(^\d*$)!)	# UNIX time
+    {
+        return $str =~ m!(^\d*$)!;
+    }
+    elsif ($str eq "now")
+    {
+        return time();
+    }
+    elsif ($str eq "last_hour")
+    {
+        return time() - 3600;
+    }
+    elsif ($str eq "last_12hours")
+    {
+        return time() - 43200;
+    }
+    elsif ($str eq "last_day")
+    {
+        return time() - 86400;
+    }
+    elsif ($str eq "last_7days")
+    {
+        return time() - 604800;
+    }
+    elsif ($str eq "last_30days")
+    {
+        return time() - 2592000;
+    }
+    elsif ($str eq "last_180days")
+    {
+        return time() - 15552000;
+    }
+
+    # YYYY-MM-DD[_hh:mm:ss]
+    my @t = $str =~ m!(\d{4})-(\d{2})-(\d{2})([\s_](\d{2}):(\d{2}):(\d{2}))?!;
+    if (not $t[3]) # no time information, assume 00:00:00
+    {
+        $t[4] = 0;
+        $t[5] = 0;
+        $t[6] = 0;
+    }
+    return POSIX::mktime($t[6], $t[5], $t[4], $t[2], $t[1]-1, $t[0]-1900);
+}
+
 1;
