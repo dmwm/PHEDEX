39a481c9c60fbefd47265bb06a34728a37792909
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-02-02 17:48:16 +0000

    change constant names from CONST_ to PHEDEX_; fix constant definition

--- perl_lib/PHEDEX/Error/Constants.pm
+++ perl_lib/PHEDEX/Error/Constants.pm
@@ -4,20 +4,20 @@ use warnings;
 use strict;
 
 use base 'Exporter';
-our @EXPORT = qw( CONST_RC_EXPIRED   CONST_XC_EXPIRED   CONST_RC_LOST_TASK CONST_XC_NOXFER 
-                  CONST_RC_LOST_FILE CONST_XC_LOST_FILE CONST_RC_VETO );
+our @EXPORT = qw( PHEDEX_RC_EXPIRED   PHEDEX_XC_EXPIRED   PHEDEX_RC_LOST_TASK PHEDEX_XC_NOXFER 
+                  PHEDEX_RC_LOST_FILE PHEDEX_XC_LOST_FILE PHEDEX_RC_VETO );
 
 # Error code definitions for PhEDEx errors
 # RC : report code
 # XC : transfer code
 use constant {
-    CONST_RC_EXPIRED   => -1;  # the task expired
-    CONST_XC_EXPIRED   => -1;  # the transfer expired
-    CONST_RC_LOST_TASK => -2;  # the task was lost
-    CONST_XC_NOXFER    => -2;  # no transfer was attempted
-    CONST_RC_LOST_FILE => -3;  # a critical state file was lost
-    CONST_XC_LOST_FILE => -3;  # a critical state file was lost
-    CONST_RC_VETO      => -86; # the transfer task was vetoed
+    PHEDEX_RC_EXPIRED   => -1,  # the task expired
+    PHEDEX_XC_EXPIRED   => -1,  # the transfer expired
+    PHEDEX_RC_LOST_TASK => -2,  # the task was lost
+    PHEDEX_XC_NOXFER    => -2,  # no transfer was attempted
+    PHEDEX_RC_LOST_FILE => -3,  # a critical state file was lost
+    PHEDEX_XC_LOST_FILE => -3,  # a critical state file was lost
+    PHEDEX_RC_VETO      => -86  # the transfer task was vetoed
 };
 
 1;
