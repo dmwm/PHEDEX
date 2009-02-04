2e8f7538e154b6b20b11ed53f4c861439358db3f
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2009-02-04 22:42:21 +0000

    add documentation

--- perl_lib/PHEDEX/Web/API/Agents.pm
+++ perl_lib/PHEDEX/Web/API/Agents.pm
@@ -1,6 +1,56 @@
 package PHEDEX::Web::API::Agents;
-# use warning;
+#use warning;
 use strict;
+
+=pod
+
+=head1 NAME
+
+PHEDEX::Web::API::Agents - return information regarding the Agents
+
+=head2 agents
+
+Return agent information in the following structure:
+
+  <node>
+    <agent/>
+    ...
+  </node>
+  ...
+
+=head3 options
+
+required inputs:  none
+optional inputs: (as filters) node, se, agent
+
+ node             node name
+ se               storage element name
+ agent            agent name
+
+=head3 output
+
+  <node>
+    <agent/>
+    ...
+  </node>
+  ...
+
+=head3 <node> elements:
+
+ name             agent name
+ node             node name
+ host             host name
+ label            label
+ state_dir        directory path ot the states
+ version          cvs release
+ cvs_version      cvs revision
+ cvs_tag          cvs tag
+ pid              process id
+ time_update      time it was updated
+
+=cut
+
+
 use PHEDEX::Web::SQL;
 use Data::Dumper;
 
