81ce16fbba57e4537105610a9be5e5485f20ebb1
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-02-10 13:17:47 +0000

    replace existing dpm.pm with one tailored to the namespace framework, add totally untested code in dpm/*.pm to accompany it. I say totally untested, it's a cut'n'paste from the castor directory, so not _totally_ untested.

--- perl_lib/PHEDEX/Namespace/dpm.pm
+++ perl_lib/PHEDEX/Namespace/dpm.pm
@@ -1,67 +1,59 @@
 package PHEDEX::Namespace::dpm;
-
-=head1 NAME
-
-PHEDEX::Namespace::dpm - implement namespace functions for DPM 
-
-=head1 SYNOPSIS
-
-The following commands are implemeted:
-
-=over
-
-=item size of a file
-
-=item delete a file
-
-=back
-
-Not implemeted due to protocol limitation:
-
-=over
-
-=item bring online
-
-=item check if a file is cached on disk
-
-=item verify a file: check size, optionally checksum - N/A
-
-=item check if a file is migrated to tape
-
-=back
-
-=cut
-
-#use strict;
+use strict;
 use warnings;
-use Data::Dumper;
-
-#Parent Class
-use base PHEDEX::Namespace::direct; 
-
-
-#NS functions implemented,
-# how many we can run in parallel etc
-#put own command like this. For unknow command
-#the default number is used
-
-my %commands = (
-                stat=>{cmd=>"dpls",opts=>["-l"],tfcproto=>"direct",n=>10},
-                delete=>{cmd=>"dprm",opts=>["-f"],tfcproto=>"direct",n=>9},
-                default=>{tfcproto=>'direct',n=>8},
-		);
+no strict 'refs';
+use base 'PHEDEX::Namespace::Common'; # All interface packages must do this
+use PHEDEX::Core::Loader;
+use Getopt::Long;
 
 sub new
 {
-    my $class  = shift;
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my %h = @_;
+  my ($help,%params,%options);
 
-    my $self = { };
-    bless($self, $class);
-    $self->_init(@_, COMMANDS=>\%commands); #values from the base class
-    
-    print Dumper($self);
-    return $self;
+# Params and options are interface-specific. If you need to set an environment
+# variable or something, that parameter should be declared in %params, accepted
+# as an input argument in %options, and used where necessary in the package.
+  %params = (
+		VERBOSE => 0,
+		DEBUG   => 0,
+            );
+  %options = (
+		'help'		=> \$help,
+		'verbose!'	=> \$params{VERBOSE},
+		'debug+'	=> \$params{DEBUG},
+             );
+  GetOptions(%options);
+  my $self = \%params;
+  bless($self, $class);
+  $self->SUPER::_init( NAMESPACE => __PACKAGE__ );
+  map { $self->{$_} = $h{$_} } keys %h;
+
+# This is where the interface-specific modules are loaded. The modules are
+# passed a reference to this object when they are loaded/created, so they
+# can pick out the parameters you define above.
+  $self->SUPER::_init_commands;
+  $self->Help if $help;
+  return $self;
 }
 
+sub Help
+{
+# This function should describe any module-specific parameters, but the rest of
+# it should remain unaltered.
+  my $self = shift;
+  print "\n Usage for ",__PACKAGE__,"\n";
+  print <<EOF;
+
+ This module takes the standard options:
+ --help, --(no)debug, --(no)verbose
+
+ Commands known to this module:
+EOF
+
+  $self->SUPER::_help();
+}
 
 1;
---
+++ perl_lib/PHEDEX/Namespace/dpm/delete.pm
@@ -0,0 +1,38 @@
+package PHEDEX::Namespace::dpm::delete;
+# Implements the 'delete' function for posix access
+use strict;
+use warnings;
+
+sub new
+{
+  my ($proto,$h) = @_;
+  my $class = ref($proto) || $proto;
+# $self is an empty hashref because there is no external command to call
+  my $self = {};
+  bless($self, $class);
+  $self->{ENV} = $h->{ENV} || '';
+  return $self;
+}
+
+sub execute
+{
+# Deletes an array of files. Returns the difference between the number of
+# files to be deleted and the number actually deleted. I.e. returns 0 for
+# success, regardless of the number of files it is given
+die "Until someone verifies that this code works, I die here. Wanna fix the code...?\n";
+  my ($self,$ns,@files) = @_;
+  return 0 unless @files;
+  return scalar @files - unlink @files;
+}
+
+sub Help
+{
+  print <<EOH;
+delete (unlink) a set of files. Returns the number of files _not_
+deleted. This allows you to call it with an empty list and still make sense
+of the return value
+EOH
+  print "N.B. This code is not tested, and will die() if you try to use it.\n";
+}
+
+1;
