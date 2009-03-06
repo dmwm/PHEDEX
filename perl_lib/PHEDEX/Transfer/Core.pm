eebdec10590bd3b2657146dd2d349f78d11cf73a
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-03-06 10:24:29 +0000

    Move remaining job management functions to the backend. - add _poe_init to initialize backend-specific POE events, called from FileDownload - turn startBatch() into a POE event 'start_batch' - add 'check_transfer_job' event, to periodicially check if a transfer job is ready to start or finished - add dummy placeholder for 'start_transfer_job' - add 'finish_transfer_job' event, which archives a completed job - move 'manage_archives' here from PHEDEX::File::Download::Agent

--- perl_lib/PHEDEX/Transfer/Core.pm
+++ perl_lib/PHEDEX/Transfer/Core.pm
@@ -5,6 +5,8 @@ use base 'PHEDEX::Core::Logging';
 use PHEDEX::Core::JobManager;
 use PHEDEX::Core::Command;
 use PHEDEX::Core::Catalogue;
+use PHEDEX::Core::Timing;
+use POE;
 use Getopt::Long;
 use File::Path qw(mkpath);
 use Data::Dumper;
@@ -45,6 +47,8 @@ sub new
 						VERBOSE	=> $self->{VERBOSE},
 						DEBUG	=> $self->{DEBUG},
 							);
+    $self->{JOBS} = {};  # Transfer jobs
+
     # Remember various useful details.
     $self->{MASTER} = $master;  # My owner
     $self->{VERBOSE} = $master->{VERBOSE} || 0;
@@ -52,6 +56,7 @@ sub new
     $self->{BOOTTIME} = time(); # "Boot" time for this agent
     $self->{BATCHID} = 0;       # Running batch counter
     $self->{WORKDIR} = $master->{WORKDIR}; # Where job state/logs are saved
+    $self->{ARCHIVEDIR} = $master->{ARCHIVEDIR}; # Where job state/logs are archived when finished
 
     # Locate the transfer wrapper script.
     $self->{WRAPPER} = $INC{"PHEDEX/Transfer/Core.pm"};
@@ -62,10 +67,21 @@ sub new
     return $self;
 }
 
-# Check whether a job is alive.  By default do nothing.
-sub check
+# Initialize POE events.  We share the session with the frontend.
+sub _poe_init
 {
-    # my ($self, $jobname, $job, $tasks) = @_;
+    my ($self, $kernel, $session) = @_;
+
+    my @poe_subs = qw( start_batch check_transfer_job
+		       start_transfer_job finish_transfer_job manage_archives);
+    $kernel->state($_, $self) foreach @poe_subs;
+
+    if ( $self->can('setup_callbacks') ) { 
+	$self->setup_callbacks($kernel,$session);
+    }
+
+    # Get periodic events going
+    $kernel->yield('manage_archives');
 }
 
 # Stop the backend.  We abandon all jobs always.
@@ -80,7 +96,8 @@ sub stop
 sub isBusy
 {
     my ($self, $to_node, $from_node) = @_;
-    return $self->{JOBMANAGER}->jobsRemaining() >= $self->{NJOBS};
+    return 0 if $self->{NJOBS} == 0;
+    return scalar keys %{$self->{JOBS}} >= $self->{NJOBS};
 }
 
 # Return the list of protocols supported by this backend.
@@ -90,24 +107,109 @@ sub protocols
     return @{$self->{PROTOCOLS}};
 }
 
-# Start off a copy job.  Nips off "BATCH_FILES" tasks to go ahead.
-sub startBatch
+# Define a transfer batch by removing BATCH_FILES tasks from
+# $tasklist.  Then prepares the job work area and saves state.
+# Finally, calls 'check_jobs' to begin looking for when it is OK to
+# begin the job.
+sub start_batch
 {
-    my ($self, $list) = @_;
+    my ($self, $kernel, $tasklist) = @_[ OBJECT, KERNEL, ARG0 ];
 
-    my @batch = splice(@$list, 0, $self->{BATCH_FILES});
+    my @batch = splice(@$tasklist, 0, $self->{BATCH_FILES});
     return undef if !@batch;
 
     my $id = $$self{BATCH_ID}++;
     my $jobid = "job.$$self{BOOTTIME}.$id";
     my $jobdir = "$$self{WORKDIR}/$jobid";
     &mkpath($jobdir);
-    my $jobinfo = { ID => $jobid, DIR => $jobdir,
-		    TASKS => { map { $_->{TASKID} => $_ } @batch } };
-    &output("$jobdir/info", Dumper($jobinfo));
-    $self->{JOBS}->{$jobid} = $jobinfo;
-
-    return ($jobid, $jobdir, $jobinfo->{TASKS});
+    my $job = { ID => $jobid, DIR => $jobdir,
+		TASKS => { map { $_->{TASKID} => $_ } @batch } };
+    $self->saveJob($job);
+    $self->{JOBS}->{$jobid} = $job;
+    $kernel->yield('check_transfer_job', $jobid);
+    return ($jobid, $jobdir, $job->{TASKS});
 }
+
+# Check to see if a job is ready to start.  Tasks in a job can
+# disappear or be finished as a result of frontend actions, this looks
+# to see if all of the existing unfinished tasks in the job have been
+# marked ready for transfer by the frontend.  If they have, the job
+# can be started.
+sub check_transfer_job
+{
+    my ($self, $kernel, $jobid) = @_[ OBJECT, KERNEL, ARG0 ];
+
+    my $job = $self->{JOBS}->{$jobid};
+
+    my ($n_pend, $n_ready, $n_done, $n_lost) = (0,0,0,0);
+    foreach my $taskid (keys %{$job->{TASKS}}) {
+	my $task = $job->{TASKS}->{$taskid};
+	if    (!$task)            { $n_lost++; delete $job->{TASKS}->{$taskid}   }
+	elsif ($task->{FINISHED}) { $n_done++; delete $job->{TASKS}->{$taskid};  }
+	elsif ($task->{READY})    { $n_ready++ }
+	else                      { $n_pend++  }
+    }
+
+    $self->Logmsg("copy job $jobid status:  pend=$n_pend ready=$n_ready done=$n_done lost=$n_lost ") 
+	if $self->{DEBUG};
+
+    if ($n_pend == 0 && $n_ready == 0) {
+	$kernel->yield('finish_transfer_job', $jobid);
+	return; # do not check this job anymore
+    } elsif ($n_pend == 0 && !$job->{STARTED}) {
+	$job->{STARTED} = &mytimeofday();
+	$self->saveJob($job);
+	$kernel->yield('start_transfer_job', $jobid);
+    }
+
+    $kernel->delay_set('check_transfer_job', 15, $jobid);
+}
+
+# Start a transfer job.  This needs to be implemented by a sub-class
+sub start_transfer_job
+{
+    my ($self, $kernel, $jobid) = @_[ OBJECT, KERNEL, ARG0 ];
+    $self->Fatal("start_transfer_job not implemented by transfer backend ", __PACKAGE__);
+}
+
+# Called to finish a transfer job.  Marks the time the job was
+# finished, then cleans the job from memory and moves the state
+# information to the archive area
+sub finish_transfer_job
+{
+   my ($self, $kernel, $jobid) = @_[ OBJECT, KERNEL, ARG0 ];
+
+   my $job = $self->{JOBS}->{$jobid};
+   
+   $job->{STARTED}  ||= -1;
+   $job->{FINISHED} = &mytimeofday();
+   $self->saveJob($job);
+   delete $self->{JOBS}->{$jobid};
+   &mv($job->{DIR}, "$$self{ARCHIVEDIR}/$jobid");
+
+   $self->Logmsg("copy job $jobid completed") if $$self{VERBOSE};
+}
+
+# Save a job to disk
+sub saveJob
+{
+    my ($self, $job) = @_;
+    &output("$job->{DIR}/info", Dumper($job));    
+}
+
+# Remove archived jobs which are over a day old, or if there are more
+# than 500 of them
+sub manage_archives
+{
+   my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
+   $kernel->delay('manage_archives', 3600);
+
+   my $archivedir = $$self{ARCHIVEDIR};
+   my @old = <$archivedir/*>;
+   my $now = &mytimeofday();
+   &rmtree($_) for (scalar @old > 500 ? @old 
+		    : grep((stat($_))[9] < $now - 86400, @old));
+}
+
 
 1;
