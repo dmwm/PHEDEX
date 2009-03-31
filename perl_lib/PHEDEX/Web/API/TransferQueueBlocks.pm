749665d514ef44b03a0e9b00b04a0f1867146086
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2009-03-31 15:37:17 +0000

    get Transfer Queue Blocks information

---
+++ perl_lib/PHEDEX/Web/API/TransferQueueBlocks.pm
@@ -0,0 +1,77 @@
+package PHEDEX::Web::API::TransferQueueBlocks;
+#use warning;
+use strict;
+
+=pod
+
+=head1 NAME
+
+PHEDEX::Web::API::TransQueueBlocks - return information regarding the Agents
+
+=head2 transferqueueblocks
+
+Return agent information in the following structure:
+
+  <node>
+    <agent/>
+    ...
+  </node>
+  ...
+
+=head3 options
+
+ required inputs: none
+ optional inputs: (as filters) node, se, agent
+
+  node             node name, could be multiple
+  se               storage element name, could be multiple
+  agent            agent name, could be multiple
+
+=head3 output
+
+  <node>
+    <agent/>
+    ...
+  </node>
+  ...
+
+=head3 <node> elements:
+
+  name             agent name
+  node             node name
+  host             host name
+  agent            list of the agents on this node
+
+=head3 <agent> elements:
+
+  label            label
+  state_dir        directory path ot the states
+  version          cvs release
+  cvs_version      cvs revision
+  cvs_tag          cvs tag
+  pid              process id
+  time_update      time it was updated
+
+=cut
+
+
+use PHEDEX::Web::SQL;
+
+sub duration { return 60 * 60; }
+sub invoke { return transferqueueblocks(@_); }
+
+sub transferqueueblocks
+{
+    my ($core, %h) = @_;
+
+    # convert parameter keys to upper case
+    foreach ( qw / from to state priority / )
+    {
+      $h{uc $_} = delete $h{$_} if $h{$_};
+    }
+
+    my $r = PHEDEX::Web::SQL::getTransferQueueBlocks($core, %h);
+    return { link => $r };
+}
+
+1;
