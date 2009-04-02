68ee7ead4274a28e4863ddf8476b2d014ccafb95
gfball <sha1-e81d948102e4d2fd9d2758d1beddf5671194bd97@cern.ch>
2009-04-02 15:13:25 +0000

    Modified phedex_proxy.py so requests for local files don't need to be prefixed 'readfile', for compatibility with cmswttest.

--- PhEDExWeb/ApplicationServer/js/phedex_proxy.py
+++ PhEDExWeb/ApplicationServer/js/phedex_proxy.py
@@ -7,6 +7,9 @@ class PhEDExProxy:
     def readfile(self,name):
     	return open(name,'r').read()
     readfile.exposed=True
+    def default(self,filename):
+      return open(filename,'r').read()
+    default.exposed=True
     def phedex(self,*args,**kwargs):
     	#cherrypy.response.headers['Content-Type'] = 'application/json'
 #	return urllib2.urlopen('http://lxbuild061.cern.ch:7001/phedex/%s'%('/'.join(args)+'?'+urllib.urlencode(kwargs))).read()
