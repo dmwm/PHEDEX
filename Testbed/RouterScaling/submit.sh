daa29e76a6f58bc8ed100cc7ed1bfb9f03419c41
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-04-30 15:46:25 +0000

    remove echo

--- Testbed/RouterScaling/submit.sh
+++ Testbed/RouterScaling/submit.sh
@@ -13,10 +13,10 @@ cmd='bsub -q cmsphedex'
 i=1
 for (( i; $i<=$t1s; i+=1 )); do
   buffer=$(printf 'T1_%03d_Buffer' $i)
-  (set -x; echo $cmd $PHEDEX_BASE/PHEDEX/Testbed/RouterScaling/worker.sh $PHEDEX_BASE $buffer)
+  (set -x; $cmd $PHEDEX_BASE/PHEDEX/Testbed/RouterScaling/worker.sh $PHEDEX_BASE $buffer)
 done
 
 for (( i; $i<=$t2s+$t1s; i+=1 )); do
   buffer=$(printf 'T2_%03d_Buffer' $i)
-  (set -x; echo $cmd $PHEDEX_BASE/PHEDEX/Testbed/RouterScaling/worker.sh $PHEDEX_BASE $buffer)
+  (set -x; $cmd $PHEDEX_BASE/PHEDEX/Testbed/RouterScaling/worker.sh $PHEDEX_BASE $buffer)
 done
