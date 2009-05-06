62e5e5070252beea3548403516e0d109446d669d
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-05-06 13:06:58 +0000

    add an array of known instances. Not that it gets used anywhere yet...

--- PhEDExWeb/ApplicationServer/js/phedex-datasvc.js
+++ PhEDExWeb/ApplicationServer/js/phedex-datasvc.js
@@ -1,5 +1,6 @@
 PHEDEX.Datasvc=function() {}
 PHEDEX.Datasvc.Instance = 'prod';
+PHEDEX.Datasvc.Instances = ['prod','dev','debug'];
 PHEDEX.Datasvc.GET = function(api,callback,argument) {
   YAHOO.util.Connect.asyncRequest('GET','/phedex/datasvc/json/'+PHEDEX.Datasvc.Instance+'/'+api,{success:callback,argument:argument});
 }
