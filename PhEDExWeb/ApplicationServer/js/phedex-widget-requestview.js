b77f4ec7cc6db1e73a6ba0d115c660fcd9ef6b01
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-05-13 14:04:38 +0000

    first YUI-based version, with a treeview inside a resizeable panel

--- PhEDExWeb/ApplicationServer/js/phedex-widget-requestview.js
+++ PhEDExWeb/ApplicationServer/js/phedex-widget-requestview.js
@@ -1,30 +1,78 @@
-req_node=null;
+// instantiate the PHEDEX.Widget.RequestView namespace
+PHEDEX.namespace('Widget.RequestView');
 
-requestview=function() {
-  var req = document.getElementById('select_for_requestview').value;
-  req_node = new PHEDEX.Widget.RequestView(req);
+requestview=function(divid) {
+  var req = document.getElementById(divid+'_select').value;
+  req_node = new PHEDEX.Widget.RequestView(divid,req);
   req_node.update();
 }
 
-PHEDEX.Widget.RequestView = function(req_num) {
-  var that = new PHEDEX.Widget('phedex_requestview',null);
+PHEDEX.Widget.RequestView = function(divid,req_num) {
+  var that = new PHEDEX.Core.Widget(divid+'_display_'+req_num,null,
+		{
+		children:false,
+		width:500,
+		height:200,
+		minwidth:400,
+		minheight:80
+		});
   that.req_num=req_num;
   that.data={};
-  that.buildHeader=function(span) {
-    this.title=document.createElement('span');
-    this.requestor=document.createElement('span');
-    this.requestor.className='col1';
-    this.volume=document.createElement('span');
-    this.volume.className='col2';
-    this.status=document.createElement('span');
-    this.status.className='col3';
-    this.xfertype=document.createElement('span');
-    this.xfertype.className='col4';
-    span.appendChild(this.title);
-    span.appendChild(this.requestor);
-    span.appendChild(this.volume);
-    span.appendChild(this.status);
-    span.appendChild(this.xfertype);
+  that.buildHeader=function(div) {
+    this.list = document.createElement('ul');
+    this.list.className='inline_list';
+    this.title = document.createElement('li');
+    this.list.appendChild(this.title);
+    this.requestor = document.createElement('li');
+    this.list.appendChild(this.requestor);
+    this.volume = document.createElement('li');
+    this.list.appendChild(this.volume);
+    this.status = document.createElement('li');
+    this.list.appendChild(this.status);
+    this.xfertype = document.createElement('li');
+    this.list.appendChild(this.xfertype);
+    div.appendChild(this.list);
+  }
+  that.fillBody = function(div) {
+    var reqNode, tmpNode, tmpLeaf;
+    var tree = new YAHOO.widget.TreeView(div);
+    var root = tree.getRoot();
+    
+    reqNode = new YAHOO.widget.TextNode({label: "Request status", expanded: false}, root);
+    tmpLeaf = new YAHOO.widget.TextNode(this.data.comments, reqNode, false); 
+    tmpLeaf.isLeaf = true;
+
+    tmpNode = new YAHOO.widget.TextNode({label: "Requestor details", expanded: false}, reqNode);
+    var requestDetail = "Created: "+this.format.date(this.data.time_create)+"<br/>By: "+this.data.requested_by.username+" ("+this.data.requested_by.name+")<br/>Host: "+this.data.requested_by.host+" using "+this.data.requested_by.agent+"<br/>DN: "+this.data.requested_by.dn;
+    new YAHOO.widget.TextNode(requestDetail, tmpNode, false); 
+/*
+    tmpNode = new YAHOO.widget.TextNode({label: this.approval(), expanded: false}, reqNode);
+    var approverDetail="";
+    for (var i in this.data['sources']) {
+      approverDetail += "SOURCE: "+this.data['sources'][i]['name']+"<br/>Approved: "+this.data['sources'][i]['decision']+"<br/>Approved by: "+this.data['sources'][i]['approved_by']['username']+" ("+this.data['sources'][i]['approved_by']['name']+")<br/>Approved at: "+this.format['date'](this.data['sources'][i]['time_decided'])+"<br/>";
+    }
+    new YAHOO.widget.TextNode(approverDetail, tmpNode, false); 
+*/
+    tmpNode = new YAHOO.widget.TextNode({label: "Approval status: "+this.approval(), expanded: false}, reqNode);
+    var destinationDetail="";
+    for (var i in this.data.destinations) {
+      destinationDetail += "DESTINATION: "+this.data.destinations[i].name+"<br/>Approved: "+this.data.destinations[i].decision+"<br/>Approved by: "+this.data.destinations[i].approved_by.username+" ("+this.data.destinations[i].approved_by.name+")<br/>Approved at: "+this.format.date(this.data.destinations[i].time_decided)+"<br/>";
+    }
+    new YAHOO.widget.TextNode(destinationDetail, tmpNode, false); 
+
+    tmpNode = new YAHOO.widget.TextNode({label: "Block details", expanded: false}, root);
+    tmpLeaf.isLeaf = true;
+    for (var i in this.data.data.dbs.dataset) {
+      var d = this.data.data.dbs.dataset[i];
+//       var b = new YAHOO.widget.TextNode({label: this.format.dataset(d.name), expanded: false}, tmpNode); // shorten dataset name for label?
+      var b = new YAHOO.widget.TextNode({label: d.name, expanded: false}, tmpNode);
+      var t = " BlockID: "+d.id+", "+d.files+" files / "+this.format['bytes'](d.bytes);
+//       if ( d.name != this.format.dataset(d.name) ) { t += "<br/>Block: "+d.name; } // not needed if full name used in label
+      tmpLeaf = new YAHOO.widget.TextNode(t, b, false);
+      tmpLeaf.isLeaf = true;
+    }
+
+    tree.render(); 
   }
   that.approval=function() {
     var dest_approve=0;
@@ -55,48 +103,40 @@ PHEDEX.Widget.RequestView = function(req_num) {
       result += ', move';
     return result;
   }
-  that.fillHeader=function() {
-    this.title.innerHTML='Request '+this.data['id'];
-    this.requestor.innerHTML=this.data['requested_by']['username'];
-    this.volume.innerHTML=this.data['files']+' files / '+this.format['bytes'](this.data['bytes']);
-    this.status.innerHTML=this.approval();
-    this.xfertype.innerHTML=this.classify();
+  that.fillHeader=function(div) {
+    that.title.innerHTML='Request '+this.data['id'];
+    that.requestor.innerHTML=this.data['requested_by']['username'];
+    that.volume.innerHTML=this.data['files']+' files / '+this.format['bytes'](this.data['bytes']);
+    that.status.innerHTML=this.approval();
+    that.xfertype.innerHTML=this.classify();
   }
   that.buildExtra=function(div) {
-    this.comment=document.createElement('div');
-    this.comment.className='comment';
+    that.comment=document.createElement('div');
+    that.comment.className='comment';
     div.appendChild(this.comment);
-    this.creator=document.createElement('div');
-    this.creator.className='border';
+    that.creator=document.createElement('div');
+    that.creator.className='border';
     div.appendChild(this.creator);
-    this.approver=document.createElement('div');
-    this.approver.className='border';
+    that.approver=document.createElement('div');
+    that.approver.className='border';
     div.appendChild(this.approver);
   }
-  that.fillExtra=function() {
-    this.comment.innerHTML = this.data['comments'];
-    this.creator.innerHTML = "<div>Created: "+this.format['date'](this.data['time_create'])+"</div><div>By: <a href='mailto:"+this.data['requested_by']['email']+"'>"+this.data['requested_by']['username']+"</a> ("+this.data['requested_by']['name']+")</div><div>Host: "+this.data['requested_by']['host']+" using "+this.data['requested_by']['agent']+"</div><div>DN: "+this.data['requested_by']['dn']+"</div>";
-    this.approver.innerHTML="";
-    for (var i in this.data['sources']) {
-      this.approver.innerHTML += "<div><div>SOURCE: "+this.data['sources'][i]['name']+"</div><div>Approved: "+this.data['sources'][i]['decision']+"</div><div>Approved by: <a href='mailto:"+this.data['sources'][i]['approved_by']['email']+"'>"+this.data['sources'][i]['approved_by']['username']+"</a> ("+this.data['sources'][i]['approved_by']['name']+")</div><div>Approved at: "+this.format['date'](this.data['sources'][i]['time_decided'])+"</div></div>";
-    }
-    for (var i in this.data['destinations']) {
-      this.approver.innerHTML += "<div><div>DESTINATION: "+this.data['destinations'][i]['name']+"</div><div>Approved: "+this.data['destinations'][i]['decision']+"</div><div>Approved by: <a href='mailto:"+this.data['destinations'][i]['approved_by']['email']+"'>"+this.data['destinations'][i]['approved_by']['username']+"</a> ("+this.data['destinations'][i]['approved_by']['name']+")</div><div>Approved at: "+this.format['date'](this.data['destinations'][i]['time_decided'])+"</div></div>";
-    }
-    
-  }
   that.update=function() {
-    Data.Call('TransferRequests',{req_num:this.req_num},this.receive,{obj:this});
-    this.startLoading();
+    PHEDEX.Datasvc.TransferRequests(this.req_num,this);
+//     this.startLoading();
   }
   that.receive=function(result) {
-    var data=eval('('+result.responseText+')')['phedex']['request'];
-    if (data.length==1) {
-      result.argument.obj.data=data[0];
+    var data=PHEDEX.Datasvc.TransferRequests[that.req_num];
+    if (data) {
+      that.data = data;
+      that.build();
+//     that.finishLoading();
+      that.populate();
     }
-    result.argument.obj.finishLoading();
-    result.argument.obj.populate();
   }
+
+// this doesn't seem to do anything particularly useful, in that it creates a broken URL
+/*
   that.buildChildren=function(div) {
     this.startLoading();
     this.markChildren();
@@ -123,11 +163,13 @@ PHEDEX.Widget.RequestView = function(req_num) {
     this.removeMarkedChildren();
     this.finishLoading();
   }
-  
-  that.build();
+*/  
+//   that.build();
   return that;
 }
 
+// Not sure what this was used for...
+/*
 PHEDEX.Widget.ReqBlockNode = function(div,parent,data) {
   var that = new PHEDEX.Widget(div,parent,{children:false});
   that.data=data;
@@ -155,23 +197,4 @@ PHEDEX.Widget.ReqBlockNode = function(div,parent,data) {
   that.build();
   return that;
 }
-
-Data=function(){}
-//PHEDEX.Datasvc.Nodes = function(site,callback,argument) {
-//  var opts = 'nodes';
-//  if ( site ) { opts += '?node='+site; }
-//  PHEDEX.Datasvc.GET(opts,callback,argument);
-//}
-Data.Call = function(query,args,callback,argument) {
-  var argstr = "";
-  if (args) {
-    argstr = "?";
-    for (a in args) {
-      argstr+=a+"="+args[a]+";";
-    }
-  }
-// var url = '/phedex/datasvc/json/prod/'+query+argstr;
-// var c = YAHOO.util.Connect.asyncRequest('GET',url,{success:callback,argument:argument});
-  query = query + argstr;
-  PHEDEX.Datasvc.GET(query,callback,argument);
-}
+*/
