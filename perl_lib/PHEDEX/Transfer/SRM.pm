ecacdd01d12a28190e06c31dce765f529d084435
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-05-27 13:20:19 +0000

    Update the SRM backend to take -syntax option, which switches the syntax of the prepared copyjob file and the generated command-line arguments

--- perl_lib/PHEDEX/Transfer/SRM.pm
+++ perl_lib/PHEDEX/Transfer/SRM.pm
@@ -8,21 +8,33 @@ use POE;
 use Getopt::Long;
 
 # Command back end defaulting to srmcp and supporting batch transfers.
+# SRM spec:  
+#  http://sdm.lbl.gov/srm-wg/doc/SRM.v2.2.html
+# srmcp command syntax(es):
+# dcache:
+#  https://twiki.grid.iu.edu/bin/view/Documentation/StorageSrmcpUsing
+# bestman:
+#  http://datagrid.lbl.gov/bestman/srmclient/srm-copy.html
+
 sub new
 {
-    my $proto = shift;
-    my $class = ref($proto) || $proto;
+    my $proto  = shift;
+    my $class  = ref($proto) || $proto;
     my $master = shift;
     
     # Get derived class arguments and defaults
     my $options = shift || {};
-    my $params = shift || {};
+    my $params  = shift || {};
 
     # Set my defaults where not defined by the derived class.
     $params->{PROTOCOLS}   ||= [ 'srmv2', 'srm' ];  # Accepted protocols
     $params->{COMMAND}     ||= [ 'srmcp' ];  # Transfer command
     $params->{BATCH_FILES} ||= 10;           # Max number of files per batch
     $params->{NJOBS}       ||= 30;           # Max number of parallel commands
+    $params->{SYNTAX}      ||= "dcache";     # SRM command flavor
+
+    # Set argument parsing at this level.
+    $options->{'syntax=s'} = \$params->{SYNTAX};
 	
     # Initialise myself
     my $self = $class->SUPER::new($master, $options, $params, @_);
@@ -42,21 +54,26 @@ sub start_transfer_job
     my ( $self, $kernel, $session, $jobid ) = @_[ OBJECT, KERNEL, SESSION, ARG0 ];
 
     my $job = $self->{JOBS}->{$jobid};
+    my @tasks = values %{$job->{TASKS}};
+    my $syntax = $self->{SYNTAX};
 
     # Prepare copyjob and report names.
     my $spec = "$job->{DIR}/copyjob";
     my $report = "$job->{DIR}/srm-report";
 
     # Now generate copyjob
-    &output ($spec, join ("", map { "$_->{FROM_PFN} ".
-		                    "$_->{TO_PFN}\n" }
-		          values %{$job->{TASKS}}));
+    $self->writeSpec($spec, @tasks);
 
+    # Prepare the command
+    my @command = (@{$self->{COMMAND}}, $self->makeArgs($spec, $report));
+
+    # Queue the command
     $self->{JOBMANAGER}->addJob( $session->postback('srm_job_done'),
 				 { TIMEOUT => $self->{TIMEOUT},
 				   LOGFILE => "$job->{DIR}/log",
 				   START => &mytimeofday(), JOBID => $jobid },
-				 @{$self->{COMMAND}}, "-copyjobfile=$spec", "-report=$report");
+                                 @command );
+
 
     $job->{STARTED} = &mytimeofday();
 }
@@ -111,6 +128,47 @@ sub srm_job_done
 	    $self->report_detail($jobinfo, $xferinfo);
 	}
 	$kernel->yield('transfer_done', $taskid, $xferinfo);
+    }
+}
+
+# The following functions prepare files and command line arguments for
+# an SRM command depending on the syntax.
+
+sub writeSpec
+{
+    my ($self, $spec, @tasks) = @_;
+    my $syntax = $self->{SYNTAX};
+
+    my $rv = 0;
+    if ($syntax eq 'dcache') {
+	$rv = &output ($spec, join ("\n", map { "$_->{FROM_PFN} $_->{TO_PFN}" } @tasks));
+    } elsif ($syntax eq 'bestman') {
+	$rv = &output ($spec, 
+		 join ("\n", 
+		       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+		       "<request>",
+		       (map { "  <file>".
+			      "    <sourceurl>$_->{FROM_PFN}</sourceurl>".
+			      "    <targeturl>$_->{TO_PFN}</targeturl>".
+			      "  </file>" } @tasks),
+		       "</request>\n"));
+    } else {
+	$self->Fatal("writeSpec: unknown syntax '$syntax'");
+    }
+    return $rv;
+}
+
+sub makeArgs
+{
+    my ($self, $spec, $report) = @_;
+    my $syntax = $self->{SYNTAX};
+    
+    if ($syntax eq 'dcache') {
+	return ("-copyjobfile=$spec", "-report=$report");
+    } elsif ($syntax eq 'bestman')  {
+	return ("-f $spec", "-report $report", "-xmlreport $report.xml");
+    } else {
+	$self->Fatal("makeArgs:  unknown syntax '$syntax'");
     }
 }
 
