488017386908341cbf5e93127a31ae7891df70c4
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-05-28 14:30:52 +0000

    Fix memory leaks and remove FTS-backend specific code in favor of more generalized methods: - Remove circular references and any interaction with   POE::Component::Child internals.  We now keep the job payloads   stored in a package global $PAYLOADS, and all references to the   caller of addJob are removed. - Remove dependency on PHEDEX::Core::Logging - Remove $payload->{result}* output which was for the FTS backend. - Add a per-job option KEEP_OUTPUT for storing the results of command   output to memory - Carefully clean up any per-job data which is kept track of,   including the log file handle - Document exactly which keys to the job hash may be set to alter the   behavior of the job manager, and which will be set when making the   callback. - Add methods for accessing the number of active jobs and the number   queued jobs

--- perl_lib/PHEDEX/Core/JobManager.pm
+++ perl_lib/PHEDEX/Core/JobManager.pm
@@ -1,5 +1,7 @@
 package PHEDEX::Core::JobManager;
 
+=pod
+
 =head1 NAME
 
 PHEDEX::Core::JobManager - a POE-based job-manager for external commands
@@ -8,16 +10,19 @@ PHEDEX::Core::JobManager - a POE-based job-manager for external commands
 
 use strict;
 use warnings;
-use base 'PHEDEX::Core::Logging';
 use POSIX;
 use POE;
 use POE::Queue::Array;
 use POE::Component::Child;
 use PHEDEX::Core::Command;
 use PHEDEX::Core::Timing;
-use Data::Dumper;
 
-our $pkg = $POE::Component::Child::PKG;
+# Contains job hashes which are returned back to the caller who
+# submitted the job.  They are identified by a POE::Wheel ID, which is
+# supposed to be unique, so there should be no collision with multiple
+# job managers.
+our %PAYLOADS = ();
+
 sub new
 {
     my $proto = shift;
@@ -32,10 +37,9 @@ sub new
 		   );
     $$self{$_} = exists $args{$_} ? $args{$_} : $params{$_} for keys %params;
 
-    $self->{JOBS} = 0; # number of jobs active jobs
+    $self->{JOB_QUEUE} = POE::Queue::Array->new();
+    $self->{JOBS_RUNNING} = {};
 
-#   A queue to hold the jobs we will run
-    $self->{QUEUE} = POE::Queue::Array->new();
     bless $self, $class;
 
 #   Start a POE session for myself
@@ -70,10 +74,6 @@ sub new
 	   },
            debug  => $self->{POCO_DEBUG},
           );
-    $self->{_child}{caller} = $self;
-
-#   Hold the output of the children...
-    $self->{payloads} = {};
 
     return $self;
 }
@@ -117,173 +117,246 @@ sub heartbeat
 sub job_queued
 {
   my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
-  if ( scalar(keys %{$self->{payloads}}) >= $self->{NJOBS} )
+
+  # Check if we can start the job now, or whether we need to wait
+  if ( scalar(keys %{$self->{JOBS_RUNNING}}) >= $self->{NJOBS} )
   {
     $kernel->delay_set('job_queued',0.3);
     return;
   }
-  my ($priority,$id,$job) = $self->{QUEUE}->dequeue_next();
+
+  # Get the job from the queue
+  my ($priority,$id,$job) = $self->{JOB_QUEUE}->dequeue_next();
+
+  # Validate the job
+  # FIXME:  Should we really ignore this?
   if ( !$job )
   {
     $kernel->delay_set('job_queued',0.1);
     return;
   }
 
+  # Start the job and record the PID
   my $wheelid = $self->{_child}->run(@{$job->{CMD}});
-  $job->{PID} = $self->{_child}{$pkg}{wheels}{$wheelid}{ref}->PID;
-  $self->{_child}{$pkg}{owner}{$wheelid} = $self;
-  $self->{payloads}{$wheelid} = $job;
-  $self->{payloads}{$wheelid}{start} = &mytimeofday();
+  $job->{PID} = $self->{_child}->wheel($wheelid)->PID;
+  
+  # Other variables we add to the job
+  $job->{_start} = &mytimeofday();
+  $job->{_cmdname} = $job->{CMD}[0];
+  $job->{_cmdname} =~ s|.*/||;
 
-  if ( $job->{TIMEOUT} )
+  # Store the job in the package global and our running catalogue
+  $PAYLOADS{$wheelid} = $job;
+  $self->{JOBS_RUNNING}{$wheelid} = $job->{PID};
+
+  # Handle the timeout.  We temporarily store some "private" keys to
+  # the job hash.
+  if ( exists $job->{TIMEOUT} && defined $job->{TIMEOUT} )
   {
     my $timer_id = $kernel->delay_set('timeout',$job->{TIMEOUT},$wheelid);
-    $self->{payloads}{$wheelid}{timer_id} = $timer_id;
-    $self->{payloads}{$wheelid}{signals} = [ qw / 1 15 9 / ];
+    $job->{_timer_id} = $timer_id;
+    $job->{_timeout_grace} = exists $job->{TIMEOUT_GRACE} ? $job->{TIMEOUT_GRACE} : 3;
+    $job->{_signals} = [ qw / 1 15 9 / ];
   }
-  $job->{CMDNAME} = $job->{CMD}[0];
-  $job->{CMDNAME} =~ s|.*/||;
 
+  # A closure to clean up after the job is finished
+  $job->{_cleanup} = sub { $self->_cleanup(@_) };
+
+  # Whether or not we keep the output in memory
+  if ( exists $job->{KEEP_OUTPUT} && $job->{KEEP_OUTPUT} ) {
+      $job->{_keep_output} = 1;
+      $job->{STDOUT} = "";
+      $job->{STDERR} = "";
+  }
+
+  # Whether or not we prefix the output
+  if (exists $job->{LOGPREFIX} && $job->{LOGPREFIX}) {
+      $job->{_logprefix} = 1;
+  }
+
+  # Add start line to log
   if (exists $job->{LOGFILE})
   {
-    $job->{LOGPREFIX} = 1;
-    open($job->{LOGFH}, '>>', $job->{LOGFILE})
+    $job->{_logprefix} = 1;
+    open($job->{_logfh}, '>>', $job->{LOGFILE})
 	or die "Couldn't open log file $job->{LOGFILE}: $!";
-    my $logfh = \*{$job->{LOGFH}};
+    my $logfh = \*{$job->{_logfh}};
     my $oldfh = select($logfh); local $| = 1; select($oldfh);
     print $logfh
 	(strftime ("%Y-%m-%d %H:%M:%S", gmtime),
-         " $job->{CMDNAME}($job->{PID}): Executing: @{$job->{CMD}}\n");
+         " $job->{_cmdname}($job->{PID}): Executing: @{$job->{CMD}}\n");
   } 
   else
   {
-    open($job->{LOGFH}, '>&', \*STDOUT);
+    open($job->{_logfh}, '>&', \*STDOUT);
   }
+}
+
+# Cleanup actions.  Only call when completely done with a job
+sub _cleanup
+{
+    my ($self, $wheelid, $payload) = @_;
+
+    # Cleanup the global and private job payload references
+    delete $PAYLOADS{$wheelid};
+    delete $self->{JOBS_RUNNING}{$wheelid};
+
+    # Cleanup any timeout alramrs
+    POE::Kernel->post( $self->{JOB_MANAGER_SESSION_ID}, 'maybe_clear_alarms', $payload->{_timer_id} ) 
+	if $payload->{_timer_id};
+
+    # Cleanup any private keys we stuck onto the job paylaod
+    delete $payload->{$_} foreach ( qw(_start _signals _timer_id _timeout_grace 
+				       _logfh _logprefix _keep_output _cmdname
+				       _priority _cleanup) );
 }
 
 sub _child_stdout {
   my ( $self, $args ) = @_[ 0 , 1 ];
-  my ($wheelid,$payload,$owner);
-  $wheelid = $args->{wheel};
-  $owner = $self->{$pkg}{owner}{$wheelid};
-  $payload = $owner->{payloads}{$wheelid};
+  my $wheelid = $args->{wheel};
+  my $payload = $PAYLOADS{$wheelid};
+  
+  if ($payload->{_keep_output}) {
+      $payload->{STDOUT} .= $args->{out};
+  }
 
-  $owner->Dbgmsg("STDOUT: $args->{out}\n") if $owner->{DEBUG} >= 2;
-  push @{$payload->{result}->{RAW_OUTPUT}}, $args->{out};
-
-  my $logfhtmp = \*{$payload->{LOGFH}};
-  my $date = strftime ("%Y-%m-%d %H:%M:%S", gmtime);
-
-  if ($payload->{LOGPREFIX})
+  my $logfhtmp = \*{$payload->{_logfh}};
+  if ($payload->{_logprefix})
   {
-    print $logfhtmp "$date $payload->{CMDNAME}($payload->{PID}): ";
+    my $date = strftime ("%Y-%m-%d %H:%M:%S", gmtime);
+    print $logfhtmp "$date $payload->{_cmdname}($payload->{PID}): ";
   }
   print $logfhtmp $args->{out},"\n";
 }
 
 sub _child_stderr {
   my ( $self, $args ) = @_[ 0 , 1 ];
-  my ($wheelid,$payload,$owner);
-  $wheelid = $args->{wheel};
-  $owner = $self->{$pkg}{owner}{$wheelid};
-  $payload = $owner->{payloads}{$wheelid};
+  my $wheelid = $args->{wheel};
+  my $payload = $PAYLOADS{$wheelid};
 
-  $owner->Dbgmsg("STDERR: $args->{out}\n") if $owner->{DEBUG} >= 2;
-  chomp $args->{out};
-  push @{$payload->{result}->{ERROR}}, $args->{out};
+  if ($payload->{_keep_output}) {
+      $payload->{STDERR} .= $args->{out};
+  }
 }
 
 sub _child_done {
   my ( $self, $args ) = @_[ 0 , 1 ];
-  my ($wheelid,$payload,$owner);
-  $wheelid = $args->{wheel};
-  $owner = $self->{$pkg}{owner}{$wheelid};
-  $payload = $owner->{payloads}{$wheelid};
-  $owner->{JOBS}--;
+  my $wheelid = $args->{wheel};
+  my $payload = $PAYLOADS{$wheelid};
+
+  my $finish   =  &mytimeofday();
+  my $duration = $finish - $payload->{_start};
 
   # Set the various return values
   @$payload{qw(STATUS EXIT SIGNAL CORE STATUS_CODE)} = &runerror ($args->{rc});
+  $payload->{START}    = $payload->{_start};
+  $payload->{FINISH}   = $finish;
+  $payload->{DURATION} = $duration;
 
-  my $duration = &mytimeofday() - $payload->{start};
-
+  # Final status line to log file
   if (exists $payload->{LOGFILE})
   {
-    my $logfh = \*{$payload->{LOGFH}};
+    my $logfh = \*{$payload->{_logfh}};
     print $logfh (strftime ("%Y-%m-%d %H:%M:%S", gmtime),
-      " $payload->{CMDNAME}($payload->{PID}): Job exited with status code",
+      " $payload->{_cmdname}($payload->{PID}): Job exited with status code",
       " $payload->{STATUS} ($payload->{STATUS_CODE})",
       sprintf(" after %.3f seconds", $duration), "\n" );
     close $logfh;
   }
 
-  $owner->Dbgmsg(sprintf("$payload->{CMDNAME} finished in %.3f seconds", $duration)) if $owner->{DEBUG};
+  # Cleanup payload reference and timeout alarms
+  $payload->{_cleanup}->( $wheelid, $payload );
 
-  my $result;
-  if ( defined($payload->{ACTION}) )
+  # _action callback
+  if ( defined($payload->{_action}) )
   {
-    $payload->{DURATION} = $duration;
-    $payload->{ACTION}->( $payload );
+    my $action = delete $payload->{_action};  # first remove the reference
+    $action->( $payload );                    # then call the action
   }
-
-# cleanup...
-  delete $owner->{payloads}{$wheelid};
-  POE::Kernel->post( $owner->{JOB_MANAGER_SESSION_ID}, 'maybe_clear_alarms', $payload->{timer_id} ) if $payload->{timer_id};
 }
 
 sub _child_died {
   my ( $self, $args ) = @_[ 0 , 1 ];
-  my ($wheelid,$payload,$owner);
-  $wheelid = $args->{wheel};
-  $owner = $self->{$pkg}{owner}{$wheelid};
-  $payload = $owner->{payloads}{$wheelid};
+  my $wheelid = $args->{wheel};
+  my $payload = $PAYLOADS{$wheelid};
 
-  $args->{out} ||= '';
-  chomp $args->{out};
-  my $text = 'child_died: [' . $args->{rc} . '] ' . $args->{out};
-  push @{$payload->{result}->{ERROR}}, $text;
+  # pipe this event into _child_done event handler
   _child_done( $self, $args );
 }
 
 sub _child_error {
   my ( $self, $args ) = @_[ 0 , 1 ];
-  my ($wheelid,$payload,$owner);
-  $wheelid = $args->{wheel};
-  $owner = $self->{$pkg}{owner}{$wheelid};
-  $payload = $owner->{payloads}{$wheelid};
+  my $wheelid = $args->{wheel};
+  my $payload = $PAYLOADS{$wheelid};
 
-  chomp $args->{error};
-  my $text = 'child_error: [' . $args->{err} . '] ' . $args->{error};
-  push @{$payload->{result}->{ERROR}}, $text;
+  # pipe this internal error to stderr event handler
+  chomp $args->{error}; 
+  $args->{out} = 'child_error: [' . $args->{err} . '] ' . $args->{error};
+  _child_stderr( $self, $args );
 }
 
 # Add a new command to the job list.  The command will only be started
 # if the current limit of available job slots is not exceeded; otherwise
 # the job simply gets added to the list of processes to start later on.
-# If the command list is empty, the job represents a delayed action to
-# be invoked on the next "pumpJobs".
+#
+# The following optional job hash keys affect the behavior of the job
+#   PRIORITY      priority for starting a job; lower number is higher priority
+#   TIMEOUT       seconds after which the job will be timed out
+#   TIMEOUT_GRACE seconds between attempts to kill a timed-out job
+#   LOGFILE       file to which STDOUT should be written
+#   LOGPREFIX     writes a timestamp and command name before each output line if true
+#   KEEP_OUTPUT   saves output to job hash, keyed as STDOUT and STDERR
+#
+# The following keys will always be added to the job hash upon
+# completion.  Do not set these keys beforehand in the job hash, or
+# they will be overwritten.
+#   CMD           arrayref of the job command and options
+#   PID           the process id the job ran as
+#   START         timestamp of the job start time
+#   FINISH        timestamp of the job finish time
+#   DURATION      duration of the job in seconds
+#   STATUS        the exit code of the command, or a string indicating a signal or other problem
+#   STATUS_CODE   unchanged exit code of the command; see perlvar for $?
+#   EXIT          the exit code of the command (only) 
+#   SIGNAL        the signal number the command received, if signaled
+#   CORE          true if there was a core dump
+#
+# The following keys will optionally be added to the job hash,
+# depending on what happened to the job:
+#   TIMED_OUT     timestamp at which point the job timed-out
+#   STDOUT        standard output of the command, if KEEP_OUTPUT was set
+#   STDERR        standard error of the command, if KEEP_OUTPUT was set
 sub addJob
 {
   my ($self, $action, $jobargs, @cmd) = @_;
-  my $job = { PID => 0, ACTION => $action, CMD => [ @cmd ], %{$jobargs || {}} };
-  $job->{PRIORITY} = POSIX::DBL_MAX   # default ideally the lowest possible.
-      unless exists $job->{PRIORITY};
-  $self->{QUEUE}->enqueue($job->{PRIORITY} ,$job);
-  $self->{JOBS}++;
+  my $job = { CMD => [ @cmd ],
+	      PID => 0,
+	      _action => $action,
+	      %{$jobargs || {}} };
+
+  # Default priority ideally the lowest possible in order to give the
+  # user the option to make certain jobs high priority.
+  $job->{_priority} = exists $job->{PRIORITY} ? $job->{PRIORITY} : POSIX::DBL_MAX;
+
+  $self->{JOB_QUEUE}->enqueue($job->{_priority} ,$job);
   POE::Kernel->post($self->{JOB_MANAGER_SESSION_ID}, 'job_queued');
 }
 
+
+
+# Event to kill a job
 sub timeout
 {
   my ( $self, $kernel, $wheelid ) = @_[ OBJECT, KERNEL, ARG0 ];
-  my $payload = $self->{payloads}{$wheelid};
+  my $payload = $PAYLOADS{$wheelid};
   return unless defined $payload;
-  my $signal = shift @{$payload->{signals}};
-  return unless $signal;
+  my $signal = shift @{$payload->{_signals}};
+  return unless $signal; # we couldn't kill -9 the job?
+
   my $wheel = $self->{_child}->wheel($wheelid);
   $payload->{TIMED_OUT} = &mytimeofday();
   $wheel->kill( $signal );
-  my $timeout = $payload->{TIMEOUT_GRACE} || 3;
-  print "Sending signal $signal to wheel $wheelid\n" if $self->{VERBOSE};
+  my $timeout = $payload->{_timeout_grace};
   $kernel->delay_set( 'timeout', $timeout, $wheelid );
 }
 
@@ -292,24 +365,18 @@ sub killAllJobs
 {
   my $self = shift;
 
-  foreach my $wheelID ( keys %{$self->{payloads}} )
+  foreach my $wheelID ( keys %{$self->{JOB_PAYLOADS}} )
   {
     POE::Kernel->post($self->{JOB_MANAGER_SESSION_ID}, 'timeout', $wheelID);
   }
 }
 
-sub pumpJobs
-{
-  print <<EOD;
-'pumpJobs' is obsolete, and can seriously damage your POE-health.
-EOD
-}
-
+# Waits for the queue to drain and makes a callback when it does
 sub whenQueueDrained
 {
-  my ($self,$callback) = @_;
+  my ($self, $callback) = @_;
   $callback = sub {} unless $callback;
-  push @{$self->{QUEUE_DRAINED_CALLBACKS}}, $callback;
+  push @{$self->{JOB_QUEUE_DRAINED_CALLBACKS}}, $callback;
   if ( ! $self->{_DOINGSOMETHING}++ )
   {
 #   Kick off the queue_drained loop if it isn't already running
@@ -328,7 +395,7 @@ sub queue_drained
 
 # use pop instead of shift to get the callbacks in the order they were
 # added to the queue, just in case that matters
-  while ( my $callback = pop @{$self->{QUEUE_DRAINED_CALLBACKS}} )
+  while ( my $callback = pop @{$self->{JOB_QUEUE_DRAINED_CALLBACKS}} )
   {
     &$callback() if $callback;
     $self->{_DOINGSOMETHING}--;
@@ -338,7 +405,19 @@ sub queue_drained
 sub jobsRemaining()
 {
   my $self = shift;
-  return scalar(keys %{$self->{payloads}}) + $self->{QUEUE}->get_item_count();
+  return $self->jobsActive() + $self->jobsQueued();
+}
+
+sub jobsQueued()
+{
+    my $self = shift;
+    return $self->{JOB_QUEUE}->get_item_count();
+}
+
+sub jobsActive()
+{
+    my $self = shift;
+    return scalar(keys %{$self->{JOB_PAYLOADS}});
 }
  
 sub maybe_clear_alarms
