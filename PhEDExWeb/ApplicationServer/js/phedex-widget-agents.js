4d5bf6ccc93ad597b32dc00dc504a7d540de7566
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-06-03 17:56:28 +0000

    both datatable-widgets now use phedex-core-widget-datatable as their base class. This provides context menus and other bells and whistles, which makes these modules much lighter

--- PhEDExWeb/ApplicationServer/js/phedex-widget-agents.js
+++ PhEDExWeb/ApplicationServer/js/phedex-widget-agents.js
@@ -8,82 +8,52 @@ PHEDEX.Page.Widget.Agents=function(divid) {
 }
 
 PHEDEX.Widget.Agents=function(site,divid) {
-	if ( !divid) { divid = PHEDEX.Util.generateDivName(); }
-	var that=new PHEDEX.Core.Widget(divid+'_'+site,null,
-		{
-		width:500,
-		height:200,
-		minwidth:300,
-		minheight:80
-		});
-	that.site=site;
-	that.fillHeader=function(div) {
-          var now = new Date() / 1000;
-          var minDate = now;
-          var maxDate = 0;
-          for ( var i in this.data) {
-	    var a = this.data[i];
-            var u = a['time_update'];
-            a['gmtDate'] = new Date(u*1000).toGMTString();
-            if ( u > maxDate ) { maxDate = u; }
-            if ( u < minDate ) { minDate = u; }
-          }
-	  var msg = "Site: "+this.site+", agents: "+this.data.length;
-          if ( maxDate > 0 )
-          {
-            var minGMT = new Date(minDate*1000).toGMTString();
-            var maxGMT = new Date(maxDate*1000).toGMTString();
-            var dMin = Math.round(now - minDate);
-            var dMax = Math.round(now - maxDate);
-            msg += " Update-times range: "+dMin+" - "+dMax+" seconds ago";
-          }
-          div.innerHTML = msg;
-	}
-	that.fillBody=function(div) {
-	  var table = [];
-	  for (var i in this.data) {
-	    var a = this.data[i];
-            var y = { Agent:a['name'], Version:a['version'], PID:a['pid'], Date:a['gmtDate'] };
-            table.push( y );
-          }
-          that.columnDefs = [
-	            {key:"Agent", sortable:true, resizeable:true},
-	            {key:"Date", formatter:YAHOO.widget.DataTable.formatDate, sortable:true, resizeable:true},
-	            {key:"PID", sortable:true, resizeable:true},
-	            {key:"Version", sortable:true, resizeable:true},
-	        ];
-          that.dataSource = new YAHOO.util.DataSource(table);
-	  that.dataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
-	  that.dataSource.responseSchema = { fields: {} };
-	  for (var i in that.columnDefs) { that.dataSource.responseSchema.fields[i] = that.columnDefs[i].key; }
-	  that.dataTable = new YAHOO.widget.DataTable(div, that.columnDefs, that.dataSource, { draggableColumns:true });
-	}
-	that.update=function() {
-	  PHEDEX.Datasvc.Agents(that.site,that);
-	}
-	that.receive=function(result) {
-	  that.data = PHEDEX.Data.Agents[that.site];
-	  if (that.data) {
-	    that.populate();
-	    }
-	}
+  if ( !divid) { divid = PHEDEX.Util.generateDivName(); }
+  var that=new PHEDEX.Core.Widget.DataTable(divid+'_'+site,null,
+    {
+	width:500,
+	height:200,
+	minwidth:300,
+	minheight:50
+    });
+  that.site=site;
+  that.me=function() { return 'PHEDEX.Core.Widget.Agents'; }
+  that.fillHeader=function(div) {
+    var now = new Date() / 1000;
+    var minDate = now;
+    var maxDate = 0;
+    for ( var i in this.data) {
+      var a = this.data[i];
+      var u = a['time_update'];
+      if ( u > maxDate ) { maxDate = u; }
+      if ( u < minDate ) { minDate = u; }
+    }
+    var msg = "Site: "+this.site+", agents: "+this.data.length;
+    if ( maxDate > 0 )
+    {
+      var minGMT = new Date(minDate*1000).toGMTString();
+      var maxGMT = new Date(maxDate*1000).toGMTString();
+      var dMin = Math.round(now - minDate);
+      var dMax = Math.round(now - maxDate);
+      msg += " Update-times range: "+dMin+" - "+dMax+" seconds ago";
+    }
+    var s = document.createElement('span');
+    div.appendChild(s);
+    s.innerHTML = msg;
+  }
+  that.buildTable(that.div_content,
+            [ 'Agent',{key:"Date", formatter:'UnixEpochToGMT'},'PID','Version','Label','Host' ],
+	    {Agent:'name', Date:'time_update'} );
+  that.onUpdateComplete.subscribe( function() {that.fillDataSource(that.data); } );
+  that.update=function() { PHEDEX.Datasvc.Agents(that.site,that); }
+  that.receive=function(result) {
+    that.data = PHEDEX.Data.Agents[that.site];
+    if (that.data) { that.populate(); }
+  }
 
-//	Gratuitously flash yellow when the mouse goes over the rows
-	that.onRowMouseOut = function(event) {
-	  event.target.style.backgroundColor = null;
-	}
-	that.onRowMouseOver = function(event) {
-	  event.target.style.backgroundColor = 'yellow';
-        }
-	that.postPopulate = function() {
-	  YAHOO.log('PHEDEX.Widget.Agents: postPopulate');
-          that.dataTable.subscribe('rowMouseoverEvent',that.onRowMouseOver);
-          that.dataTable.subscribe('rowMouseoutEvent', that.onRowMouseOut);
-	}
-
-	that.build();
-	that.onPopulateComplete.subscribe(that.postPopulate);
-	return that;
+  that.buildContextMenu('Agent');
+  that.build();
+  return that;
 }
 
 // What can I respond to...?
--- PhEDExWeb/ApplicationServer/js/phedex-widget-nodes.js
+++ PhEDExWeb/ApplicationServer/js/phedex-widget-nodes.js
@@ -8,153 +8,34 @@ PHEDEX.Page.Widget.Nodes=function(divid) {
 }
 
 PHEDEX.Widget.Nodes=function(site,divid) {
-	if ( !divid) { divid = PHEDEX.Util.generateDivName(); }
-	var that=new PHEDEX.Core.Widget(divid+'_display',null,
-		{width:500,
-		 height:200,
-		 minwidth:300,
-		 minheight:80
-		});
-	that.site=site;
-	that.data = null;
-	that.fillHeader=function(div) {
-	  div.innerHTML = 'PHEDEX Nodes: '+this.data.length+" sites found...";
-	}
-	that.buildBody=function(div) {
-         that.columnDefs = [
-	            {key:"ID", sortable:true, resizeable:true},
-	            {key:"Name", sortable:true, resizeable:true},
-	            {key:"Kind", sortable:true, resizeable:true},
-	            {key:"Technology", sortable:true, resizeable:true},
-	            {key:"SE", sortable:true, resizeable:true},
-	        ];
-          that.dataSource = new YAHOO.util.LocalDataSource();//table);
-	  that.dataTable = new YAHOO.widget.DataTable(div, that.columnDefs, that.dataSource,
-	  			 { draggableColumns:true, initialLoad:false });
-	}
-	that.fillBody=function(div) {
-          var table = [];
-	  for (var i in that.data) {
-	    var a = that.data[i];
-// Rather than fill by-hand and duplicate key-names, I take them from the columnDefs. This makes the code more generic.
-//             var y = { ID:a['id'], Name:a['name'], Kind:a['kind'], Technology:a['technology'], SE:a['se'] };
-	    var y = [];
-	    for (var j in that.columnDefs )
-	    {
-	      var k = that.columnDefs[j].key;
-	      y[k] = a[k.toLowerCase()];
-	    }
-            table.push( y );
-          }
-          that.dataSource = new YAHOO.util.DataSource(table);
-	  var oCallback = {
-	    success : that.dataTable.onDataReturnInitializeTable,
-	    failure : that.dataTable.onDataReturnInitializeTable,
-	    scope : that.dataTable
-	  };
-	  that.dataSource.sendRequest('', oCallback);
+  if ( !divid) { divid = PHEDEX.Util.generateDivName(); }
+  var that=new PHEDEX.Core.Widget.DataTable(divid+'_display',null,
+	{width:500,
+	 height:200,
+	 minwidth:400,
+	 minheight:50
+	});
+  that.site=site;
+  that.data = null;
+  that.me=function() { return 'PHEDEX.Core.Widget.Nodes'; }
+  that.fillHeader=function(div) {
+    var s = document.createElement('span');
+    div.appendChild(s);
+    s.innerHTML = 'PHEDEX Nodes: '+this.data.length+" sites";
+  }
+  that.buildTable(that.div_content,
+            [ 'ID','Name','Kind','Technology','SE' ],
+	    {} );
+  that.onUpdateComplete.subscribe( function() {that.fillDataSource(that.data); } );
+  that.update=function() {
+    PHEDEX.Datasvc.Nodes(that.site,that);
+  }
+  that.receive=function(result) {
+    that.data = PHEDEX.Data.Nodes; // use global data object, instead of result['node'];
+    that.populate();
+  }
 
-	  var menu = new YAHOO.widget.Menu('nowhere');
-	  var showColumns = new YAHOO.widget.Button(
-	    {
-	      type: "split",
-	      label: "Show all columns",
-	      name: "showColumnsButton",
-	      menu: menu,
-	      container: that.div_header,
-	      disabled:true
-	    }
-	  );
-	  var dt = that.dataTable;
-	  showColumns.on("click", function () {
-	    var m = menu.getItems();
-	    for (var i = 0; i < m.length; i++) {
-	      dt.showColumn(dt.getColumn(m[i].value));
-	    }
-	    menu.clearContent();
-	    refreshButton();
-	    debugger;
-	    that.resizePanel(that.dataTable);
-	  });
-
-	  showColumns.on("appendTo", function () {
-	    var m = this.getMenu();
-	    m.subscribe("click", function onMenuClick(sType, oArgs) {
-	      var oMenuItem = oArgs[1]; 
-	      if (oMenuItem) {
-	        that.dataTable.showColumn(dt.getColumn(oMenuItem.value));
-	        m.removeItem(oMenuItem.index);
-	        refreshButton();
-	      }
-	      that.resizePanel(that.dataTable);
-	    });
-	  });
-
-	  var refreshButton = function() {
-	    if (YAHOO.util.Dom.inDocument('nowhere')) {
-	      menu.render();
-	    } else {
-	      menu.render(document.body);
-	    }
-	  showColumns.set('disabled', menu.getItems().length === 0);
-	  };
-	  
-	  that.dataTable.subscribe('columnHideEvent', function(ev) {
-				var column = this.getColumn(ev.column);
-				menu.addItem({text: column.label || column.key,value:column.key});
-				refreshButton();
-			} );
-	  that.resizePanel=function(table) {
-	    var x = table.getTableEl().clientWidth;
-	    that.panel.cfg.setProperty('width',x+25+'px');
-	  }
-	  that.dataTable.subscribe('renderEvent', function() { that.resizePanel(dt); } );
-	}
-
-	that.onContextMenuClick = function(p_sType, p_aArgs, p_DataTable) {
-	  var label = p_aArgs[0].explicitOriginalTarget.textContent;
-          var task = p_aArgs[1];
-          if(task) {
-// 	  Extract which TR element triggered the context menu
-            var tgt = this.contextEventTarget;
-	    var elCol = p_DataTable.getColumn(tgt);
-            var elRow = p_DataTable.getTrEl(tgt);
-            if(elRow) {
-              var oRecord = p_DataTable.getRecord(elRow);
-	      var selected_site = oRecord.getData('Name')
-	      YAHOO.log('PHEDEX.Widget.Nodes: ContextMenu: "'+label+'" for '+selected_site);
-	      this.payload[task.index]({table:p_DataTable,
-	      				  row:elRow,
-					  col:elCol,
-				selected_site:selected_site});
-            }
-          }
-        }
-	that.onRowMouseOut = function(event) {
-	  event.target.style.backgroundColor = null;
-	}
-	that.onRowMouseOver = function(event) {
-	  event.target.style.backgroundColor = 'yellow';
-        }
-
-	that.postPopulate = function() {
-	  YAHOO.log('PHEDEX.Widget.Nodes: postPopulate');
-	  that.contextMenu = PHEDEX.Core.ContextMenu.Create('Node',{trigger:that.dataTable.getTbodyEl()});
-	  PHEDEX.Core.ContextMenu.Build(that.contextMenu,'Node','dataTable');
-          that.contextMenu.render(document.body); // that.div_body);
-          that.contextMenu.clickEvent.subscribe(that.onContextMenuClick, that.dataTable);
-          that.dataTable.subscribe('rowMouseoverEvent',that.onRowMouseOver);
-          that.dataTable.subscribe('rowMouseoutEvent', that.onRowMouseOut);
-	}
-	that.fillFooter=function(div) { return; }
-	that.update=function() {
-	  PHEDEX.Datasvc.Nodes(that.site,that);
-	}
-	that.receive=function(result) {
-	  that.data = PHEDEX.Data.Nodes; // use global data object, instead of result['node'];
-	  that.populate();
-	}
-	that.build();
-	that.onPopulateComplete.subscribe(that.postPopulate);
-	return that;
+  that.buildContextMenu('Node');
+  that.build();
+  return that;
 }
