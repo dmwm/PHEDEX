ff4c9bc4ce33eb0a2d347f816dfe827bc0a2c0b8
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-06-04 14:58:21 +0000

    cleaned code, hide uninteresting stuff by default, also send log messages to FireBug or Safari consoles

--- PhEDExWeb/ApplicationServer/js/phedex-core-logger.js
+++ PhEDExWeb/ApplicationServer/js/phedex-core-logger.js
@@ -5,22 +5,26 @@ PHEDEX.namespace('Logger');
 PHEDEX.Logger.Create=function(opts,divid) {
   if ( !divid ) { divid = 'phedex_logger'; }
   var myDiv = document.getElementById(divid);
-  if ( myDiv )
-  {
-    YAHOO.widget.Logger.reset();
-    var LoggerConfig = {
-      width: "500px",
-      height: "20em",
-      newestOnTop: false,
-      footerEnabled: true,
-      verboseOutput: false
-    };
-    if (opts) {
-      for (o in opts) {
-        LoggerConfig[o]=opts[o];
-      }
+  if ( !myDiv ) { return; }
+
+  YAHOO.widget.Logger.reset();
+  var LoggerConfig = {
+    width: "500px",
+    height: "20em",
+    newestOnTop: false,
+    footerEnabled: true,
+    verboseOutput: false
+  };
+  if (opts) {
+    for (o in opts) {
+      LoggerConfig[o]=opts[o];
     }
-    myDiv.style.width = LoggerConfig.width;
-    PHEDEX.Logger.Reader= new YAHOO.widget.LogReader(divid,LoggerConfig);
   }
+  myDiv.style.width = LoggerConfig.width;
+  PHEDEX.Logger.Reader = new YAHOO.widget.LogReader(divid,LoggerConfig);
+  PHEDEX.Logger.Reader.hideSource('global');
+  PHEDEX.Logger.Reader.hideSource('LogReader');
+  PHEDEX.Logger.Reader.collapse();
 }
+
+YAHOO.widget.Logger.enableBrowserConsole(); // Enable logging to firebug console, or Safari console.
