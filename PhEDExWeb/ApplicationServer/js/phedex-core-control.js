44a6fe9723d6e8250a2a68b7a3fc71d182cff120
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-07-03 12:47:10 +0000

    much cleaned, now has .Show() and .Hide() functions for manual intervention, does not make any assumption about the object it is being rendered into, and stuff like that

--- PhEDExWeb/ApplicationServer/js/phedex-core-control.js
+++ PhEDExWeb/ApplicationServer/js/phedex-core-control.js
@@ -12,35 +12,53 @@ PHEDEX.Core.Control = function(args) {
   }
   for (var i in args.payload) { ctl.payload[i] = args.payload[i]; }
   YAHOO.util.Dom.addClass(ctl.payload.target,'phedex-invisible');
-  ctl.className = args.className || 'phedex-core-control-widget-inactive';
-  if ( !ctl.payload.render ) { ctl.payload.render = document.body; }
+  ctl.className = args.className || 'phedex-core-control-widget phedex-core-control-widget-inactive';
   for (var i in args.events) {
     var ev = args.events[i].event;
     var fn = args.events[i].handler || PHEDEX.Core.Control;
     var el = args.events[i].element || ctl; // doesn't seem to work when I use something other than the ctl itself...
     YAHOO.util.Event.addListener(el,ev,fn,ctl);
   }
+  if ( ctl.payload.render ) {
+    var renderInto = ctl.payload.render;
+    if ( typeof(renderInto) != 'object' ) { renderInto = document.getElementById(renderInto); }
+    renderInto.appendChild(ctl);
+  }
+
+  ctl.Show = function() {
+    var tgt = this.payload.target;
+    if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
+    if ( !YAHOO.util.Dom.hasClass(tgt,'phedex-invisible') ) { return; }
+    if ( this.payload.fillFn ) { this.payload.fillFn(tgt); }
+    var obj = this.payload.obj || {};
+    YAHOO.util.Dom.removeClass(tgt,'phedex-invisible');
+    var eHeight = tgt.offsetHeight;
+    if ( obj.onShowControl ) { obj.onShowControl.fire(eHeight); }
+    YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-inactive');
+    YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-active');
+  }
+  ctl.Hide = function() {
+    var tgt = this.payload.target;
+    if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
+    var eHeight = tgt.offsetHeight;
+    var obj = this.payload.obj || {};
+    YAHOO.util.Dom.addClass(tgt,'phedex-invisible');
+    if ( obj.onHideControl ) { obj.onHideControl.fire(eHeight); }
+    YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-inactive');
+    YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-active');
+  }
   return ctl;
 }
 
 PHEDEX.Core.Control.controlHandler=function(ev,obj) {
     var eHeight;
-    var tgt = this.payload.target || obj.div_extra;
-    var fillFn = this.payload.fillFn || obj.fillExtra;
+    var tgt = this.payload.target;
+    if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
+    var fillFn = this.payload.fillFn;
     if ( ev.type == 'mouseover' ) {
-      if ( !YAHOO.util.Dom.hasClass(tgt,'phedex-invisible') ) { return; } // if ( !tgt.style.display ) { return; }
-      if ( fillFn ) { fillFn(tgt); }
-      YAHOO.util.Dom.removeClass(tgt,'phedex-invisible');
-      eHeight = tgt.offsetHeight;
-      if ( obj.onShowControl ) { obj.onShowControl.fire(eHeight); }
-      YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-inactive');
-      YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-active');
+      this.Show();
     } else if ( ev.type == 'click' ) {
-      eHeight = tgt.offsetHeight;
-      YAHOO.util.Dom.addClass(tgt,'phedex-invisible');
-      if ( obj.onHideControl ) { obj.onHideControl.fire(eHeight); }
-      YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-inactive');
-      YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-active');
+      this.Hide();
     }
   }
 
