9563c2979062933a6c4dde9fb8404b52d5284eaf
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-07-06 13:05:31 +0000

    add PHEDEX.Page.Widget.About, which will create the "about" information in javascript. Do not actually use it yet, not sure if it is really needed. Add PHEDEX.Page.Widget.Instance, to allow the user to select the TMDB instance they want to use.

--- PhEDExWeb/ApplicationServer/js/phedex-page.js
+++ PhEDExWeb/ApplicationServer/js/phedex-page.js
@@ -38,3 +38,61 @@ PHEDEX.Page.Create = function( config ) {
     PHEDEX.Page.Config.Order[PHEDEX.Page.Config.Count++] = myDivId;
   }
 }
+
+PHEDEX.Page.Widget.About = function(el) {
+  if ( !el ) { el = document.body; }
+  if ( typeof(el) != 'object' ) { el = document.getElementById(el); }
+  var div = document.createElement('div');
+  div.className='phedex-heading-display';
+  var divLogo = document.createElement('div');
+  div.appendChild(divLogo);
+  divLogo.className = 'phedex-heading-logo';
+  var a = document.createElement('a');
+  a.setAttribute('href','/html/phedex.html');
+  a.setAttribute('title','PhEDEx Home Page');
+  var img = document.createElement('img');
+  img.setAttribute('src','/images/phedex-logo-small.gif');
+  img.setAttribute('alt','PhEDEx');
+  img.setAttribute('height','80px');
+  a.appendChild(img);
+  divLogo.appendChild(a);
+  div.appendChild(divLogo);
+
+  var divHeading = document.createElement('div');
+  divHeading.className='phedex-heading';
+  divHeading.innerHTML = '<h1>PhEDEx Web Interface</h1>' +
+      '<h2>Next-generation Alpha Version</h1>' +
+      '<p>Send feedback to <a href="mailto:cms-phedex-admins@cern.ch">cms-phedex-admins@cern.ch</a></p>';
+  div.appendChild(divHeading);
+  el.appendChild(div);
+  return div;
+}
+
+PHEDEX.Page.Widget.Instance = function(el) {
+  if ( !el ) { throw new Error('PHEDEX.Page.Widget.Instance: expect an element name or object'); }
+  if ( typeof(el) != 'object' ) { el = document.getElementById(el); }
+  while ( el.hasChildNodes() ) { el.removeChild(el.firstChild); }
+  var div = document.createElement('div');
+  div.className='phedex-heading-instance';
+  div.appendChild(document.createTextNode('Select an instance:'));
+  var ul = document.createElement('ul');
+  var currentInstance = PHEDEX.Datasvc.Instance();
+  var instances = PHEDEX.Datasvc.Instances();
+    var clickFn = function(ev,x) { YAHOO.log('Set instance '+x,'info','Core.Control'); PHEDEX.Datasvc.Instance(x); }
+  for (var i in instances ) {
+    var instance = instances[i];
+    var a = document.createElement('a');
+    a.setAttribute('href','#');
+    a.setAttribute('title',instance.name);
+    a.appendChild(document.createTextNode(instance.name));
+    YAHOO.util.Event.on(a,'click',clickFn,instance.instance);
+    a.className = 'phedex-link';
+    if ( instance.name == currentInstance.name ) { a.className += ' phedex-link-current'; }
+    var li = document.createElement('li');
+    li.appendChild(a);
+    ul.appendChild(li);
+  };
+  div.appendChild(ul);
+  el.appendChild(div);
+  return div;
+}
