4f876a96ae7fa878f0ea80c23e7c34aa7f6f5e6b
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-07-08 08:58:15 +0000

    some rationalisation of code

--- PhEDExWeb/ApplicationServer/js/phedex-core-control.js
+++ PhEDExWeb/ApplicationServer/js/phedex-core-control.js
@@ -1,36 +1,34 @@
 PHEDEX.namespace('Core.Control');
 
 PHEDEX.Core.Control = function(args) {
-  var ctl;
   if ( !args.type ) { args.type = 'a'; }
-  ctl = document.createElement(args.type);
-  ctl.payload = [];
+  this.el = document.createElement(args.type);
+  this.payload = [];
   if ( args.type == 'img' ) {
-    ctl.src = args.src;
+    this.el.src = args.src;
   } else if ( args.type == 'a' ) {
-    ctl.href='#';
-    ctl.appendChild(document.createTextNode(args.text));
+    this.el.href='#';
+    this.el.appendChild(document.createTextNode(args.text));
   }
-  for (var i in args.payload) { ctl.payload[i] = args.payload[i]; }
-  YAHOO.util.Dom.addClass(ctl.payload.target,'phedex-invisible');
-  ctl.className = args.className || 'phedex-core-control-widget phedex-core-control-widget-inactive';
+  for (var i in args.payload) { this.payload[i] = args.payload[i]; }
+  YAHOO.util.Dom.addClass(this.payload.target,'phedex-invisible');
+  this.el.className = args.className || 'phedex-core-control-widget phedex-core-control-widget-inactive';
   if ( !args.events ) {
     args.events = [{event:'mouseover', handler:PHEDEX.Core.Control.controlHandler},
                    {event:'click',     handler:PHEDEX.Core.Control.controlHandler}];
   }
   for (var i in args.events) {
     var ev = args.events[i].event;
-    var fn = args.events[i].handler || PHEDEX.Core.Control;
-    var el = args.events[i].element || ctl; // doesn't seem to work when I use something other than the ctl itself...
-    YAHOO.util.Event.addListener(el,ev,fn,ctl);
+    var fn = args.events[i].handler || PHEDEX.Core.Control.controlHandler;
+    var el = args.events[i].element || this.el; // doesn't seem to work when I use something other than the ctl itself...
+    YAHOO.util.Event.addListener(el,ev,fn,this,true);
   }
-  if ( ctl.payload.render ) {
-    var renderInto = ctl.payload.render;
+  if ( this.payload.render ) {
+    var renderInto = this.payload.render;
     if ( typeof(renderInto) != 'object' ) { renderInto = document.getElementById(renderInto); }
-    renderInto.appendChild(ctl);
+    renderInto.appendChild(this.el);
   }
-
-  ctl.Show = function() {
+  this.Show = function() {
     var tgt = this.payload.target;
     if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
     if ( !YAHOO.util.Dom.hasClass(tgt,'phedex-invisible') ) { return; }
@@ -39,34 +37,33 @@ PHEDEX.Core.Control = function(args) {
     YAHOO.util.Dom.removeClass(tgt,'phedex-invisible');
     var eHeight = tgt.offsetHeight;
     if ( obj.onShowControl ) { obj.onShowControl.fire(eHeight); }
-    YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-inactive');
-    YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-active');
+    YAHOO.util.Dom.removeClass(this.el,'phedex-core-control-widget-inactive');
+    YAHOO.util.Dom.addClass   (this.el,'phedex-core-control-widget-active');
   }
-  ctl.Hide = function() {
+  this.Hide = function() {
     var tgt = this.payload.target;
     if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
     var eHeight = tgt.offsetHeight;
     var obj = this.payload.obj || {};
     YAHOO.util.Dom.addClass(tgt,'phedex-invisible');
     if ( obj.onHideControl ) { obj.onHideControl.fire(eHeight); }
-    YAHOO.util.Dom.addClass   (this,'phedex-core-control-widget-inactive');
-    YAHOO.util.Dom.removeClass(this,'phedex-core-control-widget-active');
+    YAHOO.util.Dom.addClass   (this.el,'phedex-core-control-widget-inactive');
+    YAHOO.util.Dom.removeClass(this.el,'phedex-core-control-widget-active');
   }
-  ctl.Label = function(text) {
-    this.innerHTML = text;
+  this.Label = function(text) {
+    this.el.innerHTML = text;
   }
-  return ctl;
-}
+  return this;
+};
 
 PHEDEX.Core.Control.controlHandler=function(ev,obj) {
     var eHeight;
-    var tgt = this.payload.target;
+    var tgt = obj.payload.target;
     if ( typeof(tgt) != 'object' ) { tgt = document.getElementById(tgt); }
-    var fillFn = this.payload.fillFn;
     if ( ev.type == 'mouseover' ) {
-      this.Show();
+      obj.Show();
     } else if ( ev.type == 'click' ) {
-      this.Hide();
+      obj.Hide();
     }
   }
 
