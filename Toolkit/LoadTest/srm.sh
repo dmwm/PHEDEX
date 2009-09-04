a3c73c23844206f3bf2203d21de7068cd2263791
Nicolo Magini <sha1-61c9148c72dc9af330b61448b73df2d65993435c@cern.ch>
2009-09-04 14:59:59 +0000

    Changed default protocol to srmv2 for srm.sh script in LoadTest creation tool

--- Toolkit/LoadTest/srm.sh
+++ Toolkit/LoadTest/srm.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 FNAME=$1
-BASEPATH="srm://io.hep.kbfi.ee:8443/srm/managerv1?SFN=/pnfs/hep.kbfi.ee/cms/LoadTest07/"
+BASEPATH="srm://io.hep.kbfi.ee:8443/srm/managerv2?SFN=/pnfs/hep.kbfi.ee/cms/LoadTest07/"
 
-srmcp -debug=true -retry_num=3 file:///`pwd`/$FNAME ${BASEPATH}${FNAME}
+PROTOCOLOPTION="-srm_protocol_version=2"
+
+if [[ $BASEPATH =~ srm/managerv1 ]]; then
+    PROTOCOLOPTION="-srm_protocol_version=1"
+fi    
+
+srmcp -debug=true $PROTOCOLOPTION -retry_num=3 file:///`pwd`/$FNAME ${BASEPATH}${FNAME}
 exit $?
