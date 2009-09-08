fbe047839c6c1aed6010fd796314d5994575523d
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-09-08 12:30:46 +0000

    - Initialization of GUI elements of Navigator - Fetching of target types from Registry - Fetching of widgets from Registry - For 'node' target, an AutoComplete input - For other targets, default to a text input

--- PhEDExWeb/ApplicationServer/js/phedex-navigator.js
+++ PhEDExWeb/ApplicationServer/js/phedex-navigator.js
@@ -8,6 +8,10 @@
 */
 PHEDEX.namespace("Navigator");
 PHEDEX.Navigator=(function() {
+  // package aliases
+  var PxU = PHEDEX.Util;
+  var PxR = PHEDEX.Core.Widget.Registry;
+
   // private properties
   
   // _target_types
@@ -29,6 +33,136 @@ PHEDEX.Navigator=(function() {
   var _filter = {};
 
   // private methods
+  var _initTypeSelector = function(el) {
+    var typediv = PxU.makeChild(el, 'div', { id: 'phedex-nav-type'});
+    
+    // get registered target types
+    var types = PxR.getInputTypes();
+    types = ['node', 'group', 'block', 'blah']; // XXX TEST
+    var menu_data = [];
+    for (var t in types) {
+      var text = types[t];
+      _target_types[text] = { name: text, label: text };
+      menu_data.push({ text:text, value:text } );
+    }
+    _cur_target_type = types[0];
+
+    var menu = new YAHOO.widget.Button({ type:"menu",
+					 label:_cur_target_type,
+					 menu:menu_data,
+					 container:typediv });
+    var onSelectedMenuItemChange = function (event) {
+      var oMenuItem = event.newValue;
+      var text = oMenuItem.cfg.getProperty("text");
+      YAHOO.log('onSelectedMenuItemChange: new value: '+text,'info','Navigator');
+      this.set("label", text);
+      _cur_target_type = text;
+      _setTargetType();
+    };
+    menu.on("selectedMenuItemChange", onSelectedMenuItemChange);
+  };
+
+  var _initTargetSelectors = function(el) {
+    var targetdiv = PxU.makeChild(el, 'div', {id:'phedex-nav-target'});
+    for (var t in _target_types) {
+      // TODO:  possible to call a vairable function name?
+      if (t == 'node') {
+	_initNodeSelector(targetdiv);
+      } else {
+	_initTextSelector(targetdiv, t);
+      }
+    }
+  };
+
+  var _node_ds; // YAHOO.util.LocalDataSource
+  var _initNodeSelector = function(el) {
+    var nodesel = PxU.makeChild(el, 'div', {id:'phedex-nav-target-nodesel'});
+    var makeNodeList = function(data) {
+      data = data.node;
+      var nodelist = [];
+      for (var node in data) {
+	nodelist.push(data[node].name);
+      }
+      _node_ds = new YAHOO.util.LocalDataSource(nodelist);
+      _buildNodeSelector(nodesel);
+    };
+    PHEDEX.Datasvc.Call({ api: 'nodes', callback: makeNodeList });
+  };
+
+  var _buildNodeSelector = function(div) {
+    var input = PxU.makeChild(div, 'input', { type:'text' });
+    var container = PxU.makeChild(div, 'div');
+    var auto_comp = new YAHOO.widget.AutoComplete(input, container, _node_ds);
+    auto_comp.prehighlightClassName = "yui-ac-prehighlight";
+    auto_comp.useShadow = true;
+    auto_comp.forceSelection = true;
+    auto_comp.queryMatchCase = false;
+    auto_comp.queryMatchContains = true; 
+  };
+
+  var _initTextSelector = function(el, type) {
+    var sel = PxU.makeChild(el, 'div', {id:'phedex-nav-target-'+type});
+    var input = PxU.makeChild(sel, 'input', { type:'text' });
+  };
+
+  var _initPageSelector = function(el) {
+    var pagediv = PxU.makeChild(el, 'div', { id:'phedex-nav-page' });
+    
+    var widgets = PxR.getWidgetsByInputType(_cur_target_type);
+    YAHOO.log('widgets='+YAHOO.lang.dump(widgets));
+    var menu_data = [];
+    for (var w in widgets) {
+      w = widgets[w];
+      menu_data.push({ text:w.label, value:w.label } );
+    }
+
+    var menu = new YAHOO.widget.Button({ type:"menu",
+					 label:widgets[0].label,
+					 menu:menu_data,
+					 container:pagediv });
+    var onSelectedMenuItemChange = function (event) {
+      var oMenuItem = event.newValue;
+      var text = oMenuItem.cfg.getProperty("text");
+      YAHOO.log('onSelectedMenuItemChange: new value: '+text,'info','Navigator');
+      this.set("label", text);
+    };
+    menu.on("selectedMenuItemChange", onSelectedMenuItemChange);
+  };
+
+  var _initGlobalFilter = function(el) {
+    var filterdiv = PxU.makeChild(el, 'div', { id:'phedex-nav-filter' });
+    var input = PxU.makeChild(filterdiv, 'input', 
+			      { id: 'phedex-nav-filter-input',
+				type: 'text' });
+  };
+
+  var _initPermaLink = function(el) {
+    var linkdiv = PxU.makeChild(el, 'div', { id:'phedex-nav-link' });
+    var a = PxU.makeChild(linkdiv, 'a',
+			  { id:'phedex-nav-filter-link',
+			    innerHTML:'Link',
+			    href:'#' });
+  };
+
+  /* TODO: hidden/visible a wise way to manage these elements? I don't
+     want to rebuild them on every target-chagne, but maybe there's a
+     better way to put them out of the way... */
+  var _setTargetType = function(type) {
+    if (!type) { type = _cur_target_type; }
+    else       { _cur_target_type = type; }
+    var div = document.getElementById('phedex-nav-target');
+    // Hide all selectors
+    var children = YAHOO.util.Dom.getChildren(div);
+    YAHOO.util.Dom.batch(children, function(c) { c.style.visibility = 'hidden'; c.style.position = 'absolute'});
+    // Show the one we want
+    var id;
+    if (type == 'node') { id = 'phedex-nav-target-nodesel'; }
+    else                { id = 'phedex-nav-target-'+type }
+    var el = document.getElementById(id);
+    el.style.visibility = 'visible';
+    el.style.position = 'relative';
+  };
+
   // parse _filter_str and set _filter
   var _parseFilter = function() {};
 
@@ -45,11 +179,26 @@ PHEDEX.Navigator=(function() {
     // init(el)
     // called when this object is created, takes the div element the navigator should be built in
     init: function(el) {
-      // get registered target types
+      // build the type selection menu
+      _initTypeSelector(el);
+
       // build TargetType selector for each type
-      //   (get list of nodes from datasvc for node menu)
-      //   (get list of groups from datasvc for group menu)
+      _initTargetSelectors(el);
+      _setTargetType();
+
+      // build Page Selector for each type
+      _initPageSelector(el);
+
       // build GlobalFilter
+      _initGlobalFilter(el);
+      
+      // build Permalink
+      _initPermaLink(el);
+
+
+
+
+
       // get desired page state (or use defaults)
       // instantiate a widget
     },    
