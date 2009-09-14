142c1cd7d2d0a70d68f32085e8a4419d7ab88b09
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-09-14 12:38:47 +0000

    cleanup

--- PhEDExWeb/ApplicationServer/js/phedex-event.js
+++ PhEDExWeb/ApplicationServer/js/phedex-event.js
@@ -2,7 +2,6 @@ PHEDEX.namespace('Event');
 
 PHEDEX.Event = {
     onWidgetDestroy:   new YAHOO.util.CustomEvent('onWidgetDestroy',   this, false, YAHOO.util.CustomEvent.LIST),
-
     onFilterValidated: new YAHOO.util.CustomEvent('onFilterValidated', this, false, YAHOO.util.CustomEvent.LIST),
     onFilterAccept:    new YAHOO.util.CustomEvent('onFilterAccept',    this, false, YAHOO.util.CustomEvent.LIST),
     onFilterCancel:    new YAHOO.util.CustomEvent('onFilterCancel',    this, false, YAHOO.util.CustomEvent.LIST),
