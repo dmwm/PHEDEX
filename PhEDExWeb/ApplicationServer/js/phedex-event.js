e37f64b7cb7c8d3132f0da76d5adfedb80a9488b
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-09-30 14:13:57 +0000

    add events for global-filter to widget communication

--- PhEDExWeb/ApplicationServer/js/phedex-event.js
+++ PhEDExWeb/ApplicationServer/js/phedex-event.js
@@ -2,8 +2,16 @@ PHEDEX.namespace('Event');
 
 PHEDEX.Event = {
     onWidgetDestroy:          new YAHOO.util.CustomEvent('onWidgetDestroy',         this, false, YAHOO.util.CustomEvent.LIST),
-    onFilterValidated:        new YAHOO.util.CustomEvent('onFilterValidated',       this, false, YAHOO.util.CustomEvent.LIST),
+    onListWidgets:            new YAHOO.util.CustomEvent('onListWidgets',           this, false, YAHOO.util.CustomEvent.LIST),
+
+    onFilterDefined:          new YAHOO.util.CustomEvent('onFilterDefined',         this, false, YAHOO.util.CustomEvent.LIST),
+
+    onWidgetFilterCancelled:  new YAHOO.util.CustomEvent('onWidgetFilterCancelled', this, false, YAHOO.util.CustomEvent.LIST),
+    onGlobalFilterCancelled:  new YAHOO.util.CustomEvent('onGlobalFilterCancelled', this, false, YAHOO.util.CustomEvent.LIST),
+
+    onWidgetFilterValidated:  new YAHOO.util.CustomEvent('onGlobalFilterValidated', this, false, YAHOO.util.CustomEvent.LIST),
     onGlobalFilterValidated:  new YAHOO.util.CustomEvent('onGlobalFilterValidated', this, false, YAHOO.util.CustomEvent.LIST),
-    onFilterCancel:           new YAHOO.util.CustomEvent('onFilterCancel',          this, false, YAHOO.util.CustomEvent.LIST),
-    onFilterDefinition:       new YAHOO.util.CustomEvent('onFilterDefinition',      this, false, YAHOO.util.CustomEvent.LIST),
+
+    onWidgetFilterApplied:    new YAHOO.util.CustomEvent('onWidgetFilterApplied',   this, false, YAHOO.util.CustomEvent.LIST),
+    onGlobalFilterApplied:    new YAHOO.util.CustomEvent('onGlobalFilterApplied',   this, false, YAHOO.util.CustomEvent.LIST),
 }
