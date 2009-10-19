e44e59b6c3f315d88ed03e9845e46bcbe7a63718
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-10-19 16:11:36 +0000

    somewhat cleaner, now loads modules on-demand.

--- PhEDExWeb/ApplicationServer/js/phedex-core-app.js
+++ PhEDExWeb/ApplicationServer/js/phedex-core-app.js
@@ -1,44 +1,83 @@
 PHEDEX.namespace('Core');
 
 PHEDEX.Core.App = function(sandbox) {
-  var _modules = [];
+  var _modules = [], _loaded = [];
   var _sbx = sandbox;
   var _parent = document.getElementById('phedex-main');
   if ( !parent ) { throw new Error('cannot find parent element for core'); }
+
+  var moduleHandler = function(obj) {
+    return function(ev,arr) {
+      var action = arr[0];
+      var who = arr[1];
+      log('Core: event='+ev+' module='+who+' has just done '+action);
+      var m = _modules[ev].obj;
+      switch ( action ) {
+        case 'ReadyForAction': {
+          log('calling "'+ev+'.initDom()"');
+          var el = m.initDom();
+          if ( el ) { _parent.appendChild(el); }
+          break;
+        }
+        case 'initDom': {
+          log('calling "'+ev+'.show()"');
+          m.show();
+          break;
+        }
+      };
+    }
+  }(this);
+
   return {
     dumpList: function() {
       for (var i in _modules) { log('dump of registered modules: "'+i+'"'); }
     },
 
     create: function() {
-      var onModuleCreate = function(obj) {
+      var onModuleExists = function(obj) {
 	return function(ev,arr) {
 	  var string = arr[0];
 	  var obj    = arr[1];
-	  if ( !_modules[string] ) { _modules[string]={obj:obj,state:'created'}; }
+	  if ( !_modules[string] ) { _modules[string]={obj:obj,state:'initialised'}; }
+	  if ( !_loaded[string] ) { _loaded[string]={}; }
+          _sbx.listen(string,moduleHandler);
 	}
       }(this);
-      _sbx.listen('moduleCreate',onModuleCreate);
-      _sbx.notify('registryCreate');
+      _sbx.listen('ModuleExists',onModuleExists);
+      _sbx.notify('CoreAppCreate');
     },
 
     createModule: function(name) {
-      var m = new PHEDEX.Core.Module(PxS,name);
-      m.connect();
+// N.B. createModule() can return _before_ the module is created, if it has to load it first. It will call itself again later.
+      if ( ! _loaded[name] ) {
+        var module = 'phedex-module-'+name.toLowerCase();
+        log ('loading "'+module+'" (for '+name+')');
+        PxL.load(function(obj) {
+          return function() {
+            log('module "'+name +'" loaded...');
+            _loaded[name] = {};
+            obj.createModule(name);
+          }
+        }(this), module);
+        return;
+      }
+      log ('creating a module "'+name+'"');
+      var m = new PHEDEX.Module[name](PxS,name);
+      m.init();
     },
 
-    init: function() {
+    initModule: function() {
       for (var i in _modules) {
 	log('initialise module: "'+i+'"');
         var el = PxU.makeChild(_parent, 'div', {});
-	_modules[i].obj.initObj(el);
+	_modules[i].obj.initModule(el);
       }
       log('all modules initialised');
     },
 
     send: function(ev,who,args) {
       log('send '+ev+' to module: "'+who+'"');
-      _sbx.notify('module',ev,who,args);
+      _sbx.notify('module',who,ev,args);
     },
   };
 }
