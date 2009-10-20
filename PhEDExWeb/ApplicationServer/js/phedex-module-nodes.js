3843177d0fc6b8652ced517b8611559364ce34be
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-10-20 14:39:55 +0000

    - minor code cleanup, better logging - use core to request data - receive data from core, update header with number of nodes

--- PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
@@ -5,14 +5,6 @@ PHEDEX.Module.Nodes = function(sandbox, string) {
   var _name = string;
   log('Module: creating a genuine "'+_name+'"');
 
-  onRegistryCreate = function(obj) {
-    return function() {
-      _sbx.notify('moduleCreate',_name);
-    }
-  }();
-
-  _sbx.listen('registryCreate',onRegistryCreate);
-  _sbx.notify('moduleCreate',_name);
   return {
     init: function() {
       log(_name+': initialising');
@@ -20,14 +12,26 @@ PHEDEX.Module.Nodes = function(sandbox, string) {
       this._initModule();
     },
     show: function() {
-      log(_name+': showing');
-      this.dom.header.innerHTML = 'this is a Nodes module...';
+      this.log('showing');
+      this.dom.title.innerHTML = 'Nodes...';
     },
     hide: function() {
-      log(_name+': hiding');
+      this.log(_name+': hiding');
     },
     destroy: function() {
-      log(_name+': destroying');
+      this.log(_name+': destroying');
     },
+    getData: function() {
+      this.log('Fetching data');
+      _sbx.notify( _name, 'getData', { api: 'nodes' } );
+    },
+    gotData: function(data) {
+      this.log('Got new data');
+      this.data = data.node;
+      this.fillHeader();
+    },
+    fillHeader: function(div) {
+      this.dom.title.innerHTML = 'Nodes: '+this.data.length+" found";
+    }
   };
 };
