eb805428074f5356d386a8dc4b085fa94eca5c99
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-10-21 15:54:47 +0000

    adopt new strategy for construction, plus some cosmetics. Oh, and actually build a data-table!

--- PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
@@ -2,36 +2,46 @@ PHEDEX.namespace('Module');
 PHEDEX.Module.Nodes = function(sandbox, string) {
 
   var _sbx = sandbox;
-  var _name = string;
-  log('Module: creating a genuine "'+_name+'"');
+  log('Module: creating a genuine "'+string+'"','info',string);
 
-  return {
-    init: function() {
-      log(_name+': initialising');
-      YAHOO.lang.augmentObject(this,new PHEDEX.Core.Module(_sbx,_name));
-      this._initModule();
-    },
-    show: function() {
-      this.log('showing');
-      this.dom.title.innerHTML = 'Nodes...';
-    },
-    hide: function() {
-      this.log(_name+': hiding');
-    },
-    destroy: function() {
-      this.log(_name+': destroying');
-    },
-    getData: function() {
-      this.log('Fetching data');
-      _sbx.notify( _name, 'getData', { api: 'nodes' } );
-    },
-    gotData: function(data) {
-      this.log('Got new data');
-      this.data = data.node;
-      this.fillHeader();
-    },
-    fillHeader: function(div) {
-      this.dom.title.innerHTML = 'Nodes: '+this.data.length+" found";
-    }
+   var _construct = function() {
+    return {
+      init: function() {
+	log('initialising','info',this._me);
+	this._initModule();
+      },
+      show: function() {
+	log('showing','info',this._me);
+	this.dom.title.innerHTML = 'Nodes...';
+      },
+      initData: function() {
+	log('initData','info',this._me);
+	this.buildTable(this.dom.content,
+                  [ {key:'ID',parser:YAHOO.util.DataSource.parseNumber },'Name','Kind','Technology','SE' ]
+                 );
+      },
+      hide: function() {
+	log('hiding','info',this._me);
+      },
+      destroy: function() {
+	log('destroying','info',this._me);
+      },
+      getData: function() {
+	log('Fetching data','info',this._me);
+	_sbx.notify( this._me, 'getData', { api: 'nodes' } );
+      },
+      gotData: function(data) {
+	log('Got new data','info',this._me);
+	this.data = data.node;
+	this.fillHeader();
+        this.fillDataSource(this.data);
+      },
+      fillHeader: function(div) {
+	this.dom.title.innerHTML = 'Nodes: '+this.data.length+" found";
+      }
+    };
   };
+  YAHOO.lang.augmentObject(this,new PHEDEX.Core.Module.DataTable(_sbx,string));
+  YAHOO.lang.augmentObject(this,_construct(),true);
+  return this;
 };
