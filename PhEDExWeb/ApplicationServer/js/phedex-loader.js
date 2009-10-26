269aa53b5b95f8585b4502e3ec263eee4d2dff5e
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-10-26 10:04:29 +0000

    absorb phedex-base.js into phedex-loader.js, for one-file bootstrapping

--- PhEDExWeb/ApplicationServer/js/phedex-loader.js
+++ PhEDExWeb/ApplicationServer/js/phedex-loader.js
@@ -1,8 +1,125 @@
+/**
+ * The PhEDEx Application.
+ * @module PHEDEX
+ */
+
+/**
+ * The PHEDEX global namespace object.
+ * @class PHEDEX
+ */
+PHEDEX= {}
+
+/** 
+ * Creates a namespace rooted from PHEDEX.
+ * @method namespace
+ * @param namespace {string} Namespace to create, optionally with  dot-notation, e.g. 'Appserv' or 'Base.Object'.
+ * @return {object} The namespace created.
+ */
+// For more information, see:
+//   http://yuiblog.com/blog/2007/06/12/module-pattern/
+PHEDEX.namespace = function() {
+    var a=arguments, o=null, i, j, d;
+    for (i=0; i<a.length; i=i+1) {
+        d=(""+a[i]).split(".");
+        o=PHEDEX;
+
+        // PHEDEX is implied, so it is ignored if it is included
+        for (j=(d[0] == "PHEDEX") ? 1 : 0; j<d.length; j=j+1) {
+            o[d[j]]=o[d[j]] || {};
+            o=o[d[j]];
+        }
+    }
+
+    return o;
+};
+
+/**
+ * Contains application globals.
+ * @namespace PHEDEX
+ * @class Appserv
+ */
+PHEDEX.Appserv = {};
+
+/**
+ * Sets the version of the application, using a string set by the RPM build or a default.
+ * @method makeVersion
+ * @namespace PHEDEX.Appserv
+ * @protected
+ * @return {string} The version string
+ */
+PHEDEX.Appserv.makeVersion = function() {
+  var version = '@APPSERV_VERSION@'; // set when RPM is built
+  return version.match(/APPSERV_VERSION/) ? '0.0.0' : version;
+};
+
+/**
+ * The version of the application.
+ * @property Version
+ * @type string
+ * @public
+ */
+PHEDEX.Appserv.Version = PHEDEX.Appserv.makeVersion();
+
+/**
+ * Base object for all graphical entities in the application.
+ * @namespace PHEDEX.Base
+ * @class Object
+ * @constructor
+ */
+PHEDEX.namespace('Base');
+PHEDEX.Base.Object = function() {
+  return {
+    /**
+     * Fired when the "extra" div has been populated.
+     * @event onFillExtra
+     */
+// TODO replace events with notifications...
+    onFillExtra: new YAHOO.util.CustomEvent("onFillExtra", this, false, YAHOO.util.CustomEvent.LIST),
+
+    /**
+     * Namespace for DOM elements managed by this object.
+     * @property dom
+     * @type object
+     * @protected
+     */
+    dom: {},
+
+    /**
+     * Namespace for control elements managed by this object.
+     * @property ctl
+     * @type object
+     * @protected
+     */
+    ctl: {},
+
+    /**
+     * Namespace for options in effect for this object.
+     * @property options
+     * @type object
+     * @protected
+     */
+    options: {},
+
+    /**
+     * Returns the class name of this object
+     * @method me
+     * @returns string
+     */
+    // TODO:  There must be a lower-level way to do this, using obj.constructor or simmilar
+//     me: function() { return this._me; },
+  };
+}
+
+/**
+ * on-demand loading of all phedex code and the YUI code it depends on
+ * @namespace PHEDEX.Loader
+ * @class Object
+ * @constructor
+ */
 PHEDEX.Loader = function(opts) {
   var _dependencies = {
     'phedex-css':     { type: 'css', fullpath: '/css/phedex.css' },
-    'phedex-base':    { requires: ['phedex-css'] },
-    'phedex-util':    { requires: ['phedex-base'] },
+    'phedex-util':    { requires: ['phedex-css'] },
     'phedex-datasvc': { requires: ['phedex-util','json'] },
 
 //  these are just guesses, and may not work as-is
