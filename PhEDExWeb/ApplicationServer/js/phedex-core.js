68ed60aee40ed67b39195d9e82d5f453c7140835
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-10-27 18:35:09 +0000

    add handler to create decorators, such as the "extra" control thingy

--- PhEDExWeb/ApplicationServer/js/phedex-core.js
+++ PhEDExWeb/ApplicationServer/js/phedex-core.js
@@ -39,11 +39,31 @@ PHEDEX.Core = function(sandbox) {
   var CreateModule = function(ev,arr) {
 // create a module and call its init() function. It is expected that the module will notify the sandbox when it is up, and
 // the core will handle it through the moduleHandler function after that.
-    var name = arr[0];
+    var name = PxU.initialCaps(arr[0]);
     log ('creating a module "'+name+'"','info',_me);
-    var m = new PHEDEX.Module[name](PxS,name);
+    try {
+      var m = new PHEDEX.Module[name](PxS,name);
+    } catch(ex) { log(ex,'error',_me); banner("Failed to construct an instance of '"+name+"'!"); }
     m.init(_global_options);
-  };
+    var _m=[],
+        _d=m.decorators;
+    for (var i in _d) {
+      if ( _d[i].module ) { _m.push(_d[i].module); }
+      if ( _m.length ) {
+//      Load the decorators first, then notify when ready...
+        log('loading decorators','info','Core');
+        PxL.load( {
+          Success: function() {
+            _sbx.notify(m.id,'decoratorsLoaded');
+          },
+          Progress: function(item) { banner('Loaded item: '+item.name); }
+        }, _m);
+      } else {
+//      nothing needs loading, I can notify the decorators directly
+        _sbx.notify(m.id,'decoratorsLoaded');
+      }
+    }
+  }
 
   var _moduleExists = function(obj) {
     return function(ev,arr) {
@@ -66,50 +86,72 @@ PHEDEX.Core = function(sandbox) {
       _clearTimeout();
       switch ( action ) {
         case 'init': {
-	  var el = m.initDom();
-	  if ( el ) { _parent.appendChild(el); }
-	  else { log('module "'+name+'" did not return a DOM element?','warn',_me); }
-	  m.initModule();
-	  m.initData();
+          var el = m.initDom();
+          if ( el ) { _parent.appendChild(el); }
+          else { log('module "'+name+'" did not return a DOM element?','warn',_me); }
+          m.initModule();
+          m.initData();
           break;
         }
         case 'initData': {
           log('calling "'+who+'.show()"','info',_me);
           m.show();
-	  m.getData();
+          m.getData();
           break;
         }
-	case 'destroy': {
-	  _modules[who] = {};
-	  break;
-	}
+        case 'destroy': {
+          _modules[who] = {};
+          break;
+        }
+        case 'decoratorsLoaded': {
+          for (var i in m.decorators) {
+            var d = m.decorators[i];
+//          deduce the constructor from the module name. 'phedex-abc-def-ghi' -> PHEDEX.Abc.Def.Ghi()
+            var x = d.module;
+            var _constructor;
+            x.match('^phedex-(.+)$');
+            x = RegExp.$1;
+            _constructor = PHEDEX;
+            while ( x ) {
+              x.match('^([^-]+)(-(.+))?$');
+              var a=PxU.initialCaps(RegExp.$1), b=RegExp.$3;
+              _constructor = _constructor[PxU.initialCaps(RegExp.$1)];
+              x = RegExp.$3;
+            }
+            setTimeout(function() {
+                                    m.ctl[d.name] = new _constructor(_sbx,d);
+                                    m.dom[d.parent].appendChild(m.ctl[d.name].el);
+                                  },0);
+          };
+          break;
+        }
         case 'getData': {
           log('fetching data for "'+who+'"','info',_me);
-	  try {
-	    banner('Connecting to data-service...');
-	    var dataReady = new YAHOO.util.CustomEvent("dataReady", this, false, YAHOO.util.CustomEvent.LIST);
-	    dataReady.subscribe(function(type,args) {
-	      return function(_m) {
-		banner('Data-service returned OK...')
-		var data = args[0],
-		    context = args[1],
-		    api = context.api;
-		try {
-		  _m.gotData(data);
-		} catch(ex) { log(ex,'error',who); banner('Error processing data!'); }
-	      }(m);
-	    });
-	    var dataFail = new YAHOO.util.CustomEvent("dataFail",  this, false, YAHOO.util.CustomEvent.LIST);
-	    dataFail.subscribe(function(type,args) {
-	      var api = args[1].api;
-	      log('api:'+api+' error fetching data','error',who);
-	      banner('Error fetching data: '+api+' '+args[0].message+'!');
-	      _clearTimeout();
-	    });
-	    args.success_event = dataReady;
-	    args.failure_event = dataFail;
-	    PHEDEX.Datasvc.Call( args );
-	  } catch(ex) { log(ex,'error',_me); banner('Error fetching data!'); }
+          try {
+            banner('Connecting to data-service...');
+            var dataReady = new YAHOO.util.CustomEvent("dataReady", this, false, YAHOO.util.CustomEvent.LIST);
+            dataReady.subscribe(function(type,args) {
+              return function(_m) {
+                banner('Data-service returned OK...')
+                var data = args[0],
+                    context = args[1],
+                    api = context.api;
+                try {
+                  _m.gotData(data);
+                } catch(ex) { log(ex,'error',who); banner('Error processing data!'); }
+              }(m);
+            });
+            var dataFail = new YAHOO.util.CustomEvent("dataFail",  this, false, YAHOO.util.CustomEvent.LIST);
+            dataFail.subscribe(function(type,args) {
+              var api = args[1].api;
+              log('api:'+api+' error fetching data','error',who);
+              banner('Error fetching data: '+api+' '+args[0].message+'!');
+              _clearTimeout();
+            });
+            args.success_event = dataReady;
+            args.failure_event = dataFail;
+            PHEDEX.Datasvc.Call( args );
+          } catch(ex) { log(ex,'error',_me); banner('Error fetching data!'); }
           break;
         }
       };
