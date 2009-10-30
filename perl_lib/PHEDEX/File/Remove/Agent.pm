ef3200f05649165b8344450f29945b22af99fae1
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2009-10-30 11:38:54 +0000

    - No longer juggle WAITTIME between WAITTIME_SLOW and WAITTIME_FAST.   Now there is only WAITTIME, which is a fast cycle for the local work   queue and does no DB interactions, and SYNCTIME, which is slow and   writes results/fetches new work from the database. - Logically delete files (remove replicas) while the drop is still in   the inbox.  Set "go" for work after logically deleting. - Physically delete (issue commands) for files which are in the   WORKDIR.  Mark "report" in the dropbox for files we are done with,   otherwise they will be retried. - In &report_work_done, iterate through WORKDIR looking for files to   report.  Write their completion time to the database. - New logline format "dstats", following FileDownload "xstats" naming   conventions and reporting some useful latencies. - Make TIMEOUT configurable.

--- perl_lib/PHEDEX/File/Remove/Agent.pm
+++ perl_lib/PHEDEX/File/Remove/Agent.pm
@@ -4,6 +4,7 @@ use warnings;
 use base 'PHEDEX::Core::Agent';
 use File::Path;
 use Data::Dumper;
+use POE;
 
 use PHEDEX::Core::Command;
 use PHEDEX::Core::Timing;
@@ -19,14 +20,14 @@ sub new
 
     my %params = (DBCONFIG => undef,		# Database configuration file
 	  	  NODES    => undef,		# Node names to run this agent for
-		  WAITTIME => 60 + rand(15),	# Agent activity cycle
-		  WAITTIME_SLOW => undef,	# for slow-cycling, when there's no work to do
-		  WAITTIME_FAST => undef,	# for fast-cycling, when there's still more to do
+		  WAITTIME => 15,               # Work cycle
+		  SYNCTIME => 600 + rand(120),	# Agent DB sync time
 		  CMD_RM   => undef,            # cmd to remove physical files
 		  PROTOCOL => 'direct',         # File access protocol
 		  CATALOGUE => {},		# TFC from TMDB
-		  LIMIT => 100,                 # Max number of files per cycle
+		  LIMIT => 5000,                # Max number of deletions to consider at once
 		  JOBS	=> 1,			# Max parallel delete jobs
+		  TIMEOUT => 30,                # Timout for deletion jobs
 		  RETRY => 1,			# Retry failed attempts, forever!
 		  );
     
@@ -42,32 +43,179 @@ sub new
 						VERBOSE	=> $self->{VERBOSE},
 						DEBUG	=> $self->{DEBUG},
 							);
-
+    
     # Handle signals
     $SIG{INT} = $SIG{TERM} = sub { $self->{SIGNALLED} = shift;
 				   $self->{JOBMANAGER}->killAllJobs() };
 
-#   Finagle the WAITTIME so I can speed up deletions if the queue is full
-    $self->{WAITTIME_SLOW} = $self->{WAITTIME} unless $self->{WAITTIME_SLOW};
-    $self->{WAITTIME_FAST} = 2                 unless $self->{WAITTIME_FAST};
-
     bless $self, $class;
     return $self;
+}
+
+sub init
+{
+    my $self = shift;
+    $self->SUPER::init(@_);
+    
+    # Remove all pending flags from the workdir
+    foreach my $drop ($self->readPending()) {
+	my $qfile = "$$self{WORKDIR}/$drop/queued";
+	unlink $qfile if -f $qfile;
+    }
+}
+
+
+sub _poe_init
+{
+    my ($self, $kernel, $session) = @_[ OBJECT, KERNEL, SESSION ];
+    my @poe_subs = qw( sync_deletions );
+    $kernel->state($_, $self) foreach @poe_subs;
+
+    # Get periodic events going
+    $kernel->yield('sync_deletions');
+}
+
+# Pick up work from the database.
+sub sync_deletions
+{
+  my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
+  $kernel->delay_set('sync_deletions', $self->{SYNCTIME});
+  eval {
+      my $dbh = $self->connectAgent();
+      $self->expandNodes();
+      my ($npending, $ndone) = $self->report_work_done();
+      my $nfetched = $self->fetch_deletions($npending);
+      my $ndeleted = $self->logically_delete();
+      $dbh->commit();
+      $self->Logmsg("synced with database: got $nfetched new files, $npending pending, $ndone finished");
+      $self->disconnectAgent();
+  };
+  $self->rollbackOnError();
+#  &output ("$$self{DROPDIR}/".++$$self{DUMPSELF}, Dumper ($self)); # XXX DEBUG
+}
+
+# Get a list of files to delete and write them to our inbox
+sub fetch_deletions
+{
+    my ($self, $npending) = @_;
+    my $dbh = $self->{DBH};
+    my $limit = $self->{LIMIT};
+    my $now = &mytimeofday();
+
+    # Get the list of files from t_xfer_delete.  Take the earliest
+    # deletion requests first, order by block in order to optimize
+    # block retransfers
+    my ($node_filter, %node_args) = $self->myNodeFilter ("xd.node");
+    my $q = &dbexec($dbh,qq{
+	select xd.node nodeid, n.name node, xd.time_request, 
+	       xd.fileid, xf.logical_name lfn
+	 from t_xfer_delete xd
+	 join t_xfer_file xf on xd.fileid = xf.id
+         join t_adm_node n on n.id = xd.node
+	where xd.time_complete is null 
+	  and $node_filter
+	order by xd.time_request asc, xd.node, xf.inblock asc
+    }, %node_args);
+
+    # Prepare catalogs for LFN->PFN translation
+    foreach my $node (keys %{$self->{NODES_ID}}) {
+	&dbStorageRules($self->{DBH},
+			$self->{CATALOGUE},
+			$self->{NODES_ID}{$node});
+    }
+
+    my $new_deletions = 0;
+    while (my $f = $q->fetchrow_hashref())
+    {
+	# Check that the file not already pending deletion
+	next if -d "$$self{WORKDIR}/$$f{FILEID}";
+
+	# Check that we're not over the limit
+	if ($new_deletions + $npending >= $limit) {
+	    $self->Warn("reached local queue limit ($self->{LIMIT}), not queuing more");
+	    last;
+	}
+
+	# LFN -> PFN
+	my $node = $f->{NODEID};
+	my $lfn =  $f->{LFN};
+	my ($tkn, $pfn) = &applyStorageRules($self->{CATALOGUE}{$node},
+					     $self->{PROTOCOL},
+					     $node,
+					     'pre',
+					     $lfn,
+					     'n' # $IS_CUSTODIAL
+					     );
+	do { $self->Alert ("no pfn for $lfn, skipping"); next } if ! $pfn;
+
+	# Augment the file object
+	$f->{PFN} = $pfn;
+	$f->{TIME_RECEIVE} = &mytimeofday();
+	
+	# Write the object to our inbox
+	my $dropdir = $self->write_inbox($f);
+	$new_deletions++ if $dropdir;
+    }
+    $q->finish();  # free resources in case we didn't use all results
+    return $new_deletions;   # return the number of dropdirs we created
+}
+
+# Create a drop for deleting a file.  We create a drop for ourselves,
+# i.e. in our own inbox, and then process the file in "processDrop".
+# This way we have a permanent record of where we are with deleting
+# the file, in case we have to give up some operation for temporary
+# failures.
+sub write_inbox
+{
+    my ($self, $file) = @_;
+
+    # Create a pending drop in my inbox
+    my $dropdir = "$$self{INBOX}/$$file{FILEID}";
+    do { $self->Alert ("$dropdir already exists"); return 0; } if -d $dropdir;
+    do { $self->Alert ("failed to submit $$file{FILEID}"); &rmtree ($dropdir); return 0; }
+	if (! &mkpath ($dropdir)
+	    || ! &output ("$dropdir/packet", Dumper ($file))
+	    || ! &touch ("$dropdir/go.pending"));
+
+    $self->Dbgmsg("created drop $dropdir for lfn=$file->{LFN}") if $self->{DEBUG};
+    return $dropdir;
+}
+
+# Iterate through all drops in the inbox and logically delete them,
+# then mark them as ready for processing
+sub logically_delete
+{
+    my ($self) = @_;
+    
+    my $ndeleted = 0;
+    foreach my $drop ($self->readInbox(1)) { # return all folders in inbox
+	my $dropdir = "$$self{INBOX}/$drop";
+	my $file = $self->loadPacket($drop, $self->{INBOX});
+
+	# Try to logically delete the file
+	next unless $self->delete_replica($file);
+
+	# Mark the drop ready for processing
+	if (! &mv ("$dropdir/go.pending", "$dropdir/go")) {
+	    $self->Alert ("failed to mark drop $dropdir ready to go, deleting it");
+	    &rmtree($dropdir);
+	    next;
+	}
+	$ndeleted++;
+    }
+    return $ndeleted;
 }
 
 # Delete a file.  We do one step at a time; if the step fails, we just
 # tell the caller to come back here again at a more opportune time.
 # The steps are ordered such that they are safe to execute several
 # times if we have to give for one reason or another.
-sub deleteOneFile
+sub delete_replica
 {
-    my ($self, $drop, $file) = @_;
-    my $dbh = undef;
-    my @nodes = ();
-
+    my ($self, $file) = @_;
     my $status = eval {
-	$dbh = $self->connectAgent();
-	@nodes = $self->expandNodes();
+	my $dbh = $self->connectAgent();
+	my @nodes = $self->expandNodes();
 
 	# Get Buffer Node if this is for an MSS node.  We will check
 	# transfers for the buffer node along with the MSS node, and
@@ -84,12 +232,11 @@ sub deleteOneFile
 	    $node_binds{':node2'} = $buffer_id if $buffer_id;
 	}
 	my $node_list = join(',', keys %node_binds);
-
 	
 	# Make sure the file is still safe to delete.  More transfers
 	# out might have been created for this file while we were not
 	# minding this particular file (sleeping or deleting things).
-	my ($npending) = &dbexec($dbh, qq{
+	my ($nxfer) = &dbexec($dbh, qq{
 	    select count(fileid) from t_xfer_task xt
  	     where xt.from_node in ($node_list)
 	      and xt.fileid = :fileid },
@@ -97,248 +244,156 @@ sub deleteOneFile
 	    ":fileid" => $$file{FILEID})
 	    ->fetchrow();
 
-	if ($npending)
+	if ($nxfer)
 	{
-	    $self->Warn ("not removing $$file{LFN}, $npending pending transfers") if !$self->{_drops}{$drop};
+	    $self->Warn ("not removing $$file{LFN}, $nxfer pending transfers");
 	    return 0;
 	}
 
-	$self->{_drops}{$drop}++;
 	# Now delete the replica entry to avoid new transfer edges.
 	&dbexec($dbh, qq{
 	    delete from t_xfer_replica where fileid = :fileid and node in ($node_list)},
 	    ":fileid" => $$file{FILEID}, %node_binds);
 
-	$$file{DROP} = $drop; # We will need this in the JobManager callback
-
-	# Issue file removal from disk now.
-	my $log = "$$self{DROPDIR}/$$file{NODEID}.$$file{FILEID}.log";
-	$self->{JOBMANAGER}->addJob( sub { $self->deleteJob ($file, @_) },
-		       { TIMEOUT => 30, LOGFILE => $log, DB => $dbh },
-		       (@{$$self{CMD_RM}}, 'post', $$file{PFN}) );
-	return 1;
+	return 1; # exit eval
     };
+    $self->rollbackOnError();
 
-    do { chomp ($@); $self->Alert ("database error: $@");
-	 eval { $dbh->rollback() } if $dbh;
-	 $status = 0 } if $@;
-    
     # Return status code to caller
     return $status;
 }
 
+sub loadPacket
+{
+    my ($self, $drop, $dir) = @_;
+    $dir ||= $$self{WORKDIR};
+    my $dropdir = "$dir/$drop";
+
+    # Read back file information
+    my $file = do { no strict "vars"; eval &input ("$dropdir/packet") };
+    if ($@ || !$file || !$$file{FILEID} || !$$file{LFN} || !$$file{PFN} || !$$file{TIME_RECEIVE})
+    {
+	$self->Alert ("corrupt packet in $drop");
+	$self->markBad ($drop);
+	return undef;
+    }
+    return $file;
+}
+
+# Process a drop directory.  Called form PHEDEX::Core::Agent::process
+# with a period of WAITTIME. Triggers the physical deletion if it hasn't already
+# been queued.
+sub processDrop
+{
+    my ($self, $drop) = @_;
+    my $dropdir = "$$self{WORKDIR}/$drop";
+    &timeStart($$self{STARTTIME});
+
+    # Sanity checking
+    return if (! $self->inspectDrop ($drop));
+    delete $$self{BAD}{$drop};
+
+    my $file = $self->loadPacket($drop) || return 0;
+
+    # Queue deletion command, if not already queued
+    if (!(-f "$dropdir/queued" || -f "$dropdir/report")) {
+	$self->Dbgmsg("queuing drop $dropdir for lfn=$file->{LFN}") if $self->{DEBUG};
+	my $log = "$dropdir/log";
+	$self->{JOBMANAGER}->addJob( sub { $self->deleteJob ($drop, @_) },
+		       { TIMEOUT => $$self{TIMEOUT}, LOGFILE => $log },
+		       (@{$$self{CMD_RM}}, 'post', $$file{PFN}) );
+	&touch ("$dropdir/queued");
+    }
+}
+
+# Callback from JobManager job.  Handles success/failure of a deletion.
 sub deleteJob
 {
-    my ($self, $file, $job) = @_;
-    if ($$job{STATUS})
+    my ($self, $drop, $job) = @_;
+    my $dropdir = "$$self{WORKDIR}/$drop";
+    unlink "$dropdir/queued";  # no longer waiting for this file
+    my $success = $$job{STATUS_CODE} ? 0 : 1;
+    my $file = $self->loadPacket($drop) || return 0;
+
+    if (!$success)
     {
 	$self->Warn("failed to delete file $$file{PFN} at node "
 	      ."$$file{NODE}, log in $$job{LOGFILE}");
     }
-    if ( !$job->{STATUS} || !$self->{RETRY} )
+    if ( $success || !$self->{RETRY} )
     {
-        if ( !$job->{STATUS} )
+        if ( $success )
         {
 	    $self->Logmsg("deleted file $$file{PFN} at node $$file{NODE}");
-	    unlink ($$job{LOGFILE});
 	}
-        if ( $job->{STATUS} && !$self->{RETRY} )
+        if ( !$success && !$self->{RETRY} )
         {
-	    $self->Logmsg("giving up on file $$file{PFN} at node $$file{NODE}");
+	    $self->Warn("giving up on file $$file{PFN} at node $$file{NODE}");
 	}
-        delete $self->{_drops}{$file->{DROP}};
 
-        my $dbh = undef;
-        my $status = eval {
-	  $dbh = $self->connectAgent();
-#	  @nodes = $self->expandNodes();
-	  # Report completition time to DB. If the physical deletion fails,
-	  # we will roll back.
-	  my $now = &mytimeofday();
-	  &dbexec($dbh, qq{
-	    update t_xfer_delete set time_complete = :now
-		where fileid = :fileid and node = :node},
-		":fileid" => $$file{FILEID}, ":node" => $$file{NODEID}, ":now" => $now);
-        };
-	$dbh->commit(); # don't hold this row lock for long.
+	$self->Dbgmsg("deletion done for drop $dropdir for lfn=$file->{LFN}") if $self->{DEBUG};
+	&output("$dropdir/time-done", $$job{FINISH}) # record the completion time
+	    && &touch("$dropdir/report");            # set the report-to-database flag
+    }
 
-        do { chomp ($@); $self->Alert ("database error: $@");
-	  eval { $dbh->rollback() } if $dbh;
-	  return; } if $@;
+    # Log deletion statistics
+    $self->Logmsg ("dstats:",
+		   " status-code=", $$job{STATUS_CODE},
+		   " node=$$file{NODE}",
+		   " file=$$file{FILEID}",
+		   " lfn=$$file{LFN}",
+		   " pfn=$$file{PFN}",
+		   " t-request=$$file{TIME_REQUEST}",
+		   " t-receive=$$file{TIME_RECEIVE}",
+		   " t-cmdstart=$$job{START}",
+		   " t-done=$$job{FINISH}",
+		   " d-cmd=",   sprintf("%0.3f", $$job{FINISH} - $$job{START}),
+		   " d-local=", sprintf("%0.3f", $$job{FINISH} - $$file{TIME_RECEIVE}),
+		   " d-total=", sprintf("%0.3f", $$job{FINISH} - $$file{TIME_REQUEST})
+		   );
+}
 
-	# Mark drop done so it will be nuked
-        my $dropdir = "$$self{WORKDIR}/$$file{DROP}";
+# Iterate through completed deletions in the work directory, and
+# report completed deletions to the database
+sub report_work_done
+{
+    my ($self) = @_;
+    my $ndone = 0;
+    my $npending = 0;
+    foreach my $drop ($self->readPending()) {
+	$npending++;
+	my $dropdir = "$$self{WORKDIR}/$drop";
+	next unless -f "$dropdir/report"; # check for the report flag
+	my $file = $self->loadPacket($drop);
+
+	# Report completition time to DB
+	eval {
+	    my $dbh = $self->connectAgent();
+	    my $time_complete = &input("$dropdir/time-done") || &mytimeofday();
+	    &dbexec($dbh, qq{
+		update t_xfer_delete 
+		    set time_complete = :time_complete
+		    where fileid = :fileid and node = :node },
+		    ":fileid" => $$file{FILEID},
+		    ":node" => $$file{NODEID},
+		    ":time_complete" => $time_complete );
+	};
+	$self->rollbackOnError() && next;
+	
+	# We are done with this drop
+	$self->Dbgmsg("deleting drop $dropdir for lfn=$file->{LFN}") if $self->{DEBUG};
 	&touch ("$dropdir/done");
-
-	# Log transfer delay stats
-	my $dtransfer = &mytimeofday() - $$file{TIME_START};
-	$self->Logmsg ("xstats: $$file{NODE} " . sprintf('%.2fs', $dtransfer)
-	     . " $$file{LFN} => $$file{PFN}");
-
-	# OK, got far enough to nuke and log it
-	$self->Logmsg("Delete drop $file->{DROP} ($file->{LFN})") if $self->{DEBUG};
-	$self->relayDrop ($$file{DROP});
+	$self->relayDrop ($drop);
+	$ndone++;
     }
+    return ($npending - $ndone, # number of pending jobs
+	    $ndone);            # number done, reported
 }
 
-sub processDrop
-{
-    my ($self, $drop) = @_;
-    # Sanity checking
-    return if (! $self->inspectDrop ($drop));
-    delete $$self{BAD}{$drop};
-    &timeStart($$self{STARTTIME});
-
-    # Read back file information
-    my $dropdir = "$$self{WORKDIR}/$drop";
-    my $file = do { no strict "vars"; eval &input ("$dropdir/packet") };
-    if ($@ || !$file || !$$file{FILEID} || !$$file{LFN} || !$$file{PFN} || !$$file{TIME_START})
-    {
-	$self->Alert ("corrupt packet in $drop");
-	$self->markBad ($drop);
-	return;
-    }
-
-    # Try deleting this file.  If something fails, keep this drop as
-    # is, we'll come back to it later.
-    # Add the drop to the file, so the callback from the JobManager
-    # can delete the drop if the file-delete succeeds
-    $self->deleteOneFile ($drop, $file);
-}
-
-# Get a list of files to delete.
-sub filesToDelete
-{
-    my ($self, $dbh, $limit, $node, $pending) = @_;
-
-    my $now = &mytimeofday();
-
-    # Find all the files that we are allowed to delete: 
-    # Get the list of files from t_xfer_delete
-    # We take the files oldest first.
-    my @result;
-    my %files = ();
-    my $q = &dbexec($dbh,qq{
-	select xd.fileid, xf.logical_name
-	from t_xfer_delete xd
-	join t_xfer_file xf
-	on xd.fileid = xf.id
-	where xd.time_complete is null and xd.node = :node
-	order by xd.time_request asc},
-        ":node" => $$self{NODES_ID}{$node});
-    while (my ($id, $lfn) = $q->fetchrow())
-    {
-	next if grep ($_ eq $id, @{$pending});
-	$files{$lfn} = $id;
-	if ( scalar keys %files >= $limit )
-	{
-	  $self->{WAITTIME} = $self->{WAITTIME_FAST};
-	  last;
-	}
-    }
-    $q->finish();  # free resources in case we didn't use all results
-
-    # Now get PFNs for all those files.
-    my $cat = dbStorageRules($self->{DBH},
-			     $self->{CATALOGUE},
-			     $self->{NODES_ID}{$node});
-    my $pfns;
-    foreach ( keys %files )
-    {
-      $pfns->{$_} = [applyStorageRules
-                      (
-                        $cat,
-                        $self->{PROTOCOL},
-                        $self->{NODES_ID}{$node},
-                        'pre',
-                        $_,
-                        'n', # $IS_CUSTODIAL
-                      )];
-    }
-
-    while (my ($lfn, $pfn2) = each %$pfns)
-    {
-        my $pfn = $pfn2->[1];
-        my $space_token = $pfn2->[0];
-	do { $self->Alert ("no pfn for $lfn"); next } if ! $pfn;
-	push (@result, { LFN => $lfn, PFN => $pfn, FILEID => $files{$lfn},
-			 NODEID => $$self{NODES_ID}{$node}, NODE => $node,
-			 TIME_START => &mytimeofday() });
-    }
-
-    return @result;
-}
-
-# Create a drop for deleting a file.  We create a drop for ourselves,
-# i.e. in our own inbox, and then process the file in "processDrop".
-# This way we have a permanent record of where we are with deleting
-# the file, in case we have to give up some operation for temporary
-# failures.
-sub startOne
-{
-    my ($self, $file) = @_;
-
-    # Create a pending drop in my inbox
-    my $drop = "$$self{DROPDIR}/inbox/$$file{FILEID}";
-    do { $self->Alert ("$drop already exists"); return 0; } if -d $drop;
-    do { $self->Alert ("failed to submit $$file{FILEID}"); &rmtree ($drop); return 0; }
-	if (! &mkpath ($drop)
-	    || ! &output ("$drop/packet", Dumper ($file))
-	    || ! &touch ("$drop/go.pending"));
-
-    # OK, kick it go
-    $self->Warn ("failed to mark $$file{FILEID} ready to go")
-	if ! &mv ("$drop/go.pending", "$drop/go");
-
-    return 1;
-}
-
-# Pick up work from the database.
 sub idle
 {
     my ($self, @pending) = @_;
-    my $dbh = undef;
-    my @nodes = ();
-
-    $self->{WAITTIME} = $self->{WAITTIME_SLOW};
-
-    my $count=0;
-    eval
-    {
-	$dbh = $self->connectAgent();
-	@nodes = $self->expandNodes();
-
-	#   If I am already swamped with work, don't get more!
-	if ( $self->{JOBMANAGER}->jobsQueued() > $self->{LIMIT} )
-	{
-	    $self->Warn("number of queued jobs (",$self->{JOBMANAGER}->jobsQueued(),
-			") exceeds work-limit per cycle (",$self->{LIMIT},"). Waiting...");
-	    return; # exit eval block
-	}
-
-	# Get a list of victims to evict.
-	foreach my $node (@nodes)
-	{
-	    foreach my $file ($self->filesToDelete ($dbh, $$self{LIMIT}, $node, \@pending))
-	    {
-		# If we are already processing this file, ignore it
-		next if grep ($_ eq $$file{FILEID}, @pending);
-		
-		# Otherwise initiate destruction and doom
-		$self->startOne ($file);
-		$count++;
-	    }
-	    # Intermediate commit after having dealt with all files at a node
-	    $dbh->commit();
-	}
-    };
-    do { chomp ($@); $self->Alert ("database error: $@");
-	 eval { $dbh->rollback() } if $dbh } if $@;
-    $self->{WAITTIME} = $self->{WAITTIME_SLOW} unless $count;
-    $self->Logmsg("Started $count jobs with " . scalar @pending . " pending") if $count;
-
-    # Disconnect from the database
-    $self->{JOBMANAGER}->whenQueueDrained( sub { $self->disconnectAgent(); } );
+    # nothing to do
 }
 
 sub reloadConfig
@@ -346,7 +401,7 @@ sub reloadConfig
   my ($self,$Config) = @_;
   my $config = $Config->select_agents($self->{LABEL});
   my $val;
-  foreach ( qw / LIMIT VERBOSE DEBUG WAITTIME WAITTIME_SLOW WAITTIME_FAST / )
+  foreach ( qw / LIMIT VERBOSE DEBUG WAITTIME / )
   {
     next unless defined ($val = $config->{OPTIONS}{$_});
     $self->Logmsg("reloadConfig: set $_=$val");
