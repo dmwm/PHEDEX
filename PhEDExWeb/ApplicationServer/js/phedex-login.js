8df2b3fbfd1c4f17c3ec8ef8a6f8e82aafd6f14e
psundarr <sha1-ca6aca0cd13efc276a617890434a29b0e8b6b4d8@cern.ch>
2009-11-11 06:40:47 +0000

    Implemented changes to login component to display the authentication info to user depending on type

--- PhEDExWeb/ApplicationServer/js/phedex-login.js
+++ PhEDExWeb/ApplicationServer/js/phedex-login.js
@@ -6,37 +6,61 @@ PHEDEX.Login = (function() {
     var PxU = PHEDEX.Util;
     var PxD = PHEDEX.Datasvc;
 
+    //_mode decides the mode of authentication
+    //_mode = 1 indicates password
+    //_mode = 2 indicates certificate
+    var _mode = 2; //default is password authentication
     var _username = "";
-    var _pwd = "";
     var _cur_state = "login";
 
     var _initLoginComponent = function(divlogin) {
         var logincomp = PxU.makeChild(divlogin, 'div', { id: 'phedex-nav-login', className: 'phedex-login' });
-        var labelname = PxU.makeChild(logincomp, 'span');
+        var username = PxU.makeChild(logincomp, 'a', { className: 'phedex-login-username' });
+        var statusmsg = PxU.makeChild(logincomp, 'span', { className: 'phedex-login-status' });
+        var logininput = PxU.makeChild(logincomp, 'span');
+        var labelname = PxU.makeChild(logininput, 'span');
         labelname.innerHTML = "User Name: ";
-        var inputname = PxU.makeChild(logincomp, 'input', { type: 'text' });
-        labelname = PxU.makeChild(logincomp, 'span');
-        labelname.innerHTML = "&nbsp;&nbsp;Password: ";
-        var inputpwd = PxU.makeChild(logincomp, 'input', { type: 'password' });
+        var inputname = PxU.makeChild(logininput, 'input', { type: 'text' });
+        labelname = PxU.makeChild(logininput, 'span');
+        labelname.innerHTML = "&nbsp;Password: ";
+        var inputpwd = PxU.makeChild(logininput, 'input', { type: 'password' });
         var btnsubmit = PxU.makeChild(logincomp, 'span');
-        var statusmsg = PxU.makeChild(logincomp, 'div', { className: 'phedex-login-status' });
 
-        var processlogin = function(data) {
+        var _showUserInfo = function() { //Show user info when user name link is clicked
+            alert("User Info function not yet implemented");
+        };
+
+        YAHOO.util.Event.addListener(username, 'click', _showUserInfo, this, true);
+        var _processLogin = function(data) {
             var bsucceed = true;
             if (bsucceed) {
                 _updateLoginButton("Log Out");
-                statusmsg.innerHTML = inputname.value + " logged in via cert";
+                _username = inputname.value;
+                username.innerHTML = _username;
+                statusmsg.innerHTML = " logged in via password";
                 inputpwd.value = "";
                 _cur_state = 'logout';
+                YAHOO.util.Dom.addClass(logininput, 'phedex-invisible'); //To Hide the element
             }
             else {
+                username.innerHTML = "";
                 statusmsg.innerHTML = "";
                 _cur_state = 'login';
                 alert("Login failed. Please enter correct username and password.");
             }
         };
 
-        var onLogin = function(event) {
+        var _loginCallFailure = function(data) {
+            alert("Login check failed. Please try again.");
+            return;
+        }
+
+        var _eventSuccess = new YAHOO.util.CustomEvent("event success");
+        var _eventFailure = new YAHOO.util.CustomEvent("event failure");
+
+        _eventSuccess.subscribe(function(type, args) { _processLogin(args[0]); });
+        _eventFailure.subscribe(function(type, args) { _loginCallFailure(args[0]); });
+        var _onLogin = function(event) {
             if (_cur_state == 'login') {
                 if (!inputname.value) {
                     alert("Please enter user name");
@@ -48,22 +72,44 @@ PHEDEX.Login = (function() {
                 }
                 _pwd = inputpwd.value;
                 _username = inputname.value;
-                PHEDEX.Datasvc.Call({ api: 'bounce', callback: processlogin });
+                PHEDEX.Datasvc.Call({ api: 'bounce', success_event: _eventSuccess, failure_event: _eventFailure });
             }
             else if (_cur_state == 'logout') {
+                YAHOO.util.Dom.removeClass(logininput, 'phedex-invisible'); //To Hide the element
                 inputpwd.value = "";
                 inputname.value = "";
                 statusmsg.innerHTML = "";
+                username.innerHTML = "";
+                _updateLoginButton("Login");
+                _cur_state = 'login';
+            }
+            else if (_cur_state == 'usepassword') {
+                YAHOO.util.Dom.removeClass(logininput, 'phedex-invisible'); //To Hide the element
+                _username = "";
+                inputpwd.value = "";
+                inputname.value = "";
+                statusmsg.innerHTML = "";
+                username.innerHTML = "";
                 _updateLoginButton("Login");
                 _cur_state = 'login';
             }
         };
 
-        var objBtn = new YAHOO.widget.Button({ label: "Login", id: "buttonOK", container: btnsubmit, onclick: { fn: onLogin} });
+        var objBtn = new YAHOO.widget.Button({ label: "Login", id: "buttonOK", container: btnsubmit, onclick: { fn: _onLogin} });
 
         _updateLoginButton = function(status) {
             objBtn.set("label", status);
         };
+
+        //Check if authenticated using certificate info
+        if (_mode == 2) { //If mode is certificate
+            _username = "Test User"; //Get user name from certificate
+            username.innerHTML = _username;
+            statusmsg.innerHTML = " logged in via certificate";
+            _cur_state = 'usepassword';
+            _updateLoginButton("Use Password");
+            YAHOO.util.Dom.addClass(logininput, 'phedex-invisible'); //To Hide the element
+        }
     };
     return {
         init: function(el) {
