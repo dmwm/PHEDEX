e21aa19e8404af2c257f0b8da27f70febe7c0cfe
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2009-12-17 15:56:29 +0000

    change the logger to be loadable and instantiable via the core like any other module, i.e. to have an init() method instead of Create(). Add a log2Server array which holds booleans for the log-levels, if true, the log-message is used to construct a URL which is "fetched" from the server (the actual return value is ignored). This allows us to log messages to the server - or more usefully for debugging, to the proxy-server.

--- PhEDExWeb/ApplicationServer/js/phedex-logger.js
+++ PhEDExWeb/ApplicationServer/js/phedex-logger.js
@@ -2,50 +2,67 @@
 // production pages by simply not declaring the div in the html.
 PHEDEX.namespace('Logger');
 
-PHEDEX.Logger.Create=function(args,opts,divid) {
-  if ( !divid ) { divid = 'phedex-logger'; }
-  var _div = document.getElementById(divid);
-  if ( !_div ) { return; }
-  _div.innerHTML = '';
+PHEDEX.Logger = function() {
+  return {
+    log2Server: { info:false, warn:false, error:false },
+    init: function(args) {
+      var divid = 'phedex-logger',
+          div   = document.getElementById(divid);
+      if ( !div ) { return; }
+      div.innerHTML = '';
 
-  YAHOO.widget.Logger.reset();
-  var _conf = {
-    width: "500px",
-    height: "20em",
-    fontSize: '100%',
-    newestOnTop: false,
-    footerEnabled: true,
-    verboseOutput: false
-  };
-  if (args) {
-    for (var i in args) {
-      _conf[i]=args[i];
+      YAHOO.widget.Logger.reset();
+      var conf = {
+        width: "500px",
+        height: "20em",
+        fontSize: '100%',
+        newestOnTop: false,
+        footerEnabled: true,
+        verboseOutput: false
+      };
+
+      if (args.config) {
+        for (var i in args.config) {
+          conf[i]=args.config[i];
+        }
+      }
+      div.style.width    = conf.width;
+      div.style.fontSize = conf.fontSize;
+      PHEDEX.Logger.Reader = new YAHOO.widget.LogReader(divid,conf);
+      PHEDEX.Logger.Reader.hideSource('global');
+      PHEDEX.Logger.Reader.hideSource('LogReader');
+      if ( args.opts )
+      {
+        if ( args.opts.hideSource )
+        {
+          for (var s in args.opts.hideSource) { PHEDEX.Logger.Reader.hideSource(args.opts.hideSource[s]); }
+        }
+        if ( args.opts.collapse ) { PHEDEX.Logger.Reader.collapse(); }
+      }
+      if ( args.log2server ) { this.log2Server = args.log2server; }
+      YAHOO.widget.Logger.enableBrowserConsole(); // Enable logging to firebug console, or Safari console.
+
+//    Attempt to harvest any temporarily bufferred log messages
+      this.log = function(obj) {
+        return function(str,level,group) {
+          if ( !level ) { level = 'info'; }
+          if ( !group ) { group = 'app'; }
+          YAHOO.log(str, level, group);
+          if ( obj.log2Server[level] ) {
+            var url = '/log/'+level+'/'+group+'/'+str;
+            YAHOO.util.Connect.asyncRequest('GET', url, { onSuccess:function(){}, onFailure:function(){} } );
+          }
+        };
+      }(this);
+      try {
+        var buffer = log();
+        log = this.log;
+        for (var i in buffer) {
+          this.log(buffer[i][0],buffer[i][1],buffer[i][2]);
+        }
+      } catch(e) {
+        log = this.log; // if I had an error I ignore it, but still need to (re-)define the global 'log' function
+      };
     }
   }
-  _div.style.width = _conf.width;
-  _div.style.fontSize = _conf.fontSize;
-  PHEDEX.Logger.Reader = new YAHOO.widget.LogReader(divid,_conf);
-  PHEDEX.Logger.Reader.hideSource('global');
-  PHEDEX.Logger.Reader.hideSource('LogReader');
-  if ( opts )
-  {
-    if ( opts.hideSource )
-    {
-      for (var s in opts.hideSource) { PHEDEX.Logger.Reader.hideSource(opts.hideSource[s]); }
-    }
-    if ( opts.collapse ) { PHEDEX.Logger.Reader.collapse(); }
-  }
-  YAHOO.widget.Logger.enableBrowserConsole(); // Enable logging to firebug console, or Safari console.
-
-// Attempt to harvest any temporarily bufferred log messages
-  var _log = function(str,level,group) { YAHOO.log(str, level || 'info', group || 'app'); };
-  try {
-    var buffer = log();
-    log = _log;
-    for (var i in buffer) {
-      log(buffer[i][0],buffer[i][1],buffer[i][2]);
-    }
-  } catch(e) {
-    log = _log; // if I had an error I ignore it, but still need to (re-)define the global 'log' function
-  };
 }
