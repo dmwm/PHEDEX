901939e11e15371e5ef002e3c1af98f1d34d42a4
psundarr <sha1-ca6aca0cd13efc276a617890434a29b0e8b6b4d8@cern.ch>
2010-02-10 03:34:31 +0000

    Added the script file to compress the js and css files using YUI compressor. The prerequisites to run the script are java and YUI compressor jar file.

---
+++ PhEDExWeb/ApplicationServer/util/phedex-minify.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Script to compress (minify) the phedex js files
+yuicompressor_path=~/Desktop/ApplicationServer/yuicompressor/build/yuicompressor-2.4.2.jar
+phedex_js_path=~/Desktop/ApplicationServer/js
+phedex_css_path=~/Desktop/ApplicationServer/css
+phedex_min_js_path=~/Desktop/ApplicationServer/minjs
+phedex_min_css_path=~/Desktop/ApplicationServer/mincss
+
+echo "========================================================"
+# Check if the YUI compressor file exist or not
+if [ ! -f $yuicompressor_path ]; then
+  echo "YUI compressor jar file is missing"
+  exit 0
+fi
+
+# Check if the raw js files directory exist or not
+if [ ! -d $phedex_js_path ]; then
+  echo "Phedex raw js files directory is missing"
+  exit 0
+fi
+
+# Check if the raw css files directory exist or not
+if [ ! -d $phedex_css_path ]; then
+  echo "Phedex raw css files directory is missing"
+  exit 0
+fi
+
+# Check if the min js files directory exist or not
+if [ ! -d $phedex_min_js_path ]; then
+    mkdir $phedex_min_js_path
+    echo "Phedex min js files directory is created as it was missing!"
+fi
+
+# Check if the min css files directory exist or not
+if [ ! -d $phedex_min_css_path ]; then
+    mkdir $phedex_min_css_path
+    echo "Phedex min css files directory is created as it was missing!"
+fi
+
+cd $phedex_js_path
+for file in `dir -d *.js` ; do
+echo "Converting $file"
+java -jar $yuicompressor_path $file -o $phedex_min_js_path/${file/.js/-min.js}
+done
+
+echo "Phedex min js files are in $phedex_min_js_path directory"
+echo "========================================================"
+
+cd $phedex_css_path
+for file in `dir -d *.css` ; do
+echo "Converting $file"
+java -jar $yuicompressor_path $file -o $phedex_min_css_path/${file/.css/-min.css}
+done
+
+echo "Phedex min css files are in $phedex_min_css_path directory"
+echo "========================================================"
+exit 0
