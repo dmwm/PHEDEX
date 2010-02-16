f44e9800c57cd7840bc249e4e382d0a6e55c63e0
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-02-16 16:53:20 +0000

    change "extra" message appropriately if no node has been set yet

--- PhEDExWeb/ApplicationServer/js/phedex-module-agents.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-agents.js
@@ -149,6 +149,7 @@ PHEDEX.Module.Agents = function(sandbox, string) {
             now = new Date() / 1000,
             minDate = now,
             maxDate = 0;
+        if ( !node ) { msg = 'No extra information available (no node selected yet!)'; }
         for ( var i in this.data) {
           var u = this.data[i]['time_update'];
           if ( u > maxDate ) { maxDate = u; }
