839bcde985cf52461bfd3eb979f5d9d82d57d96d
psundarr <sha1-ca6aca0cd13efc276a617890434a29b0e8b6b4d8@cern.ch>
2010-03-02 07:32:22 +0000

    Modified filldataSource function call as the function declaration has been changed.

--- PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-nodes.js
@@ -62,8 +62,8 @@ PHEDEX.Module.Nodes = function(sandbox, string) {
         log('Got new data','info',this.me);
         this.dom.title.innerHTML = 'Parsing data';
         this.data = data.node;
-        this.dom.title.innerHTML = this.me+': '+this.data.length+" found";
-        this.fillDataSource(this.data, true);
+        this.dom.title.innerHTML = this.me + ': ' + this.data.length + " found";
+        this.fillDataSource(this.data);
         _sbx.notify( this.id, 'gotData' );
       }
     };
