8501044284bec6f9a3df05de385fac50c767f0cb
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-03-24 10:21:48 +0000

    add profiling to the registry code, just in case it turns out to be useful

--- PhEDExWeb/ApplicationServer/js/phedex-registry.js
+++ PhEDExWeb/ApplicationServer/js/phedex-registry.js
@@ -40,7 +40,8 @@ PHEDEX.Registry = function(sandbox) {
                       none     :1 };
   this.id = _me+'_'+PxU.Sequence();
 
-  return {
+  var _construct = function() {
+    return {
 /** add a new widget to the registry
  * @method add
  * @param widget {string} the name of the module, e.g. phedex-module-agents
@@ -48,68 +49,68 @@ PHEDEX.Registry = function(sandbox) {
  * @param label {string} the label to display on-screen to represent this module
  * @param extrakeys {object} any additional keys needed to work with this object.
  */
-    add: function(widget, inputType, label, extrakeys ) {
-      if (!_validTypes[inputType]) {
-        throw new Error("input type '"+inputType+"' is not valid");
-      }
-      if (!_widgets[inputType]) {
-        _widgets[inputType] = {};
-        _sbx.notify('Registry','InputTypes',this.getInputTypes());
-      }
-      if (!_widgets[inputType][widget]) {
-        _widgets[inputType][widget] = {};
-      }
-      if (_widgets[inputType][widget][label]) {
-        throw new Error("widget '"+widget+"' already registered for input type '"+inputType+"'");
-      }
-      // TODO the 'widget' key is probably redundant, test that hypothesis at some point
-      var w = { widget:widget, short_name:widget, type:inputType, label:label, id:PxU.Sequence() };
-      if ( widget.match('^phedex-module-(.+)$') ) { w.short_name = RegExp.$1; }
-      w.short_name = w.short_name.toLowerCase();
-      if (extrakeys) {
-        for (var k in extrakeys) {
-          w[k] = extrakeys[k];
+      add: function(widget, inputType, label, extrakeys ) {
+        if (!_validTypes[inputType]) {
+          throw new Error("input type '"+inputType+"' is not valid");
         }
-      }
-      _widgets[inputType][widget][label] = w;
-      _inputTypes[w.short_name] = inputType;
-    },
+        if (!_widgets[inputType]) {
+          _widgets[inputType] = {};
+          _sbx.notify('Registry','InputTypes',this.getInputTypes());
+        }
+        if (!_widgets[inputType][widget]) {
+          _widgets[inputType][widget] = {};
+        }
+        if (_widgets[inputType][widget][label]) {
+          throw new Error("widget '"+widget+"' already registered for input type '"+inputType+"'");
+        }
+        // TODO the 'widget' key is probably redundant, test that hypothesis at some point
+        var w = { widget:widget, short_name:widget, type:inputType, label:label, id:PxU.Sequence() };
+        if ( widget.match('^phedex-module-(.+)$') ) { w.short_name = RegExp.$1; }
+        w.short_name = w.short_name.toLowerCase();
+        if (extrakeys) {
+          for (var k in extrakeys) {
+            w[k] = extrakeys[k];
+          }
+        }
+        _widgets[inputType][widget][label] = w;
+        _inputTypes[w.short_name] = inputType;
+      },
 
 /** get a list of inputTypes that have registered widgets
  * @method getInputTypes
  * @return {array} array of names of registered input-types
  */
-    getInputTypes: function() {
-      var types = [];
-      for (var t in _widgets) {
-        types.push(t);
-      }
-      return types;
-    },
+      getInputTypes: function() {
+        var types = [];
+        for (var t in _widgets) {
+          types.push(t);
+        }
+        return types;
+      },
 
 /** get a list of registered widgets by an inputType
  * @method getWidgetsByInputType
  * @param inputType {string}
  * @return {array} array of widgets matching the inputType, or null if no match is found
  */
-    getWidgetsByInputType: function(inputType) {
-      if (_widgets[inputType]) { 
-        var widgets = [];
-        for (var w in _widgets[inputType]) {
-          for (var l  in _widgets[inputType][w]) {
-            widgets.push(_widgets[inputType][w][l]);
+      getWidgetsByInputType: function(inputType) {
+        if (_widgets[inputType]) { 
+          var widgets = [];
+          for (var w in _widgets[inputType]) {
+            for (var l  in _widgets[inputType][w]) {
+              widgets.push(_widgets[inputType][w][l]);
+            }
           }
+          return widgets;
         }
-        return widgets;
-      }
-      else { return null; }
-    },
+        else { return null; }
+      },
 
 /** initialise the Registry. Or rather, invoke the sandbox to listen for events that will start the ball rolling. Until <strong>create</strong> is called, the Registry will sit there, doing nothing at all.
  * @method create
  */
-    create: function() {
-      _sbx.notify('RegistryExists',this.id);
+      create: function() {
+        _sbx.notify('RegistryExists',this.id);
 /**
  * Handle messages sent directly to this module. This function is subscribed to listen for <strong>Registry</strong> events, and will take action accordingly.
  * @method selfHandler
@@ -117,39 +118,43 @@ PHEDEX.Registry = function(sandbox) {
  * @param arr {array} array of arguments for the given event
  * @private
  */
-      this.selfHandler = function(obj) {
-        return function(ev,arr) {
-          log('selfHandler: ev='+ev+' args='+YAHOO.lang.dump(arr,1),'info',_me);
-          var action = arr[0];
-          switch (action) {
-            case 'add': {
-              obj[action](arr[1],arr[2],arr[3],arr[4]);
-              break;
-            }
-            case 'getWidgetsByInputType': {
-              var value = obj[action](arr[1]);
-              _sbx.notify(arr[2],'WidgetsByInputType',arr[1],value);
-              break;
-            }
-            case 'getInputTypes': {
-              var value = obj[action]();
-              _sbx.notify(arr[1],'InputTypes',value);
-              break;
-            }
-            case 'getTypeOfModule': {
-              var value;
-              try { value = _inputTypes[arr[1].toLowerCase()]; } catch (ex) {};
-              if ( value ) {
-                _sbx.notify(arr[2],'TypeOfModule',value);
+        this.selfHandler = function(obj) {
+          return function(ev,arr) {
+            log('selfHandler: ev='+ev+' args='+YAHOO.lang.dump(arr,1),'info',_me);
+            var action = arr[0];
+            switch (action) {
+              case 'add': {
+                obj[action](arr[1],arr[2],arr[3],arr[4]);
+                break;
               }
-              break;
+              case 'getWidgetsByInputType': {
+                var value = obj[action](arr[1]);
+                _sbx.notify(arr[2],'WidgetsByInputType',arr[1],value);
+                break;
+              }
+              case 'getInputTypes': {
+                var value = obj[action]();
+                _sbx.notify(arr[1],'InputTypes',value);
+                break;
+              }
+              case 'getTypeOfModule': {
+                var value;
+                try { value = _inputTypes[arr[1].toLowerCase()]; } catch (ex) {};
+                if ( value ) {
+                  _sbx.notify(arr[2],'TypeOfModule',value);
+                }
+                break;
+              }
             }
           }
-        }
-      }(this);
-      _sbx.listen('Registry',this.selfHandler);
+        }(this);
+        _sbx.listen('Registry',this.selfHandler);
+      }
     }
   };
+  Yla(this,_construct(this),true);
+  YtP.registerObject('Registry',this);
+  return this;
 };
 
 log('loaded...','info','registry');
