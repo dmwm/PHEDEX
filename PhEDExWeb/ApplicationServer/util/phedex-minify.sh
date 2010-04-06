3529118253cbc4005448d7b84c3a2c41a9be8a12
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-04-06 16:15:31 +0000

    fetch YUI compressor if it was not found

--- PhEDExWeb/ApplicationServer/util/phedex-minify.sh
+++ PhEDExWeb/ApplicationServer/util/phedex-minify.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 # Script to compress (minify) the phedex js files
-yuicompressor_path=/home/tony/YUI/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar
 cd `dirname $0`
 cd ../..
 phedex_base=`pwd`
 echo "Using $phedex_base for setup..."
+yuicompressor_url=http://yuilibrary.com/downloads/yuicompressor/yuicompressor-2.4.2.zip
+yuicompressor_path=$phedex_base/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar
 phedex_js_path=$phedex_base/ApplicationServer/js
 phedex_css_path=$phedex_base/ApplicationServer/css
-phedex_min_js_path=$phedex_base/ApplicationServer/min/js
-phedex_min_css_path=$phedex_base/ApplicationServer/min/css
+phedex_min_js_path=$phedex_base/ApplicationServer/build/js
+phedex_min_css_path=$phedex_base/ApplicationServer/build/css
 
 echo "========================================================"
 # Check if the YUI compressor file exist or not
 if [ ! -f $yuicompressor_path ]; then
-  echo "YUI compressor jar file is missing"
-  exit 0
+  echo "YUI compressor jar file is missing, attempting to download it"
+  wget -q $yuicompressor_url
+  unzip `basename $yuicompressor_url`
+  if [ ! -f $yuicompressor_path ]; then
+    echo "YUI compressor jar file is still missing, giving up..."
+    exit 1
+  fi
 fi
 
 # Check if the raw js files directory exist or not
 if [ ! -d $phedex_js_path ]; then
   echo "Phedex raw js files directory is missing"
-  exit 0
+  exit 1
 fi
 
 # Check if the raw css files directory exist or not
 if [ ! -d $phedex_css_path ]; then
   echo "Phedex raw css files directory is missing"
-  exit 0
+  exit 1
 fi
 
 # Check if the min js files directory exist or not
@@ -59,6 +65,7 @@ cd $phedex_js_path
 for file in `dir -d *.js` ; do
 echo "Converting $file"
 java -jar $yuicompressor_path $file -o $phedex_min_js_path/${file/.js/-min.js}
+cp $file $phedex_min_js_path/$file # for the debug-version
 done
 
 echo "Phedex min js files are in $phedex_min_js_path directory"
@@ -68,6 +75,7 @@ cd $phedex_css_path
 for file in `dir -d *.css` ; do
 echo "Converting $file"
 java -jar $yuicompressor_path $file -o $phedex_min_css_path/${file/.css/-min.css}
+cp $file $phedex_min_css_path/$file # for the debug-version
 done
 
 echo "Phedex min css files are in $phedex_min_css_path directory"
