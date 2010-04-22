48daec6e4175188fb86757442e68f517d6c1e3c6
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-04-22 16:43:47 +0000

    - add 'last 2 weeks' and 'last 4 weeks' to time-select menu - add second-level branches, for test results - add filter definition

--- PhEDExWeb/ApplicationServer/js/phedex-module-consistencyresults.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-consistencyresults.js
@@ -55,7 +55,7 @@ PHEDEX.Module.ConsistencyResults=function(sandbox, string) {
             type: 'menu',
             initial: function() { return opts.since; },
             container: 'buttons',
-            menu: { 1:'Last Hour', 3:'Last 3 Hours', 6:'Last 6 Hours', 12:'Last 12 Hours', 24:'Last Day', 48:'Last 2 Days', 96:'Last 4 Days', 168:'Last Week', 9999:'Forever' },
+            menu: { 1:'Last Hour', 3:'Last 3 Hours', 6:'Last 6 Hours', 12:'Last 12 Hours', 24:'Last Day', 48:'Last 2 Days', 96:'Last 4 Days', 168:'Last Week', 336:'Last 2 Weeks', 672:'Last 4 Weeks', 9999:'Forever' },
             map: {
               onChange:'changeTimebin',
             },
@@ -72,7 +72,7 @@ PHEDEX.Module.ConsistencyResults=function(sandbox, string) {
             format: [
               {width:160,text:'Node', className:'phedex-tree-node',    otherClasses:'align-left',  ctxArgs:['node','sort-alpha'], ctxKey:'node' },
               {width: 60,text:'ID',   className:'phedex-tree-node-id', otherClasses:'align-right', ctxArgs:'sort-num',   hide:true },
-              {width:100,text:'SE',   className:'phedex-tree-se',      otherClasses:'align-right', ctxArgs:'sort-alpha', hide:true },
+              {width:200,text:'SE',   className:'phedex-tree-se',      otherClasses:'align-right', ctxArgs:'sort-alpha', hide:true },
             ]
           },
           {
@@ -84,55 +84,51 @@ PHEDEX.Module.ConsistencyResults=function(sandbox, string) {
               {width: 80,text:'Bytes',      className:'phedex-tree-block-bytes', otherClasses:'align-right', ctxArgs:'sort-num', format:PxUf.bytes, hide:true },
             ]
           },
-//           {
-//             name:'Replica',
-//             format:[
-//               {width:160,text:'Node',        className:'phedex-tree-replica-node',       otherClasses:'align-left',  ctxArgs:['node','sort-alpha'], ctxKey:'node' },
-//               {width:100,text:'SE',          className:'phedex-tree-replica-se',         otherClasses:'align-right', ctxArgs:'sort-alpha', hide:true },
-//               {width: 80,text:'Files',       className:'phedex-tree-replica-files',      otherClasses:'align-right', ctxArgs:'sort-num' },
-//               {width: 80,text:'Bytes',       className:'phedex-tree-replica-bytes',      otherClasses:'align-right', ctxArgs:'sort-num', format:PxUf.bytes },
-//               {width:120,text:'Create Time', className:'phedex-tree-replica-timecreate', otherClasses:'align-right', ctxArgs:'sort-alpha', format:'UnixEpochToGMT' },
-//               {width:120,text:'Update Time', className:'phedex-tree-replica-timeupdate', otherClasses:'align-right', ctxArgs:'sort-alpha', format:'UnixEpochToGMT' },
-//               {width: 90,text:'Subscribed',  className:'phedex-tree-replica-subscribed', otherClasses:'align-right', ctxArgs:'sort-alpha' },
-//               {width: 60,text:'Complete',    className:'phedex-tree-replica-complete',   otherClasses:'align-right', ctxArgs:'sort-alpha' },
-//               {width: 80,text:'Group',       className:'phedex-tree-replica-group',      otherClasses:'align-right', ctxArgs:['group','sort-alpha'], ctxKey:'group' },
-//             ]
-//           }
+          {
+            name:'Test',
+            format:[
+              {width: 60,text:'ID',           className:'phedex-tree-test-id',           otherClasses:'align-left',  ctxArgs:['node','sort-alpha'] },
+              {width: 60,text:'Kind',         className:'phedex-tree-test-kind',         otherClasses:'align-right', ctxArgs:'sort-alpha' },
+              {width:120,text:'Report Time',  className:'phedex-tree-test-timereport',   otherClasses:'align-right', ctxArgs:'sort-alpha', format:'UnixEpochToGMT' },
+              {width: 90,text:'Status',       className:'phedex-tree-test-status',       otherClasses:'align-right', ctxArgs:'sort-alpha' },
+              {width: 80,text:'Files',        className:'phedex-tree-test-files',        otherClasses:'align-right', ctxArgs:'sort-num' },
+              {width: 80,text:'Files OK',     className:'phedex-tree-test-files-ok',     otherClasses:'align-right', ctxArgs:'sort-num' },
+              {width: 80,text:'Files Tested', className:'phedex-tree-test-files-tested', otherClasses:'align-right', ctxArgs:'sort-num' },
+            ]
+          }
         ],
 // Filter-structure mimics the branch-structure. Use the same classnames as keys.
         filter: {
-             'Node-level attributes':{
-                map:{from:'phedex-tree-node-', to:'N'},
-                fields:{
-                  'phedex-tree-node-name' :{type:'regex',  text:'Node-name', tip:'javascript regular expression' },
-                  'phedex-tree-node-id'   :{type:'int',    text:'Node-ID',   tip:'Node-ID in TMDB'},
-                  'phedex-tree-node-se'   :{type:'regex',  text:'SE-name',   tip:'javascript regular expression'},
-                },
-              },
+         'Node-level attributes':{
+            map:{from:'phedex-tree-node-', to:'N'},
+            fields:{
+              'phedex-tree-node-name' :{type:'regex',  text:'Node-name', tip:'javascript regular expression' },
+              'phedex-tree-node-id'   :{type:'int',    text:'Node-ID',   tip:'Node-ID in TMDB'},
+              'phedex-tree-node-se'   :{type:'regex',  text:'SE-name',   tip:'javascript regular expression'},
+            },
+          },
           'Block-level attributes':{
             map:{from:'phedex-tree-block-', to:'B'},
             fields:{
               'phedex-tree-block-name'  :{type:'regex',  text:'Block-name',  tip:'javascript regular expression' },
-              'phedex-tree-block-id'    :{type:'int',    text:'Block-ID',    tip:'ID of this block in TMDB' },
+              'phedex-tree-block-id'    :{type:'int',    text:'Block-ID',    tip:'Block-ID in TMDB' },
               'phedex-tree-block-files' :{type:'minmax', text:'Block-files', tip:'number of files in the block' },
               'phedex-tree-block-bytes' :{type:'minmax', text:'Block-bytes', tip:'number of bytes in the block' },
-//               'phedex-tree-block-open'  :{type:'yesno',  text:'Open',        tip:'is the block still open?' }
             }
           },
-//           'Replica-level attributes':{
-//             map:{from:'phedex-tree-replica-', to:'R'},
-//             fields:{
-//               'phedex-tree-replica-node'       :{type:'regex',  text:'Node-name',     tip:'javascript regular expression' },
-//               'phedex-tree-replica-se'         :{type:'regex',  text:'SE name',       tip:'javascript regular expression' },
-//               'phedex-tree-replica-files'      :{type:'minmax', text:'Files',         tip:'number of files in the replica' },
-//               'phedex-tree-replica-bytes'      :{type:'minmax', text:'Bytes',         tip:'number of bytes in the replica' },
-//               'phedex-tree-replica-complete'   :{type:'yesno',  text:'Complete',      tip:'is the replica complete?' },
-//               'phedex-tree-replica-timecreate' :{type:'regex',  text:'Creation time', tip:'Unix epoch seconds' },
-//               'phedex-tree-replica-timeupdate' :{type:'regex',  text:'Update time',   tip:'Unix epoch seconds' },
-//               'phedex-tree-replica-subscribed' :{type:'yesno',  text:'Subscribed',    tip:'is the replica subscribed?' },
-//               'phedex-tree-replica-group'      :{type:'regex',  text:'Group name',    tip:'Group that owns this replica' }
-//             }
-//           }
+          'Test-level attributes':{
+            map:{from:'phedex-tree-test-', to:'T'},
+            fields:{
+              'phedex-tree-test-id'           :{type:'regex',  text:'ID',           tip:'Test-ID in TMDB' },
+              'phedex-tree-test-kind'         :{type:'regex',  text:'Kind',         tip:'javascript regular expression' },
+              'phedex-tree-test-timereport'   :{type:'minmax', text:'Report time',  tip:'Unix epoch seconds' },
+//  status          "OK", "Fail", "Queued", "Active", "Timeout", "Expired", "Suspended", "Error", "Rejected" or "Indeterminate"
+              'phedex-tree-test-status'       :{type:'regex',  text:'Status',       tip:'javascript regular expression' },
+              'phedex-tree-test-files'        :{type:'minmax', text:'Files',        tip:'number of files' },
+              'phedex-tree-test-files-ok'     :{type:'minmax', text:'Files OK',     tip:'number of files OK' },
+              'phedex-tree-test-files-tested' :{type:'minmax', text:'Files tested', tip:'number of files tested' },
+            }
+          }
         },
       },
 
@@ -165,29 +161,41 @@ PHEDEX.Module.ConsistencyResults=function(sandbox, string) {
         this.getData();
       },
       fillBody: function() {
-        var root = this.tree.getRoot(),
+        var root  = this.tree.getRoot(),
+            mtree = this.meta.tree,
             tLeaf, tNode, tNode1, tNode2, i, j, k, b, n, t,
             nodes = this.data.node;
-        if ( !nodes.length )
-        {
+        if ( !nodes.length ) {
           tLeaf = new YAHOO.widget.TextNode({label: 'Nothing found, try another block or node...', expanded: false}, root);
           tLeaf.isLeaf = true;
         }
         for (i in nodes) {
           n = nodes[i];
           tNode = this.addNode(
-            { format:this.meta.tree[0].format },
+            { format:mtree[0].format },
             [ n.node,n.id,n.se ]
           );
           if ( n.block ) {
-            tNode.title = n.block.length+' tests';
+            tNode.title = n.block.length+' blocks';
             for (j in n.block) {
               b = n.block[j];
               tNode1 = this.addNode(
-                { format:this.meta.tree[1].format },
+                { format:mtree[1].format },
                 [ b.name,b.id,b.files,b.bytes ],
                 tNode
               );
+              if ( b.test ) {
+                tNode1.title = b.test.length+' tests';
+                for (k in b.test) {
+                  t = b.test[k];
+                  tNode2 = this.addNode(
+                    { format:mtree[2].format },
+                    [ t.id,t.kind,t.time_reported,t.status,t.files,t.files_ok,t.files_tested ],
+                    tNode1
+                  );
+                  tNode2.isLeaf = true;
+                }
+              } else { tNode1.isLeaf = true; }
             }
           } else { tNode.isLeaf = true; }
         }
