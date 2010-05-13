b53ac8eab909808bf2fa65280f836ce590db7b3e
Rick Egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2010-05-13 14:15:53 +0000

    more examples

--- perl_lib/PHEDEX/Testbed/ModPerl/Examples.pm
+++ perl_lib/PHEDEX/Testbed/ModPerl/Examples.pm
@@ -19,8 +19,8 @@ sub handler {
 
     # check to see if function exists.  If so, call it
     no strict 'refs';
-    if (exists ${"TestModPerl::Examples\::"}{$func}) {
-	return &{${"TestModPerl::Examples\::"}{$func}}($r);
+    if (exists ${"PHEDEX::Testbed::ModPerl::Examples\::"}{$func}) {
+	return &{${"PHEDEX::Testbed::ModPerl::Examples\::"}{$func}}($r);
     }
     else {
 	return Apache2::Const::NOT_FOUND; # 404
@@ -38,6 +38,80 @@ sub hello {
 # HTTP 400 bad request
 sub bad {
     return Apache2::Const::HTTP_BAD_REQUEST;
+}
+
+# Example of long output.  Does the response header get set to 200
+# regardless of the 'return' value?
+sub long_output {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+    for (1..1_000_000) {
+	print "$_ Fe fi fo fum...\n";
+    }
+
+    # The long operation above is sent to the client as HTTP 200
+    # But the return statement below causes it to be logged as HTTP 400!
+    return Apache2::Const::HTTP_BAD_REQUEST;
+}
+
+# in this function, headers are sent immediately, but the output is
+# only sent after a short sleep
+sub short_sleep {
+    my $r = shift;
+    
+    $r->content_type('text/plain');
+    $r->rflush();
+    
+    my $secs = 30;
+    sleep($secs);
+    print "Yawn... I was sleeping for $secs seconds.  Are you still there?\n";
+    return Apache2::Const::OK;
+}
+
+# In this function, headers are sent immediately, but the sleep is
+# very long and the connection times out before any output is received
+# by the client.
+sub long_sleep {
+    my $r = shift;
+    
+    $r->content_type('text/plain');
+    $r->rflush();
+    
+    my $secs = 330;
+    sleep($secs);
+    print "Yawn... I was sleeping for $secs seconds.  Are you still there?\n";
+    return Apache2::Const::OK;
+}
+
+# This version does make it to the end message, because it
+# periodically sends some data to the client.
+sub long_sleep2 {
+    my $r = shift;
+    
+    $r->content_type('text/plain');
+    $r->rflush();
+    
+    my $secs = 330;
+    for (1..$secs) {
+	sleep(1);
+	print ".";
+	$r->rflush();
+    }
+    print "Yawn... I was sleeping for $secs seconds.  Are you still there?\n";
+    return Apache2::Const::OK;
+}
+
+use CGI;
+use Data::Dumper;
+sub echo_params {
+    my $r = shift;
+    my $q = new CGI ($r);
+    my $params = $q->Vars();
+
+    $r->content_type('text/plain');
+    print "params:\n", Dumper($params), "\n";
+    return Apache2::Const::OK;
 }
 
 1;
