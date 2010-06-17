417ecb4df7f9beccc7271b3f15a5a1fc30cb0eb2
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-06-17 10:25:31 +0000

    cleanup and comment the code

--- PhEDExWeb/ApplicationServer/js/phedex-module-protovisdemo.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-protovisdemo.js
@@ -1,7 +1,7 @@
 /**
- * This is the base class for all PhEDEx data-related modules. It provides the basic interaction needed for the core to be able to control it.
+ * This is the base class for all PhEDEx Protovis-related modules. It provides the basic interaction needed for the core to be able to control it.
  * @namespace PHEDEX.Module
- * @class Agents
+ * @class ProtovisDemo
  * @constructor
  * @param sandbox {PHEDEX.Sandbox} reference to a PhEDEx sandbox object
  * @param string {string} a string to use as the base-name of the <strong>Id</strong> for this module
@@ -18,6 +18,7 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
       decorators: [
       ],
 
+// these define the canvas size, and are used elsewhere too
       options: {
         width:800,
         height:400,
@@ -25,14 +26,20 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
         minheight:50
       },
 
+// most of this is boilerplate, only 'getData' and 'gotData' need customising.
       meta: {
       },
+
+      isValid: function() {
+        if ( node ) { return true; }
+        return false;
+      },
+      getState: function() { return {node:node}; },
 
 /** final preparations for receiving data. This is the last thing to happen before the module gets data, and it should notify the sandbox that it has done its stuff. Otherwise the core will not tell the module to actually ask for the data it wants. Modules may override this if they want to sanity-check their parameters first, e.g. the <strong>Agents</strong> module might want to check that the <strong>node</strong> is set before allowing the cycle to proceed. If the module does not have enough parameters defined, it can notify the sandbox with <strong>needArguments</strong>, and someone out there (e.g. the global filter or the navigator history) can attempt to supply them
  * @method initData
  */
       initData: function() {
-        node = 'T0_CH_CERN_Export';
         this.dom.title.innerHTML = 'Waiting for parameters to be set...';
         if ( node ) {
           _sbx.notify( this.id, 'initData' );
@@ -52,37 +59,45 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
         this.dom.title.innerHTML = 'setting parameters...';
         _sbx.notify(this.id,'setArgs');
       },
-      getData: function() {
 
+      getData: function() {
+// customisable stuff starts here. Check that the 'node' is defined. If not, go ask for a value. If yes, get the data
         if ( !node ) {
           this.initData();
           return;
         }
         this.dom.title.innerHTML = 'fetching data...';
         log('Fetching data','info',this.me);
+// tell the core to get the 'agents' data for the appropriate node
         _sbx.notify( this.id, 'getData', { api:'agents', args:{node:node} } );
       },
+/*
+ * This is where the meat of the application resides. The gotData function receives two arguments, 'data' and 'context'. 'data contains the
+ * data returned by the API call, see the phedex dataservice API documentation for details. This data is then massaged into a suitable form
+ * for protovis to digest, and used in whatever representation you like.
+ */
       gotData: function(data,context) {
         log('Got new data','info',this.me);
         this.dom.title.innerHTML = 'Parsing data';
 
-//           var data = 
-// {"node":[{"agent":[{"time_update":"1276696583.94697","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"10552","version":"PHEDEX_3_3_1","label":"exp-pfn-local"}],"name":"FileExport","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276697851.88557","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"10516","version":"PHEDEX_3_3_1","label":"exp-pfn"}],"name":"FileExport","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699510.4564","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"25776","version":"PHEDEX_3_3_1","label":"mgmt-blockmon"}],"name":"BlockMonitor","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699538.78552","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"25164","version":"PHEDEX_3_3_1","label":"blockverify"}],"name":"BlockDownloadVerify","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699535.77539","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"27824","version":"PHEDEX_3_3_1","label":"mgmt-pump"}],"name":"FilePump","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699280.83667","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"10657","version":"PHEDEX_3_3_1","label":"download-remove"}],"name":"FileRemove","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699565.30076","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"26833","version":"PHEDEX_3_3_1","label":"info-invariant"}],"name":"InvariantMonitor","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276698998.37744","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"25881","version":"PHEDEX_3_3_1","label":"info-fs"}],"name":"InfoFileSize","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699090.30835","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"10476","version":"PHEDEX_3_3_1","label":"exp-stager-caf"}],"name":"FileStager","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699291.24255","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"25938","version":"PHEDEX_3_3_1","label":"mgmt-blockactiv"}],"name":"BlockActivate","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276697507.72133","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"26265","version":"PHEDEX_3_3_1","label":"mgmt-blockverifyinjector"}],"name":"BlockDownloadVerifyInjector","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699593.68942","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"25870","version":"PHEDEX_3_3_1","label":"mgmt-reqalloc"}],"name":"RequestAllocator","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699507.34888","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"27073","version":"PHEDEX_3_3_1","label":"info-tc"}],"name":"InfoStatesClean","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699027.33946","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"26020","version":"PHEDEX_3_3_1","label":"mgmt-blockdeact"}],"name":"BlockDeactivate","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699488.46464","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"10963","version":"PHEDEX_3_3_1","label":"mgmt-router"}],"name":"FileRouter","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699256.21639","state_dir":"/data/ProdNodes/Prod_T0_CH_CERN_Export/state","pid":"10439","version":"PHEDEX_3_3_1","label":"exp-stager-others"}],"name":"FileStager","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699559.35547","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"26080","version":"PHEDEX_3_3_1","label":"mgmt-blockdelete"}],"name":"BlockDelete","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699553.72433","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"3292","version":"PHEDEX_3_3_1","label":"info-pm"}],"name":"PerfMonitor","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"},{"agent":[{"time_update":"1276699486.4837","state_dir":"/data/ProdNodes/Prod_Mgmt/state","pid":"26571","version":"PHEDEX_3_3_1","label":"mgmt-blockalloc"}],"name":"BlockAllocator","node":"T0_CH_CERN_Export","host":"vocms02.cern.ch"}]};
-
         if ( !data.node ) {
           throw new Error('data incomplete for '+context.api);
         }
-        var jAgents, nAgents, iAgent, jProcs, pvData={ agents:[], times:[] }, value, max=0;
+
+// parse the data into the pvData structure.
+// Correctly, for the 'agents' API, I should loop over data.node as an array. 
+        var jAgents, nAgents, iAgent, jProcs, pvData={ agents:[], times:[] }, value, max=0, now = new Date().getTime()/1000;
         this.data = jAgents = data.node;
         nAgents=jAgents.length;
         this.dom.title.innerHTML = node + ': ' + this.data.length + " agents";
-            var now = new Date().getTime()/1000;
 
         for (iAgent = 0; iAgent < nAgents; iAgent++) {
           jProcs = jAgents[iAgent].agent;
           for (iProc in jProcs) {
             value = now - jProcs[iProc]['time_update'];
             if ( value > max ) { max = value; }
+
+//          store the update-times in one array-oject, and the labels in another
             pvData['times'].push(value);
             pvData['agents'].push(jProcs[iProc]['label']);
           }
@@ -90,12 +105,12 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
 
         this.dom.body.style.border = '1px solid red';
 
+//      base options for the canvas. Take some from the module definition, others from the data, etc.
         max = max * 1.5;
         var w = this.options.width,
             h = this.options.height,
             x = pv.Scale.linear(0, max).range(0, w),
             y = pv.Scale.ordinal(pv.range(pvData['agents'].length)).splitBanded(0, h, 4/5);
-//         var data = pv.range(10).map(function(d) { return Math.random() + .1; });
 
 /* The root panel. */
         var vis = new pv.Panel()
@@ -114,17 +129,16 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
             .height(y.range().band)
             .left(0)
             .width(x)
-            .fillStyle(pv.Scale.linear(1800, 3600, 7200).range('green', 'yellow', 'red'));
+            .textStyle("white")
+            .fillStyle(pv.Scale.linear(0, 1800, 3600, 7200).range('green', 'green', 'yellow', 'red'));
 
 /* The value label. */
         bar.anchor("right").add(pv.Label)
-            .textStyle("white")
-            .text(function(d) d.toFixed(1));
+            .text(function(d) { if ( d>1800 ) { return d.toFixed(1); } return '';} );
 
 /* The variable label. */
         bar.anchor("left").add(pv.Label)
             .textMargin(5)
-//             .textAlign("right")
             .text(function() pvData['agents'][this.index]);
 
 /* X-axis ticks. */
@@ -139,16 +153,9 @@ PHEDEX.Module.ProtovisDemo = function(sandbox, string) {
           .anchor("bottom").add(pv.Label)
             .text(x.tickFormat);
 
-vis.render();
+          vis.render();
 
-//         new pv.Panel()
-//             .width(this.options.width)
-//             .height(this.options.height)
-//             .canvas(this.dom.body)
-//           .anchor("center").add(pv.Label)
-//             .text("Hello, world!")
-//           .root.render();
-
+//      This tells the core that all went well, and allows it to react properly to other things
         _sbx.notify( this.id, 'gotData' );
       },
     };
@@ -156,4 +163,4 @@ vis.render();
   Yla(this,_construct(this),true);
   return this;
 };
-log('loaded...','info','agents');
+log('loaded...','info','protovisdemo');
