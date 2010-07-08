a75d89978892ae51f3ebeef88d2b9f3f232006a3
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2010-07-08 03:14:03 +0000

    Rewrite to [1] use flat2tree() and [2] use spooling

--- perl_lib/PHEDEX/Web/API/TransferQueueFiles.pm
+++ perl_lib/PHEDEX/Web/API/TransferQueueFiles.pm
@@ -25,7 +25,7 @@ Serves files in the transfer queue, along with their state.
   priority         one of the following:
                      high, normal, low
   state            one of the following:
-                     transferred, transfering, exported, done
+                     assigned, exported, transferring, done
 
 =head2 Output
 
@@ -59,6 +59,8 @@ Serves files in the transfer queue, along with their state.
 
   name             block name
   id               block id
+  files            number of files in this block in this queue
+  bytes            total bytes in this block in this queue
   time_assign      minimum assignment time for a file in the block
   time_expire      minimum expiration time for a file in the block
   time_state       minimum time a file achieved its current state
@@ -78,7 +80,39 @@ Serves files in the transfer queue, along with their state.
 
 use PHEDEX::Web::SQL;
 use PHEDEX::Core::Util;
-use Data::Dumper;
+use PHEDEX::Web::Spooler;
+
+my $map = {
+    _KEY => 'FROM_ID+TO_ID',
+    from => 'FROM_NAME',
+    from_id => 'FROM_ID',
+    from_se => 'FROM_SE',
+    to => 'TO_NAME',
+    to_id => 'TO_ID',
+    to_se => 'TO_SE',
+    transfer_queue => {
+        _KEY => 'STATE+PRIORITY',
+        state => 'STATE',
+        priority => 'PRIORITY',
+        block => {
+            _KEY => 'BLOCK_ID',
+            name => 'BLOCK_NAME',
+            id => 'BLOCK_ID',
+            file => {
+                _KEY => 'FILEID',
+                id => 'FILEID',
+                name => 'LOGICAL_NAME',
+                bytes => 'FILESIZE',
+                checksum => 'CHECKSUM',
+                is_custodial => 'IS_CUSTODIAL',
+                time_assign => 'TIME_ASSIGN',
+                time_expire => 'TIME_EXPIRE',
+                time_state => 'TIME_STATE'
+            }
+        }
+    }
+};
+
 
 sub duration { return 60 * 60; }
 sub invoke { 
@@ -90,8 +124,125 @@ sub invoke {
 	$h{uc $_} = delete $h{$_} if $h{$_};
     }
     
-    my $links = PHEDEX::Web::SQL::getTransferQueue($core, %h, LEVEL => 'FILE');    
-    return { link => [ values %$links ] };
+    my $r = PHEDEX::Core::Util::flat2tree($map, PHEDEX::Web::SQL::getTransferQueue($core, %h, LEVEL => 'FILE'));
+
+    # make up stats for blocks
+    foreach my $link (@$r)
+    {
+        foreach my $transfer_queue (@{$link->{transfer_queue}})
+        {
+            foreach my $block (@{$transfer_queue->{block}})
+            {
+                my $bytes = 0;
+                my $files = 0;
+                my $time_state;
+                my $time_assign;
+                my $time_expire;
+                foreach my $file (@{$block->{file}})
+                {
+                    $bytes += $file->{bytes};
+                    $files += 1;
+                    if ((! defined $time_state) or
+                        (int($time_state) > int($file->{time_state})))
+                    {
+                        $time_state = $file->{time_state};
+                    }
+
+                    if ((! defined $time_assign) or
+                        (int($time_assign) > int($file->{time_assign}))) 
+                    {
+                        $time_assign = $file->{time_assign};
+                    }
+
+                    if ((! defined $time_expire) or
+                        (int($time_expire) > int($file->{time_expire})))
+                    {
+                        $time_expire = $file->{time_expire};
+                    }
+                    $block->{files} = $files;
+                    $block->{bytes} = $bytes;
+                    $block->{time_state} = $time_state;
+                    $block->{time_assign} = $time_assign;
+                    $block->{time_expire} = $time_expire;
+                }
+            }
+        }
+    }
+
+    return { link => $r };
+}
+
+my $sth;
+my $limit = 1000;
+my @keys = ('FROM_ID', 'TO_ID');
+
+sub spool{ 
+    my ($core, %h) = @_;
+
+    # convert parameter key to upper case
+    foreach ( qw / from to state priority block dataset / )
+    {
+	$h{uc $_} = delete $h{$_} if $h{$_};
+    }
+    $h{'__spool__'} = 1;
+
+    $sth = PHEDEX::Web::Spooler->new(PHEDEX::Web::SQL::getTransferQueue($core, %h, LEVEL => 'FILE'), $limit, @keys) if !$sth;
+    
+    my $r2 = $sth->spool();
+    if ($r2)
+    {
+        my $r = PHEDEX::Core::Util::flat2tree($map, $r2);
+
+        # make up stats for blocks
+        foreach my $link (@$r)
+        {
+            foreach my $transfer_queue (@{$link->{transfer_queue}})
+            {
+                foreach my $block (@{$transfer_queue->{block}})
+                {
+                    my $bytes = 0;
+                    my $files = 0;
+                    my $time_state;
+                    my $time_assign;
+                    my $time_expire;
+                    foreach my $file (@{$block->{file}})
+                    {
+                        $bytes += $file->{bytes};
+                        $files += 1;
+                        if ((! defined $time_state) or
+                            (int($time_state) > int($file->{time_state})))
+                        {
+                            $time_state = $file->{time_state};
+                        }
+    
+                        if ((! defined $time_assign) or
+                            (int($time_assign) > int($file->{time_assign}))) 
+                        {
+                            $time_assign = $file->{time_assign};
+                        }
+    
+                        if ((! defined $time_expire) or
+                            (int($time_expire) > int($file->{time_expire})))
+                        {
+                            $time_expire = $file->{time_expire};
+                        }
+                        $block->{files} = $files;
+                        $block->{bytes} = $bytes;
+                        $block->{time_state} = $time_state;
+                        $block->{time_assign} = $time_assign;
+                        $block->{time_expire} = $time_expire;
+                    }
+                }
+            }
+        }
+
+        return { link => $r };
+    }
+    else
+    {
+        $sth = undef;
+        return $r2;
+    }
 }
 
 1;
