e03fe014edca48d6410530d6f72a5723f5180742
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-08-27 14:05:28 +0000

    "Ratio" now comes already formatted. Alas, sorting on this column now applies a string-sort, not a numeric. Who cares...?

--- PhEDExWeb/ApplicationServer/js/phedex-module-shift-requestedqueued.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-shift-requestedqueued.js
@@ -1,33 +1,6 @@
 /*
- TODO:
-1) For T1_%_MSS, "Queued data" should show the sum of data queued to T1_%_Buffer and T1_%_MSS Since here we're trying to understand if the data isn't being queued at all, the interesting thing in this comparison is the total amount of queued data on the route, not just the queue to the final hop. Of course this will become a mess if we ever enable multi-hop routing!
- -> done!
-
-2) Sorting by "Max Queued" or "Max Requested" is sorting the values based on the string representation; a sorting on the numerical value would be more useful.
- -> parsing of string to number is not happening, because _processData isn't being called - or rather, does nothing. This would be fine if the datasvc returned numerical values instead of strings, and it is supposed to, but it doesn't. Somewhere inside JSON::XS::encode_json, numerical values are not being recognised correctly, and are being returned as strings.
- -> done!
-
-3) I'm wondering if some sort of 'red alert' flashing triangle (or KIT-style  unhappy face, which is more color-blind-friendly) would be more appreciated by the shifters instead of a simple 'Problem' text label
- -> done!
-
-4) warning-state for expert operators, where ratio is lower than the threshold (9) but still significantly greater than 1 (???)
-
-5) the OK/Problem logic doesn't seem to be correct for some sites. For example T1_CH_CERN_MSS:
-http://cmswttest.cern.ch/phedex/datasvc/app#page=instance~Production+type~none+widget~shift-requestedqueued+module~sort{RQ.Status%20desc}filter{RQ.Node%3DT1_CH_CERN_MSS}
-The volumes requested/queued are 0 since 8:00 am, so the Status should now be OK with comment 'No data requested'
-On the other hand, for T2_BE_IIHE I see that volume queued is consistently 1/609th of volume requested; this is clearly a Problem, but the site is marked OK:
-http://cmswttest.cern.ch/phedex/datasvc/app#page=instance~Production+type~none+widget~shift-requestedqueued+module~sort{RQ.Status%20desc}filter{RQ.Node%3DT2_BE_IIHE}
-Maybe the issue is that the 'volume queued entry' for the latest timebin is 0, which is displayed as ratio = 0 while in this case it should be interpreted as ratio = infinity?
-
-6) It makes sense to increase the threshold for 'very little data requested'. For T0/T1_%_MSS we even ask the shifters to report problems only when volume requested > 1 TB (the typical size of a tape, which is also the typical threshold to trigger buffer->tape migration). Maybe we should now implement a more refined logic now actually:
-a) If volume requested > 1 TB, then mark 'Problem' after 4 hours
-b) If volume requested < 1 TB, then mark 'Problem' after 12 hours (the files shouldn't sit on buffer forever waiting to be lost in a disk server crash).
- -> threshold changed to 1TB. Can set 'mindata' in API call to change this (value in bytes)
-
-7) It might be more useful to display 'Current Queued', 'Current Requested' (i.e. the latest timebin) on the main page - after all, if the situation went away we don't need to worry anymore. Also 'Max Queued' vs. 'Max Requested' isn't a very useful comparison, since the two values could be from different timebins - maybe the most interesting comparisons would be 'Queued vs. Requested for timebin with max Ratio' and 'Queued vs. Requested for timebin with min Ratio'; this will clutter the display so maybe they should be hidden columns?
- -> Current Queued and Current Requested now added, will not do max/min-ratio just yet
-
-*/
+ * FIXME: sorting the nested table by ratio sorts by string, not value
+ */
 /**
  * This is the base class for all PhEDEx data-related modules. It provides the basic interaction needed for the core to be able to control it.
  * @namespace PHEDEX.Module
@@ -134,7 +107,7 @@ PHEDEX.Module.Shift.RequestedQueued = function(sandbox, string) {
             {key:'timebin',       label:'Timebin',   formatter:'UnixEpochToGMT' },
             {key:'pend_bytes',    label:'Queued',    className:'align-right', parser:'number', formatter:'customBytes' },
             {key:'request_bytes', label:'Requested', className:'align-right', parser:'number', formatter:'customBytes' },
-            {key:'ratio',         label:'Ratio',     className:'align-right', parser:'number', formatter:YwDF.customFixed(3) },
+            {key:'ratio',         label:'Ratio',     className:'align-right', parser:'number' },
           ],
         },
         sort:{field:'Node'},
