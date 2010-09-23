445549b9cee8138ce0849d0a3dfbcbbc6ef03828
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-09-23 16:40:28 +0000

    a simple API to list DBSes

---
+++ perl_lib/PHEDEX/Web/API/DBS.pm
@@ -0,0 +1,27 @@
+package PHEDEX::Web::API::DBS;
+use PHEDEX::Web::SQL;
+use warnings;
+use strict;
+
+=pod
+
+=head1 NAME
+
+PHEDEX::Web::API::DBS - simple data service debugging tool
+
+=head2 DESCRIPTION
+
+Return a hash of DBS ids and names. Used in the next-gen website.
+
+=cut
+
+sub duration { return 0; }
+sub invoke { return dbs(@_); }
+sub dbs
+{
+  my ($core,%h) = @_;
+  my $r = PHEDEX::Web::SQL::getDBS($core, %h);
+  return { dbs => $r };
+}
+
+1;
