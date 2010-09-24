02c1a8a554bf571daed93a9832d844b0a03c4345
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2010-09-24 16:19:01 +0000

    first implementation: abstract the "Refresh" control

---
+++ PhEDExWeb/ApplicationServer/js/phedex-component-refresh.js
@@ -0,0 +1,111 @@
+/**
+ * This class creates a Refresh decorator specification, to allow the user to fetch new data for the module.
+ * @namespace PHEDEX.Component
+ * @class Refresh
+ * @constructor
+ * @param sandbox {PHEDEX.Sandbox} reference to a PhEDEx sandbox object
+ * @param args {object} reference to an object that specifies details of how the control should operate.
+ */
+PHEDEX.namespace('Component');
+PHEDEX.Component.Refresh = function(sandbox,args) {
+  Yla(this, new PHEDEX.Base.Object());
+  var _me = 'component-refresh',
+      _sbx = sandbox,
+      partner = args.partner,
+      ap = args.payload,
+      defaults = {
+        handler: 'getData',
+        animate:  false,
+        disabled: true,
+        control:{
+          payload: {
+            text:'Refresh',
+            hidden:false,
+            disabled:true,
+            obj:this
+          }
+        }
+      };
+  Yla(ap,defaults);
+
+  _construct = function() {
+    var payload=args.payload, obj=payload.obj, apc=payload.control;
+    return {
+      me: _me,
+      id: _me+'_'+PxU.Sequence(),
+      payload: {},
+
+      setRefreshTimeout: function() {
+        var delta = new Date().getTime(),
+            expires = obj.data_expires,
+            align=payload.align;
+        if ( !expires ) {
+          expires=new Date().getTime()/1000;
+          if ( align ) {
+            expires = PxU.epochAlign(expires+align,align);
+          }
+          this.expires = expires;
+        }
+        if ( expires ) {
+          setTimeout( function(obj) {
+              if ( !obj.id ) { return; } // I may bave been destroyed before this timer fires
+              obj.Enable();
+            }, (expires*1000-delta), this.ctl.Refresh );
+        }
+      },
+/**
+ * Initialise the control. Called internally. Simply creates the link object.
+ * @method _init
+ * @private
+ * @param args {object} the arguments passed into the contructor
+ */
+      _init: function(args) {
+        apc.payload.tooltip = function() {
+                var o=this.obj, delta;
+                if ( !o.expires ) { return; }
+                delta = new Date().getTime()/1000;
+                delta = Math.round(o.expires - delta);
+                if ( delta < 0 ) { return; }
+                return 'Data expires in '+delta+' seconds';
+            };
+        apc.payload.handler = 'doRefresh';
+        var ctl = this.ctl.Refresh = new PHEDEX.Component.Control( _sbx, apc );
+        obj.dom.control.appendChild(ctl.el);
+        this.setRefreshTimeout();
+        this.selfHandler = function(o) {
+          return function(ev,arr) {
+            switch (arr[0]) {
+              case 'gotData': {
+                debugger;
+                break;
+              }
+              case 'activate': {
+                o.ctl.Refresh.Hide();
+                o.ctl.Refresh.Disable();
+                obj.getData();
+                break;
+              }
+            }
+          }
+        }(this);
+        _sbx.listen(this.id,this.selfHandler);
+        this.partnerHandler = function(o) {
+          return function(ev,arr) {
+            switch (arr[0]) {
+              case 'gotData': {
+                o.setRefreshTimeout();
+                break;
+              }
+            }
+          }
+        }(this);
+        _sbx.listen(obj.id,this.partnerHandler);
+      },
+    }
+  };
+  Yla(this,_construct(this),true);
+  this._init(args);
+  return this;
+};
+
+log('loaded...','info','component-refresh');
