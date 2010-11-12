1264aa5dd2a8b8ae56660067751e91db3027ac72
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2010-11-12 21:13:28 +0000

    add level option

--- perl_lib/PHEDEX/Web/API/Data.pm
+++ perl_lib/PHEDEX/Web/API/Data.pm
@@ -17,11 +17,17 @@ Show data which is registered (injected) to PhEDEx
   dataset                  dataset name to output data for (wildcard support)
   block                    block name to output data for (wildcard support)
   file                     file name to output data for (wildcard support)
+  level                    display level, 'file' or 'block'. when level=block
+                           no file details would be shown. Default is 'file'.
   file_create_since        returns files which were created since this time *
   block_create_since       return blocks which were created since this time
   dataset_create_since     returns datasets which were created since this time
 
- * when no parameters are given, default file_create_since is one day ago
+ * when no parameters are given, default file_create_since or
+   block_create_since is set to one day ago depending on the level
+
+ ** WARNING, even with just 24 hours ago, level=file may return huge
+   among of results. Please be more specific.
 
 =head2 Output
 
@@ -110,9 +116,16 @@ sub data
 {
     my ($core, %h) = @_;
 
-    if (!$h{dataset}&&!$h{block}&&!$h{file}&&!$h{file_create_since})
+    if (!$h{dataset}&&!$h{block}&&!$h{file})
     {
-        $h{file_create_since} = time() - 86400;
+	if (($h{level} eq 'block') && !$h{block_create_since})
+	{
+	    $h{block_create_since} = time() - 86400;
+	}
+	elsif (!$h{file_create_since})
+	{
+            $h{file_create_since} = time() - 86400;
+        }
     }
 
     # check for time format
@@ -124,10 +137,21 @@ sub data
         }
     }
 
+    # check for level
+    if (exists $h{level} && $h{level} ne 'file' && $h{level} ne 'block')
+    {
+        die "level has to be either 'file' or 'block'\n";
+    }
+
     # convert parameter keys to upper caseq
-    foreach ( qw / dataset block file file_create_since block_create_since dataset_create_since / )
+    foreach ( qw / dataset block file level file_create_since block_create_since dataset_create_since / )
     {
       $h{uc $_} = delete $h{$_} if $h{$_};
+    }
+
+    if (not exists $h{LEVEL})
+    {
+        $h{LEVEL} = 'file';
     }
 
     my $r = PHEDEX::Core::Util::flat2tree($map, PHEDEX::Web::SQL::getData($core, %h));
