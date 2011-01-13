0f3c24deaef00ff72138cdfaa730155448a4c1e6
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2011-01-13 16:55:56 +0000

    cleanup, plus better handling of special cases, and styling of the body if needed

--- PhEDExWeb/ApplicationServer/js/nextgen-override.js
+++ PhEDExWeb/ApplicationServer/js/nextgen-override.js
@@ -5,8 +5,6 @@ YAHOO.util.Event.onDOMReady(function() {
   log('initialising','info','app');
   PxL  = new PHEDEX.Loader();
   banner('Loading core application...');
-//   PxL.load(function() {},'phedex-profiler');
-//   PxW.nocache = true;
   PxL.load(createCoreApp,'core','sandbox');
 
   var phedex_app_version = document.getElementById('phedex-app-version'),
@@ -29,67 +27,52 @@ function createCoreApp() {
     PxC = new PHEDEX.Core(PxS,PxL);
     PxC.create();
   } catch(ex) { log(ex,'error',name); banner('Error creating Core application!','error'); return; }
-//  try {
-//    PxR = new PHEDEX.Registry(PxS);
-//    PxR.create();
-//  } catch(ex) { log(ex,'error',name); banner('Error creating Registry!','error'); return; }
 
   banner('Core application is running, ready to create PhEDEx data-modules...');
+//   PxU.bannerIdleTimer(PxL,{active:'&nbsp;'});
 
-//  if ( PxR ) { // TODO this needs a better home!
-//    PxS.notify('Registry', 'add', 'phedex-module-agents',            'node',    'Show Agents',             {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-agentlogs',         'node',    'Show Agent Logs',         {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-protovisdemo',      'node',    'Show Agent Update times', {context_item:true, feature_class:'alpha'});
-//    PxS.notify('Registry', 'add', 'phedex-module-pendingrequests',   'node',    'Show Pending Requests',   {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-queuedmigrations',  'node',    'Show Queued Migrations',  {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-linkview',          'node',    'Show Links',              {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-nodes',             'none',    'Show Nodes');
-//    PxS.notify('Registry', 'add', 'phedex-module-custodiallocation', 'node',    'Show Custodial Data',     {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-custodiallocation', 'block',   'Show Custodial Location', {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-databrowser',       'dataset', 'Show Data',               {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-databrowser',       'block',   'Show Data',               {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-consistencyresults','node',    'Show Consistency Results',{context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-unroutabledata',    'node',    'Show Unroutable Data',    {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-consistencyresults','block',   'Show Consistency Results',{context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-blocklocation',     'block',   'Show Block Location',     {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-missingfiles',      'block',   'Show Missing Files',      {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-groupusage',        'group',   'Show Group Usage',        {context_item:true});
-//    PxS.notify('Registry', 'add', 'phedex-module-pendingrequests',   'group',   'Show Pending Requests',   {context_item:true});
-//
-//    PxS.notify('Registry', 'add', 'phedex-module-shift-requestedqueued', 'none',     'Shift: Requested vs. Queued data', {context_item:false, feature_class:'beta'});
-//  }
-
-  PxU.bannerIdleTimer(PxL);
-
-  var page = location.href;
-  if ( page.match(/([^/]*)$/) ) { page = RegExp.$1; }
-  if ( page.match(/^(.*)\?/) ) { page = RegExp.$1; }
-
-  var el = document.getElementById(page);
+  var page=location.href, el, uri, params={}, substrs, i, ngoSuccess;
+  if ( page.match(/^(.*)\?/)  )        { page = RegExp.$1; }
+  if ( page.match(/([^/]*)(.html)$/) ) { page = RegExp.$1; }
+  if ( page.match(/([^/]*)?$/) )       { page = RegExp.$1; }
+  el = document.getElementById(page);
 
   if ( el ) {
+    banner('loading, please wait...');
     page = page.replace(/::/g,'-');
     page = page.toLowerCase();
     page = 'phedex-nextgen-'+page;
-    var ngoSuccess = function(item,e) {
+
+    uri = location.search;
+    if ( uri.match(/^\?(.*)$/) ) {
+      substrs = RegExp.$1.split('&')
+      for (i in substrs ) {
+        if ( substrs[i].match(/^([^=]*)=(.*)$/) ) {
+          params[RegExp.$1] = RegExp.$2;
+        } else {
+          params[substrs[i]] = true;
+        }
+      }
+    }
+    ngoSuccess = function(item,e) {
       return function() {
         var cTor = PxU.getConstructor(item);
         try {
           var obj = new cTor(PxS,item);
-          obj.init();
-          PxS.notify(obj.id,'useElement',e);
+          obj.useElement(e);
+          obj.init(params);
         } catch(ex) {
           var a = ex;
         }
       };
     }(page,el);
-    var ngoFailure = function(item) { }
     var callbacks = {
                       Success:  ngoSuccess,
-                      Failure:  ngoFailure,
-                      Timeout:  function(item) { banner('Timeout loading javascript modules'); }
+                      Failure:  function(item) { },
+                      Timeout:  function(item) { banner('Timeout loading javascript modules'); },
                       Progress: function(item) { banner('Loaded item: '+item.name); }
                     };
     PxL.load(callbacks,page,'datasvc');
+    document.body.className = 'yui-skin-sam';
   }
 };
