5149eb96a8f0c4d382df6182469fa8bc59ec261e
Alberto Sanchez Hernandez <sha1-3eef4a9475415d049cde0062419e962bb6786a20@cern.ch>
2011-01-28 17:11:43 +0000

    making summary report plugin for watchdog more human readable

--- perl_lib/PHEDEX/Monitoring/Reports/summary.pm
+++ perl_lib/PHEDEX/Monitoring/Reports/summary.pm
@@ -19,20 +19,24 @@ sub new
 
 }
 
+# It will generate a human readable summary based on the agent statistics information.
+# this report is generate for all agents monitored by Watch Agent
+# It is expected that output is return in a text string varable to be printed, mailed, etc.
+
 sub generate_it {
   my ($self,%h) = @_;
   map { $self->{$_} = $h{$_} } keys %h;
 
-  my $summary = "STATISTICS SUMMARY\n";
+  my $summary = "Statistics summary for all agents\n";
   foreach my $agent ( sort keys %{$self->{AGENTS}} )
   {
 #   skip me
     next if $self->{AGENTS}{$agent}{self}{stats};
-    $summary .= "\t $agent: \n";
-    foreach my $key ( keys %{$self->{AGENTS}{$agent}{resources}} )
-    {
-       $summary .= "\t \t $key = $self->{AGENTS}{$agent}{resources}{$key} \n";
-    } 
+    $summary .= "Agent:$agent\t Total\t Delta(since last report)\n";
+    $summary .= " User CPU Time\t\t $self->{AGENTS}{$agent}{resources}{utime}\t $self->{AGENTS}{$agent}{resources}{dutime}\n";
+    $summary .= " System CPU Time\t $self->{AGENTS}{$agent}{resources}{stime}\t $self->{AGENTS}{$agent}{resources}{dstime}\n";
+    $summary .= " Memory\t\t $self->{AGENTS}{$agent}{resources}{vsize}\t $self->{AGENTS}{$agent}{resources}{dvsize}\n";
+    $summary .= "\n";
   }
 
   return $summary;
