fae4ca3f5f1cab04c1313ca0708766a42238c47d
Nicolo Magini <sha1-61c9148c72dc9af330b61448b73df2d65993435c@cern.ch>
2011-03-25 17:16:28 +0000

    Don't remove subscriptions for delete&retransfer requests

--- perl_lib/PHEDEX/Web/API/UpdateRequest.pm
+++ perl_lib/PHEDEX/Web/API/UpdateRequest.pm
@@ -258,8 +258,10 @@ sub approve
 	      }
             }
           } elsif ( $request->{TYPE} eq 'delete' ) {
-	    &PHEDEX::RequestAllocator::Core::deleteSubscriptionsForRequest($core, $rid, $node->{NODE_ID}, $now);
-	    &PHEDEX::RequestAllocator::Core::addDeletionsForRequest($core, $rid, $node->{NODE_ID}, $now);
+              if ( $request->{RM_SUBSCRIPTIONS} eq 'y' ) {
+	        &PHEDEX::RequestAllocator::Core::deleteSubscriptionsForRequest($core, $rid, $node->{NODE_ID}, $now);
+	      }
+              &PHEDEX::RequestAllocator::Core::addDeletionsForRequest($core, $rid, $node->{NODE_ID}, $now);
           } else {
             # This is impossible because of the checks above, but anyway...
             die("Request $rid: TYPE is neither 'xfer' nor 'delete' ($request->{TYPE})");
