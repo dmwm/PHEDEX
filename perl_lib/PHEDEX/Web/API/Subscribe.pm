4cbfff0aa9b93107e2e18f829fa60916c3e5bb04
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2011-04-11 14:03:14 +0000

    allow password authentication, comment out (in preparation for removal) the old 3.3 schema code

--- perl_lib/PHEDEX/Web/API/Subscribe.pm
+++ perl_lib/PHEDEX/Web/API/Subscribe.pm
@@ -6,7 +6,7 @@ use PHEDEX::Core::XML;
 use PHEDEX::Core::Timing;
 use PHEDEX::Core::Util qw( arrayref_expand );
 use PHEDEX::Core::Identity;
-#use PHEDEX::RequestAllocator::Core;
+use PHEDEX::RequestAllocator::Core;
 use PHEDEX::Web::Util;
 use PHEDEX::Core::Mail;
 use URI::Escape;
@@ -74,11 +74,11 @@ sub invoke { return subscribe(@_); }
 sub subscribe
 {
     my ($core, %args) = @_;
-eval ("use PHEDEX::RequestAllocator::Core");
-die $@ if $@;
+#eval ("use PHEDEX::RequestAllocator::Core");
+#die $@ if $@;
 
-    my @version = PHEDEX::Core::SQL::getSchemaVersion($core);
-    if ( $version[0] < 4 ) { return subscribe_33($core,%args); }
+#    my @version = PHEDEX::Core::SQL::getSchemaVersion($core);
+#    if ( $version[0] < 4 ) { return subscribe_33($core,%args); }
 
     &checkRequired(\%args, qw(data node group));
     die "group $args{group} is forbidden" if ($args{group} =~ m/^deprecated-/);
@@ -106,15 +106,16 @@ die $@ if $@;
     }
 
     # check authentication
-    $core->{SECMOD}->reqAuthnCert();
+    if ( ! ($core->{SECMOD}->isSecure() &&
+            $core->{SECMOD}->isAuthenticated() ) )
+    {
+      die "You must be authenticated to access this API\n";
+    }
     my $auth;
-    if ( $args{request_only} == 'n' ) {
+    if ( $args{request_only} eq 'n' ) {
       $auth = $core->getAuth('datasvc_subscribe');
     } else {
       $auth = $core->getAuth();
-    }
-    if (! $auth->{STATE} eq 'cert' ) {
-	die("Certificate authentication failed\n");
     }
 
     # ok, now try to make the request and subscribe it
@@ -133,33 +134,36 @@ die $@ if $@;
     my $rid2;
     eval
     {
-	my $id_params = &PHEDEX::Core::Identity::getIdentityFromSecMod( $core, $core->{SECMOD} );
-	my $identity = &PHEDEX::Core::Identity::fetchAndSyncIdentity( $core,
-								      AUTH_METHOD => 'CERTIFICATE',
-								      %$id_params );
-	my $client_id = &PHEDEX::Core::Identity::logClientInfo($core,
-							       $identity->{ID},
-							       "Remote host" => $core->{REMOTE_HOST},
-							       "User agent"  => $core->{USER_AGENT} );
+	my ($rid,@valid_args,$client_id,$identity,$id_params,$auth_method);
+	if ( $core->{SECMOD}->isCertAuthenticated() ) { $auth_method = 'CERTIFICATE'; }
+	if ( $core->{SECMOD}->isPasswdAuthenticated() ) { $auth_method = 'PASSWORD'; }
+	$id_params = &PHEDEX::Core::Identity::getIdentityFromSecMod( $core, $core->{SECMOD} );
+	$identity = &PHEDEX::Core::Identity::fetchAndSyncIdentity( $core,
+								   AUTH_METHOD => $auth_method,
+								   %$id_params );
+	$client_id = &PHEDEX::Core::Identity::logClientInfo($core,
+							    $identity->{ID},
+							    "Remote host" => $core->{REMOTE_HOST},
+							    "User agent"  => $core->{USER_AGENT} );
    
-	my @valid_args = &PHEDEX::RequestAllocator::Core::validateRequest($core, $data, $nodes,
-									  TYPE => 'xfer',
-									  LEVEL => $args{level},
-									  PRIORITY => $args{priority},
-									  IS_MOVE => $args{move},
-									  IS_STATIC => $args{static},
-									  IS_CUSTODIAL => $args{custodial},
-									  USER_GROUP => $args{group},
-									  TIME_START => $args{time_start},
-									  IS_TRANSIENT => 'n',
-									  IS_DISTRIBUTED => 'n',
-									  COMMENTS => $args{comments},
-									  CLIENT_ID => $client_id,
-									  INSTANCE => $core->{INSTANCE},
-									  NOW => $now
-									  );
+	@valid_args = &PHEDEX::RequestAllocator::Core::validateRequest($core, $data, $nodes,
+								       TYPE => 'xfer',
+								       LEVEL => $args{level},
+								       PRIORITY => $args{priority},
+								       IS_MOVE => $args{move},
+								       IS_STATIC => $args{static},
+								       IS_CUSTODIAL => $args{custodial},
+								       USER_GROUP => $args{group},
+								       TIME_START => $args{time_start},
+								       IS_TRANSIENT => 'n',
+								       IS_DISTRIBUTED => 'n',
+								       COMMENTS => $args{comments},
+								       CLIENT_ID => $client_id,
+								       INSTANCE => $core->{INSTANCE},
+								       NOW => $now
+								      );
 
-	my $rid = &PHEDEX::RequestAllocator::Core::createRequest($core, @valid_args);
+	$rid = &PHEDEX::RequestAllocator::Core::createRequest($core, @valid_args);
         $rid2 = $rid;
 	$requests = &PHEDEX::RequestAllocator::Core::getTransferRequests($core, REQUESTS => [$rid]);
 	unless ($args{request_only} eq 'y') {
@@ -224,139 +228,142 @@ die $@ if $@;
     return { request_created  => \@req_ids };
 }
 
-sub subscribe_33
-{
-    my ($core, %args) = @_;
-eval ("require PHEDEX::RequestAllocator::wrapper");
-die $@ if $@;
-    &checkRequired(\%args, qw(data node));
-    # default values for options
-    $args{priority} ||= 'low';
-    $args{move} ||= 'n';
-    $args{static} ||= 'n';
-    $args{custodial} ||= 'n';
-    $args{request_only} ||= 'n';
-    $args{level} ||= 'DATASET'; $args{level} = uc $args{level};
-    $args{no_mail} ||= 'n';
-
-    # check values of options
-    my %priomap = ('high' => 0, 'normal' => 1, 'low' => 2);
-    die "unknown priority, allowed values are 'high', 'normal' or 'low'" 
-	unless exists $priomap{$args{priority}};
-    $args{priority} = $priomap{$args{priority}}; # translate into numerical value
-
-    foreach (qw(move static custodial request_only)) {
-	die "'$_' must be 'y' or 'n'" unless $args{$_} =~ /^[yn]$/;
-    }
-
-    unless (grep $args{level} eq $_, qw(DATASET BLOCK)) {
-	die "'level' must be either 'dataset' or 'block'";
-    }
-
-    # check authentication
-    $core->{SECMOD}->reqAuthnCert();
-    my $auth = $core->getAuth('datasvc_subscribe');
-    if (! $auth->{STATE} eq 'cert' ) {
-	die("Certificate authentication failed\n");
-    }
-
-    # check authorization
-    my $nodes = [ arrayref_expand($args{node}) ];  
-    foreach my $node (@$nodes) {
-	my $nodeid = $auth->{NODES}->{$node} || 0;
-	die("You are not authorised to subscribe data to node $node") unless $nodeid;
-    }
-
-    # ok, now try to make the request and subscribe it
-    my $now = &mytimeofday();
-    my $data = PHEDEX::Core::XML::parseData( XML => $args{data} );
-    # only one DBS allowed for the moment...  (FIXME??)
-    die "multiple DBSes in data XML.  Only data from one DBS may be subscribed at a time\n"
-	if scalar values %{$data->{DBS}} > 1;
-    ($data) = values %{$data->{DBS}};
-    $data->{FORMAT} = 'tree';
-
-    my $requests;
-    my $rid2;
-    eval
-    {
-	my $id_params = &PHEDEX::Core::Identity::getIdentityFromSecMod( $core, $core->{SECMOD} );
-	my $identity = &PHEDEX::Core::Identity::fetchAndSyncIdentity( $core,
-								      AUTH_METHOD => 'CERTIFICATE',
-								      %$id_params );
-	my $client_id = &PHEDEX::Core::Identity::logClientInfo($core,
-							       $identity->{ID},
-							       "Remote host" => $core->{REMOTE_HOST},
-							       "User agent"  => $core->{USER_AGENT} );
-
-	my @valid_args = &PHEDEX::RequestAllocator::Core::validateRequest($core, $data, $nodes,
-									  TYPE => 'xfer',
-									  LEVEL => $args{level},
-									  PRIORITY => $args{priority},
-									  IS_MOVE => $args{move},
-									  IS_STATIC => $args{static},
-									  IS_CUSTODIAL => $args{custodial},
-									  USER_GROUP => $args{group},
-									  IS_TRANSIENT => 'n',
-									  IS_DISTRIBUTED => 'n',
-									  COMMENTS => $args{comments},
-									  CLIENT_ID => $client_id,
-									  NOW => $now
-									  );
-
-	my $rid = &PHEDEX::RequestAllocator::Core::createRequest($core, @valid_args);
-        $rid2 = $rid;
-	$requests = &PHEDEX::RequestAllocator::Core::getTransferRequests($core, REQUESTS => [$rid]);
-	unless ($args{request_only} eq 'y') {
-	    foreach my $request (values %$requests) {
-		$rid = $request->{ID};
-		foreach my $node (values %{$request->{NODES}}) {
-		    # Check if user is authorized for this node
-		    if (! $auth->{NODES}->{ $node->{NODE} }) {
-			die "You are not authorised to subscribe data to node $node->{NODE}\n";
-		    }
-		    # Set the decision
-		    &PHEDEX::RequestAllocator::Core::setRequestDecision($core, $rid, 
-									$node->{NODE_ID}, 'y', $client_id, $now);
-		    
-		    # Add the subscriptions (or update the move source)
-		    if ($node->{POINT} eq 'd') {
-			&PHEDEX::RequestAllocator::Core::addSubscriptionsForRequest($core, $rid, $node->{NODE_ID}, $now);
-		    } elsif ($node->{POINT} eq 's') {
-			&PHEDEX::RequestAllocator::Core::updateMoveSubscriptionsForRequest($core, $rid, 
-											   $node->{NODE_ID}, $now);
-		    }
-		}
-	    }
-	}
-    };
-    if ( $@ )
-    {
-	$core->{DBH}->rollback(); # Processes seem to hang without this!
-	die $@;
-    }
-
-    # determine if we commit
-    my $commit = 0;
-    if (%$requests) {
-	$commit = 1;
-    } else {
-	die "no requests were created\n";
-	$core->{DBH}->rollback();
-    }
-    $commit = 0 if $args{dummy};
-    $commit ? $core->{DBH}->commit() : $core->{DBH}->rollback();
-    # send out notification
-    if ($args{no_mail} eq 'n')
-    {
-      eval { # try to send mail, but don't die if it fails!
-        PHEDEX::Core::Mail::send_request_create_email($core, $rid2) if $commit;
-      };
-    }
-    
-    # for output, we return a list of the generated request IDs
-    my @req_ids = map { { id => $_ } } keys %$requests;
-    return { request_created  => \@req_ids };
-}
+#sub subscribe_33
+#{
+#    my ($core, %args) = @_;
+#eval ("require PHEDEX::RequestAllocator::wrapper");
+#die $@ if $@;
+#    &checkRequired(\%args, qw(data node));
+#    # default values for options
+#    $args{priority} ||= 'low';
+#    $args{move} ||= 'n';
+#    $args{static} ||= 'n';
+#    $args{custodial} ||= 'n';
+#    $args{request_only} ||= 'n';
+#    $args{level} ||= 'DATASET'; $args{level} = uc $args{level};
+#    $args{no_mail} ||= 'n';
+#
+#    # check values of options
+#    my %priomap = ('high' => 0, 'normal' => 1, 'low' => 2);
+#    die "unknown priority, allowed values are 'high', 'normal' or 'low'" 
+#	unless exists $priomap{$args{priority}};
+#    $args{priority} = $priomap{$args{priority}}; # translate into numerical value
+#
+#    foreach (qw(move static custodial request_only)) {
+#	die "'$_' must be 'y' or 'n'" unless $args{$_} =~ /^[yn]$/;
+#    }
+#
+#    unless (grep $args{level} eq $_, qw(DATASET BLOCK)) {
+#	die "'level' must be either 'dataset' or 'block'";
+#    }
+#
+#    # check authentication
+#    $core->{SECMOD}->reqAuthnCert();
+#    my $auth = $core->getAuth('datasvc_subscribe');
+#    if (! $auth->{STATE} eq 'cert' ) {
+#	die("Certificate authentication failed\n");
+#    }
+#
+#    # check authorization
+#    my $nodes = [ arrayref_expand($args{node}) ];  
+#    foreach my $node (@$nodes) {
+#	my $nodeid = $auth->{NODES}->{$node} || 0;
+#	die("You are not authorised to subscribe data to node $node") unless $nodeid;
+#    }
+#
+#    # ok, now try to make the request and subscribe it
+#    my $now = &mytimeofday();
+#    my $data = PHEDEX::Core::XML::parseData( XML => $args{data} );
+#    # only one DBS allowed for the moment...  (FIXME??)
+#    die "multiple DBSes in data XML.  Only data from one DBS may be subscribed at a time\n"
+#	if scalar values %{$data->{DBS}} > 1;
+#    ($data) = values %{$data->{DBS}};
+#    $data->{FORMAT} = 'tree';
+#
+#    my $requests;
+#    my $rid2;
+#    eval
+#    {
+#	my ($rid,@valid_args,$client_id,$identity,$id_params,$auth_method);
+#	if ( $core->{SECMOD}->isCertAuthenticated() ) { $auth_method = 'CERTIFICATE'; }
+#	if ( $core->{SECMOD}->isPasswdAuthenticated() ) { $auth_method = 'PASSWORD'; }
+#	$id_params = &PHEDEX::Core::Identity::getIdentityFromSecMod( $core, $core->{SECMOD} );
+#	$identity = &PHEDEX::Core::Identity::fetchAndSyncIdentity( $core,
+#								   AUTH_METHOD => $auth_method,
+#								   %$id_params );
+#	$client_id = &PHEDEX::Core::Identity::logClientInfo($core,
+#							    $identity->{ID},
+#							    "Remote host" => $core->{REMOTE_HOST},
+#							    "User agent"  => $core->{USER_AGENT} );
+#
+#	@valid_args = &PHEDEX::RequestAllocator::Core::validateRequest($core, $data, $nodes,
+#								       TYPE => 'xfer',
+#								       LEVEL => $args{level},
+#								       PRIORITY => $args{priority},
+#								       IS_MOVE => $args{move},
+#								       IS_STATIC => $args{static},
+#								       IS_CUSTODIAL => $args{custodial},
+#								       USER_GROUP => $args{group},
+#								       IS_TRANSIENT => 'n',
+#								       IS_DISTRIBUTED => 'n',
+#								       COMMENTS => $args{comments},
+#								       CLIENT_ID => $client_id,
+#								       NOW => $now
+#								      );
+#
+#	$rid = &PHEDEX::RequestAllocator::Core::createRequest($core, @valid_args);
+#        $rid2 = $rid;
+#	$requests = &PHEDEX::RequestAllocator::Core::getTransferRequests($core, REQUESTS => [$rid]);
+#	unless ($args{request_only} eq 'y') {
+#	    foreach my $request (values %$requests) {
+#		$rid = $request->{ID};
+#		foreach my $node (values %{$request->{NODES}}) {
+#		    # Check if user is authorized for this node
+#		    if (! $auth->{NODES}->{ $node->{NODE} }) {
+#			die "You are not authorised to subscribe data to node $node->{NODE}\n";
+#		    }
+#		    # Set the decision
+#		    &PHEDEX::RequestAllocator::Core::setRequestDecision($core, $rid, 
+#									$node->{NODE_ID}, 'y', $client_id, $now);
+#		    
+#		    # Add the subscriptions (or update the move source)
+#		    if ($node->{POINT} eq 'd') {
+#			&PHEDEX::RequestAllocator::Core::addSubscriptionsForRequest($core, $rid, $node->{NODE_ID}, $now);
+#		    } elsif ($node->{POINT} eq 's') {
+#			&PHEDEX::RequestAllocator::Core::updateMoveSubscriptionsForRequest($core, $rid, 
+#											   $node->{NODE_ID}, $now);
+#		    }
+#		}
+#	    }
+#	}
+#    };
+#    if ( $@ )
+#    {
+#	$core->{DBH}->rollback(); # Processes seem to hang without this!
+#	die $@;
+#    }
+#
+#    # determine if we commit
+#    my $commit = 0;
+#    if (%$requests) {
+#	$commit = 1;
+#    } else {
+#	die "no requests were created\n";
+#	$core->{DBH}->rollback();
+#    }
+#    $commit = 0 if $args{dummy};
+#    $commit ? $core->{DBH}->commit() : $core->{DBH}->rollback();
+#    # send out notification
+#    if ($args{no_mail} eq 'n')
+#    {
+#      eval { # try to send mail, but don't die if it fails!
+#        PHEDEX::Core::Mail::send_request_create_email($core, $rid2) if $commit;
+#      };
+#    }
+#    
+#    # for output, we return a list of the generated request IDs
+#    my @req_ids = map { { id => $_ } } keys %$requests;
+#    return { request_created  => \@req_ids };
+#}
 
 1;
