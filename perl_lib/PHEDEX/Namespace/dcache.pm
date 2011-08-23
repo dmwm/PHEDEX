f10d9d80d1fc33eff8110d65e2269745de1d692a
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2011-08-23 14:21:41 +0000

    remove debug lines

--- perl_lib/PHEDEX/Namespace/dcache.pm
+++ perl_lib/PHEDEX/Namespace/dcache.pm
@@ -40,13 +40,11 @@ sub new
   if ( exists($self->{AGENT})) {
     if ( exists($self->{AGENT}->{NOCACHE}) &&
        $self->{AGENT}->{NOCACHE} != $self->{NOCACHE}) { $self->{NOCACHE} = $self->{AGENT}->{NOCACHE}; }
-$DB::single=1;
     foreach ( qw / INPUT_FILE PRELOAD / ) {
       if ( exists($self->{AGENT}->{$_}) ) { $self->{$_} = $self->{AGENT}->{$_}; }
     }
   }
   $self->SUPER::_init( NAMESPACE => __PACKAGE__ );
-$DB::single=1;
   $self->{ENV} = 'LD_PRELOAD=' . $self->{PRELOAD};
 
 # This is where the interface-specific modules are loaded. The modules are
