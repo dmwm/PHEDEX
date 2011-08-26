b7a3ddf4bce3bca7a58e61c0ed55e94ee412e548
Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch>
2011-08-26 12:33:50 +0000

    In execute: now expects lfn and tfc input arguments insteadof just pfn, and does lfn2pfn conversion internally.

--- perl_lib/PHEDEX/Namespace/srmv2/Common.pm
+++ perl_lib/PHEDEX/Namespace/srmv2/Common.pm
@@ -1,7 +1,7 @@
 package PHEDEX::Namespace::srmv2::Common;
 # Factor out some common functionality, specifically, the 'execute' routine
 use File::Basename;
-
+use PHEDEX::Core::Catalogue ( qw / lfn2pfn / );
 sub new
 {
 # Dummy new returns a valid but empty object, so the UNIVERSAL 'can' method
@@ -14,17 +14,13 @@ sub execute
 # 'execute' will use the common 'Command' function to do the work, but on the
 # base directory, not on the file itself. This lets it cache the results for
 # an entire directory instead of having to go back to the SE for every file 
-  my ($self,$ns,$file,$call) = @_;
-  my ($dir,$result);
+  my ($self,$ns,$file,$tfc,$call) = @_;
+
+  my ($dir,$pfn,$result);
   $ns->proxy_check;
-  return $ns->Command($call,$file);
+  $pfn = $tfc->lfn2pfn($file,$ns->Protocol());
+  return $ns->Command($call,$pfn);
 
-#  return $ns->Command($call,$file) if $ns->{NOCACHE};
-
-#  $dir = dirname $file;
-#  $ns->Command($call,$dir);
-## Explicitly pull the right value from the cache
-#  return $ns->{CACHE}->fetch($call,$file);
 }
 
 1;
