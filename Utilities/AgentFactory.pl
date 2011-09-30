d6c880a96c652283df905efeb76c88c83ac4c961
Nicolo Magini <sha1-61c9148c72dc9af330b61448b73df2d65993435c@cern.ch>
2011-09-30 13:15:37 +0000

    - Fix type of 'summary_interval' option (integer instead of string) - Add basic help for options

--- Utilities/AgentFactory.pl
+++ Utilities/AgentFactory.pl
@@ -8,6 +8,23 @@ use warnings;
 ##H start multiple agents. Please don't use it from the command-line unless
 ##H you really know what you are doing.
 ##H
+##H -state             agent state directory
+##H -node              node name for which this agent runs
+##H -db                database connection configuration parameter file
+##H -log               where to redirect logging information
+##H -agent_list        label (as defined in the config file) of the
+##H                     agent started and monitored by the watchdog.
+##H                     To run multiple agents, the option should be
+##H                     repeated multiple times. 
+##H -limit             limit imposed on agent resource usage, passed
+##H                     in the format 'agentlabel,resource,limitvalue'
+##H                     Can be repeated multiple times.
+##H -summary_interval  time (in seconds) between summary report generation.
+##H                     Default is 86400 (24 hours)
+##H -report_plugin     format used to produce the summary report. Default is
+##H                     'summary' (human readable text)
+##H -notify_plugin     plugin used to notify the summary report. Default is
+##H                     'log' (print report to watchdog logfile)
 
 ######################################################################
 my (%args,$Factory,$Config);
@@ -26,7 +43,7 @@ use PHEDEX::Core::Config::Factory;
 	     "agent_list=s@"	=> sub { push(@{$args{AGENT_LIST}}, split(/,/, $_[1])) },
 	     "limit=s@"		=> \$args{LIMIT},
 	     "memuse"		=> sub { eval "use Devel::Size"; },
-             "summary_interval=s" => \$args{_SUMMARY_INTERVAL},
+             "summary_interval=i" => \$args{_SUMMARY_INTERVAL},
              "report_plugin=s"    => \$args{_REPORT_PLUGIN},
              "notify_plugin=s"    => \$args{_NOTIFY_PLUGIN},
 	     );
