88eeffd7bf9c3ce4f1bcae34cc4612b4e47ac31f
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2011-10-14 13:24:12 +0000

    correct a typo in documentation

--- perl_lib/PHEDEX/Web/API/UpdateSubscription.pm
+++ perl_lib/PHEDEX/Web/API/UpdateSubscription.pm
@@ -58,7 +58,7 @@ Update user_group, priority and/or suspend_until of a existing subscription
   suspend_until    time suspension expires 
   percent_files    percentage of files at destination
   percent_bytes    percentage of bytes at destination
-  start_time       time when transfer started (only active transfers have start time)
+  time_start       time when transfer started (only active transfers have start time)
 
 =head3 <dataset> attributes:
 
