612d3e6c0f0cb12fa8878d7cc919442e37e10105
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2011-10-26 16:03:21 +0000

    now it works (always a good thing...)

--- PhEDExWeb/ApplicationServer/js/phedex-history.js
+++ PhEDExWeb/ApplicationServer/js/phedex-history.js
@@ -1,8 +1,13 @@
-PHEDEX.History = new (function() {
+PHEDEX.History = function( config ) {
   var _sbx = new PHEDEX.Sandbox(),
       Dom = YAHOO.util.Dom,
       YuH = YAHOO.util.History,
-      id = 'history_'+PxU.Sequence();
+      id = 'history_'+PxU.Sequence(),
+      module = 's',
+      undefined = 'undefined';
+  if ( typeof(config) == 'object' ) {
+    if ( config.module ) { module = config.module; }
+  }
   log('creating "'+id+'"','info','history');
 
   _construct = function() {
@@ -10,27 +15,57 @@ PHEDEX.History = new (function() {
       id: id,
       me: 'history',
       meta: {},
+      handler: function(obj) {
+        return function(ev,arr) {
+          switch (arr[0]) {
+            case 'navigate': {
+              var s = YAHOO.util.History.getQueryStringParameter(module,arr[1]) || undefined;
+              YuH.navigate(module,s);
+              break;
+            }
+            default: {
+              break;
+            }
+          }
+        };
+      }(this),
+      onStateChange: function(state) {
+        _sbx.notify('History','onStateChange',state);
+      },
       init: function() {
+        var initialState = YuH.getBookmarkedState(module) ||
+                           YuH.getQueryStringParameter(module) ||
+                           undefined;
+        YuH.register(module,initialState,this.onStateChange,null,this);
+
         try {
-          YuH.initialize("yui-history-field", "yui-history-iframe");
+          YuH.initialize('yui-history-field','yui-history-iframe');
         } catch (ex) {
           var _ex=ex;
           return;
         }
 
-debugger;
-        var initialState = YuH.getBookmarkedState('page');
-        if ( initialState ) { _sbx.notify(this.id,'History',initialState); }
-      },
-      setState: function() {
-debugger;
+        YuH.onReady(function(obj) {
+          return function() {
+            var currentState = YuH.getCurrentState(module);
+            _sbx.notify('History','initialiseApplication',currentState);
+          }
+        }(this));
+
+        _sbx.listen('History',this.handler);
+        _sbx.listen(this.id,  this.handler);
       },
     };
   };
 
   Yla(this, _construct());
+try {
   this.init();
+} catch (ex) {
+var _ex = ex;
+debugger;
+}
   return this;
-})();
+}
 
 log('loaded...','info','history');
