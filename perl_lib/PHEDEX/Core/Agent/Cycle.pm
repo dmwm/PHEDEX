2ce6fb243be1c2a1337b9afd1ce93165e9bc0934
Alberto Sanchez Hernandez <sha1-3eef4a9475415d049cde0062419e962bb6786a20@cern.ch>
2011-11-01 02:10:25 +0000

    modified cycle methods. They need some work

--- perl_lib/PHEDEX/Core/Agent/Cycle.pm
+++ perl_lib/PHEDEX/Core/Agent/Cycle.pm
@@ -2,8 +2,10 @@ package PHEDEX::Core::Agent::Cycle;
 
 use strict;
 use warnings;
+use POE;
+use Data::Dumper;
 
-our @all_cycle =  qw //; #/ parameters needed from calling class
+our @all_cycle =  qw / SESSION_ID stats _start _DONTSTOPME _DOINGSOMETHING WAITTIME VERBOSE pmon STATISTICS_DETAIL ME /; #/ parameters needed from calling class
 
 sub new
 {
@@ -11,18 +13,20 @@ sub new
   my $class = ref($proto) || $proto;
   my %h = @_;
   my $self = {};
+  bless $self, $class;
 
 # Map requiered parameters from calling Class 
-  map { $self->{$_} = ${$h{_AC}}{$_} } @all_cycle;
-  $self->{_AC} = $h{_AC};
+#  map { $self->{$_} = ${$h{_AC}}{$_} } @all_cycle;
+
+  $self->{_AL} = $h{_AL};
 
 # Start a POE session for myself
   POE::Session->create
-      (
-        object_states =>
-        [
-          $self =>
-          {
+     (
+      object_states =>
+      [
+         $self =>
+         {
             _preprocess         => '_preprocess',
             _process_start      => '_process_start',
             _process_stop       => '_process_stop',
@@ -33,11 +37,11 @@ sub new
             _stop    => '_stop',
             _child   => '_child',
             _default => '_default',
-          },
-        ],
-      );
+         },
+      ],
+     );
 
-  return bless $self, $class;
+  return $self;
 }
 
 # this workaround is ugly but allow us NOT rewrite everything
@@ -48,32 +52,45 @@ sub AUTOLOAD
   $attr =~ s/.*:://;
   return unless $attr =~ /[^A-Z]/;      # skip all-cap methods
 
+  print "Calling $attr from Cycle module\n";
 # if $attr exits, catch the reference to it
-  if ( $self->{_AC}->can($attr) ) { $self->{_AC}->$attr(@_); } 
+  if ( $self->{_AL}->can($attr) ) { $self->{_AL}->$attr(@_); } 
   else { PHEDEX::Core::Logging::Alert($self,"Un-known method $attr for Cycle"); }     
 }
+
+# Introduced for POE-based agents to allow process to become a true loop
+sub preprocess
+{
+  my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
+
+  # Restore signals.  Oracle apparently is in habit of blocking them.
+  $SIG{INT} = $SIG{TERM} = $SIG{QUIT} = sub { $self->doStop() };
+}
+
+
+
+# Actual session methods below
 
 sub _start
 {
   my ( $self, $kernel, $session ) = @_[ OBJECT, KERNEL, SESSION ];
   $self->Logmsg("starting Agent session (id=",$session->ID,")");
-  $self->{SESSION_ID} = $session->ID;
-# $kernel->post($self->{SESSION_ID},'_preprocess') if $self->can('preprocess');
-  $kernel->yield('_preprocess'); # if $self->can('preprocess');
-  if ( $self->can('_poe_init') )
+  $self->{_AL}->{SESSION_ID} = $session->ID;
+  $kernel->yield('_preprocess');
+  if ( $self->{_AL}->can('_poe_init') )
   {
-    $kernel->state('_poe_init',$self);
+    $kernel->state('_poe_init',$self->{_AL});
     $kernel->yield('_poe_init');
+  }
+  else {
+      print "No poe_init found\n";
   }
   $kernel->yield('_process_start');
   $kernel->yield('_maybeStop');
 
-  $self->Logmsg('STATISTICS: Reporting every ',$self->{STATISTICS_INTERVAL},' seconds, detail=',$self->{STATISTICS_DETAIL});
+  $self->Logmsg('STATISTICS: Reporting every ',$self->{_AL}->{STATISTICS_INTERVAL},' seconds, detail=',$self->{_AL}->{STATISTICS_DETAIL});
   $self->{stats}{START} = time;
-# $self->{stats}{maybeStop} = 0;
-# $self->{stats}{process} = {};
- $kernel->yield('_make_stats');
-
+  $kernel->yield('_make_stats');
   $self->Logmsg("has successfully initialised");
 }
 
@@ -87,7 +104,6 @@ sub _process_start
 {
   my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
   my ($t,$t1);
-# $self->Dbgmsg("starting '_process_start'") if $self->{DEBUG};
 
   if ( exists($self->{stats}{process}) )
   {
@@ -116,11 +132,9 @@ sub _process_start
 # The second, _DONTSTOPME, tells the maybeStop event loop not to allow the
 # agent to exit. Set this if you have critical ongoing events, such as
 # waiting for a subprocess to finish.
-  $self->{_DONTSTOPME} = 1;
-
+  $self->{_AL}->{_DONTSTOPME} = 1;
   $self->process();
 
-# $self->Dbgmsg("ending '_process_start'") if $self->{DEBUG};
   $kernel->yield('_process_stop');
 }
 
@@ -129,14 +143,13 @@ sub _process_stop
   my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
   my $t;
 
-  if ( $self->{_DOINGSOMETHING} )
+  if ( $self->{_AL}->{_DOINGSOMETHING} )
   {
-    $self->Dbgmsg("waiting for something: ",$self->{_DOINGSOMETHING}) if $self->{DEBUG};
+    $self->Dbgmsg("waiting for something: ",$self->{_AL}->{_DOINGSOMETHING}) if $self->{_AL}->{DEBUG};
     $kernel->delay_set('_process_stop',1);
     return;
   }
 
-# $self->Dbgmsg("starting '_process_stop'") if $self->{DEBUG};
   if ( exists($self->{stats}{process}) )
   {
     $t = time;
@@ -144,9 +157,9 @@ sub _process_stop
     $self->{stats}{process}{_offCPU} = $t;
   }
 
-  $self->{_DONTSTOPME} = 0;
-# $self->Dbgmsg("ending '_process_stop'") if $self->{DEBUG};
-  $kernel->delay_set('_process_start',$self->{WAITTIME}) if $self->{WAITTIME};
+  $self->{_AL}->{_DONTSTOPME} = 0;
+
+  $kernel->delay_set('_process_start',$self->{_AL}->{WAITTIME}) if $self->{_AL}->{WAITTIME};
 }
 
 sub _maybeStop
@@ -154,21 +167,22 @@ sub _maybeStop
   my ( $self, $kernel ) = @_[ OBJECT, KERNEL ];
 
   $kernel->delay_set('_maybeStop', 1);
-  my $DontStopMe = $self->{_DONTSTOPME} || 0;
+  my $DontStopMe = $self->{_AL}->{_DONTSTOPME} || 0;
   if ( !$DontStopMe )
   {
-    $self->Dbgmsg("starting '_maybeStop'") if $self->{VERBOSE} >= 3;
+    $self->Dbgmsg("starting '_maybeStop'") if $self->{_AL}->{VERBOSE} >= 3;
     $self->{stats}{maybeStop}++ if exists $self->{stats}{maybeStop};
 
     $self->maybeStop();
-    $self->Dbgmsg("ending '_maybeStop'") if $self->{VERBOSE} >= 3;
+    $self->Dbgmsg("ending '_maybeStop'") if $self->{_AL}->{VERBOSE} >= 3;
   }
+
 }
 
 sub _stop
 {
   my ( $self, $kernel, $session ) = @_[ OBJECT, KERNEL, SESSION ];
-  print $self->Hdr, "ending, for lack of work...\n";
+  print $self->Hdr("ending, for lack of work...\n");
 }
 
 sub _make_stats
@@ -178,7 +192,7 @@ sub _make_stats
   my ($pmon,$h,$onCPU,$offCPU,$count);
 
   $totalWall = $totalOnCPU = $totalOffCPU = 0;
-  $pmon = $self->{pmon};
+  $pmon = $self->{_AL}->{pmon};
   $summary = '';
   $h = $self->{stats};
   if ( exists($h->{maybeStop}) )
@@ -202,7 +216,7 @@ sub _make_stats
       $max = $a[-1];
       $median = $a[int($count/2)];
       $summary .= sprintf(" onCPU(wall=%.2f median=%.2f max=%.2f)",$onCPU,$median,$max);
-      if ( $self->{STATISTICS_DETAIL} > 1 )
+      if ( $self->{_AL}->{STATISTICS_DETAIL} > 1 )
       {
         $summary .= ' onCPU_details=(' . join(',',map { $_=int(1000*$_)/1000 } @a) . ')';
       }
@@ -215,7 +229,7 @@ sub _make_stats
       $totalOffCPU += $offCPU;
       $max = $a[-1];
       $median = $a[int($count/2-0.9)];
-      my $waittime = $self->{WAITTIME} || 0;
+      my $waittime = $self->{_AL}->{WAITTIME} || 0;
       if ( !defined($median) ) { print "median not defined\n"; }
       if ( !defined($max   ) ) { print "max    not defined\n"; }
       $summary .= sprintf(" offCPU(median=%.2f max=%.2f)",$median,$max);
@@ -224,7 +238,7 @@ sub _make_stats
         $delay = $median / $waittime;
         $summary .= sprintf(" delay_factor=%.2f",$delay);
       }
-      if ( $self->{STATISTICS_DETAIL} > 1 )
+      if ( $self->{_AL}->{STATISTICS_DETAIL} > 1 )
       {
         $summary .= ' offCPU_details=(' . join(',',map { $_=int(1000*$_)/1000 } @a) . ')';
       }
@@ -235,13 +249,15 @@ sub _make_stats
 
   if ( $summary )
   {
+
     $summary = 'AGENT_STATISTICS' . $summary;
-    $self->Logmsg($summary) if $self->{STATISTICS_DETAIL};
-    $self->Notify($summary);# ,"\n") if $delay > 1.25;
+    $self->Logmsg($summary) if $self->{_AL}->{STATISTICS_DETAIL};
+    $self->Notify($summary);
   }
 
   my $now = time;
   $totalWall = $now - $self->{stats}{START};
+  print Dumper($self->{stats},$now);
   my $busy= 100*$totalOnCPU/$totalWall;
   $summary = 'AGENT_STATISTICS';
   $summary=sprintf('TotalCPU=%.2f busy=%.2f%%',$totalOnCPU,$busy);
@@ -254,14 +270,14 @@ sub _make_stats
 # If the user explicitly loaded the Devel::Size module, report the size of this agent
   my $size;
   if ( $size = PHEDEX::Monitoring::Process::total_size($self) )
-  { $summary .= " Sizeof($self->{ME})=$size"; }
+  { $summary .= " Sizeof($self->{_AL}->{ME})=$size"; }
   if ( $size = PHEDEX::Monitoring::Process::TotalSizes() )
   { $summary .= " $size"; }
   $summary .= "\n";
 
   $self->Logmsg($summary);
   $self->Notify($summary);
-  $kernel->delay_set('_make_stats',$self->{STATISTICS_INTERVAL});
+  $kernel->delay_set('_make_stats',$self->{_AL}->{STATISTICS_INTERVAL});
 }
 
 # Dummy handler in case it's needed. Let's _default catch the real errors
