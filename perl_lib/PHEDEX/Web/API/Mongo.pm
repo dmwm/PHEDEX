89c7757a771cbdc5cd5618eb4d68c0587beb998b
Xiaomei Zhang <sha1-7dbc72ac7a09390743e9d2224567d4054a171663@cern.ch>
2011-11-17 09:22:55 +0000

    only for post

--- perl_lib/PHEDEX/Web/API/Mongo.pm
+++ perl_lib/PHEDEX/Web/API/Mongo.pm
@@ -13,11 +13,11 @@ PHEDEX::Web::API::Mongo - simple mongodb interface
 
 =head2 DESCRIPTION
 
-GET data, or PUT the posted object
+PUT the posted object
 
 =cut
 
-sub methods_allowed { return ('GET','POST'); }
+sub methods_allowed { return ('POST'); }
 sub duration { return 0; }
 sub invoke { return mongo(@_); }
 sub mongo
@@ -33,36 +33,26 @@ sub mongo
   $db = $conn->SiteSpace;
   $table = $db->$site();
 
-  if ( $method eq 'POST' ) {
-    $dir{_id} = $args{_id} + 0.0;
-    delete($args{_id});
-    $dir{rootdir} = $args{rootdir};
-    delete($args{rootdir});
-    $dir{totalsize} = $args{totalsize} + 0.0;
-    delete($args{totalsize});
-    foreach  (keys %args) {
-      my %temp;
-      $temp{size} = $args{$_} + 0.0;
-      $temp{name} = $_;
-      push(@dirarray, \%temp);
-    }
-    $dir{dir} =\@dirarray;
-    warn "dumping converted arguments ",Data::Dumper->Dump([ \%dir ]);
-    $table->insert(\%dir);
-    push @records, \%dir;
-  } else {
-    my %temp;
-    $temp{'$gt'} = $args{time_since} + 0.0;
-    $temp{'$lt'} = $args{time_until} + 0.0;
-    $dir{_id} = \%temp;
-    warn "dumping converted arguments ",Data::Dumper->Dump([ \%dir ]);
-    $cursor = $table->find(\%dir);
-    #$cursor = $table->find({_id =>{'$gt' => 0,'$lt' => 11}});
-    while ($data = $cursor->next) {
-      warn Data::Dumper->Dump([ $data ]);
-      push @records,$data;
-    }
+  $dir{_id} = $args{_id} + 0.0;
+  $dir{rootdir} = $args{rootdir};
+  $dir{totalsize} = $args{totalsize} + 0.0;
+  $dir{totalfiles} = $args{totalfiles} + 0.0;
+  $dir{totaldirs} = $args{totaldirs} + 0.0;
+  foreach ( qw / _id rootdir totalsize totaldirs totalfiles / )
+  {
+       delete($args{$_});
   }
+  foreach  (keys %args) {
+    my %temp;
+    $temp{size} = $args{$_} + 0.0;
+    $temp{name} = $_;
+    push(@dirarray, \%temp);
+  }
+  $dir{dir} =\@dirarray;
+  warn "dumping converted arguments ",Data::Dumper->Dump([ \%dir ]);
+  $table->insert(\%dir);
+  push @records, \%dir;
+
   warn "dumping records ",Data::Dumper->Dump([ \@records ]);
   return { mongo => \@records };
 }
