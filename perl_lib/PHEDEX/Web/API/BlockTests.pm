6a545b45a580ddef7add736b8b8268ebcd1d5be5
Chih-Hao Huang <sha1-41d12c60362c59dd183b764f2f99853e4aba15ad@cern.ch>
2011-11-18 06:02:31 +0000

    use PHEDEX::Web::Util::validate_params() to validate all input parameters.

--- perl_lib/PHEDEX/Web/API/BlockTests.pm
+++ perl_lib/PHEDEX/Web/API/BlockTests.pm
@@ -64,6 +64,7 @@ Shows block-level results of consistency check tests.
 
 use PHEDEX::Web::SQL;
 use PHEDEX::Core::Util;
+use PHEDEX::Web::Util;
 
 my $map = {
     _KEY => 'NODE',
@@ -95,23 +96,37 @@ sub invoke { return blocktests(@_); }
 sub blocktests
 {
     my ($core, %h) = @_;
-
-    # convert parameter keys to upper case
-    foreach ( qw / node block kind status test_since test / )
+    my %p;
+    eval
     {
-      $h{uc $_} = delete $h{$_} if $h{$_};
+        %p = &validate_params(\%h,
+                uc_keys => 1,
+                allow => [ qw / node block kind status test_since test / ],
+                spec =>
+                {
+                    node => { using => 'node', multiple => 1 },
+                    block => { using => 'block_*', multiple => 1 },
+                    kind => { regex => qr/^cksum$|^size$|^dbs$|^migration$/, multiple => 1 },
+                    status => { regex => qr/^OK$|^Fail$|^Queued$|^Active$|^Timeout$|^Expired$|^Suspended$|^Error$/, multiple => 1 },
+                    test_since => { using => 'time' },
+                 }
+        );
+    };
+    if ($@)
+    {
+        return PHEDEX::Web::Util::http_error(400,$@);
     }
 
     # if there is no argument, set default test_since to 24 hours ago
-    if (scalar keys %h == 0)
+    if (scalar keys %p == 0)
     {
-        $h{TEST_SINCE} = time() - 3600*24;
+        $p{TEST_SINCE} = time() - 3600*24;
     }
 
-    $h{'#DETAILED#'} = 0; # no file info at all
+    $p{'#DETAILED#'} = 0; # no file info at all
 
     # remember to handle the case for status
-    return { node => PHEDEX::Core::Util::flat2tree($map, PHEDEX::Web::SQL::getBlockTestFiles($core, %h)) };
+    return { node => PHEDEX::Core::Util::flat2tree($map, PHEDEX::Web::SQL::getBlockTestFiles($core, %p)) };
 }
 
 1;
