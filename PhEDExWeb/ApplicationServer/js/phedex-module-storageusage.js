d251eda417f1afffce73281c2c9efbb5f73ebc29
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2011-12-02 15:46:04 +0000

    adapt to change of argument name: collName -> se

--- PhEDExWeb/ApplicationServer/js/phedex-module-storageusage.js
+++ PhEDExWeb/ApplicationServer/js/phedex-module-storageusage.js
@@ -2,7 +2,7 @@ PHEDEX.namespace('Module');
 PHEDEX.Module.StorageUsage = function(sandbox, string) {
   Yla(this,new PHEDEX.Protovis(sandbox,string));
 
-  var _sbx = sandbox, collName, level=3;
+  var _sbx = sandbox, se, level=3;
   log('Module: creating a genuine "'+string+'"','info',string);
 
    _construct = function(obj) {
@@ -18,13 +18,46 @@ PHEDEX.Module.StorageUsage = function(sandbox, string) {
       meta: {
       },
       isValid: function() {
-        if ( collName ) { return true; }
+        if ( se ) { return true; }
         return false;
       },
-      getState: function() { return {collName:collName}; },
+      initMe: function() {
+        var el = document.createElement('div');
+        this.dom.content.appendChild(el);
+        this.dom.treemap = el;
+        el = document.createElement('div');
+        el.style.clear = 'both';
+        this.dom.content.appendChild(el);
+        el = document.createElement('div');
+        this.dom.content.appendChild(el);
+        this.dom.stackedChart = el;
+
+        this.selfHandler = function(o) {
+          return function(ev,arr) {
+            var action = arr[0],
+                value = arr[1];
+            switch (action) {
+              case 'mouseover': {
+                break;
+              }
+              case 'mouseout': {
+                break;
+              }
+              case 'click': {
+                break;
+              }
+              default: {
+                break;
+              }
+            }
+          }
+        }(this);
+        _sbx.listen(this.id,this.selfHandler);
+      },
+      getState: function() { return {se:se}; },
       initData: function() {
         this.dom.title.innerHTML = 'Waiting for parameters to be set...';
-        if ( collName ) {
+        if ( se ) {
           _sbx.notify( this.id, 'initData' );
           return;
         }
@@ -32,25 +65,23 @@ PHEDEX.Module.StorageUsage = function(sandbox, string) {
       },
      setArgs: function(arr) {
         if ( !arr )          { return; }
-        if ( !arr.collName ) { return; }
-        if ( arr.collName == collName ) { return; }
-        collName = arr.collName;
+        if ( !arr.se ) { return; }
+        if ( arr.se == se ) { return; }
+        se = arr.se;
         this.dom.title.innerHTML = 'setting parameters...';
         _sbx.notify(this.id,'setArgs');
       },
-
       getData: function() {
-// customisable stuff starts here. Check that the 'collName' is defined. If not, go ask for a value. If yes, get the data
-        if ( !collName ) {
+// customisable stuff starts here. Check that the 'se' is defined. If not, go ask for a value. If yes, get the data
+        if ( !se ) {
           this.initData();
           return;
         }
         this.dom.title.innerHTML = 'fetching data...';
         log('Fetching data','info',this.me);
-        _sbx.notify( this.id, 'getData', { api:'storageusage', args:{collName:collName,level:level} } );
+        _sbx.notify( this.id, 'getData', { api:'storageusage', args:{se:se,level:level} } );
       },
      gotData: function(data,context,response) {
-        var max=0, tmp={}, tree={}, i, j, k, _level, item, node, timebins, timebin, total=0, path;
         PHEDEX.Datasvc.throwIfError(data,response);
         log('Got new data','info',this.me);
         this.dom.title.innerHTML = 'Parsing data';
@@ -59,11 +90,65 @@ PHEDEX.Module.StorageUsage = function(sandbox, string) {
           throw new Error('data incomplete for '+context.api);
         }
 
+//         this.makeStackChart(data);
+        this.makeTreemap(data);
+      },
+      makeStackChart: function(data) {
+        var w = 400,
+            h = 200,
+            x = pv.Scale.linear(0, 9.9).range(0, w),
+            y = pv.Scale.linear(0, 14).range(0, h);
+
+        data = pv.range(4).map(function() {
+          return pv.range(0, 10, .1).map(function(x) {
+              return {x: x, y: Math.sin(x) + Math.random() * .5 + 2};
+            });
+        });
+
+/* The root panel. */
+        var vis = new pv.Panel()
+            .canvas(this.dom.stackedChart)
+            .width(w)
+            .height(h)
+            .bottom(20)
+            .left(20)
+            .right(10)
+            .top(5);
+
+/* X-axis and ticks. */
+        vis.add(pv.Rule)
+            .data(x.ticks())
+            .visible(function(d) { return d;})
+            .left(x)
+            .bottom(-5)
+            .height(5)
+          .anchor('bottom').add(pv.Label)
+            .text(x.tickFormat);
+
+/* The stack layout. */
+        vis.add(pv.Layout.Stack)
+            .layers(data)
+            .x(function(d) { return x(d.x); })
+            .y(function(d) { return y(d.y); })
+          .layer.add(pv.Area);
+
+/* Y-axis and ticks. */
+        vis.add(pv.Rule)
+            .data(y.ticks(3))
+            .bottom(y)
+            .strokeStyle(function(d) { return d ? 'rgba(128,128,128,.2)' : '#000'; })
+          .anchor('left').add(pv.Label)
+            .text(y.tickFormat);
+
+        vis.render();
+      },
+      makeTreemap: function(data) {
+        var max=0, tmp={}, tree={}, i, j, k, _level, item, node, timebins, timebin, total=0, path;
 // parse the data into the pvData structure.
         for ( i in data.nodes ) {
           node = data.nodes[i];
           timebin = node.timebins[0];
-          this.dom.title.innerHTML = 'Node: '+collName+' Root: "'+node.subdir+'" level:'+level+' Time: '+PxUf.UnixEpochToUTC(timebin.timestamp);
+          this.dom.title.innerHTML = 'Node: '+se+' Root: "'+node.subdir+'" level:'+level+' Time: '+PxUf.UnixEpochToUTC(timebin.timestamp);
           for ( j in timebin.levels ) {
             _level = timebin.levels[j];
             if ( _level.level == level ) {
@@ -106,7 +191,7 @@ PHEDEX.Module.StorageUsage = function(sandbox, string) {
 
 /* The root panel. */
         var vis = new pv.Panel()
-            .canvas(this.dom.body)
+            .canvas(this.dom.treemap)
             .width(w)
             .height(h)
             .bottom(20)
@@ -114,35 +199,40 @@ PHEDEX.Module.StorageUsage = function(sandbox, string) {
             .right(10)
             .top(5);
 
-        function title(d) {
-  var v;
-  if ( d.parentNode ) {
-    v = title(d.parentNode) + '/' + d.nodeName;
-  } else {
-    v = '';
-  }
-  return v; }
-// return d.parentNode ? (title(d.parentNode) + "/" + d.nodeName) : d.nodeName; }
+        function title(d) { return d.parentNode ? (title(d.parentNode) + '/' + d.nodeName) : ''; }
 
-        var re = "",
-            color = pv.Colors.category19().by(function(d) { d.parentNode.nodeName; })
-            nodes = pv.dom(tree).root("tree").nodes();
+        var re = '',
+            color = pv.Colors.category20().by(function(d) { return title(d.parentNode); }),
+            nodes = pv.dom(tree).root('tree').nodes();
 
         var treemap = vis.add(pv.Layout.Treemap)
             .nodes(nodes)
             .round(true);
 
         treemap.leaf.add(pv.Panel)
-            .fillStyle(function(d) color(d).alpha(title(d).match(re) ? 1 : .2))
-            .strokeStyle("#fff")
+            .fillStyle(function(d) { return color(d).alpha(title(d).match(re) ? 1 : .2); } )
+            .strokeStyle('#fff')
             .lineWidth(1)
             .antialias(false);
 
         treemap.label.add(pv.Label)
-            .textStyle(function(d) pv.rgb(0, 0, 0, title(d).match(re) ? 1 : .2));
+            .textStyle(function(d) { return pv.rgb(0, 0, 0, title(d).match(re) ? 1 : .2); } );
 
+//         treemap.event('mouseover',this.callback('mouseover') )
+//                .event('mouseout', this.callback('mouseout') )
+//                .event('click',    this.callback('click') );
         vis.render();
-      }
+        this.visTreemap = vis;
+      },
+      callback: function(event) {
+        var args = [obj.id, event];
+        return function() {
+          var _args = Array.apply(null,args);
+          _args.push(this);
+          _args.push(obj.visTreemap);
+          PxS.notify.apply(PxS,_args);
+        }
+      },
     };
   };
   Yla(this,_construct(this),true);
