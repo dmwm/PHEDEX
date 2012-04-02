172f8084a6de4bbf8e2702aaab8e45fb3bdcfffc
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2012-04-02 15:54:56 +0000

    add actions for making blocks and files too

--- perl_lib/PHEDEX/Testbed/Lifecycle/DataProvider.pm
+++ perl_lib/PHEDEX/Testbed/Lifecycle/DataProvider.pm
@@ -5,16 +5,14 @@ use strict;
 use warnings;
 use base 'PHEDEX::Core::Logging';
 use POE qw( Queue::Array );
-use Clone qw(clone);
 use JSON::XS;
 use Data::Dumper;
 
 our %params = (
-	  generator => 'generator --system phedex',
+	  GENERATOR => 'generator --system phedex',
 	);
 
-sub new
-{
+sub new {
   my $proto = shift;
   my $parent = shift;
   my $workflow = shift;
@@ -28,8 +26,8 @@ sub new
   $self->{ME} = $package;
 
   my $p = $workflow->{$package};
-  map { $self->{params}{uc $_} = $params{$_} } keys %params;
-  map { $self->{params}{uc $_} = $p->{$_} } keys %{$p};
+$DB::single=1;
+  map { $self->{$_} = $params{$_} } keys %params;
   map { $self->{$_} = $p->{$_} } keys %{$p};
 
   bless $self, $class;
@@ -37,8 +35,9 @@ sub new
   return $self;
 }
 
-sub generate
-{
+sub register { PHEDEX::Testbed::Lifecycle::Lite::register(@_); }
+
+sub generate {
   my ($self,$kernel,$session,$payload,$args) = @_;
   my ($workflow,@cmd,$cmd,$json,$postback,$in,$out,$log,$tmp,$timeout);
   $self->{PARENT_SESSION} = $session unless $self->{PARENT_SESSION};
@@ -62,26 +61,29 @@ sub generate
   @cmd = split(' ',$cmd);
   push @cmd, ('--in',$in) unless $args->{no_input};
   push @cmd, ('--out',$out);
-  $json = encode_json($payload);
-  open IN, ">$in" or $self->Fatal("open $in: $!\n");
-  print IN $json;
-  close IN;
-  $postback = $session->postback($args->{callback},$workflow,$payload,$in,$out,$log);
+
+# TW
+# Should not need to protect for $no_input, and should pass $payload to the
+# JSON encoder, but the generator doesn't yet understand the full payload
+# structure
+  if ( ! $args->{no_input} ) {
+    $json = encode_json($workflow->{data}); # TODO should be ($payload);
+    open IN, ">$in" or $self->Fatal("open $in: $!\n");
+    print IN $json;
+    close IN;
+  }
+  $postback = $session->postback($args->{callback},$payload,$in,$out,$log,join(' ',@cmd));
   $timeout = $workflow->{Timeout} || 999;
   $self->{parent}{JOBMANAGER}->addJob( $postback, { TIMEOUT=>$timeout, KEEP_OUTPUT=>1, LOGFILE=>$log }, @cmd);
 }
 
-sub makeDataset
-{
+sub makeDataset {
   my ($self, $kernel, $session, $payload) = @_[ OBJECT, KERNEL, SESSION, ARG0 ];
   my ($cmd,$workflow);
 
-  if ( ! $self->{registered}{madeDataset}++ ) {
-    $kernel->state( 'madeDataset', $self, 'madeDataset' );
-  }
-
+  $self->register('madeDataset');
   $workflow = $payload->{workflow};
-  $cmd  = $self->{params}{GENERATOR} . ' --generate datasets ';
+  $cmd  = $self->{GENERATOR} . ' --generate datasets ';
   $cmd .= "--num $workflow->{Datasets} ";
   
   $self->generate($kernel,
@@ -95,24 +97,79 @@ sub makeDataset
 		  );
 }
 
-sub madeDataset
-{
-  my ($self,$kernel,$payload,$obj,$target,$params) = @_[ OBJECT, KERNEL, ARG0, ARG1, ARG2, ARG3 ];
-$self->Logmsg('Now you have to do something with the output!');
-$DB::single=1;
-#  my ($agents,$agent,$re,$tmp);
-#
-#  $self->Logmsg("got: Agents($target,",Data::Dumper->Dump([$params]),")\n");
-#  $agents = $obj->{PHEDEX}{NODE};
-#  foreach $agent (@{$agents}) {
-#    next if ( $agent->{AGENT}[0]{LABEL} =~ m%^mgmt-%  && $agent->{NODE} ne 'T0_CH_CERN_Export' );
-#    foreach ( @{$agent->{AGENT}} ) {
-#      $tmp = clone $payload;
-#      $tmp->{workflow}{Agent} = $_;
-#      foreach ( qw/ HOST NAME NODE / ) { $tmp->{workflow}{Agent}{$_} = $agent->{$_}; }
-#      $kernel->yield('nextEvent',$tmp);
-#    }
-#  }
+sub madeDataset {
+  my ($self,$kernel,$session,$arg0,$arg1) = @_[OBJECT,KERNEL,SESSION,ARG0,ARG1];
+  my ($payload,$workflow,$in,$out,$log,$result);
+
+  $result = $self->{parent}->post_exec($arg0,$arg1);
+  ($payload,$in,$out,$log) = @{$arg0};
+  $workflow = $payload->{workflow};
+
+  if ( $workflow->{Dataset} ) {
+    $result->[0]{dataset}{name} = $workflow->{Dataset};
+  }
+
+  $workflow->{data} = $result;
+  $kernel->yield('nextEvent',$payload);
+}
+
+sub makeBlocks {
+  my ($self, $kernel, $session, $payload) = @_[ OBJECT, KERNEL, SESSION, ARG0 ];
+  my ($cmd,$workflow);
+
+  $self->register('madeBlocks');
+  $workflow = $payload->{workflow};
+  $cmd  = $self->{GENERATOR} . ' --action add_blocks ';
+  $cmd .= "--num $workflow->{Blocks} ";
+  
+  $self->generate($kernel,
+		  $session,
+		  $payload,
+		  {
+		   callback => 'madeBlocks',
+		   cmd      => $cmd,
+		  }
+		  );
+}
+
+sub madeBlocks {
+  my ($self,$kernel,$session,$arg0,$arg1) = @_[OBJECT,KERNEL,SESSION,ARG0,ARG1];
+  my ($payload,$workflow,$in,$out,$log,$result);
+
+  $result = $self->{parent}->post_exec($arg0,$arg1);
+  ($payload,$in,$out,$log) = @{$arg0};
+  $payload->{workflow}{data} = $result;
+  $kernel->yield('nextEvent',$payload);
+}
+
+sub makeFiles {
+  my ($self, $kernel, $session, $payload) = @_[ OBJECT, KERNEL, SESSION, ARG0 ];
+  my ($cmd,$workflow);
+
+  $self->register('madeFiles');
+  $workflow = $payload->{workflow};
+  $cmd  = $self->{GENERATOR} . ' --action add_files ';
+  $cmd .= "--num $workflow->{Files} ";
+  
+  $self->generate($kernel,
+		  $session,
+		  $payload,
+		  {
+		   callback => 'madeFiles',
+		   cmd      => $cmd,
+		  }
+		  );
+}
+
+sub madeFiles {
+  my ($self,$kernel,$session,$arg0,$arg1) = @_[OBJECT,KERNEL,SESSION,ARG0,ARG1];
+  my ($payload,$workflow,$in,$out,$log,$result);
+
+  $result = $self->{parent}->post_exec($arg0,$arg1);
+  ($payload,$in,$out,$log) = @{$arg0};
+  $payload->{workflow}{data} = $result;
+$self->Logmsg("Data structure is ",Dumper($result));
+  $kernel->yield('nextEvent',$payload);
 }
 
 1;
