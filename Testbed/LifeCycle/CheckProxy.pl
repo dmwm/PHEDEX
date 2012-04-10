6a30d197ecf286d844ee0a7e520242eb52d8a2f2
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2012-04-10 10:59:42 +0000

    patch to allow to work without --in and --out arguments

--- Testbed/LifeCycle/CheckProxy.pl
+++ Testbed/LifeCycle/CheckProxy.pl
@@ -5,33 +5,34 @@ use Getopt::Long;
 use Clone qw(clone);
 
 my ($in,$out,$json,$workflow,$payload,$interval,$status,$gracePeriod,$event);
-my ($minGrace);
+my ($minGrace,$left);
+$payload = {};
 $minGrace = 60;
-$status = 0;
+$gracePeriod = $status = 0;
 GetOptions(
                 'in=s'  => \$in,
                 'out=s' => \$out,
           );
-$in  || die "No input file specified\n";
-$out || die "No output file specified\n";
 
-open IN, "<$in" or die "open input $in: $!\n";
+if ( $in ) {
+  open IN, "<$in" or die "open input $in: $!\n";
 
-$json = <IN>;
-close IN;
-$payload = decode_json($json);
-$workflow = $payload->{workflow};
-$event = $workflow->{Event};
-$interval = $workflow->{Intervals}{$event};
-$gracePeriod = $workflow->{GracePeriod} || $minGrace;
-if ( $gracePeriod < $minGrace ) { $gracePeriod = $minGrace; }
+  $json = <IN>;
+  close IN;
+  $payload = decode_json($json);
+  $workflow = $payload->{workflow};
+  $event = $workflow->{Event};
+  $interval = $workflow->{Intervals}{$event};
+  $gracePeriod = $workflow->{GracePeriod} || $minGrace;
+  if ( $gracePeriod < $minGrace ) { $gracePeriod = $minGrace; }
+}
 
 open VPINFO, 'voms-proxy-info --actimeleft |' or do {
   $payload->{report} = { status => 'fatal', 'reason' => "voms-proxy-info: $!" };
   $status = -1;
 };
 if ( !$status ) {
-  my $left = <VPINFO>;
+  $left = <VPINFO>;
   close VPINFO or do { 
     $payload->{report} = { status => 'fatal', 'reason' => "close voms-proxy-info: $!" };
     $status = -2;
@@ -55,10 +56,16 @@ if ( !$status ) {
     if ( $interval < 10 ) { $interval = 10; }
   }
 }
-$workflow->{Jitter} = 0;
-$workflow->{Intervals}{$event} = int($interval);
-$payload->{events} = [ $event ];
-open  OUT, ">$out" or die "open output $out: $!\n";
-print OUT encode_json($payload);
-close OUT;
+if ( $out ) {
+  $workflow->{Jitter} = 0;
+  $workflow->{Intervals}{$event} = int($interval);
+  $payload->{events} = [ $event ];
+  open  OUT, ">$out" or die "open output $out: $!\n";
+  print OUT encode_json($payload);
+  close OUT;
+} else {
+  if ( $payload->{report} ) {
+    print $payload->{report}{status},': ',$payload->{report}{reason},"\n";
+  }
+}
 exit $status;
