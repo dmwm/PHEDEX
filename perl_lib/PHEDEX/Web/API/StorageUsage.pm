2d2ee70d30010d332366d626eda18d5ad85defa8
Xiaomei Zhang <sha1-7dbc72ac7a09390743e9d2224567d4054a171663@cern.ch>
2012-04-12 07:16:26 +0000

    change to use oracle backend

--- perl_lib/PHEDEX/Web/API/StorageUsage.pm
+++ perl_lib/PHEDEX/Web/API/StorageUsage.pm
@@ -1,8 +1,8 @@
 package PHEDEX::Web::API::StorageUsage;
 use warnings;
 use strict;
-use MongoDB;
-use MongoDB::OID;
+use PHEDEX::Web::SQLSpace;
+use PHEDEX::Core::Inject;
 use Data::Dumper;
 
 =pod
@@ -25,125 +25,97 @@ sub storageusage
 {
   my ($core, %args) = @_;
   warn "dumping arguments ",Data::Dumper->Dump([ \%args ]);
-  my ($method,$site,@inputSitelist, @sitelist,@records, $core);
-  my ($db, $data, $table, $cursor);
-  my $dbname = "SiteSpace";
+  my ($method,$site,$result,@inputSitelist,@records);
 
   $method = $core->{REQUEST_METHOD};
 
-  if ($args{level} > 6) {
-    die PHEDEX::Web::Util::http_error(400,"the level required is too deep");
+  foreach ( keys %args ) {
+     $args{lc($_)} = $args{$_};
+  }
+ 
+  if ($args{level}) {
+     if ($args{level} > 6) {
+        die PHEDEX::Web::Util::http_error(400,"the level required is too deep");
+     }
+  }
+  else {
+     $args{level} = 4;
   }
 
-  if (!$args{collName}) {
+  if (!$args{site}) {
     die PHEDEX::Web::Util::http_error(400,"no nodes are specified");
   }
+
   if (!$args{rootdir}) {
     $args{rootdir} = "/";
   } 
   
-  if ($args{collName} =~ m/^\*$/) {
+  if ($args{site} =~ m/^\*$/) {
      push @inputSitelist, "*";
   } 
   else {
-     @inputSitelist = split(",", $args{collName});
+     @inputSitelist = split(",", $args{site});
   }
-  $db = connDB($dbname);
- 
-  $site = "";
+
   foreach $site (@inputSitelist) {
-     #db.system.indexes.find({ns:/$dbname.test/},{ns:1})
-     $table = "system.indexes";
-     if ($site =~ m/\*/) {
-       $cursor=$db->$table->find({ns=>qr/$dbname.$site/});
-     }
-     else {
-       $cursor=$db->$table->find({ns=>"$dbname.$site"});
-     }
-     while ($data = $cursor->next) {
-        #print $data->{"ns"}, "\n";
-        if ($data->{"ns"} =~ m/SiteSpace.(\S+)/) {
-           push @sitelist, $1; 
-        }
-     }
-   }
-  foreach $site (@sitelist) {
      my $node = {};
      $args{site} = $site;
-     $node->{node} = $site;
      $node->{subdir} = $args{rootdir};
-     $node->{timebins} = getNodeInfo($db,%args); 
+     $result = PHEDEX::Web::SQLSpace::querySpace($core, %args);
+     $node->{node} = @{$result}[0]->{SITENAME};
+     $node->{timebins} = getNodeInfo($core, $result, %args); 
      warn "dumping node ",Data::Dumper->Dump([ $node ]);
      push @records, $node;
   }
   warn "dumping records ",Data::Dumper->Dump([ \@records ]);
   return { nodes => \@records };
+  #return { storageusage => @records };
 
-}
-
-sub connDB {
-  my ($dbname) = @_;
-  my ($conn, $db);
-  $conn = MongoDB::Connection->new(host => 'localhost', port => 8230);
-  $db = $conn->$dbname;
-  return $db;
 }
 
 sub getNodeInfo 
 {
-  my ($db, %args) = @_;
+  my ($core, $result, %args) = @_;
   warn "dumping arguments for getNodeInfo",Data::Dumper->Dump([ \%args ]);
-  my ($table,$data,$cursor);
-  my ($level,$rootdir,%dir, $dirs);
-  my ($site,$dirarray, $dirhash, $dirhashSep, $levelarray, $levelhash, $timebin, $timebins);
-  $site = $args{site};
-  $table = $db->$site();
+  my ($level,$rootdir,%dir, $dirs, $data);
+  my ($dirtemp, $timetemp, $dirstemp, $last, $time);
+  my ($dirarray, $dirhash, $dirhashSep, $levelarray, $levelhash, $timebin, $timebins);
   my %temp;
-  $level = 4;
-  if (exists $args{level}) {
-     $level = $args{level};
-  }
-  $rootdir = $args{rootdir};
 
-  if(!(exists $args{time_since}) && !(exists $args{time_until})) {
-    # return latest one
-    $cursor = $table->find()->skip($table->find()->count()-1);
-  }
-  else {
-    if (!$args{time_since}) {
-      $temp{'$lt'} = $args{time_until} + 0.0;
-    }
-    elsif (!$args{time_until}) {
-      $temp{'$gt'} = $args{time_since} + 0.0;
-    }  
-    else {
-      $temp{'$gte'} = $args{time_since} + 0.0;
-      $temp{'$lte'} = $args{time_until} + 0.0;
-    }
-    $dir{_id} = \%temp;
-    warn "dumping converted arguments ",Data::Dumper->Dump([ \%dir ]);
-    $cursor = $table->find(\%dir);
-    #$cursor = $table->find({_id =>{'$gt' => 0,'$lt' => 11}});
-  }
   $timebins = ();
+  $rootdir = $args{rootdir};
+  $level = $args{level};
 
-  while ($data = $cursor->next) {
-    warn "dumping data from db ", Data::Dumper->Dump([ $data ]);
+  # classify data by timestamp
+  $timetemp = {};
+  $last = @{$result}[0]->{TIMESTAMP};
+  warn "dumping last ",Data::Dumper->Dump([ $last ]);
+  $dirtemp = ();
+  foreach $data (@{$result}) {
+    #warn "dumping data from db ", Data::Dumper->Dump([ \$data ]);
+    $dirtemp = {};
+    $dirtemp->{dir} = $data->{DIR};
+    $dirtemp->{space} = $data->{SPACE};
+    push @$dirstemp, $dirtemp;
+    $timetemp->{$data->{TIMESTAMP}} = $dirstemp;
+    if ($last != $data->{TIMESTAMP}) {
+       $dirstemp = ();
+       $last = $data->{TIMESTAMP};
+    }
+  }
+  #warn "dumping timetemp ",Data::Dumper->Dump([ $timetemp ]);
+
+  foreach $time ( keys %{$timetemp} ) {
     $timebin = {};
-    $timebin->{timestamp} = $data->{_id};
+    $timebin->{timestamp} = $time;
     $levelarray = ();
     for (my $i = 1; $i<= $level; $i++) {
        $dirhash = {};
        $dirarray = ();
        $levelhash = {};
-       foreach $dirs ( @{$data->{dir}} ) {
-         #print $i, " " , dirlevel($rootdir,$i), " ", $dirs->{name}, "\n";
-         if (!$rootdir || (index($dirs->{name},dirlevel($rootdir,$i)) == 0)) {
-         #if (!$rootdir || (index($dirs->{name},$rootdir) != -1)) {
-            #if (!dirlevel($dirs->{name}, $i)) {
-            #   $dirhash->{dirlevel($dirs->{name}, $i)} += $dirs->{size}; 
-            #}
-            $dirhash->{dirlevel($dirs->{name}, $i)} += $dirs->{size};
+       foreach $dirs ( @{$timetemp->{$time}} ) {
+         if (!$rootdir || (index($dirs->{dir},dirlevel($rootdir,$i)) == 0)) {
+            $dirhash->{dirlevel($dirs->{dir}, $i)} += $dirs->{space};
          }
        }
        my $count = 0;
@@ -157,22 +129,22 @@ sub getNodeInfo
 
        if ($count==0) { $level = $i; }
 
-       warn "dump dirarray ", Data::Dumper->Dump([ $dirarray ]);
+       #warn "dump dirarray ", Data::Dumper->Dump([ $dirarray ]);
        $levelhash->{level} = $i;
        $levelhash->{data} = $dirarray;
-       warn "dump levelhash ", Data::Dumper->Dump([ $levelhash ]);
+       #warn "dump levelhash ", Data::Dumper->Dump([ $levelhash ]);
        push @$levelarray, $levelhash;
     }
-
-    warn "dump levelarray ", Data::Dumper->Dump([ $levelarray ]);
+    #warn "dump levelarray ", Data::Dumper->Dump([ $levelarray ]);
     $timebin->{levels} = $levelarray;
-    warn "dump timebin ", Data::Dumper->Dump([ $timebin ]);
+    #warn "dump timebin ", Data::Dumper->Dump([ $timebin ]);
     push @$timebins, $timebin;
   }
     
-  warn "dumping timebins ",Data::Dumper->Dump([ $timebins ]);
+  #warn "dumping timebins ",Data::Dumper->Dump([ $timebins ]);
   return $timebins;
 }
+
 sub dirlevel {
   my $path=shift;
   my $depth=shift;
