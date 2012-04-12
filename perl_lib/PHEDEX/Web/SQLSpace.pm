5c88a16f628412d9b9ffa5e4b64a83c52df264bc
Xiaomei Zhang <sha1-7dbc72ac7a09390743e9d2224567d4054a171663@cern.ch>
2012-04-12 07:15:16 +0000

    add query part

--- perl_lib/PHEDEX/Web/SQLSpace.pm
+++ perl_lib/PHEDEX/Web/SQLSpace.pm
@@ -59,7 +59,7 @@ sub insertSpace {
    my ($sql,%p, %p_d,%p_site,%p_s,$q,$dir_id,$site_id,$strict,$space);
    my ($temp_id);
    
-   warn "dumping arguments in SQL.pm",Data::Dumper->Dump([ \%h ]);
+   #warn "dumping arguments in SQL.pm",Data::Dumper->Dump([ \%h ]);
    $strict  = defined $h{strict}  ? $h{strict}  : 1;
 
    $sql = qq{ select id from t_sites where sitename=:sitename };
@@ -69,24 +69,24 @@ sub insertSpace {
    if (!$site_id) {
      $sql = qq{insert into t_sites values (:sitename, t_sites_sequence.nextval) returning id into :site_id};
      $q = execute_sql( $self, $sql , %p_site);
-     $self->{DBH}->commit();
+     #$self->{DBH}->commit();
      $p_site{':site_id'}=\$site_id;
    }
-   warn "dumping site_id in SQL.pm",Data::Dumper->Dump([ $site_id ]);
+   #warn "dumping site_id in SQL.pm",Data::Dumper->Dump([ $site_id ]);
 
    $sql = qq{ select id from t_directories where dir=:dir };
    $p_d{':dir'} = $h{dir};
    $q = execute_sql( $self, $sql, %p_d );
    $dir_id = $q->fetchrow_hashref();
-   warn "dumping dir_id in SQL.pm",Data::Dumper->Dump([ $dir_id ]);
+   #warn "dumping dir_id in SQL.pm",Data::Dumper->Dump([ $dir_id ]);
    if (!$dir_id) {
      $sql = qq{insert into t_directories values (:dir, t_directories_sequence.nextval) returning id into :dir_id};
      $p_d{':dir_id'}=\$temp_id;
      $q = execute_sql( $self, $sql, %p_d );
-     $self->{DBH}->commit();
+     #$self->{DBH}->commit();
      $dir_id->{ID} = $temp_id + 0;
    }
-   warn "dumping dir_id in SQL.pm",Data::Dumper->Dump([ $dir_id ]);
+   #warn "dumping dir_id in SQL.pm",Data::Dumper->Dump([ $dir_id ]);
 
    $sql = qq{ select space from t_space_usage where timestamp=:timestamp and site_id=:site_id and dir_id=:dir_id };
    $p_s{':timestamp'} = $h{time};
@@ -94,7 +94,7 @@ sub insertSpace {
    $p_s{':dir_id'} = $dir_id->{ID};
    $q = execute_sql( $self, $sql ,%p_s);
    $space = $q->fetchrow_hashref();
-   warn "dumping p_s in SQL.pm",Data::Dumper->Dump([ %p_s ]);
+   #warn "dumping p_s in SQL.pm",Data::Dumper->Dump([ %p_s ]);
 
    if (!$space) {
       $sql = qq{insert into t_space_usage values (:timestamp, :site_id, :dir_id, :space)};
@@ -127,6 +127,55 @@ sub insertDirectory {
 
    return $q;
 }
+
+sub querySpace {
+   my ($self, %h) = @_;
+   my ($sql,$q,$row,%p,%p_d,$time,@r);
+ 
+   if(!(exists $h{time_since}) && !(exists $h{time_until})) {
+      # return latest one
+      $sql = qq{ select max(timestamp) from t_space_usage };
+      $q = execute_sql($self, $sql);
+      $time = $q->fetchrow_hashref();
+      $h{time_since} = $time->{'MAX(TIMESTAMP)'};
+      $h{time_until} = $time->{'MAX(TIMESTAMP)'};
+      warn "dumping timestamp in SQL.pm",Data::Dumper->Dump([ \$time ]);
+   }
+   elsif(!$h{time_since}) {
+      $h{time_since} = 0;
+   }
+   elsif (!$h{time_until}) {
+      $h{time_until} = 10000000000;
+   }
+   else {
+      $h{time_since} = $h{time_since} + 0;
+      $h{time_until} = $h{time_until} + 0;
+   }
+
+   warn "dumping args in SQL.pm",Data::Dumper->Dump([ \%h ]);
+ 
+   if($h{site}!='*') { 
+      $sql = qq{ select dirs.dir, spaces.timestamp, spaces.space, sites.sitename from t_space_usage spaces 
+              join t_directories dirs on dirs.id = spaces.dir_id
+              join t_sites sites on sites.id = spaces.site_id
+              where timestamp >= :time_since and timestamp <= :time_until and site_id = (select id from t_sites where sitename=:site) };
+      $p{':site'} = $h{site};
+   }
+   else {
+      $sql = qq{ select dirs.dir, spaces.timestamp, spaces.space, sites.sitename from t_space_usage spaces 
+              join t_directories dirs on dirs.id = spaces.dir_id
+              join t_sites sites on sites.id = spaces.site_id
+              where timestamp >= :time_since and timestamp <= :time_until };
+   }
+   $p{':time_since'} = $h{time_since};
+   $p{':time_until'} = $h{time_until};
+   $q = execute_sql( $self, $sql, %p );
+   while ($_ = $q->fetchrow_hashref()) {push @r, $_;}
+   #warn "dumping space query in SQL.pm",Data::Dumper->Dump([ \@r ]);
+
+   return \@r;
+}
+
 
 
 1;
