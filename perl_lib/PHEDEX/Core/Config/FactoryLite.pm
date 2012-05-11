f1bdc2d253d81db4e4c291716d973f924633d5b3
Alberto Sanchez Hernandez <sha1-3eef4a9475415d049cde0062419e962bb6786a20@cern.ch>
2012-05-11 01:25:21 +0000

    A working version of Watchdog of the Watchdog

--- perl_lib/PHEDEX/Core/Config/FactoryLite.pm
+++ perl_lib/PHEDEX/Core/Config/FactoryLite.pm
@@ -13,27 +13,22 @@ use Data::Dumper;
 
 our %params =
 	(
-          ME            => 'WatchdogLite',
-	  WAITTIME	=> 90 + rand(3),	# This agent cycle time
-	  VERBOSE	=> $ENV{PHEDEX_VERBOSE} || 0,
-	  DEBUG		=> $ENV{PHEDEX_DEBUG} || 0,
-	  NODAEMON	=> 1,			# Don't daemonise by default!
-	  REALLY_NODAEMON=> 0,			# Do daemonise eventually!
+          ME                    => 'WatchdogLite',
+	  VERBOSE	        => $ENV{PHEDEX_VERBOSE} || 0,
+	  DEBUG		        => $ENV{PHEDEX_DEBUG} || 0,
+          WAITTIME	        => 75 + rand(15), # This agent cycle time
+	  NODAEMON	        => 1,		  # Don't daemonise by default!
+	  REALLY_NODAEMON       => 0,		  # Do daemonise eventually!
+	  LAST_SEEN_ALERT	=> 60*10,	  # 60*120 send alerts & restart after this much inactivity
+	  LAST_SEEN_WARNING	=> 60*7,	  # 60*75 send warnings after this much inactivity
+	  TIMEOUT		=> 11,		  # interval between signals
+	  STATISTICS_INTERVAL	=> 3600*12,	  # My own reporting frequency
+	  STATISTICS_DETAIL	=> 0,	          # reporting level: 0, 1, or 2
+          LOAD_DROPBOX          => 0,             # Do not load Dropbox module
+          LOAD_CYCLE            => 1,             # Load Cycle module
+          LOAD_DB               => 0,             # Do not load DB module
 
-	  LAST_SEEN_ALERT	=> 60*10,	# 60*120 send alerts & restart after this much inactivity
-	  LAST_SEEN_WARNING	=> 60*7,	# 60*75 send warnings after this much inactivity
-	  LAST_REPORTED_INTERVAL=> 60*15,	# interval between 'is deliberately down' repeats
-	  RETRY_BACKOFF_COUNT	=> 3,		# number of times to try starting agent before backing off
-	  RETRY_BACKOFF_INTERVAL=> 3600,	# back off this long before attempting to restart the agent again
-	  TIMEOUT		=> 11,		# interval between signals
-
-	  STATISTICS_INTERVAL	=> 3600*12,	# My own reporting frequency
-	  STATISTICS_DETAIL	=>    0,	# reporting level: 0, 1, or 2
-          LOAD_DROPBOX          => 0,     # Load Dropbox module
-          LOAD_CYCLE            => 1,     # Load Cycle module
-          LOAD_DB               => 0,     # Load DB module
-          STOPFLAG              => 'WatchdogLite',
-          WATCHDOG_NOTIFICATION_PORT => 9999,
+          WATCHDOG_NOTIFICATION_PORT => $ENV{PHEDEX_WATCHDOG_NOTIFICATION_PORT} || 9999,   # Port to listen
 	);
 
 our @array_params = qw / AGENT_NAMES /;
@@ -46,7 +41,6 @@ sub new
   my $self = $class->SUPER::new(%params,@_);
 
   $self->{NOTIFICATION_PORT} = $self->{WATCHDOG_NOTIFICATION_PORT};
-  $self->{TimesIdle} = 0;
 
   bless $self, $class;
   return $self;
@@ -86,8 +80,6 @@ sub createAgents
     $Agents{lc($agent)}{cmd} = \@cmd;
   }
 
-# Monitor myself too!
-  $Agents{lc($self->{ME})}{self} = $self;
   $self->Logmsg('I am running these agents: ',join(', ',sort keys %Agents));
   return ($self->{AGENTS} = \%Agents);
 }
@@ -104,23 +96,17 @@ sub processIdle
 {
   my $self = shift;
 
-  my ($agent,$Agent,$Config,$pidfile,$pid,$env,$stopfile);
-  my ($now,$last_seen,$last_reported,$mtime);
+  my ($agent,$pid);
+  my ($now,$last_seen);
 
   $now = time();
-  $Config = $self->{CONFIGURATION};
-
-  print "Idle -> $self->{TimesIdle} at $now\n";
-  return if $self->{TimesIdle} < 1; 
 
   foreach $agent ( keys %{$self->{AGENTS}} )
   {
-    print "idle -> Looking at $agent\n";
     next if lc($self->ME()) eq $agent;
     if ( $self->{AGENTS}{$agent}{cmd} )
     {
 #     Check the PID and time since we last heard from it
-      print Dumper($self->{AGENTS}{$agent});
       undef $pid;
       $pid = $self->{AGENTS}{$agent}{pid};
       if ( $pid && (kill 0 => $pid)) {
@@ -140,18 +126,18 @@ sub processIdle
       } 
       elsif ( $pid ) 
       {
-#     Agent is down and should not be.reset count so we should be able to get the new pid
+#       Agent is down and should not be. Reset pid
         $self->Alert("Agent=$agent, PID=$pid, is dead, trying to restart it ...");
-        $self->{TimesIdle} = -1;
-        $self->startAgent($agent);
+        delete $self->{AGENTS}{$agent}{pid};
+        next;
       }
+      $self->startAgent($agent);
     }
     else
     {
 #     This is either a session in the current process, or not in my list...
      $self->Dbgmsg("Agent=$agent, in this process, ignore for now...") if $self->{DEBUG};
     }
-    $self->{TimesIdle}++;
   }
 }
 
@@ -198,7 +184,8 @@ sub _poe_init
       Proto     => 'udp',
       LocalPort => $self->{WATCHDOG_NOTIFICATION_PORT},
     );
-    $self->Fatal("Could not bind to port $self->{WATCHDOG_NOTIFICATION_PORT} (are you sure the previous watchdog lite  is dead?)") unless $socket;
+    $self->Fatal("Could not bind to port $self->{WATCHDOG_NOTIFICATION_PORT} (are you sure the previous watchdog lite  is dead?)") 
+    unless $socket;
     $kernel->select_read($socket,'_udp_listen');
   }
 }
@@ -215,7 +202,7 @@ sub killAgent
 # If the process is dead, there is nothing more to do...
   if ( ! (kill 0 => $pid) )
   {
-    delete $self->{AGENT_PID}{$pid};
+    delete $self->{AGENTS}{$agent}{pid};
     return;
   }
 
@@ -246,14 +233,10 @@ sub _udp_listen
     $pid = $2;
     $message_left = $3;
     return if ( $agent eq lc($self->ME()) );
-    print "It's not ME, message from $agent ($pid)  received : $message_left\n";
     if ( $message_left eq 'ping' ) {
       $self->{AGENTS}{$agent}{last_seen} = time();
       $self->{AGENTS}{$agent}{pid} = $pid;
-      $self->{TimesIdle}++;
-      print Dumper($self->{AGENTS}{$agent});
       return;
-# allows contact without flooding the logfile
     }
   }
 }
