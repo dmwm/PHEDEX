cb41f73e09569e19ce66749eb39db576e33226a5
Xiaomei Zhang <sha1-7dbc72ac7a09390743e9d2224567d4054a171663@cern.ch>
2012-06-16 04:54:15 +0000

    add doc

--- perl_lib/PHEDEX/Web/API/StorageInsert.pm
+++ perl_lib/PHEDEX/Web/API/StorageInsert.pm
@@ -2,17 +2,29 @@ package PHEDEX::Web::API::StorageInsert;
 use warnings;
 use strict;
 use PHEDEX::Web::SQLSpace;
+use PHEDEX::Core::Inject;
 use Data::Dumper;
 
 =pod
 
 =head1 NAME
 
-PHEDEX::Web::API::StorageInsert - simple storageInsert interface
+PHEDEX::Web::API::StorageInsert - insert node storage info 
 
-=head2 DESCRIPTION
+=head1 DESCRIPTION
 
-PUT the posted object
+insert node storage info into Oracle for later query
+
+=head2 Options
+
+ required inputs: node, timestamp, dirinfo 
+ optional inputs: strict
+
+  node             node name
+  timestamp        the date for the storage info(unix time)
+  strict           allow overwrite or not(0 or 1), the default is strict(1)
+  dirinfo          the directory and its size, could be multiple, the format:
+                   "/store/mc"=1000000000
 
 =cut
 
@@ -22,18 +34,18 @@ sub invoke { return storageinsert(@_); }
 sub storageinsert 
 {
   my ($core,%args) = @_;
-  warn "dumping arguments ",Data::Dumper->Dump([ \%args ]);
+  #warn "dumping arguments ",Data::Dumper->Dump([ \%args ]);
 
-  my ($timestamp,$method,@records,%test,$site,%word,%input);
+  my ($timestamp,$method,@records,%test,$node,%word,%input);
   my ($strict, $find, $nospecify, $status);
 
   $method = $core->{REQUEST_METHOD};
   $strict  = defined $args{strict}  ? $args{strict}  : 1;
 
-  $site = $args{sitename};
+  $node = $args{node};
 
   $timestamp = $args{timestamp};
-  foreach ( qw / totalsize totaldirs totalfiles sitename timestamp strict/ )
+  foreach ( qw / totalsize totaldirs totalfiles node timestamp strict/ )
   {
        delete($args{$_});
   }
@@ -41,19 +53,20 @@ sub storageinsert
   $status = 0 ;
   foreach  (keys %args) {
     $input{time} = $timestamp;
-    $input{site} = $site;
+    $input{node} = $node;
     $input{size} = $args{$_} + 0.0;
     $input{dir} = $_;
     $input{strict} = $strict;
-    warn "dumping converted arguments ",Data::Dumper->Dump([ \%input ]);
+    #warn "dumping converted arguments ",Data::Dumper->Dump([ \%input ]);
     $status = PHEDEX::Web::SQLSpace::insertSpace($core, %input);
     #$status = PHEDEX::Web::SQLSpace::insertDirectory($core, %input);
   }
- 
-  $word{inserted} = "get insert status: $status........\n";
+  if ($status) {
+    $word{inserted} = "Insert records successfully!........\n";
+  }
   push @records, \%word;
 
-  warn "dumping records ",Data::Dumper->Dump([ \@records ]);
+ # warn "dumping records ",Data::Dumper->Dump([ \@records ]);
   return { storageinsert => \@records };
 }
 
