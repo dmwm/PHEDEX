0eccfa8271e7b0c09fbbe5cf431790ee0591d79e
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2012-07-19 09:54:54 +0000

    make the change from AgentLite to Agent. Change OUTDIR to OUTBOX, for obscure but good reasons. Allow loading of dropbox module without creating all the working directories (set LOAD_DROPBOX_WORKDIRS=0). Create TASKDIR and ARCHIVEDIR if they are set in the parent agent

--- perl_lib/PHEDEX/Core/Agent/Dropbox.pm
+++ perl_lib/PHEDEX/Core/Agent/Dropbox.pm
@@ -7,9 +7,9 @@ use File::Path;
 use File::Basename;
 use PHEDEX::Core::Command;
 
-our @required_params = qw / DROPDIR DBCONFIG /;
-our @writeable_dirs  = qw / DROPDIR INBOX WORKDIR OUTDIR /;
-our @writeable_files = qw / LOGFILE PIDFILE /; #/ required global arrays
+#our @required_params = qw / DROPDIR /;
+#our @writeable_dirs  = qw / DROPDIR INBOX WORKDIR OUTBOX /;
+#our @writeable_files = qw / LOGFILE PIDFILE /; #/ required global arrays
 
 sub new
 {
@@ -19,115 +19,80 @@ sub new
   my $self = {};
 
   no warnings 'redefine';
-  *PHEDEX::Core::AgentLite::isInvalid = \&PHEDEX::Core::Agent::Dropbox::isInvalid;
-  *PHEDEX::Core::AgentLite::_cleanDropbox = \&PHEDEX::Core::Agent::Dropbox::_cleanDropbox;
-  *PHEDEX::Core::AgentLite::readInbox = \&PHEDEX::Core::Agent::Dropbox::readInbox;
-  *PHEDEX::Core::AgentLite::readPending = \&PHEDEX::Core::Agent::Dropbox::readPending;
-  *PHEDEX::Core::AgentLite::readOutbox = \&PHEDEX::Core::Agent::Dropbox::readOutbox;
-  *PHEDEX::Core::AgentLite::renameDrop = \&PHEDEX::Core::Agent::Dropbox::renameDrop;
-  *PHEDEX::Core::AgentLite::relayDrop = \&PHEDEX::Core::Agent::Dropbox::relayDrop;
-  *PHEDEX::Core::AgentLite::inspectDrop = \&PHEDEX::Core::Agent::Dropbox::inspectDrop;
-  *PHEDEX::Core::AgentLite::markBad = \&PHEDEX::Core::Agent::Dropbox::markBad;
-  *PHEDEX::Core::AgentLite::processInbox = \&PHEDEX::Core::Agent::Dropbox::processInbox;
-  *PHEDEX::Core::AgentLite::processOutbox = \&PHEDEX::Core::Agent::Dropbox::processOutbox;
-  *PHEDEX::Core::AgentLite::processWork = \&PHEDEX::Core::Agent::Dropbox::processWork;
-  *PHEDEX::Core::AgentLite::processIdle = \&PHEDEX::Core::Agent::Dropbox::processIdle;
+  *PHEDEX::Core::Agent::cleanDropbox = \&PHEDEX::Core::Agent::Dropbox::cleanDropbox;
+  *PHEDEX::Core::Agent::processIdle = \&PHEDEX::Core::Agent::Dropbox::processIdle;
+  if ( $agentLite->{LOAD_DROPBOX_WORKDIRS} ) {
+    *PHEDEX::Core::Agent::readInbox = \&PHEDEX::Core::Agent::Dropbox::readInbox;
+    *PHEDEX::Core::Agent::readPending = \&PHEDEX::Core::Agent::Dropbox::readPending;
+    *PHEDEX::Core::Agent::readOutbox = \&PHEDEX::Core::Agent::Dropbox::readOutbox;
+    *PHEDEX::Core::Agent::renameDrop = \&PHEDEX::Core::Agent::Dropbox::renameDrop;
+    *PHEDEX::Core::Agent::relayDrop = \&PHEDEX::Core::Agent::Dropbox::relayDrop;
+    *PHEDEX::Core::Agent::inspectDrop = \&PHEDEX::Core::Agent::Dropbox::inspectDrop;
+    *PHEDEX::Core::Agent::markBad = \&PHEDEX::Core::Agent::Dropbox::markBad;
+    *PHEDEX::Core::Agent::processInbox = \&PHEDEX::Core::Agent::Dropbox::processInbox;
+    *PHEDEX::Core::Agent::processOutbox = \&PHEDEX::Core::Agent::Dropbox::processOutbox;
+    *PHEDEX::Core::Agent::processWork = \&PHEDEX::Core::Agent::Dropbox::processWork;
+  }
 
   bless $self, $class;
 
 # Before basic validation, we need to derive a few other parameters.
+  $self->{LOAD_DROPBOX_WORKDIRS} = 0; # yes, self! Avoid cluttering $agentLite
+  if ( defined($agentLite->{LOAD_DROPBOX_WORKDIRS}) ) {
+    $self->{LOAD_DROPBOX_WORKDIRS} = $agentLite->{LOAD_DROPBOX_WORKDIRS};
+  }
+  push @PHEDEX::Core::Agent::required_params, qw / DROPDIR /;
+  push @PHEDEX::Core::Agent::writeable_dirs,  qw / DROPDIR /;
   $agentLite->{DROPDIR} .= '/' unless $agentLite->{DROPDIR} =~ m%\/$%;
-  $agentLite->{INBOX}    = $agentLite->{DROPDIR} . 'inbox'; 
-  $agentLite->{OUTDIR}   = $agentLite->{DROPDIR} . 'outbox';
-  $agentLite->{PIDFILE}  = $agentLite->{DROPDIR} . 'pid';
-  $agentLite->{STOPFLAG} = $agentLite->{DROPDIR} . 'stop';
-  $agentLite->{WORKDIR}  = $agentLite->{DROPDIR} . 'work';
+  if ( $self->{LOAD_DROPBOX_WORKDIRS} ) {
+    foreach ( qw / INBOX OUTBOX WORKDIR / ) {
+      $agentLite->{$_} = $agentLite->{DROPDIR} . lc $_
+        unless defined $agentLite->{$_};
+      push @PHEDEX::Core::Agent::writeable_dirs, $_;
+    }
+  }
+# Special case of TASKDIR and ARCHIVEDIR, for FileDownload and FileRemove
+  foreach ( qw / TASKDIR ARCHIVEDIR / ) {
+    if ( exists $agentLite->{$_} && ! defined $agentLite->{$_} ) {
+      push @PHEDEX::Core::Agent::writeable_dirs, $_;
+      my $val;
+      ($val = $_) =~ s%DIR$%%;
+      $agentLite->{$_} = $agentLite->{DROPDIR} . lc $val;
+    }
+  }
+  if ( !$agentLite->{NODAEMON} ) {
+    $agentLite->{PIDFILE}  = $agentLite->{DROPDIR} . 'pid'
+      unless $agentLite->{PIDFILE};
+    $agentLite->{STOPFILE} = $agentLite->{DROPDIR} . 'stop'
+      unless $agentLite->{STOPFILE};
+    push @PHEDEX::Core::Agent::writeable_files, qw / LOGFILE PIDFILE /;
+  }
 
   $agentLite->{_Dropbox} = $self;
   return $self;
 }
 
-sub isInvalid
-{
-  my $self = shift;
-  my %h = @_;
-  @{$h{REQUIRED}} = @required_params unless $h{REQUIRED};
-  @{$h{WRITEABLE_DIRS}}  = @writeable_dirs  unless $h{WRITEABLE_DIRS};
-  @{$h{WRITEABLE_FILES}} = @writeable_files unless $h{WRITEABLE_FILES};
-
-  my $errors = 0;
-  foreach ( @{$h{REQUIRED}} )
-  {
-   next if defined $self->{$_};
-    $errors++;
-    $self->Warn("Required parameter \"$_\" not defined!\n");
-  }
-
-# Some parameters must be writeable directories
-  foreach my $key ( @{$h{WRITEABLE_DIRS}} )
-  {
-    $_ = $self->{$key};
-    while ( my $x = readlink($_) ) { $_ = $x; } # Follow symlinks!
-
-#   If the directory doesn't exist, attempt to create it...
-    eval { mkpath $_ } unless -e;
-    $self->Fatal("PERL_FATAL: $key directory $_ does not exist")   unless -e;
-    $self->Fatal("PERL_FATAL: $key exists but is not a directory") unless -d;
-    $self->Fatal("PERL_FATAL: $key directory $_ is not writeable") unless -w;
-  }
-
-# Some parameters must be writeable files if they exist, or the parent
-# directory must be writeable. Non-definition is tacitly allowed
-  foreach my $key ( @{$h{WRITEABLE_FILES}} )
-  {
-    if ( defined($_=$self->{$key}) )
-    {
-      while ( my $x = readlink($_) ) { $_ = $x; } # Follow symlinks!
-      if ( -e $_ )
-      {
-#       If it exists, it better be a writeable file
-        $self->Fatal("PERL_FATAL: $key exists but is not a file") unless -f;
-        $self->Fatal("PERL_FATAL: $key file $_ is not writeable") unless -w;
-      }
-      else
-      {
-#       If it doesn't exist, the parent must be a writeable directory
-#       If that parent directory doesn't exist, attempt to create it...
-        if ( ! -e )
-        {
-          $_ = dirname($_);
-          eval { mkpath $_ } unless -e;
-        }
-        $self->Fatal("PERL_FATAL: $key directory $_ does not exist")   unless -e;
-        $self->Fatal("PERL_FATAL: $key exists but is not a directory") unless -d;
-        $self->Fatal("PERL_FATAL: $key directory $_ is not writeable") unless -w;
-      }
-    }
-  }
-
-  if ( !defined($self->{LOGFILE}) && !$self->{NODAEMON} )
-  {
-#   LOGFILE not defined is fatal unless NODAEMON is set!
-    $self->Fatal("PERL_FATAL: LOGFILE not set but process will run as a daemon");
-  }
-
-  return $errors;
-}
-
 # Make a basic cleanup
-sub _cleanDropbox
-{
-    my ($self,$me) = @_;
+sub cleanDropbox {
+  my ($self,$me) = @_;
 
-    foreach my $dir (@{$self->{NEXTDIR}}) {
-        if ($dir =~ /^([a-z]+):/) {
-            die "$me: fatal error: unrecognised bridge $1" if ($1 ne "scp" && $1 ne "rfio");
-        } else {
-            die "$me: fatal error: no downstream drop box\n" if ! -d $dir;
-        }
-    }
-    $self->CleanDropbox($me);
+  if (-f $self->{PIDFILE})
+  {
+     if (my $oldpid = &input($self->{PIDFILE}))
+     {
+        chomp ($oldpid);
+        die "$me: pid $oldpid already running in $self->{DROPDIR}\n"
+        if kill(0, $oldpid);
+        print "$me: pid $oldpid dead in $self->{DROPDIR}, overwriting\n";
+        unlink ($self->{PIDFILE});
+     }
+  }
 
+  if (-f $self->{STOPFILE})
+  {
+     print "$me: removing old stop flag $self->{STOPFILE}\n";
+     unlink ($self->{STOPFILE});
+  }
 }
 
 # Look for pending drops in inbox.
@@ -181,13 +146,13 @@ sub readPending
 sub readOutbox
 {
     my $self = shift;
-    die "$self->{ME}: fatal error: no outbox directory given\n" if ! $self->{OUTDIR};
+    die "$self->{ME}: fatal error: no outbox directory given\n" if ! $self->{OUTBOX};
 
     # Scan the outbox directory.  If this fails, file an alert but keep
     # going, the problem might be transient.
     my @files = ();
-    $self->Alert("cannot list outdir(".$self->{OUTDIR}."): $!")
-	if (! getdir ($self->{OUTDIR}, \@files));
+    $self->Alert("cannot list outdir(".$self->{OUTBOX}."): $!")
+	if (! getdir ($self->{OUTBOX}, \@files));
 
     return @files;
 }
@@ -205,105 +170,105 @@ sub renameDrop
 sub relayDrop
 {
     my ($self,$drop) = @_;
-
-    # Move to output queue if not done yet
-    if (-d "$self->{WORKDIR}/$drop")
-    {
-        &mv ("$self->{WORKDIR}/$drop", "$self->{OUTDIR}/$drop") || return;
-    }
-
-    # Check if we've already successfully copied this one downstream.
-    # If so, just nuke it; manual recovery is required to kick the
-    # downstream ones forward.
-    if (-f "$self->{OUTDIR}/$drop/gone") {
-	&rmtree ("$self->{OUTDIR}/$drop");
-	return;
-    }
-
-    # Clean up our markers
-    &rmtree("$self->{OUTDIR}/$drop/go");
-    &rmtree("$self->{OUTDIR}/$drop/gone");
-    &rmtree("$self->{OUTDIR}/$drop/bad");
-    &rmtree("$self->{OUTDIR}/$drop/done");
-
-    # Copy to the next ones.  We want to be careful with the ordering
-    # here -- we want to copy the directory exactly once, ever.  So
-    # execute in an order that is safe even if we get interrupted.
-    if (scalar @{$self->{NEXTDIR}} == 0)
-    {
-	&rmtree ("$self->{OUTDIR}/$drop");
-    }
-    elsif (scalar @{$self->{NEXTDIR}} == 1 && $self->{_Al}->{NEXTDIR}[0] !~ /^([a-z]+):/)
-    {
-	-d "$self->{NEXTDIR}[0]/inbox"
-	    || mkdir "$self->{NEXTDIR}[0]/inbox"
-	    || -d "$self->{NEXTDIR}[0]/inbox"
-	    || return $self->Alert("cannot create $self->{NEXTDIR}[0]/inbox: $!");
-
-	# Make sure the destination doesn't exist yet.  If it does but
-	# looks like a failed copy, nuke it; otherwise complain and give up.
-	if (-d "$self->{NEXTDIR}[0]/inbox/$drop"
-	    && -f "$self->{NEXTDIR}[0]/inbox/$drop/go-pending"
-	    && ! -f "$self->{NEXTDIR}[0]/inbox/$drop/go") {
-	    &rmtree ("$self->{NEXTDIR}[0]/inbox/$drop")
-        } elsif (-d "$self->{NEXTDIR}[0]/inbox/$drop") {
-	    return $self->Alert("$self->{NEXTDIR}[0]/inbox/$drop already exists!");
-	}
-
-	&mv ("$self->{OUTDIR}/$drop", "$self->{NEXTDIR}[0]/inbox/$drop")
-	    || return $self->Alert("failed to copy $drop to $self->{NEXTDIR}[0]/$drop: $!");
-	&touch ("$self->{NEXTDIR}[0]/inbox/$drop/go")
-	    || $self->Alert ("failed to make $self->{NEXTDIR}[0]/inbox/$drop go");
-    }
-    else
-    {
-        foreach my $dir (@{$self->{NEXTDIR}})
-        {
-	    if ($dir =~ /^scp:/ || $dir =~ /^rfio:/ ) {
-               $self->Alert("Fail, scp or rfio not supported anymore");
-               next;
-	    }
-
-	    # Local.  Create destination inbox if necessary.
-	    -d "$dir/inbox"
-	        || mkdir "$dir/inbox"
-	        || -d "$dir/inbox"
-	        || return $self->Alert("cannot create $dir/inbox: $!");
-
-	    # Make sure the destination doesn't exist yet.  If it does but
-	    # looks like a failed copy, nuke it; otherwise complain and give up.
-	    if (-d "$dir/inbox/$drop"
-	        && -f "$dir/inbox/$drop/go-pending"
-	        && ! -f "$dir/inbox/$drop/go") {
-	        &rmtree ("$dir/inbox/$drop")
-            } elsif (-d "$dir/inbox/$drop") {
-	        return $self->Alert("$dir/inbox/$drop already exists!");
-	    }
-
-	    # Copy to the next stage, preserving everything
-	    my $status = &runcmd  ("cp", "-Rp", "$self->{OUTDIR}/$drop", "$dir/inbox/$drop");
-	    return $self->Alert ("can't copy $drop to $dir/inbox: $status") if $status;
-
-	    # Mark it almost ready to go
-	    &touch("$dir/inbox/$drop/go-pending");
-        }
-
-        # Now mark myself gone downstream so we won't try copying again
-        # (FIXME: error checking?)
-        &touch ("$self->{OUTDIR}/$drop/gone");
-
-        # All downstream versions copied safely now.  Now really let them
-        # go onwards.  If this fails, it's not fatal because someone can
-        # still manually fix them to be in ready state.  We haven't lost
-        # anything.  (FIXME: avoidable?)
-        foreach my $dir (@{$self->{NEXTDIR}}) {
-	    next if $dir =~ /^([a-z]+):/; # FIXME: also handle here?
-	    &mv("$dir/inbox/$drop/go-pending", "$dir/inbox/$drop/go");
-        }
-
-        # Now junk it here
-        &rmtree("$self->{OUTDIR}/$drop");
-    }
+die "Someone really wants to use relayDrop???\n";
+#    # Move to output queue if not done yet
+#    if (-d "$self->{WORKDIR}/$drop")
+#    {
+#        &mv ("$self->{WORKDIR}/$drop", "$self->{OUTBOX}/$drop") || return;
+#    }
+#
+#    # Check if we've already successfully copied this one downstream.
+#    # If so, just nuke it; manual recovery is required to kick the
+#    # downstream ones forward.
+#    if (-f "$self->{OUTBOX}/$drop/gone") {
+#	&rmtree ("$self->{OUTBOX}/$drop");
+#	return;
+#    }
+#
+#    # Clean up our markers
+#    &rmtree("$self->{OUTBOX}/$drop/go");
+#    &rmtree("$self->{OUTBOX}/$drop/gone");
+#    &rmtree("$self->{OUTBOX}/$drop/bad");
+#    &rmtree("$self->{OUTBOX}/$drop/done");
+#
+#    # Copy to the next ones.  We want to be careful with the ordering
+#    # here -- we want to copy the directory exactly once, ever.  So
+#    # execute in an order that is safe even if we get interrupted.
+#    if (scalar @{$self->{NEXTDIR}} == 0)
+#    {
+#	&rmtree ("$self->{OUTBOX}/$drop");
+#    }
+#    elsif (scalar @{$self->{NEXTDIR}} == 1 && $self->{_Al}->{NEXTDIR}[0] !~ /^([a-z]+):/)
+#    {
+#	-d "$self->{NEXTDIR}[0]/inbox"
+#	    || mkdir "$self->{NEXTDIR}[0]/inbox"
+#	    || -d "$self->{NEXTDIR}[0]/inbox"
+#	    || return $self->Alert("cannot create $self->{NEXTDIR}[0]/inbox: $!");
+#
+#	# Make sure the destination doesn't exist yet.  If it does but
+#	# looks like a failed copy, nuke it; otherwise complain and give up.
+#	if (-d "$self->{NEXTDIR}[0]/inbox/$drop"
+#	    && -f "$self->{NEXTDIR}[0]/inbox/$drop/go-pending"
+#	    && ! -f "$self->{NEXTDIR}[0]/inbox/$drop/go") {
+#	    &rmtree ("$self->{NEXTDIR}[0]/inbox/$drop")
+#        } elsif (-d "$self->{NEXTDIR}[0]/inbox/$drop") {
+#	    return $self->Alert("$self->{NEXTDIR}[0]/inbox/$drop already exists!");
+#	}
+#
+#	&mv ("$self->{OUTBOX}/$drop", "$self->{NEXTDIR}[0]/inbox/$drop")
+#	    || return $self->Alert("failed to copy $drop to $self->{NEXTDIR}[0]/$drop: $!");
+#	&touch ("$self->{NEXTDIR}[0]/inbox/$drop/go")
+#	    || $self->Alert ("failed to make $self->{NEXTDIR}[0]/inbox/$drop go");
+#    }
+#    else
+#    {
+#        foreach my $dir (@{$self->{NEXTDIR}})
+#        {
+#	    if ($dir =~ /^scp:/ || $dir =~ /^rfio:/ ) {
+#               $self->Alert("Fail, scp or rfio not supported anymore");
+#               next;
+#	    }
+#
+#	    # Local.  Create destination inbox if necessary.
+#	    -d "$dir/inbox"
+#	        || mkdir "$dir/inbox"
+#	        || -d "$dir/inbox"
+#	        || return $self->Alert("cannot create $dir/inbox: $!");
+#
+#	    # Make sure the destination doesn't exist yet.  If it does but
+#	    # looks like a failed copy, nuke it; otherwise complain and give up.
+#	    if (-d "$dir/inbox/$drop"
+#	        && -f "$dir/inbox/$drop/go-pending"
+#	        && ! -f "$dir/inbox/$drop/go") {
+#	        &rmtree ("$dir/inbox/$drop")
+#            } elsif (-d "$dir/inbox/$drop") {
+#	        return $self->Alert("$dir/inbox/$drop already exists!");
+#	    }
+#
+#	    # Copy to the next stage, preserving everything
+#	    my $status = &runcmd  ("cp", "-Rp", "$self->{OUTBOX}/$drop", "$dir/inbox/$drop");
+#	    return $self->Alert ("can't copy $drop to $dir/inbox: $status") if $status;
+#
+#	    # Mark it almost ready to go
+#	    &touch("$dir/inbox/$drop/go-pending");
+#        }
+#
+#        # Now mark myself gone downstream so we won't try copying again
+#        # (FIXME: error checking?)
+#        &touch ("$self->{OUTBOX}/$drop/gone");
+#
+#        # All downstream versions copied safely now.  Now really let them
+#        # go onwards.  If this fails, it's not fatal because someone can
+#        # still manually fix them to be in ready state.  We haven't lost
+#        # anything.  (FIXME: avoidable?)
+#        foreach my $dir (@{$self->{NEXTDIR}}) {
+#	    next if $dir =~ /^([a-z]+):/; # FIXME: also handle here?
+#	    &mv("$dir/inbox/$drop/go-pending", "$dir/inbox/$drop/go");
+#        }
+#
+#        # Now junk it here
+#        &rmtree("$self->{OUTBOX}/$drop");
+#    }
 }
 
 # Check what state the drop is in and indicate if it should be
@@ -423,6 +388,5 @@ sub processIdle
   $self->idle (@pending);
   $pmon->State('idle','stop');
 }
-
 
 1;
