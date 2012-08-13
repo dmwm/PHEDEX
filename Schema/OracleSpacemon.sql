bb44052847cb59b7f5b80a5e90ee241d3924f138
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2012-08-13 08:46:35 +0000

    change t_sites to t_adm_node, for compatibility with PhEDEx. Change sequence and constraint names accordingly.

--- Schema/OracleSpacemon.sql
+++ Schema/OracleSpacemon.sql
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------
 -- Create sequences
 
-create sequence t_sites_sequence
+create sequence t_adm_node_sequence
     increment by 1
     start with 1
     nomaxvalue
@@ -30,16 +30,6 @@ create table t_adm_node (
     unique (name)
 );
 
--- create table t_sites (
---     sitename    varchar(50)        not null,
---     id          integer            not null,
---   --
---   constraint pk_sites
---     primary key (id),
---   constraint unique_sitename
---     unique (sitename)
--- );
-
 create table t_directories (
     dir       varchar(1000)        not null,
     id          integer            not null,
@@ -62,6 +52,6 @@ create table t_space_usage (
   constraint fk_space_usage_dir_id 
     foreign key (dir_id) references t_directories (id),
   --
-  constraint fk_space_usage_site_id
-    foreign key (site_id) references t_sites (id)
+  constraint fk_space_usage_node_id
+    foreign key (site_id) references t_adm_node (id)
 );
