f5ae42bd9e327f79ebebb7dbe91a5d63cc30c97b
Nicolo Magini <sha1-61c9148c72dc9af330b61448b73df2d65993435c@cern.ch>
2012-10-10 17:46:35 +0000

    Pass catalogue object to pnfs namespace for lfn2pfn conversion

--- perl_lib/PHEDEX/Namespace/pnfs.pm
+++ perl_lib/PHEDEX/Namespace/pnfs.pm
@@ -32,7 +32,8 @@ sub new
     if ( exists($self->{AGENT}->{VERBOSE}) )    { $self->{VERBOSE}    = $self->{AGENT}->{VERBOSE}; }
     if ( exists($self->{AGENT}->{DEBUG}) )      { $self->{DEBUG}      = $self->{AGENT}->{DEBUG}; }
   }
-  $self->SUPER::_init( NAMESPACE => __PACKAGE__ );
+  $self->SUPER::_init( NAMESPACE => __PACKAGE__,
+		       CATALOGUE => $h{CATALOGUE});
   $self->SUPER::_init_commands;
   print Dumper($self) if $self->{DEBUG};
   $self->Help if $params{HELP};
