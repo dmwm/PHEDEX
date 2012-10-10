4f81602881ebc7fbd54450a3faffc7847d033797
Nicolo Magini <sha1-61c9148c72dc9af330b61448b73df2d65993435c@cern.ch>
2012-10-10 19:01:41 +0000

    Removing test chimera_dump namespace

--- perl_lib/PHEDEX/Namespace/chimera_dump/Cache.pm
+++
@@ -1,8 +0,0 @@
-package PHEDEX::Namespace::chimera_dump::Cache;
-# Implement caching of results in the namespace framework for chimera_dump-access
-# It just inherits the base class
-use strict;
-use warnings;
-use base 'PHEDEX::Namespace::Cache';
-
-1;
