d29fc623c593d06df70376d2ea86d71df2b6ed8b
Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch>
2012-10-23 15:26:38 +0000

    Merge role and domain in one table t_adm2_role with two columns: role and domain.

--- Schema/OracleCoreAdm2.sql
+++ Schema/OracleCoreAdm2.sql
@@ -9,57 +9,33 @@ create sequence seq_adm2_domain;
 
 create table t_adm2_role
   (id			integer		not null, -- ID for PhEDEx use
-   name			varchar (200)	not null, -- Role name like in SiteDB
+   role			varchar (200)	not null, -- Role name like in SiteDB
+   domain		varchar (200)   not null, -- To distinguish between
+                                                  -- sites, groups, etc
    --
-   constraint uk_adm2_role_id_name
-     unique (id, name),
+   constraint uk_adm2_role_id_role_domain
+     unique (role, domain),
    --
    constraint pk_adm2_role_id
      primary key (id)
 );
 
-create table t_adm2_domain
-  (id			integer		not null, -- ID for PhEDEx use
-   name			varchar (200)	not null, -- Domain name to distinguish between sites, groups, etc
-   --
-   constraint uk_adm2_domain_id_name
-     unique (id, name),
-   --
-   constraint pk_adm2_domain_id
-     primary key (id)
-);
-
 /* Some static roles */
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Data Manager');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Site Admin');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Group Manager');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Admin');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Operator');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Unprivileged User');
-insert into t_adm2_role (id, name)
-  values (seq_adm2_role.nextval, 'Agent');
-
-/* Domains */
-insert into t_adm2_domain (id, name)
-  values (seq_adm2_domain.nextval, 'site');
-insert into t_adm2_domain (id, name)
-  values (seq_adm2_domain.nextval, 'group');
-insert into t_adm2_domain (id, name)
-  values (seq_adm2_domain.nextval, 'phedex');
-insert into t_adm2_domain (id, name)
-  values (seq_adm2_domain.nextval, 'phedex/prod');
-
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Data Manager', 'site');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Data Manager', 'group');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Site Admin', 'site');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Group Manager', 'group');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Admin', 'phedex');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Operator', 'phedex/prod');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Unprivileged User', 'any');
+insert into t_adm2_role (id, role, domain)
+  values (seq_adm2_role.nextval, 'Agent', 'phedex');
+ 
 ----------------------------------------------------------------------
--- Create indices
-
-create index ix_adm2_roles_name
-  on t_adm2_role (name);
-
-create index ix_adm2_domain_name
-  on t_adm2_domain (name);
