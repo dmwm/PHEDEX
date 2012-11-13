77ab9d95aa4bcc49ffccd3d0b6addae06af56efa
Tony Wildish <sha1-97224746068ac030e0fdfd6c221b79f5288ead5b@cern.ch>
2012-11-13 18:47:01 +0000

    Add procedures proc_add_node and proc_delete_node. I bet you can't guess what they do?
    They both contain the logic for creating or deleting the partitions in the
    t_xfer_* tables, as well as the inserts/deletions in t_adm_node. This means
    that by granting execute privilege on these procedures they can then be
    run by someone who does _not_ have the alter-table privilege. Until now,
    that has been required to explicitly add or delete a partition.
    
    N.B. I tried doing this with triggers on insert/delete into t_adm_node
    instead, but that was not reliable. You cannot trigger on the 'commit',
    so if the user rolled back the transaction instead, you would end up
    with an inconsistent state. Worst case, the partitions would be deleted
    but the node would remain, and could still be used.
    
    NodeNew and NodeRemove have been updated to use these procedures.

--- Schema/OracleCoreFunctions.sql
+++ Schema/OracleCoreFunctions.sql
@@ -35,3 +35,48 @@ end schema_version;
 /
 
 grant execute on schema_version to public;
+
+-- adds a node and creates the required partitions in the xfer tables
+-- access to this function should be strictly controlled, not public
+create or replace procedure proc_add_node(name varchar2,
+                                     kind varchar2,
+                                     technology varchar2,
+                                     se_name varchar2
+                                     ) AS
+  dml varchar(200);
+  partition_name varchar(100);
+  node_id number;
+  begin
+    insert into t_adm_node (id,name,kind,technology,se_name,capacity,bandwidth_cap)
+                    values (seq_adm_node.nextval,name,kind,technology,se_name,1000,1000)
+                    returning id into node_id;
+    dbms_output.put_line('Inserted node ' || name || ' with id = ' || node_id);
+    partition_name := 'node_' || lower(name);
+    dml := ' add partition ' || partition_name || ' values (' || node_id || ')';
+    execute immediate 'alter table t_xfer_replica' || dml;
+    execute immediate 'alter table t_xfer_request' || dml;
+    execute immediate 'alter table t_xfer_task'    || dml;
+    commit;
+    dbms_output.put_line('Created partitions and committed transaction');
+  end;
+/
+
+-- delete a node and the corresponding partitions
+-- access to this function should be strictly controlled, not public
+create or replace procedure proc_delete_node(node varchar2) AS
+  p_name varchar(100);
+  begin
+    delete from t_adm_node where name = node;
+    dbms_output.put_line('Deleted node ' || node);
+    p_name := 'NODE_' || upper(node);
+    for p in (select table_name from user_tab_partitions
+                      where partition_name = p_name
+                      and table_name not like 'BIN$%') loop
+      dbms_output.put_line('drop partition ' || p_name || ' from ' || p.table_name);
+      execute immediate('alter table ' || p.table_name || ' drop partition ' || p_name);
+    end loop;
+    commit;
+    dbms_output.put_line('Deleted partitions and committed transaction');
+  end;
+/
+
