b5d6ba4c0e110ce8794e1e9c170a23ee9a91a1d1
Phedex Admin <sha1-c7d2573c21ca1e8ac30fc7c3614b0ecac2b252a9@cern.ch>
2012-11-14 18:52:31 +0000

    Add cache for srmv2lcg namespace

---
+++ perl_lib/PHEDEX/Namespace/srmv2lcg/Cache.pm
@@ -0,0 +1,8 @@
+package PHEDEX::Namespace::srmv2lcg::Cache;
+# Implement caching of results in the namespace framework for srmv2-access
+# It just inherits the base class
+use strict;
+use warnings;
+use base 'PHEDEX::Namespace::Cache';
+
+1;
