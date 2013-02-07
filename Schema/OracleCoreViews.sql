c7bcafa7652c9e4e3ed8c4b7f2537b30f695a2b2
Xiaomei Zhang <sha1-7dbc72ac7a09390743e9d2224567d4054a171663@cern.ch>
2013-02-07 13:05:38 +0000

    create materialed view t_history_link_history

--- Schema/OracleCoreViews.sql
+++ Schema/OracleCoreViews.sql
@@ -22,3 +22,17 @@ select distinct b.name block_name,
    and n.se_name is not null
    and dbs.name = 'https://cmsdbsprod.cern.ch:8443/cms_dbs_prod_global_writer/servlet/DBSServlet'
 ;
+
+create or replace materialized view t_history_link_summary 
+  refresh start with sysdate
+  next sysdate+1 as 
+select substr(gmtime(timebin), 0, 7) as timebin,
+       trunc(sum(done_bytes)/power(1024,4), 2) as sum_done_bytes
+ from t_history_link_events x
+ join t_adm_node ns on ns.id = x.from_node
+ join t_adm_node nd on nd.id = x.to_node
+where nd.kind != 'MSS' and ns.kind != 'MSS'
+ and timebin > 0
+ group by substr(gmtime(timebin), 0, 7)
+ order by 1
+;
