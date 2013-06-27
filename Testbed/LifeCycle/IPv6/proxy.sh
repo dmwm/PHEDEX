a60c3d6c3301fca2864710ac25712a9b3d9203fb
Tony Wildish <Tony.Wildish@cern.ch>
2013-06-27 17:53:02 +0200

    remove files that are not needed on this branch

--- Testbed/LifeCycle/IPv6/proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /data/ipv6/env.sh
-voms-proxy-info --actimeleft
-voms-proxy-init --voms ipv6.hepix.org --valid 24:00 \
-		--cert ~/.globus/usercert.pem --key ~/.globus/userkey.pem.nok
-voms-proxy-info --actimeleft
