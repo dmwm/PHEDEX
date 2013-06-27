1cf6be60c2447feb33c8394e047b2b8a1285983a
Tony Wildish <Tony.Wildish@cern.ch>
2013-06-27 15:51:06 +0200

    remove files that do not belong on this branch

--- perl_lib/PHEDEX/Namespace/dpm/Cache.pm
+++
@@ -1,8 +0,0 @@
-package PHEDEX::Namespace::dpm::Cache;
-# Implement caching of results in the namespace framework for dpm-access
-# It just inherits the base class
-use strict;
-use warnings;
-use base 'PHEDEX::Namespace::Cache';
-
-1;
