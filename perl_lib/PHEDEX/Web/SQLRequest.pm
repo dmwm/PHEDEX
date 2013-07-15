ac8f29da5056d8ab609582457d174cc3279bc3bc
Xiaomei Zhang <xmzhang@lxplus0239.cern.ch>
2013-07-15 09:03:58 +0200

    Add ViewApproval

--- perl_lib/PHEDEX/Web/SQLRequest.pm
+++ perl_lib/PHEDEX/Web/SQLRequest.pm
@@ -45,24 +45,55 @@ sub viewApprovalPlan {
    my ($self, %h) = @_;
    my ($sql,$q,@r,%p);
 
-   $p{':request_id'} = $h{request_id};
+   $p{':request_id'} = $h{REQUEST_ID};
 
    $sql = qq{ 
       select a.request,
                r.type,
                a.type,
-               d.decision,
+               a.decision,
                a.parent,
                a.decide_before,
                a.default_decision,
-               i.name,
-               i.email
+               ar.role  
          from t_req_approval_plan a
          join t_req_request r on r.id = a.request
-         join t_req_decision d on d.id = a.decision
-         join t_adm_client c on c.id = d.decided_by
-         join t_adm_identity i on i.id = c.identity
+         join t_adm_role ar on ar.id = a.role
          where r.id = :request_id 
+         order by a.request asc
+    };
+
+   $q = execute_sql( $self, $sql ,%p);
+
+   while ( $_ = $q->fetchrow_hashref() ) { push @r, $_; }
+   #warn "dump results ", Data::Dumper->Dump([ \@r ]);
+   return \@r;
+}
+
+sub myapproval {
+   my ($self, %h) = @_;
+   my ($sql,$q,@r,%p);
+
+   $p{':request_id'} = $h{REQUEST_ID};
+
+   $sql = qq{
+      select a.request as "request id",
+             r.type as "request type",
+             a.decision as "decision name",
+             s.name as "state name",
+             ra.name as "action name",
+             ar.role 
+         from t_req_action_ability aa
+         join t_req_request r on r.type = aa.request_type
+         join t_req_action ra on ra.id = aa.action
+         join t_req_transition t on t.current_state = r.state
+         join t_req_state s on s.id = r.state
+         join t_req_approval_plan a on a.request = r.id
+         join t_adm_role ar on ar.id = aa.role 
+         where r.id = :request_id
+           and ar.id = 5 
+           and a.decision = 'u'
+           and t.action = aa.action
          order by a.request asc
     };
 
@@ -70,5 +101,6 @@ sub viewApprovalPlan {
    while ( $_ = $q->fetchrow_hashref() ) { push @r, $_; }
    return \@r;
 }
+
 
 1;
