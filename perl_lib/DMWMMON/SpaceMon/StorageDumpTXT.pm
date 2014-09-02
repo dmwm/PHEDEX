8fb21e725ee5af6e300b1a842ac54523d5e102d0
nataliaratnikova <natasha@fnal.gov>
2014-09-02 16:12:58 -0500

    some intermediate state, working

---
+++ perl_lib/DMWMMON/SpaceMon/StorageDumpTXT.pm
@@ -0,0 +1,26 @@
+package DMWMMON::SpaceMon::StorageDumpTXT;
+use strict;
+use warnings;
+use Data::Dumper;
+ 
+sub new
+{
+my $proto = shift;
+my $class = ref($proto) || $proto;
+my $self = {};
+my %params = (
+	      DEBUG => 1,
+	      VERBOSE => 1,
+);
+my %args = (@_);
+map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
+print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+bless $self, $class;
+# FR: validate record parameters
+
+return $self;
+}
+
+sub Dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
+
+1;
