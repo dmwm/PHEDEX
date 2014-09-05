aded2d53e78f2bfdec15281769a772b5e90abe34
nataliaratnikova <natasha@fnal.gov>
2014-09-05 18:00:48 -0500

    Added basic workflow with all necessary classes with dummy  implementation (w.i.p.)

--- perl_lib/DMWMMON/SpaceMon/RecordIO.pm
+++ perl_lib/DMWMMON/SpaceMon/RecordIO.pm
@@ -1,29 +1,56 @@
-package DMWMMON::SpaceMon::Record;
+package DMWMMON::SpaceMon::RecordIO;
 use strict;
 use warnings;
 use Data::Dumper;
+use DMWMMON::SpaceMon::Record;
 
 sub new
 {
-my $proto = shift;
-my $class = ref($proto) || $proto;
-my $self = {};
-my %params = (
-	      DEBUG => 1,
-	      VERBOSE => 1,
-	      NODE => undef,
-	      TIMESTAMP => undef,
-	      DIRS => {},
-);
-my %args = (@_);
-map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
-print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
-bless $self, $class;
-# FR: validate record parameters
-
-return $self;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = {};
+    my %params = (
+		  DEBUG => 1,
+		  VERBOSE => 1,
+		  );
+    my %args = (@_);
+    map { if (defined $args{$_}) {$self->{$_} = $args{$_}} else { $self->{$_} = $params{$_}} } keys %params;        
+    
+    print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+    bless $self, $class;    
+    return $self;
 }
 
-sub Dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
+sub dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
+
+sub readFromFile 
+{
+    my $self = shift;
+    my ($file,) = (@_);
+    print "(dummy) Reading data from file: $file\n";
+    return DMWMMON::SpaceMon::Record->new();
+    
+}
+sub writeToFile
+{
+    my $self = shift;
+    my ($what, $where) = (@_);
+    print "I am in ",__PACKAGE__,"->writeToFile()\n" if $self->{VERBOSE};
+    print "(dummy) Writing to file $where:\n", $what->dump();
+}
+
+sub upload
+{    
+    my $self = shift;
+    print "I am in ",__PACKAGE__,"->upload()\n" if $self->{VERBOSE};
+    print "Dummy upload: here goes all Datasvc/UA stuff.\n";
+    return 1;
+}
+
+sub show
+{
+    my $self = shift;
+    print "I am in ",__PACKAGE__,"->show()\n" if $self->{VERBOSE};
+}
 
 1;
--- perl_lib/DMWMMON/SpaceMon/StorageDumpTXT.pm
+++ perl_lib/DMWMMON/SpaceMon/StorageDumpTXT.pm
@@ -2,25 +2,24 @@ package DMWMMON::SpaceMon::StorageDumpTXT;
 use strict;
 use warnings;
 use Data::Dumper;
+use base 'DMWMMON::SpaceMon::StorageDump';
  
 sub new
 {
-my $proto = shift;
-my $class = ref($proto) || $proto;
-my $self = {};
-my %params = (
-	      DEBUG => 1,
-	      VERBOSE => 1,
-);
-my %args = (@_);
-map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
-print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
-bless $self, $class;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = $class->SUPER::new(@_);
+#my %params = (
+#	      DEBUG => 1,
+#	      VERBOSE => 1,
+#);
+#my %args = (@_);
+#map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
+    print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+    bless $self, $class;
 # FR: validate record parameters
 
-return $self;
+    return $self;
 }
-
-sub Dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
 
 1;
