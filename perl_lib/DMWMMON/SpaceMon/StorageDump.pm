4060a15eee14a2546f9e87cb460f821146ed07f8
nataliaratnikova <natasha@fnal.gov>
2014-10-14 16:43:00 -0500

    Added code to instantiate an instnace of a subclass based on a format guessed by the base constructor.

--- perl_lib/DMWMMON/SpaceMon/StorageDump.pm
+++ perl_lib/DMWMMON/SpaceMon/StorageDump.pm
@@ -3,15 +3,16 @@ use strict;
 use warnings;
 use Carp;
 use File::Basename;
+use File::Spec::Functions;
 use Data::Dumper;
 
 # Allow g and b2 zipped files to uncompress on the fly: 
 my %extractor = ( ".gz" => "| gzip -d - ", ".bz2" =>  "| bzip2 -d - " );
 
 # Mapping for file suffices: 
-my %format = ( ".txt" => "TEXT", ".xml" => "XML" );
+my %formats = ( ".txt" => "TXT", ".xml" => "XML" );
 
-sub new
+sub instantiate
 {
     my $proto = shift;
     my $class = ref($proto) || $proto;
@@ -19,17 +20,41 @@ sub new
     my %params = (
 		  DEBUG => 1,
 		  VERBOSE => 1,
-		  DUMPFILE => undef,
+		  DUMPFILE => undef,		  
 		  TIMESTAMP => undef,
 		  );
     my %args = (@_);
     map { if (defined $args{$_}) {$self->{$_} = $args{$_}} else { $self->{$_} = $params{$_}} } keys %params;
-    print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+    print "I am in a base class ",__PACKAGE__,"->instantiate()\n" if $self->{VERBOSE};
     validate($self);
-    $self->{TIMESTAMP} = lookupTimeStamp($self);
-    &openDump($self);
-    bless $self, $class;
-    return $self;
+
+    my $format = undef;
+    #if ($self->looksLikeTXT()) {
+    if (looksLikeTXT(\%params)) {
+	print "Looks like TXT file\n";
+	$format = "TXT";
+    } else {
+	print "Does not look like TXT file\n";    
+    }
+    if ( not defined $format)
+    {
+	if ($self->looksLikeXML()) {
+	    print "Looks like XML file\n";
+	    $format = "XML";
+	} else {
+	    print "Does not look like XML file\n";    
+	}
+    }
+    
+    $class = join "::" , (__PACKAGE__ , $format);
+    my $plugin = catfile (split "::", $class) . ".pm";
+    print " require $plugin \n";
+    require $plugin;
+    #$self->{TIMESTAMP} = lookupTimeStamp($self);
+    #&openDump($self);
+    #bless $self, $class;
+    #return $self;
+    return  $class->new(@_);
 }
 
 sub validate {
@@ -91,7 +116,7 @@ sub lookupTimeStamp{
     # Discard all known suffices first:
     my @suffices;
     push (@suffices, keys %extractor);
-    push (@suffices, keys %format);
+    push (@suffices, keys %formats);
     my ($name, $path, $suffix) = fileparse( $basename, @suffices);
     while ( $suffix ) {
 	$basename = $path . $name;
@@ -127,12 +152,13 @@ sub looksLikeXML{
 }
 
 sub looksLikeTXT{
-    my $self = shift;
-    print "I am in ",__PACKAGE__,"->looksLikeTXT\n" if $self->{VERBOSE};
-    my ($firstline) = $self->readDumpHead();
-    if ($firstline !~ /^\// ) {
-	return 0;
-    }
+    #my $self = shift;
+    #print "I am in ",__PACKAGE__,"->looksLikeTXT\n" if $self->{VERBOSE};
+    print "I am in ",__PACKAGE__,"->looksLikeTXT\n";
+    #my ($firstline) = $self->readDumpHead();
+    #if ($firstline !~ /^\// ) {
+#	return 0;
+ #   }
     return 1;
 }
 
