41bfb0d96c9cc308f74efc6f2e7db47ec577d080
nataliaratnikova <natasha@fnal.gov>
2014-10-17 22:26:22 -0500

    Added format parameter and option

--- perl_lib/DMWMMON/SpaceMon/FormatFactory.pm
+++ perl_lib/DMWMMON/SpaceMon/FormatFactory.pm
@@ -1,15 +1,25 @@
 package DMWMMON::SpaceMon::FormatFactory;
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub instantiate 
 {
-    my $class          = shift;
-    my $requested_type = shift;
-    my $location       = "DMWMMON/SpaceMon/Format/$requested_type.pm";
-    $class             = "DMWMMON::SpaceMon::Format::$requested_type";
+    my $class = shift;
+    my %params = @_;
+    my $h = \%params;
+    if (defined $h->{FORMAT}){
+	print "FORMAT: $h->{FORMAT}\n" if $h->{VERBOSE};
+    } elsif (defined $h->{DUMPFILE} ) {
+	die "Checking format of file: $h->{DUMPFILE}\n";
+    } else {
+	die "ERROR: Format not defined \n";
+    }
+
+    my $location       = "DMWMMON/SpaceMon/Format/" . $h->{FORMAT} . ".pm";
+    $class             = "DMWMMON::SpaceMon::Format::" . $h->{FORMAT};
     require $location;
-    return $class->new(@_);
+    return $class->new(%{$h});
 }
 
 1;
