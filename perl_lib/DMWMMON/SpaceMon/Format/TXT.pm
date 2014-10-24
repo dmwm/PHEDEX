010deccd6bcfee67f26a46133a7dcf5f38c1a009
nataliaratnikova <natasha@fnal.gov>
2014-10-24 16:26:23 -0500

    Fixes in lookupFileSize - make it object method - return empty array for non-matching line

--- perl_lib/DMWMMON/SpaceMon/Format/TXT.pm
+++ perl_lib/DMWMMON/SpaceMon/Format/TXT.pm
@@ -4,6 +4,33 @@ use warnings;
 use Data::Dumper;
 use base 'DMWMMON::SpaceMon::StorageDump';
 
+# Class methods: 
+
+sub formattingHelp
+{
+    my $message = <<'EOF';
+======== Formatting help =========
+TXT formating recommendations: 
+
+Content of each file: one line pre file with:
+LFN (or PFN) | file size (bytes) | file creation date (epoch s) | checksum
+
+e.g.
+
+[lxplus441] $ head -1 LHCb-Disk.1312201138.txt
+/lhcb/MC/MC10/ALLSTREAMS.DST/00010680/0000/00010680_00000028_1.allstreams.dst|4046740455|1305988966000|N/A
+
+If a field is not available, write 'na' (e.g. the checksum will not be available for some storage implementations, no problem) 
+
+For more details see: 
+https://twiki.cern.ch/twiki/bin/view/LCG/ConsistencyChecksSEsDumps#Format_of_SE_dumps. 
+===================================
+EOF
+    print $message;
+}
+
+#Object methods: 
+
 sub new
 {
     my $proto = shift;
@@ -16,38 +43,15 @@ sub new
 
 sub lookupFileSize
 {
+    my $self = shift;
     $_ = shift;
     my ($file, $size, $rest) = split /\|/;
     if ($file) {
+	#print "Found match for file: $file and size: $size \n" if $self->{VERBOSE};
 	return ($file, $size);
     } else {
-	return 0;
+	return ();
     }
-}
-
-sub formattingHelp
-{
-    my $message = <<'EOF';
-
-TXT formating recommendations: 
-
-Content of each file: one line pre file with:
-
-LFN (or PFN) | file size (bytes) | file creation date (epoch s) | checksum
-
-e.g.
-
-[lxplus441] $ head -1 LHCb-Disk.1312201138.txt
-/lhcb/MC/MC10/ALLSTREAMS.DST/00010680/0000/00010680_00000028_1.allstreams.dst|4046740455|1305988966000|N/A
-
-if a field is not available, write 'na' (e.g. the checksum will not be available for some storage implementations, no problem) 
-
-
-For more details see: 
-https://twiki.cern.ch/twiki/bin/view/LCG/ConsistencyChecksSEsDumps#Format_of_SE_dumps. 
-
-EOF
-    print $message;
 }
 
 1;
--- perl_lib/DMWMMON/SpaceMon/Format/XML.pm
+++ perl_lib/DMWMMON/SpaceMon/Format/XML.pm
@@ -4,6 +4,25 @@ use warnings;
 use Data::Dumper;
 use base 'DMWMMON::SpaceMon::StorageDump';
 
+# class methods:
+sub formattingHelp
+{
+    my $message = <<'EOF';
+======== Formatting help =========
+XML formatting recommendations: 
+
+XML format has been agreed with dCache sites, as they can produce the dump using 
+the pnfs-dump or the chimera-dump tools, that support XML output format. 
+
+More details here: http://www.desy.de/~paul/SynCat/syncat-1.0.tar.gz
+
+Similar tools exist for DPM storage. 
+===================================
+EOF
+    print $message;
+}
+# Object methods:
+
 sub new
 {
     my $proto = shift;
@@ -16,30 +35,14 @@ sub new
 
 sub lookupFileSize 
 {
+    my $self = shift;
     $_ = shift;
-    if (m/^\S+\s(\/\S+)\s(\d+)$/) {
+    if (m/\S+\sname=\"(\S+)\"\>\<size\>(\d+)\<\S+$/) {
+	#print "Found match for file: $1 and size: $2 \n" if $self->{VERBOSE};
 	return ($1, $2);
     } else {
-	return 0;
+	return ();
     }
-}
-
-sub formattingHelp
-{
-    my $message = <<'EOF';
-
-XML formatting recommendations: 
-
-XML format has been agreed with dCache sites, as they can produce the dump using 
-the pnfs-dump or the chimera-dump tools, that support XML output format. 
-
-More details here: http://www.desy.de/~paul/SynCat/syncat-1.0.tar.gz
-
-Similar tools exist for DPM storage. 
-
-
-EOF
-    print $message;
 }
 
 1;
