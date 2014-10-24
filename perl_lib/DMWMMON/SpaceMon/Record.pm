da26e751ab61f6a477aef713bcddebe9ba8f8a93
nataliaratnikova <natasha@fnal.gov>
2014-10-24 16:35:30 -0500

    W.i.p. - added methods to fill in data - fixed indentation in the constructor code

--- perl_lib/DMWMMON/SpaceMon/Record.pm
+++ perl_lib/DMWMMON/SpaceMon/Record.pm
@@ -5,23 +5,39 @@ use Data::Dumper;
 
 sub new
 {
-my $proto = shift;
-my $class = ref($proto) || $proto;
-my $self = {};
-my %params = (
-	      DEBUG => 1,
-	      VERBOSE => 1,
-	      NODE => undef,
-	      TIMESTAMP => undef,
-	      DIRS => {},
-);
-my %args = (@_);
-map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
-bless $self, $class;
-return $self;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = {};
+    my %params = (
+		  DEBUG => 1,
+		  VERBOSE => 1,
+		  NODE => undef,
+		  TIMESTAMP => undef,
+		  DIRS => [],
+		  );
+    my %args = (@_);
+    print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
+    map { $self->{$_} = $args{$_} || $params{$_} } keys %params;
+    bless $self, $class;
+    return $self;
 }
 
 sub dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
+
+sub setTimeStamp
+{
+    my $self = shift;
+    $self->{TIMESTAMP}=shift;    
+}
+
+sub addDir
+{
+    my $self = shift;
+    my ($pfn, $size) = @_;
+    # We could add checks here, or rely on parsing algorithm to validate the input:
+    push @{$self->{DIRS}}, ($pfn, $size);
+    print "Added dir: $pfn ==> $size \n" if $self-> {VERBOSE};
+}
 
 sub matches 
 {
