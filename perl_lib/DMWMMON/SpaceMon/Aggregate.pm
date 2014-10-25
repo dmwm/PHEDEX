a846dcd60409b94042fbac01cca6adc2f8e34a0e
nataliaratnikova <natasha@fnal.gov>
2014-10-24 21:26:57 -0500

    W.i.p. Added methods countSpace and createRecord. SpaceCount constructs DIRS (analog of %dirsizes hash) and stores it in the StorageDump object. For better efficiency, DIRS must be dropped, and fully aggregated list constructed directly in the Record object.

--- perl_lib/DMWMMON/SpaceMon/Aggregate.pm
+++ perl_lib/DMWMMON/SpaceMon/Aggregate.pm
@@ -2,6 +2,7 @@ package DMWMMON::SpaceMon::Aggregate;
 use strict;
 use warnings;
 use Data::Dumper;
+use File::Basename;
 use DMWMMON::SpaceMon::Record;
 
 sub new
@@ -12,6 +13,9 @@ sub new
     my %params = (
 		  DEBUG => 1,
 		  VERBOSE => 1,
+		  LEVEL => 6,
+		  # we search for this directory and count levels down from there:
+		  STARTPATH => "/store/",
 		  );
     my %args = (@_);
     
@@ -23,9 +27,45 @@ sub new
 
 sub dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
 
-sub createRecord
-{
+sub countSpace {
+# Parses dump and saves results in the dumpfile object:
     my $self = shift;
-    $self->{RECORD}= DMWMMON::SpaceMon::Record->new();
+    my $dumpfile = shift;
+    print "I am in ",__PACKAGE__,"->countSpace()\n" if $self->{VERBOSE};
+    my ($size, $file, $dir);
+    my $fh = $dumpfile->openDump();
+    print $dumpfile->dump();
+    while (<$fh>) {
+	($file, $size) = $dumpfile->lookupFileSize($_);
+	if ($file) {
+	    $dumpfile->{totalfiles}++;
+	    $dir = dirname $file;
+	    $dumpfile->{DIRS}{$dir}+=$size;
+	    $dumpfile->{totalsize}+=$size;
+	}
+    }
+    close $fh;
+    $dumpfile->{totaldirs} = keys %{$dumpfile->{DIRS}};
 }
+ 
+sub createRecord {
+# Does aggregation:
+    my $self = shift;
+    my $dumpfile = shift;
+    print "I am in ",__PACKAGE__,"->createRecord()\n" if $self->{VERBOSE};
+    print "Processing file $dumpfile->{DUMPFILE}\n" if $self->{VERBOSE};
+    my $record= DMWMMON::SpaceMon::Record->new();
+    #while (<$fh>) {
+    #	#print "Parsing line: $_" if $self->{VERBOSE};
+#	my @result = $dumpfile->lookupFileSize($_);
+#	if (@result) {
+#	    $record->addDir(@result); # example 
+#	}
+#    }
+    #$record->addDir('/some/other/pfn', 9876543); # example 
+    print $dumpfile->dump();
+    return $record;    
+}
+
+
 1;
