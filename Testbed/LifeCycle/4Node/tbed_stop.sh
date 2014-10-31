8e596bc8a8dec4e3fd6f592a61c2dfed6f7d6a6b
Tony Wildish <Tony.Wildish@cern.ch>
2014-10-31 18:57:30 +0100

    update config file, prepare the start/stop scripts

---
+++ Testbed/LifeCycle/4Node/tbed_stop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Master=$PHEDEX_ROOT/Utilities/Master
+Config=$LIFECYCLE/2Node
+eval `$Master -config $Config/Config.Mgmt.Testbed environ`
+
+node="T0_Test_Buffer"
+echo "Stopping node $node"
+$Master -config $Config/Config.T0.MSS stop
+
+for cfg in $Config/Config.Test*.MSS
+do
+  i=`echo $cfg | awk -F/ '{ print $NF }' | tr -d '[a-z,A-Z,._]'`
+  node="T1_Test${i}_Buffer"
+  echo "Stopping node $node"
+  $Master -config $Config/Config.Test$i.MSS stop
+done
+
+echo "Stopping Mgmt agents"
+$Master --config $Config/Config.Mgmt.Testbed stop
+echo removing stop-files
+rm $TESTBED_ROOT/2Node/*/state/*/stop
