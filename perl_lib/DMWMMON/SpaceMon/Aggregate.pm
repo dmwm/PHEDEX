d7575ff706fd060e4b9e2162d498b3350582b609
nataliaratnikova <natasha@fnal.gov>
2014-12-04 23:03:27 -0600

    Count space is now done in createRecord; params are public.

--- perl_lib/DMWMMON/SpaceMon/Aggregate.pm
+++ perl_lib/DMWMMON/SpaceMon/Aggregate.pm
@@ -5,18 +5,18 @@ use Data::Dumper;
 use File::Basename;
 use DMWMMON::SpaceMon::Record;
 
+our %params = (
+	      DEBUG => 1,
+	      VERBOSE => 1,
+	      LEVEL => 6,
+	      # we search for this directory and count levels down from there:
+	      STARTPATH => "/store/",
+	      );
 sub new
 {
     my $proto = shift;
     my $class = ref($proto) || $proto;
     my $self = {};
-    my %params = (
-		  DEBUG => 1,
-		  VERBOSE => 1,
-		  LEVEL => 6,
-		  # we search for this directory and count levels down from there:
-		  STARTPATH => "/store/",
-		  );
     my %args = (@_);
     
     map { if (defined $args{$_}) {$self->{$_} = $args{$_}} else { $self->{$_} = $params{$_}} } keys %params;        
@@ -27,45 +27,31 @@ sub new
 
 sub dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
 
-sub countSpace {
-# Parses dump and saves results in the dumpfile object:
+sub createRecord {
+# Does dumpfile parsing and aggregation:
     my $self = shift;
     my $dumpfile = shift;
-    print "I am in ",__PACKAGE__,"->countSpace()\n" if $self->{VERBOSE};
     my ($size, $file, $dir);
+    print "I am in ",__PACKAGE__,"->createRecord()\n" if $self->{VERBOSE};
+    my $record= DMWMMON::SpaceMon::Record->new();
+    print "Record initialized:\n", $record->dump() if $self->{DEBUG};
+    print "Processing file $dumpfile->{DUMPFILE}\n" if $self->{VERBOSE};
+    print $dumpfile->dump() if $self->{DEBUG};
     my $fh = $dumpfile->openDump();
-    print $dumpfile->dump();
     while (<$fh>) {
+	#print "Parsing line: $_" if $self->{VERBOSE};
 	($file, $size) = $dumpfile->lookupFileSize($_);
 	if ($file) {
-	    $dumpfile->{totalfiles}++;
+	    $record->{totalfiles}++;
 	    $dir = dirname $file;
-	    $dumpfile->{DIRS}{$dir}+=$size;
-	    $dumpfile->{totalsize}+=$size;
+	    $record->{DIRS}{$dir}+=$size;
+	    $record->{totalsize}+=$size;
 	}
     }
     close $fh;
-    $dumpfile->{totaldirs} = keys %{$dumpfile->{DIRS}};
+    $record->{totaldirs} = keys %{$record->{DIRS}};
+    return $record;
 }
- 
-sub createRecord {
-# Does aggregation:
-    my $self = shift;
-    my $dumpfile = shift;
-    print "I am in ",__PACKAGE__,"->createRecord()\n" if $self->{VERBOSE};
-    print "Processing file $dumpfile->{DUMPFILE}\n" if $self->{VERBOSE};
-    my $record= DMWMMON::SpaceMon::Record->new();
-    #while (<$fh>) {
-    #	#print "Parsing line: $_" if $self->{VERBOSE};
-#	my @result = $dumpfile->lookupFileSize($_);
-#	if (@result) {
-#	    $record->addDir(@result); # example 
-#	}
-#    }
-    #$record->addDir('/some/other/pfn', 9876543); # example 
-    print $dumpfile->dump();
-    return $record;    
-}
-
+# $record->addDir('/some/other/pfn', 9876543); # example
 
 1;
