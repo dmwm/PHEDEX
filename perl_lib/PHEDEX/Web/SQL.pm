451e5833cc13a5e474e33edd4a839c2c68d6e80f
Tony Wildish <Tony.Wildish@cern.ch>
2015-02-19 20:09:29 +0100

    re-enable sorting by block-id in getFileReplicas. It's needed for spooling, and not easy to get round :-(

--- perl_lib/PHEDEX/Web/SQL.pm
+++ perl_lib/PHEDEX/Web/SQL.pm
@@ -297,8 +297,7 @@ sub getFileReplicas
 	$p{':update_since'} = &str2time($h{UPDATE_SINCE});
     }
 
-#   Suppress this because it can cause the use of excessive temporary space
-#   $sql .= qq{ order by block_id };
+    $sql .= qq{ order by block_id };
     $q = execute_sql( $self, $sql, %p );
     $q = PHEDEX::Web::STH->new($q);
 
