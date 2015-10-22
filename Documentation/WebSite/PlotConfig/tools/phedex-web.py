0f87b51593ec1c826a03e6b692f36014d5d1c768
Diego da Silva Gomes <diego@cern.ch>
2015-10-22 14:42:33 +0200

    From hardcoded PHEDEX-web.spec: read config file from command line argument.

--- Documentation/WebSite/PlotConfig/tools/phedex-web.py
+++ Documentation/WebSite/PlotConfig/tools/phedex-web.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-
 from graphtool.base.xml_config import XmlConfig
-import cherrypy
-
-if __name__ == '__main__':
-  xc = XmlConfig( file='$GRAPHTOOL_CONFIG_ROOT/website_prod.xml' ) 
-  cherrypy.server.quickstart()
-  cherrypy.engine.start() 
-  xc.globals['web'].kill()
-
+import sys, cherrypy
+xc = XmlConfig(file=sys.argv[1])
+cherrypy.quickstart()
+cherrypy.engine.start()
+xc.globals['web'].kill()
