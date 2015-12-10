9bd0a57888d7137f95e79ac5ff58d36b80004e5d
Natalia Ratnikova <natasha@fnal.gov>
2015-12-10 15:32:26 -0600

    save w.i.p.

--- Utilities/t.pl
+++ Utilities/t.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use DMWMMON::SpaceMon::Record;
 
 my $VAR1;
 my $file = $ARGV[0]; 
@@ -18,10 +19,26 @@ my $data;
   $data = <FILE>;
   close FILE;
 }
-eval $data;
-print Dumper ($VAR1);
+eval $data;  # $VAR1 now contains data service output data
+#print Dumper ($VAR1);
 
+# Assuming we have data for a single node (true for GetLastRecord API), 
+# loop over the timestamps and create a record for each one
+# (wrap this in a loop for multiple nodes): 
 
-foreach my $p (sort keys %$VAR1) {
-    print "Key: ", $p, "\n";
+print "Node: ", $VAR1->{PHEDEX}{NODES}->[0]->{NODE}, "\n"; 
+
+foreach my $timebin ( $VAR1->{PHEDEX}{NODES}->[0]->{TIMEBINS}) {
+    foreach (keys %$timebin){
+	my $record = DMWMMON::SpaceMon::Record->new();
+	$record->setNodeName($VAR1->{PHEDEX}{NODES}->[0]->{NODE});
+	$record->setTimeStamp($_);
+	print "Timestamp = ", $_, "\n";
+	foreach my $dirlist( $timebin->{$_}) {
+	    foreach my $dir (@$dirlist) {
+		print "DIR: ", $dir->{DIR}," = ", $dir->{SPACE}, "\n";
+		$record->addDir($dir->{DIR},$dir->{SPACE});
+	    }
+	}
+    }
 }
