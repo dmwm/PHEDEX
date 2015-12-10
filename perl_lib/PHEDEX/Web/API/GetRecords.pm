876960f404638d433b4b3f5b914e90de544b31df
Natalia Ratnikova <natasha@fnal.gov>
2015-12-10 15:35:13 -0600

    Save w.i.p.  this API may be obsoleted by getlastrecord, already released in HG1512

--- perl_lib/PHEDEX/Web/API/GetRecords.pm
+++ perl_lib/PHEDEX/Web/API/GetRecords.pm
@@ -1,4 +1,4 @@
-package PHEDEX::Web::API::GetRecords;
+package PHEDEX::Web::API::GetLastRecord;
 use warnings;
 use strict;
 use PHEDEX::Web::SQLSpace;
@@ -42,7 +42,6 @@ Query storage info with options from oracle backend
 
 =head3 <nodes> elements
 
-  subdir             the path searched
   node               node name
 
 =head3 <timebins> elements
@@ -64,155 +63,47 @@ sub methods_allowed { return ('GET'); }
 sub duration { return 0; }
 sub invoke { return getrecords(@_); }
 
-sub nrdebug {
-    my $message = shift;
-    open(my $fh, '>>', '/tmp/nrdebug_report.txt');
-    print $fh $message . "\n";
-    close $fh;
-}
-sub getrecords  {
-  my ($core, %h) = @_;
-  my ($method,$inputnode,$result,@inputnodes,@records, $last, $data);
-  my ($dirtemp,$dirstemp,$node);
-  $method = $core->{REQUEST_METHOD};
-  my %args;
-  &nrdebug ('trace0 in storageusage');
-  eval {
+sub getlastrecord  {
+    my ($core, %h) = @_;
+    my ($method, $result, $record, $last, $data);
+    my ($dirtemp,$dirstemp);
+    $method = $core->{REQUEST_METHOD};
+    my %args;
+    eval {
         %args = &validate_params(\%h,
-                allow => [ qw ( node time_until ) ],
-                required => [ qw ( node ) ],
-                spec =>
-                {
-                    node => { using => 'node', multiple => 0 },
-                    time_until => { using => 'time' }
-                });
-        };
-  if ( $@ ) {
+				 allow => [ qw ( node ) ],
+				 required => [ qw ( node ) ],
+				 spec =>
+				 {
+				     node => { using => 'node', multiple => 0 },
+				 });
+    };
+    if ( $@ ) {
         return PHEDEX::Web::Util::http_error(400, $@);
-  } 
-
-
-  foreach ( keys %args ) {
-     $args{lc($_)} = delete $args{$_};
-  }
- 
-  if ( $args{time_until} ) {
-    $args{time_until} = PHEDEX::Core::Timing::str2time($args{time_until});
-  }
-
-  if ($args{node} =~ m/^T\*$/) {
-     eval {
-        $result = PHEDEX::Web::SQLSpace::querySpace($core, %args);
-     };
-     if ( $@ ) {
-	 die PHEDEX::Web::Util::http_error(400,$@);
-     }
-     $last = @{$result}[0]->{NAME};
-     $dirstemp = ();
-     foreach $data (@{$result}) {
-       $dirtemp = {};
-       $dirtemp->{DIR} = $data->{DIR};
-       $dirtemp->{SPACE} = $data->{SPACE};
-       $dirtemp->{TIMESTAMP} = $data->{TIMESTAMP};
-       $dirtemp->{NAME} = $data->{NAME};
-       push @$dirstemp, $dirtemp;
-       if ($last !~ m/$data->{NAME}/) {
-          $node = {};
-          $node->{subdir} = $args{rootdir};
-          $node->{node} = $last;
-          $node->{timebins} = getNodeInfo($core, $dirstemp, %args);
-          push @records, $node;
-          $dirstemp = ();
-          $last = $data->{NAME};
-       }
+    } 
+    # Convert all arguments to lowcase:
+    foreach ( keys %args ) {
+	$args{lc($_)} = delete $args{$_};
     }
-    if ($last =~ m/@{$result}[0]->{NAME}/) {
-       $node = {};
-       $node->{subdir} = $args{rootdir};
-       $node->{node} = $last;
-       $node->{timebins} = getNodeInfo($core, $dirstemp, %args);
-       push @records, $node;
-    }
-  } 
-  else {
-     if (ref $args{node} eq 'ARRAY') {
-        @inputnodes = @{$args{node}};
-     }
-     else {
-        @inputnodes = $args{node};
-     }
-     foreach $inputnode (@inputnodes) {
-        my $node = {};
-        $args{node} = $inputnode;
-        $node->{subdir} = $args{rootdir};
-        eval {
-          $result = PHEDEX::Web::SQLSpace::querySpace($core, %args);
-        };
-        if ( $@ ) {
-          die PHEDEX::Web::Util::http_error(400,$@);
-        }
-        $node->{node} = @{$result}[0]->{NAME};
-        $node->{timebins} = getNodeInfo($core, $result, %args); 
-        push @records, $node;
-     }
-  }
 
-  return { records => \@records };
-}
-
-sub getNodeInfo {
-  my ($core, $result, %args) = @_;
-  my ($level,$rootdir,%dir, $dirs, $data);
-  my ($dirtemp, $timetemp, $dirstemp, $last, $time);
-  my ($dirarray, $dirhash, $dirhashSep, $levelarray, $levelhash, $timebin, $timebins);
-  my %temp;
-  # New vars: 
-  my ($reldepth, $levelshash, $dirsize, $depth);
-  my @datalist = ();
-  $timebins = ();
-  $rootdir = $args{rootdir};
-  $level = $args{level};
-
-  # classify data by timestamp
-  $timetemp = {};
-  my $current = undef;
-  $dirstemp = ();
-  foreach $data (@{$result}) {
-      if (!$current || ( $data->{TIMESTAMP} != $current ) ) {
-	  if ( $dirstemp ) {
-	      $timetemp->{$current} = $dirstemp;
-	      $dirstemp = ();
-	  }
-	  $current = $data->{TIMESTAMP};
-      }
-    $dirtemp = {};
-    $dirtemp->{size} = $data->{SPACE};
-    $dirtemp->{dir} = $data->{DIR};
-    push @$dirstemp, $dirtemp;
-  }
-  if ( $dirstemp ) {
-      $timetemp->{$current} = $dirstemp;
-  }
-  return $timetemp;
-}
-
-sub checklevel {
-    my ($rootdir,$path)=@_;
-    my @p = split "/", $path;
-    my @r = split "/", $rootdir;
-    return -1 if (@p < @r);
-    my $result=1;
-    for (my $i=1; $i < @p; $i += 1) {
-	if ( ! $r[$i]){
-	    $result += 1;
-	    next;
+    # Check node name syntax (X-names are discarded here)
+    if ($args{node} =~ m/^T\*$/) {
+	eval {
+	    $result = PHEDEX::Web::SQLSpace::querySpace($core, %args);
+	};
+	if ( $@ ) {
+	    die PHEDEX::Web::Util::http_error(400,$@);
 	}
-	if ( ($p[$i] ne $r[$i] )){
-	    return -1;
-	} 	
+	$record = {
+	    node => $results[0]->{NAME};
+	    timestamp => $result[0]->{TIMESTAMP}
+	};	
+	foreach $data (@{$result}) {
+	    $dirtemp->{DIR} = $data->{DIR};
+	    $dirtemp->{SPACE} = $data->{SPACE};
+	    push @$dirstemp, $dirtemp;
+	}
     }
-    ( $rootdir eq "/" ) && ($result-=1);
-    return $result;
+    return { record => $record };
 }
-
 1;
