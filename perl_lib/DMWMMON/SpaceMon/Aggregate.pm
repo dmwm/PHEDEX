ba6f7a6480ddf492dccd0e345fecd0c88dd1a23d
Natalia Ratnikova <natasha@fnal.gov>
2015-12-10 21:03:35 -0600

    Support for levels based on configuration, include top level directories into the record.

--- perl_lib/DMWMMON/SpaceMon/Aggregate.pm
+++ perl_lib/DMWMMON/SpaceMon/Aggregate.pm
@@ -4,14 +4,13 @@ use warnings;
 use Data::Dumper;
 use File::Basename;
 use DMWMMON::SpaceMon::Record;
+use DMWMMON::SpaceMon::NamespaceConfig qw/ find_top_parents /;
 
-our %params = (
-	      DEBUG => 1,
-	      VERBOSE => 1,
-	      LEVEL => 6,
-	      # we search for this directory and count levels down from there:
-	      STARTPATH => "/store/",
-	      );
+our %params = ( 
+    DEBUG => 1,
+    VERBOSE => 1,
+    );
+
 sub new
 {
     my $proto = shift;
@@ -19,9 +18,11 @@ sub new
     my $self = {};
     my %args = (@_);
     
-    map { if (defined $args{$_}) {$self->{$_} = $args{$_}} else { $self->{$_} = $params{$_}} } keys %params;        
+    map { if (defined $args{$_}) {$self->{$_} = $args{$_}} 
+	  else {$self->{$_} = $params{$_}} } keys %params;
     print "I am in ",__PACKAGE__,"->new()\n" if $self->{VERBOSE};
     bless $self, $class;
+    print $self->dump() if $self->{DEBUG};
     return $self;
 }
 
@@ -30,6 +31,7 @@ sub dump { return Data::Dumper->Dump([ (shift) ],[ __PACKAGE__ ]); }
 sub createRecord {
 # Does dumpfile parsing and aggregation:
     my $self = shift;
+    my $config = shift;
     my $dumpfile = shift;
     my ($size, $file, $dir);
     print "I am in ",__PACKAGE__,"->createRecord()\n" if $self->{VERBOSE};
@@ -38,17 +40,22 @@ sub createRecord {
     print "Processing file $dumpfile->{DUMPFILE}\n" if $self->{VERBOSE};
     print $dumpfile->dump() if $self->{DEBUG};
     my $fh = $dumpfile->openDump();
+    # Parse the dump and count all file sizes: 
     while (<$fh>) {
 	#print "Parsing line: $_" if $self->{VERBOSE};
 	($file, $size) = $dumpfile->lookupFileSize($_);
 	if ($file) {
 	    $record->{totalfiles}++;
-	    $dir = dirname $file;
-	    $record->{DIRS}{$dir}+=$size;
 	    $record->{totalsize}+=$size;
+	    # Compile a list of parent directories up to a given
+	    # aggregation level and include them in the record:
+	    foreach ($config->find_top_parents($file)) {
+		$record->{DIRS}{$_}+=$size;
+	    }
 	}
     }
     close $fh;
+
     $record->{totaldirs} = keys %{$record->{DIRS}};
     if ($dumpfile->{'TIMESTAMP'}) {
 	$record->setTimeStamp($dumpfile->{'TIMESTAMP'});
