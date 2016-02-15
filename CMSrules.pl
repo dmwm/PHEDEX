3458133224debbc24793acb913d6591882ccc59f
Natalia Ratnikova <natasha@fnal.gov>
2016-02-15 15:28:41 -0600

    Added conflict resolution. Passed basic tests with real CMS NS use cases.

--- CMSrules.pl
+++ CMSrules.pl
@@ -4,16 +4,15 @@ use Data::Dumper;
 # Global vars: 
 my $level;
 my $rules={}; # hashref
+my $count=0;
+my $trace;
 
-sub hr {print '-' x 80;print "\n";}
+sub hr {print '-' x 80; print "\n"}
+sub replace_node (\%$$) { $_[0]->{$_[2]} = delete $_[0]->{$_[1]}}
 sub showrules {
-    my $word = shift;
-    print "\n", $word;
+    shift;
+    print "\n", $_;
     print Data::Dumper->Dump([ $rules ],[qw (rules)]), "\n";
-}
-
-sub replace_node (\%$$) {
-  $_[0]->{$_[2]} = delete $_[0]->{$_[1]};
 }
 
 sub addrule {
@@ -22,22 +21,39 @@ sub addrule {
     is_an_integer ($rule->{depth})
 	or die "ERROR: depth value is not an integer: \"$rule->{depth}\"";
     my $depth =  int($rule->{depth});
-    print "\n============  New rule  \"$rule->{path}=" .$depth."\"\n\n";
+    print "============ Processing rule  $rule->{path}=$depth\n";
     my $path = $rule->{path} . "/";
     $path =~ tr/\///s;
-    print Data::Dumper->Dump([ $rules ],[qw(rules)]);
+    #showrules ("BEFORE:  ");
     addnode($rules, $path, $depth);
+    #showrules ("AFTER:  ");
 }
 
 sub addnode {
     my ($r, $p, $d) = @_; # rule hash, path and depth
     return unless $p;
-    print "path = $p\n";
     my ($nodename, $remainder) = split(/\//, $p, 2);
+    # Assign real depth to the leaves only, otherwise use zero:
     my $newrule = $nodename . ($remainder ? "/=0" : "/=$d");
-    print "newrule = $newrule\n";
+    #print "newrule = $newrule\n"; # key for the new node
+    # Check for existing rules matching our dirname: 
+    my ($newn, $newd) = split("=", $newrule);
+    # Add the very first rule on the new level w/o checking for conflicts
+    keys %{$r} or $r->{$newrule} = {};
+    foreach ( keys %{$r} ) {
+	my ($oldn, $oldd) = split("=", $_);
+	($newn ne $oldn) and next;	
+	($newd eq $oldd) and next;
+	if ( int($oldd) == 0 ) {
+	    print "Overriding a weak rule $_  with a new rule $newrule\n";
+	    replace_node %{$r}, $_ => $newrule;
+	}else{
+	    print "Overriding a new rule $newrule with a strong rule $_\n";
+	    $newrule = $_;
+	}
+    }
     if ( not exists $r->{$newrule}) {
-	$r->{$newrule} = {};
+    	$r->{$newrule} = {};
     }
     addnode ($r->{$newrule}, $remainder, $d);
 }
@@ -48,24 +64,6 @@ sub is_an_integer {
     return $val =~ m/^[-]*\d+$/;
 }
 
-# Define and print the configuration example:
-my $example = {
-    '/=0' => {
-	"a/=0" => {
-	    "b/=4" => {
-		"c/=2" => {},
-		"d/=-1" => {},
-		"e/=0" => {
-		    "f/=0" =>{}
-		}
-	    }
-	}
-    }
-};
-
-#&hr();
-#print Data::Dumper->Dump([ $example ],[qw(example)]);
-#&hr();
 while ( <DATA>) {
 # Create path/depth hash for each rule and add rule 
 # to the config tree structure:
@@ -75,24 +73,18 @@ while ( <DATA>) {
     addrule($rule);
 }
 &hr();
-print Data::Dumper->Dump([ $rules ],[qw(rules)]) ;
+print Data::Dumper->Dump([ $rules ],[qw(FINAL-RESULT)]) ;
 &hr();
-#/a/b/c/=2
-#/a/b/e/f=0
-#/a1/b1/=4
-#/a/b/d/=-1
 __DATA__
-/=0
-/a1/a2/a3/=0
-/b1/b2/b3/=0
-/b1/c1=0
-/store/user=0
-/store/user/cmsprod/=0
-/store/user/samtests/=0
+/store/local/private/=-1
 /store/group/=1
 /store/temp/=0
 /store/temp/user/=1
 /store/backfill/1/=0
 /store/backfill/2/=0
 /store/local=0
-/store/local/private/=-1
+/=0
+/store/=2
+/store/user=0
+/store/user/cmsprod/=0
+/store/user/samtests/=0
