e0593fc461345cb09cfa2c5d593cc2e3b518f863
Nicolo Magini <nikmagini@users.noreply.github.com>
2016-07-26 16:42:45 +0200

    Fix for https://github.com/dmwm/PHEDEX/issues/1038

--- perl_lib/PHEDEX/Core/Agent/DB.pm
+++ perl_lib/PHEDEX/Core/Agent/DB.pm
@@ -137,7 +137,7 @@ sub identifyAgent
   if ($dbh->{private_phedex_newconn})
   {
     my ($ident) = qx(ps -p $$ wwwwuh 2>/dev/null)
-      or self->Fatal("Unable to identify agent process with 'ps -p $$ wwwwuh 2>/dev/null'");
+      or $self->Fatal("Unable to identify agent process with 'ps -p $$ wwwwuh 2>/dev/null'");
     chomp($ident) if $ident;
     &dbexec($dbh, qq{
           insert into t_agent_log
