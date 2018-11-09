18ff64cc0b7e6b061bb276d27cca6c62c3c5b180
nataliaratnikova <natasha@fnal.gov>
2018-11-08 19:22:32 -0600

    Check for undefined DN , e.g. hn-cms-admin does not have it.

--- perl_lib/PHEDEX/Web/LocalAuth.pm
+++ perl_lib/PHEDEX/Web/LocalAuth.pm
@@ -435,7 +435,8 @@ sub getUserInfoFromDN {
   }
   $people = decode_json($json_people);
   foreach (@{$people->{'result'}}) {
-    die "Die in getUserInfoFromDN for undefined DN for user " . ${$_}[0] if not defined ${$_}[4];
+    #die "Die in getUserInfoFromDN for undefined DN for user " . ${$_}[0] if not defined ${$_}[4];
+    continue if not defined ${$_}[4];
     if ( ${$_}[4] eq $dn ) {
       $self->{USEREMAIL} = ${$_}[1];
       $self->{USERSURNAME} = ${$_}[2];
